5fc780c37729a7b3286653aef1ed65aa3812c2f5
Alex Gergel <alexgergel@gmail.com>
2022-02-03 18:42:53 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 def get_human_age(cat_age, dog_age):
     cat_to_human = convert_to_human(cat_age, 15, 9, 4)
     dog_to_human = convert_to_human(dog_age, 15, 9, 5)
-    return [cat_to_human, dog_to_human]
+    print([cat_to_human, dog_to_human])
 
 
 def convert_to_human(cat_age, first_year, second_year, each_year):
@@ -10,3 +10,6 @@ def convert_to_human(cat_age, first_year, second_year, each_year):
     if cat_age < first_year + second_year:
         return 1
     return 2 + (cat_age - first_year - second_year) // each_year
+
+
+get_human_age(56, 78)
