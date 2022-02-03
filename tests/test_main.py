4cbddce0f0b4fddbfcdcc0fd4752f14c14df9715
nikon4uk <nikonchukmax4@gmail.com>
2022-02-03 17:24:13 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,26 @@
-# write your code here
+from app.main import get_human_age
+
+
+def test_first_fifteen_years_for_cat():
+    actual = get_human_age(15, 14)
+    assert actual == [1, 0]
+
+
+def test_first_fifteen_years_for_dog():
+    actual = get_human_age(14, 15)
+    assert actual == [0, 1]
+
+
+def test_next_nine_years_for_pets():
+    actual = get_human_age(24, 24)
+    assert actual == [2, 2]
+
+
+def test_every_four_next_years_for_cat():
+    actual = get_human_age(28, 28)
+    assert actual == [3, 2]
+
+
+def test_every_five_next_years_for_dog():
+    actual = get_human_age(29, 29)
+    assert actual == [3, 3]
