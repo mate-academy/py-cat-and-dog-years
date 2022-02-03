ce19035e073c7de9d3559196cf4a394a76f5ce6f
Rybak Dmytro <codingclaud@gmail.com>
2022-02-03 14:48:19 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,10 @@
-# write your code here
+from app.main import get_human_age
+
+
+def test_right_convert_in_human_age():
+    assert get_human_age(100, 100) == [21, 17]  # checking the differences between large numbers
+    assert get_human_age(28, 28) == [3, 2]  # checking the differences between low numbers
+
+
+def test_if_age_in_function_equal_to_0():
+    assert get_human_age(0, 0) == [0, 0]
