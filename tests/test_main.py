a25ec2939155d87a68bb5131bd76609240473787
Kalyta Valerii <deptovka@gmail.com>
2022-02-03 14:43:20 +0200

    'Solution'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,1 +1,19 @@
-# write your code here
+import pytest
+from app.main import get_human_age
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, human_age",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ],
+)
+def test_of_different_age_variations(cat_age, dog_age, human_age):
+    assert get_human_age(cat_age, dog_age) == human_age
