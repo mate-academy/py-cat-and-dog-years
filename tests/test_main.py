538c5d86a9ee6c0bd7ff637f31ed49e5cd29c813
Arthur Shavula <52936635+Krimat@users.noreply.github.com>
2022-02-04 12:19:09 +0200

    Refactored tests

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,12 +2,12 @@ from app.main import get_human_age
 
 
 def test_first_human_year():
-    assert get_human_age(cat_years=0, dog_years=14) == [0, 0]
+    assert get_human_age(cat_age=0, dog_age=14) == [0, 0]
 
 
 def test_second_human_year():
-    assert get_human_age(cat_years=15, dog_years=23) == [1, 1]
+    assert get_human_age(cat_age=15, dog_age=23) == [1, 1]
 
 
 def test_human_year_greater_2():
-    assert get_human_age(100, 100) == [21, 17]
+    assert get_human_age(cat_age=100, dog_age=100) == [21, 17]
