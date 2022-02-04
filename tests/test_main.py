041af2c0e6525a91e6ab36a72d47769fd6baa3d2
Arthur Shavula <52936635+Krimat@users.noreply.github.com>
2022-02-04 12:16:33 +0200

    Refactored code

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,21 +1,12 @@
-from random import randint
-
-
 from app.main import get_human_age
 
 
 def test_first_human_year():
-    cat_years = 14
-    dog_years = 7
-
-    assert get_human_age(cat_years, dog_years) == [0, 0]
+    assert get_human_age(cat_years=0, dog_years=14) == [0, 0]
 
 
 def test_second_human_year():
-    cat_years = 15
-    dog_years = 23
-
-    assert get_human_age(cat_years, dog_years) == [1, 1]
+    assert get_human_age(cat_years=15, dog_years=23) == [1, 1]
 
 
 def test_human_year_greater_2():
