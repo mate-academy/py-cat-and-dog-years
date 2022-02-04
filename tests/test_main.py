ab692217ce8ebe405de558484235cf5da94651ed
Arthur Shavula <52936635+Krimat@users.noreply.github.com>
2022-02-04 12:14:45 +0200

    Upadated solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -5,15 +5,15 @@ from app.main import get_human_age
 
 
 def test_first_human_year():
-    cat_years = randint(0, 14)
-    dog_years = randint(0, 14)
+    cat_years = 14
+    dog_years = 7
 
     assert get_human_age(cat_years, dog_years) == [0, 0]
 
 
 def test_second_human_year():
-    cat_years = randint(15, 23)
-    dog_years = randint(15, 23)
+    cat_years = 15
+    dog_years = 23
 
     assert get_human_age(cat_years, dog_years) == [1, 1]
 
