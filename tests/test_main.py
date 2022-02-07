b4cd6a32a7da9716153ecd7b6ed7e352d61d577e
Nazar-kob <kobrinnazar05@mail.com>
2022-02-07 09:50:54 +0200

    Solution

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,11 +3,12 @@ import pytest
 from app.main import get_human_age
 
 
-def test_when_age_animals_until_14_years():
+def test_when_age_of_animals_is_up_to_14_years():
     assert get_human_age(14, 14) == [0, 0]
 
 
-def test_when_age_animals_until_23_years():
+def test_when_age_of_animals_is_up_to_23_years():
+    assert get_human_age(15, 15) == [1, 1]
     assert get_human_age(23, 23) == [1, 1]
 
 
@@ -18,11 +19,12 @@ def test_when_age_both_animals_is_negative():
 @pytest.mark.parametrize(
     'cat_age, dog_age, expected',
     [
+        (24, 24, [2, 2]),
         (27, 27, [2, 2]),
-        (23, 30, [1, 3]),
+        (28, 28, [3, 2]),
         (72, 81, [14, 13]),
         (103, 103, [21, 17])
     ]
 )
-def test_mix_age_animals_after_23_years(cat_age, dog_age, expected):
+def test_age_of_animal_is_grater_than_23(cat_age, dog_age, expected):
     assert get_human_age(cat_age, dog_age) == expected
