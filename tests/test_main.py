1c0e397dca28377d13593277aca0e1d5176fd8bf
Vlad Tsvigun <vl.tsv@outlook.com>
2022-02-08 21:20:31 +0200

    Fix

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,16 @@
 from app.main import get_human_age, convert_to_human
 
 
-def test_return_zeroes_if_animals_age_equal_to_0():
+def test_one_year_or_less():
     assert get_human_age(0, 0) == [0, 0]
-
-
-def test_return_zeros_if_animals_age_less_then_15():
-    assert get_human_age(5, 1) == [0, 0]
     assert get_human_age(14, 14) == [0, 0]
-
-
-def test_animals_age_more_then_15():
+    assert get_human_age(15, 15) == [1, 1]
     assert get_human_age(23, 23) == [1, 1]
+
+
+def test_two_years():
     assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(28, 29) == [3, 3]
-    assert get_human_age(100, 100) == [21, 17]
+
+
+def test_second_year_and_greater():
+    assert get_human_age(30, 30) == [3, 3]
