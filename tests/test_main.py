f02f2d667666e9799be86d9695cc50a82608acf2
Rybak Dmytro <codingclaud@gmail.com>
2022-02-12 18:02:15 +0200

    'Separate_tests_created'

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,12 +1,18 @@
 from app.main import get_human_age
 
 
-def test_right_convert_in_human_age():
-    assert get_human_age(100, 100) == [21, 17]  # checking the differences between large numbers
-    assert get_human_age(28, 28) == [3, 2]      # checking the differences between low numbers
-    assert get_human_age(14, 14) == [0, 0]      # checking when age is under 15
-    assert get_human_age(15, 24) == [1, 2]
-
-
 def test_if_age_in_function_equal_to_0():
     assert get_human_age(0, 0) == [0, 0]
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_one_when_age_is_between_15():
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_checking_differences_between_low_numbers():
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_checking_a_differences_between_large_numbers():
+    assert get_human_age(100, 100) == [21, 17]
