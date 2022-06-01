87f3dbcaabe1e487028a7c0727e9072b52c536c7
Dima Svirsa <august4smile@gmail.com>
2022-06-01 13:30:38 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,49 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "age,expected",
+    [
+        pytest.param(
+            14,
+            [0, 0],
+            id="zero year check"
+        ),
+        pytest.param(
+            15,
+            [1, 1],
+            id="first year start check"
+        ),
+        pytest.param(
+            23,
+            [1, 1],
+            id="first year end check"
+        ),
+        pytest.param(
+            24,
+            [2, 2],
+            id="second year start check"
+        ),
+        pytest.param(
+            27,
+            [2, 2],
+            id="second year for cat end check"
+        ),
+        pytest.param(
+            28,
+            [3, 2],
+            id="count years for cat, second year for dog end check"
+        ),
+        pytest.param(
+            100,
+            [21, 17],
+            id="count years"
+        ),
+    ]
+)
+def test_(age, expected):
+    human_age = get_human_age(age, age)
+    assert human_age == expected
+
