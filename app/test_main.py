04132eb372409075e9d5a1e707eeeba00652d1fd
Roman <romanschneider9@gmail.com>
2022-06-02 16:58:05 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,50 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "values, expected_result",
+        [
+            (
+                (0, 0),
+                [0, 0]
+            ),
+            (
+                (14, 14),
+                [0, 0]
+            ),
+            (
+                (15, 15),
+                [1, 1]
+            ),
+            (
+                (23, 23),
+                [1, 1]
+            ),
+            (
+                (24, 24),
+                [2, 2]
+            ),
+            (
+                (27, 27),
+                [2, 2]
+            ),
+            (
+                (28, 28),
+                [3, 2]
+            ),
+            (
+                (100, 100),
+                [21, 17]
+            )
+        ]
+    )
+    def test_should_return_correct_values(
+        self,
+        values,
+        expected_result
+    ):
+        res = get_human_age(*values)
+        assert res == expected_result
