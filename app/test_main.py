d03e2d430699efe5e4278e95b3e62dac75843890
Dorius <kornelydorius@gmail.com>
2022-06-02 14:03:41 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,17 @@
 from app.main import get_human_age
 
 
-def test_age_before_15():
+def test_before_15():
     assert get_human_age(12, 12) == [0, 0]
 
 
-def test_age_before_23():
-    assert get_human_age(22, 22) == [1, 1]
+def test_15():
+    assert get_human_age(15, 15) == [1, 1]
 
 
-def test_age_after_24():
-    assert get_human_age(24, 24) == [1, 1]
+def test_cat_from_24():
+    assert get_human_age(25, 0)[0] == 2
+
+
+def test_dog_from_24():
+    assert get_human_age(0, 26)[1] == 2
