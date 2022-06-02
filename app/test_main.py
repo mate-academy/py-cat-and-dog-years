306d7b1592221f8fefcd43f0e8b3edf0961fbf03
Katya Vasylenko <kettyblack20@gmail.com>
2022-06-02 16:44:11 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_cat_and_dog_ages_are_zeros():
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_cat_and_dog_ages_give_zero_human_age():
+    assert get_human_age(10, 12) == [0, 0]
+
+
+def test_cat_and_dog_ages_give_one_human_age():
+    assert get_human_age(16, 23) == [1, 1]
+
+
+def test_cat_and_dog_ages_give_two_human_age():
+    assert get_human_age(16, 23) == [1, 1]
+
+
+def test_the_same_cat_and_dog_age_give_different_human_age():
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_big_value_of_cat_and_dog_ages():
+    assert get_human_age(100, 100) == [21, 17]
