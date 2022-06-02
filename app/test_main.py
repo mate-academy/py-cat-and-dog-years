9a32f81af1f279483be25bb068402e39067fb1f3
Ed Orlovskyy <fedorov12816@gmail.com>
2022-06-02 17:51:25 +0300

    DogCatAgeSolution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,15 @@
 from app.main import get_human_age
 
-# write your code here
+
+class TestHumanAge:
+
+    def test_get_human_age_animal_age_younger_one(self):
+        assert get_human_age(10, 10) == [0, 0]
+
+    def test_get_human_age_animal_age_elder_one(self):
+        assert get_human_age(16, 16) == [1, 1]
+
+    def test_get_human_age_animal_very_old(self):
+        assert get_human_age(100, 100) == [21, 17]
+
+
