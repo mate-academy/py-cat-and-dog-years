d3889387a36ef51b2df6e514e1b85267fff09c80
Elena <lenasharabura@gmail.com>
2022-06-06 11:09:19 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,37 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "age, expected",
+    [
+        pytest.param(
+            13,
+            [0, 0],
+            id="animal age less than 1 year"
+        ),
+        pytest.param(
+            15,
+            [1, 1],
+            id="animal age - 1 year"
+        ),
+        pytest.param(
+            24,
+            [2, 2],
+            id="animal age - 2 years"
+        ),
+        pytest.param(
+            28,
+            [3, 2],
+            id="different animal age"
+        ),
+        pytest.param(
+            72,
+            [14, 11],
+            id="test random age"
+        )
+    ],
+)
+def test_get_human_age(age, expected):
+    assert get_human_age(age, age) == expected
