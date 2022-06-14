5c84d988ed109297abe997cd672582c02386ab69
Anton Lasko <anton.lasko1993@gmail.com>
2022-06-14 17:46:47 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -14,3 +14,18 @@ def test_human_age_should_be_one():
 def test_human_age_should_be_three():
     age = get_human_age(28, 29)
     assert age == [3, 3]
+
+
+def test_human_age_when_animal_age_is_huge():
+    age = get_human_age(100, 100)
+    assert age == [21, 17]
+
+
+def test_human_age_when_animal_age_is_zero():
+    age = get_human_age(0, 0)
+    assert age == [0, 0]
+
+
+def test_human_age_when_animal_age_is_negative():
+    age = get_human_age(-1, -1)
+    assert age == [0, 0]
