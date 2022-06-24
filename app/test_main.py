198f7f584c8e691dc2da2f22de23f69238e9a65d
Artem Kazakov <artemkazakov947@gmail.com>
2022-06-24 10:53:02 +0300

    More tests were added, mark.parametrize was used

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,19 @@
 from app.main import get_human_age
+import pytest
 
 
-def test_should_return_list_and_int():
-    result = get_human_age(15, 15)
-    for year in result:
-        assert isinstance(year, (list, int))
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_result", [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
 
-
-def test_28_cat_dog_years_should_convert_to_3_2_human_year():
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_24_cat_dog_years_should_convert_into_2_human_ages():
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_15_cat_dog_years_should_convert_into_1_human_age():
-    assert get_human_age(15, 15) == [1, 1]
+    ]
+)
+def test_get_human_age(cat_age, dog_age, expected_result):
+    assert get_human_age(cat_age, dog_age) == expected_result
