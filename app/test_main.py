60ec29f7aa5f572070b746ed6f5a3415131d10b3
Andriy Demeshko <ande777complaint@gmail.com>
2022-07-26 16:46:37 +0300

    Edited names for functions

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,33 @@
 from app.main import get_human_age
 
 
-def test_zero_year_age_1():
+def test_zero_year_age_minimum():
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_zero_year_age_2():
+def test_zero_year_age_maximum():
     assert get_human_age(14, 14) == [0, 0]
 
 
-def test_one_year_age_1():
+def test_first_year_age_minimum():
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_one_year_age_2():
+def test_first_year_age_maximum():
     assert get_human_age(23, 23) == [1, 1]
 
 
-def test_two_year_age_1():
+def test_second_year_age_minimum():
     assert get_human_age(24, 24) == [2, 2]
 
 
-def test_two_year_age_2():
+def test_second_year_age_maximum():
     assert get_human_age(27, 28) == [2, 2]
 
 
-def test_more_year_age_1():
+def test_more_year_age_minimum():
     assert get_human_age(28, 29) == [3, 3]
 
 
-def test_more_year_age_2():
+def test_more_year_age():
     assert get_human_age(100, 100) == [21, 17]
