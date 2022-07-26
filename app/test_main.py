2f5e66af0d3dbe9bb850e73fc048020c541c178f
Andrii Padalko <a.padalko@gmail.com>
2022-07-26 21:10:48 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_zero_human_age_when_animal_age_less_then_first_year():
+    assert get_human_age(14, 0) == [0, 0]
+
+
+def test_one_human_age_when_animal_age_less_then_second_year():
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_two_human_age_when_animal_age_less_then_second_year_plus_each_yeah():
+    assert get_human_age(27, 28) == [2, 2]
+
+
+def test_three_human_age():
+    assert get_human_age(28, 29) == [3, 3]
