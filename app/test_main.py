c54b9053d246fa192b99bc2139cf12b621513959
Andrii Padalko <a.padalko@gmail.com>
2022-07-27 17:34:50 +0300

    Two tests were added

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,10 @@
 from app.main import get_human_age
 
 
+def test_zero_human_age_when_animal_age_are_negative():
+    assert get_human_age(-14, -1) == [0, 0]
+
+
 def test_zero_human_age_when_animal_age_less_then_first_year():
     assert get_human_age(14, 0) == [0, 0]
 
@@ -15,3 +19,7 @@ def test_two_human_age_when_animal_age_less_then_second_year_plus_each_yeah():
 
 def test_three_human_age():
     assert get_human_age(28, 29) == [3, 3]
+
+
+def test_large_human_age_when_animal_age_are_large():
+    assert get_human_age(234, 456) == [54, 88]
