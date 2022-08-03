63eab4d98b9563ade85b7c4b3b012e4a74022ed3
Inna Kushnir <innakushnir63@gmail.com>
2022-08-03 11:00:09 +0300

    Solutions

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,36 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_cat_dog_age_zero_year():
+    ages = get_human_age(14, 14)
+    assert ages == [0, 0]
+
+
+def test_cat_dog_age_exactly_one_year():
+    ages = get_human_age(15, 15)
+    assert ages == [1, 1]
+
+
+def test_cat_dog_age_still_one_year():
+    ages = get_human_age(23, 23)
+    assert ages == [1, 1]
+
+
+def test_cat_dog_age_exactly_two_years():
+    ages = get_human_age(24, 24)
+    assert ages == [2, 2]
+
+
+def test_cat_dog_age_still_two_years():
+    ages = get_human_age(26, 26)
+    assert ages == [2, 2]
+
+
+def test_cat_age_more_than_two_years():
+    ages = get_human_age(28, 28)
+    assert ages == [3, 2]
+
+
+def test_cat_dog_age_random_years():
+    ages = get_human_age(100, 100)
+    assert ages == [21, 17]
