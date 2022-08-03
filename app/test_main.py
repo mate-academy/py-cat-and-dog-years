95a1287335b23905568166b57489009d891329be
Inna Kushnir <innakushnir63@gmail.com>
2022-08-03 15:59:33 +0300

    simplify

--- app/test_main.py
+++ app/test_main.py
@@ -2,35 +2,28 @@ from app.main import get_human_age
 
 
 def test_cat_dog_age_zero_year():
-    ages = get_human_age(14, 14)
-    assert ages == [0, 0]
+    assert get_human_age(14, 14) == [0, 0]
 
 
 def test_cat_dog_age_exactly_one_year():
-    ages = get_human_age(15, 15)
-    assert ages == [1, 1]
+    assert get_human_age(15, 15) == [1, 1]
 
 
 def test_cat_dog_age_still_one_year():
-    ages = get_human_age(23, 23)
-    assert ages == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
 
 
 def test_cat_dog_age_exactly_two_years():
-    ages = get_human_age(24, 24)
-    assert ages == [2, 2]
+    assert get_human_age(24, 24) == [2, 2]
 
 
 def test_cat_dog_age_still_two_years():
-    ages = get_human_age(26, 26)
-    assert ages == [2, 2]
+    assert get_human_age(26, 26) == [2, 2]
 
 
 def test_cat_age_more_than_two_years():
-    ages = get_human_age(28, 28)
-    assert ages == [3, 2]
+    assert get_human_age(28, 28) == [3, 2]
 
 
 def test_cat_dog_age_random_years():
-    ages = get_human_age(100, 100)
-    assert ages == [21, 17]
+    assert get_human_age(100, 100) == [21, 17]
