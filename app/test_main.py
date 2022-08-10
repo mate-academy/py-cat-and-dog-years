f82cbf6071f2a7c58ded1c443fa030d07d4d4fb5
Timur <timur.hike@gmail.com>
2022-08-10 21:38:28 +0300

    add negatig, large, zero numbers

--- app/test_main.py
+++ app/test_main.py
@@ -15,3 +15,15 @@ def test_cat_add_extra_1_year_each_4_years():
 
 def test_dog_add_extra_1_year_each_5_years():
     assert get_human_age(27, 29) == [2, 3]
+
+
+def test_when_age_equal_negative():
+    assert get_human_age(-1, -2) == [0, 0]
+
+
+def test_when_age_equal_zero():
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_when_age_large_numbers():
+    assert get_human_age(100, 100) == [21, 17]
