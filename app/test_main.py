972ac22b3f6d3fb2d8001486f245dd24e334af24
AnDy <ansmbox@gmail.com>
2022-08-12 16:38:27 +0300

    Add tests for negative arguments

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,8 @@ from app.main import get_human_age
 
 class TestGetHumanAgeClass:
     @pytest.mark.parametrize(
-        "initial_cat_age,initial_dog_age,expected_cat_to_human,expected_dog_to_human",
+        "initial_cat_age,initial_dog_age,expected_cat_to_human,"
+        "expected_dog_to_human",
         [
             (0, 0, 0, 0),
             (14, 14, 0, 0),
@@ -14,7 +15,15 @@ class TestGetHumanAgeClass:
             (27, 27, 2, 2),
             (28, 28, 3, 2),
             (100, 100, 21, 17),
+            (-23, 23, 0, 1),
+            (24, -24, 2, 0),
+            (-27, -27, 0, 0),
         ]
     )
-    def test_get_human_age_validation(self, initial_cat_age, initial_dog_age, expected_cat_to_human, expected_dog_to_human):
-        assert get_human_age(initial_cat_age, initial_dog_age) == [expected_cat_to_human, expected_dog_to_human]
+    def test_get_human_age_validation(self,
+                                      initial_cat_age,
+                                      initial_dog_age,
+                                      expected_cat_to_human,
+                                      expected_dog_to_human):
+        result = get_human_age(initial_cat_age, initial_dog_age)
+        assert result == [expected_cat_to_human, expected_dog_to_human]
