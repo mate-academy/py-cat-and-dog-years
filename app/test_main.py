53a277d98bfe65c538841e10be1b5c6bcd2538e4
Dima Povkh <dimapovkh@gmail.com>
2022-08-14 00:20:26 +0200

    cat_and_dog_years_v1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_zero_when_ages_less_15():
+    assert get_human_age(0, 14) == [0, 0]
+
+
+def test_should_return_1_when_ages_between_15_and_23():
+    assert get_human_age(15, 23) == [1, 1]
+
+
+def test_should_add_1_year_every_4_pets_years_when_ages_24_and_more():
+    assert get_human_age(100, 28) == [21, 2]
+
+
+def test_should_return_zero_when_age_equal_0():
+    assert get_human_age(-3, -1) == [0, 0]
