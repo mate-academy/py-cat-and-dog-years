02b6bfdafa6330ebdd16e2306c0cef90531a0676
Yan Purdenko <purdenko22@gmail.com>
2022-08-22 14:00:11 +0200

    My Solution3

--- app/test_main.py
+++ app/test_main.py
@@ -3,18 +3,17 @@ import pytest
 from app.main import get_human_age
 
 
-class TestGetHumanAge:
-    @pytest.mark.parametrize(
-        "cat_age, dog_age, expected_list",
-        [
-            (-13, -10, [0, 0]),
-            (0, 0, [0, 0]),
-            (14, 14, [0, 0]),
-            (23, 23, [1, 1]),
-            (27, 27, [2, 2]),
-            (100, 100, [21, 17]),
-            (1024, 536, [252, 104])
-        ]
-    )
-    def test_get_human_age(self, cat_age, dog_age, expected_list):
-        assert get_human_age(cat_age, dog_age) == expected_list
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_list",
+    [
+        (-13, -10, [0, 0]),
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (23, 23, [1, 1]),
+        (27, 27, [2, 2]),
+        (100, 100, [21, 17]),
+        (1024, 536, [252, 104])
+    ]
+)
+def test_get_human_age(cat_age, dog_age, expected_list):
+    assert get_human_age(cat_age, dog_age) == expected_list
