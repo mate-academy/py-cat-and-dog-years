c2c64bc7be677fca4c9a33a20e42b464ea308e7b
Yan Purdenko <purdenko22@gmail.com>
2022-08-22 11:37:44 +0200

    My Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,20 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_should_return_0_when_age_is_less_15():
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_should_return_1_when_age_is_less_23():
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_should_return_2_when_age_is_less_27():
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_should_return_correct_list_when_age_is_over_27():
-    assert get_human_age(100, 100) == [21, 17]
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_list",
+        [
+            (-13, -10, [0, 0]),
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (23, 23, [1, 1]),
+            (27, 27, [2, 2]),
+            (100, 100, [21, 17]),
+            (1024, 536, [252, 104])
+        ]
+    )
+    def test_get_human_age(self, cat_age, dog_age, expected_list):
+        assert get_human_age(cat_age, dog_age) == expected_list
