3567526389635b621528f72327b16612168efa91
yaroslav-demchenko <dastex1411@gmail.com>
2022-08-22 18:06:28 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_value",
+        [
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
+            (100, 100, [21, 17])
+        ]
+    )
+    def test_should_return_valid_values(self, cat_age, dog_age, expected_value):
+        result = get_human_age(cat_age, dog_age)
+        assert result == expected_value
