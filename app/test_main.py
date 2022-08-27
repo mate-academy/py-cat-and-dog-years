61a245618b41e26740013cb119cc32fbbe1084c2
Oleg Usenko <Oleg911Usenko@gmail.com>
2022-08-27 19:12:41 +0300

    Task Complete

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_if_all_arguments_are_int():
+    assert get_human_age(14, 14) == [0, 0]
+
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_if_arguments_are_large():
+    assert get_human_age(527, 324) == [127, 62]
+
+
+def test_func_return_list():
+    assert isinstance(get_human_age(0, 0), list)
+
+
+def test_if_arguments_equal_zero():
+    try:
+        assert get_human_age(0, 0) == [0, 1]
+    except AssertionError:
+        print("0 cat/dog years should convert into 0 human age.")
+
+
+def test_if_arguments_equal_negative_value():
+    assert get_human_age(-2, -5) == [0, 0]
