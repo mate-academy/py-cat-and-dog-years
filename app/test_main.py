ee31d4973c19ce8b2a35b0588fac838d0ae5bdc8
Katya Vasylieva <katevvasylyeva@gmail.com>
2022-09-06 17:10:15 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_correct_age():
+    age = get_human_age(28, 28)
+    assert age == [3, 2]
+
+
+def test_should_return_0_when_age_is_0():
+    age = get_human_age(0, 17)
+    assert age == [0, 1]
+
+
+def test_should_return_correct_age_when_input_is_float():
+    age = get_human_age(15.9, 0.7)
+    assert age == [1, 0]
+
+
+def test_should_return_0_when_age_is_negative():
+    age = get_human_age(-9, 19)
+    assert age == [0, 1]
