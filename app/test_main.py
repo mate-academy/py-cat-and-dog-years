929c78802ce33b17a9077d02dfcbf18b87d78634
Katya Vasylieva <katevvasylyeva@gmail.com>
2022-09-07 14:58:53 +0300

    was rewritten into class, border tests were added

--- app/test_main.py
+++ app/test_main.py
@@ -1,16 +1,40 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_should_return_correct_age():
-    age = get_human_age(28, 28)
-    assert age == [3, 2]
+class TestGetHumanAge:
 
-
-def test_should_return_0_when_age_is_0():
-    age = get_human_age(0, 17)
-    assert age == [0, 1]
-
-
-def test_should_return_correct_age_when_input_is_float():
-    age = get_human_age(15.9, 0.7)
-    assert age == [1, 0]
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_age_list",
+        [
+            pytest.param(
+                0, 17,
+                [0, 1],
+                id="should return 0 when age is 0"
+            ),
+            pytest.param(
+                15, 14,
+                [1, 0],
+                id="should return 0 when age is less than first border"
+            ),
+            pytest.param(
+                23, 24,
+                [1, 2],
+                id="should return 1 when age between first and second border"
+            ),
+            pytest.param(
+                28, 28,
+                [3, 2],
+                id="should return 2 when age between second and third border"
+            ),
+            pytest.param(
+                38, 34,
+                [5, 4],
+                id="should return correct age when input is above third border"
+            )
+        ]
+    )
+    def test_returns_correct_age(self, cat_age, dog_age, expected_age_list):
+        age = get_human_age(cat_age, dog_age)
+        assert age == expected_age_list
