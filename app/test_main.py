1a6a7932f2f9b9017619753dac8059536d96c418
Paul Starodub <amorallex@gmail.com>
2022-09-07 10:31:24 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,31 @@
 from app.main import get_human_age
 
 
-def test_should_return_zeros_when_age_is_0():
+def test_should_return_zeros_when_age_animal_less_than_15():
     list_of_age = get_human_age(0, 0)
 
     assert list_of_age == [0, 0]
 
 
-def test_should_return_one_year_when_age_is_15():
+def test_should_return_one_year_when_age_animal_from_15_to_24():
     list_of_age = get_human_age(15, 15)
 
     assert list_of_age == [1, 1]
 
 
-def test_should_return_two_years_when_age_is_24():
+def test_should_return_two_years_when_age_from_24():
     list_of_age = get_human_age(24, 24)
 
     assert list_of_age == [2, 2]
 
 
-def test_should_return_cat_years_when_age_is_28():
+def test_should_return_cat_years_when_age_is_more_28():
     list_of_age = get_human_age(28, 0)
 
     assert list_of_age == [3, 0]
 
 
-def test_should_return_dog_years_when_age_is_28():
+def test_should_return_dog_years_when_age_is_more_29():
     list_of_age = get_human_age(0, 29)
 
     assert list_of_age == [0, 3]
