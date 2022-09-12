2a1846c388f7365235363c78d9660e414daac255
Foggy-Cat <rosky.yaroslav@gmail.com>
2022-09-12 12:02:46 +0300

    Del class

--- app/test_main.py
+++ app/test_main.py
@@ -3,18 +3,18 @@ import pytest
 from app.main import get_human_age
 
 
-class TestError:
-    def test_type_error(self):
-        with pytest.raises(TypeError):
-            get_human_age("cat", "dog")
+def test_type_error():
+    with pytest.raises(TypeError):
+        get_human_age("cat", "dog")
 
 
-class TestGetHumanAge:
-    def test_first_15_cat_and_dog_years_give_1_human_year(self):
-        assert get_human_age(7, 9) == [0, 0]
+def test_first_15_cat_and_dog_years_give_1_human_year():
+    assert get_human_age(7, 9) == [0, 0]
 
-    def test_the_next_9_cat_and_dog_years_give_1_more_human_year(self):
-        assert get_human_age(17, 23) == [1, 1]
 
-    def test_every_4_next_cat_and_5_next_dog_years_give_1_extra_year(self):
-        assert get_human_age(32, 44) == [4, 6]
+def test_the_next_9_cat_and_dog_years_give_1_more_human_year():
+    assert get_human_age(17, 23) == [1, 1]
+
+
+def test_every_4_next_cat_and_5_next_dog_years_give_1_extra_year():
+    assert get_human_age(32, 44) == [4, 6]
