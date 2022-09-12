ad5b94330bf22fbfa0b166ed366c5ba4a21d25f2
Foggy-Cat <rosky.yaroslav@gmail.com>
2022-09-12 12:21:12 +0300

    Added tests

--- app/test_main.py
+++ app/test_main.py
@@ -18,3 +18,15 @@ def test_the_next_9_cat_and_dog_years_give_1_more_human_year():
 
 def test_every_4_next_cat_and_5_next_dog_years_give_1_extra_year():
     assert get_human_age(32, 44) == [4, 6]
+
+
+def test_for_negative_ages():
+    assert get_human_age(-2, -5) == [0, 0]
+
+
+def test_for_zeroes_ages():
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_for_large_age():
+    assert get_human_age(2350, 4320) == [583, 861]
