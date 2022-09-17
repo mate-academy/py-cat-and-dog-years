f2a0739e54f180ac1ef67d16841f0e98b27e1d89
Vlad Magdenko <vladearth7@gmail.com>
2022-09-17 17:03:34 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,31 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_correct_result_if_ages_bigger_then_14():
+    cat_age, dog_age = 28, 28
+    assert get_human_age(cat_age, dog_age) == [3, 2]
+
+
+def test_should_return_correct_result_if_cat_age_smaller_then_15():
+    cat_age, dog_age = 11, 15
+    assert get_human_age(cat_age, dog_age) == [0, 1]
+
+
+def test_should_return_correct_result_if_dog_age_smaller_then_15():
+    cat_age, dog_age = 15, 10
+    assert get_human_age(cat_age, dog_age) == [1, 0]
+
+
+def test_should_return_zeros_if_ages_are_negative():
+    cat_age, dog_age = -1, -12
+    assert get_human_age(cat_age, dog_age) == [0, 0]
+
+
+def test_should_return_correct_result_with_big_numbers():
+    cat_age, dog_age = 340, 280
+    assert get_human_age(cat_age, dog_age) == [81, 53]
+
+
+
+
+
