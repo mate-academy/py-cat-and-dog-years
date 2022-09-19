af1cdfc461eef69f59238ea88f9ce06206db12e2
Vlad Milkov <vladmilkov@gmail.com>
2022-09-19 15:59:23 +0300

    Redundant comments removed

--- app/test_main.py
+++ app/test_main.py
@@ -47,35 +47,3 @@ def test_count_human_age(
         expected_result
 ):
     assert get_human_age(cat_age, dog_age) == expected_result
-
-
-# def test_should_return_zeros_when_ages_are_less_than_15():
-#     assert get_human_age(14, 14) == [0, 0]
-#
-#
-# def test_should_return_zeros_when_ages_are_equal_to_zero():
-#     assert get_human_age(0, 0) == [0, 0]
-#
-#
-# def test_should_return_ones_when_ages_are_equal_first_year():
-#     assert get_human_age(15, 15) == [1, 1]
-#
-#
-# def test_should_return_ones_when_ages_are_between_first_and_second_years():
-#     assert get_human_age(23, 23) == [1, 1]
-#
-#
-# def test_should_return_two_when_ages_are_equal_second_year():
-#     assert get_human_age(24, 24) == [2, 2]
-#
-#
-# def test_should_return_two_when_ages_are_below_third_year():
-#     assert get_human_age(27, 28) == [2, 2]
-
-
-# def test_should_return_three_when_ages_are_equal_third_year():
-#     assert get_human_age(28, 29) == [3, 3]
-#
-#
-# def test_should_return_correct_value_when_ages_are_bigger_than_third_year():
-#     assert get_human_age(100, 100) == [21, 17]
