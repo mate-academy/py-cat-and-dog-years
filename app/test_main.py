5f1ba0b715573314ed1e6ca5178d834a1e48c362
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-09-19 14:33:34 +0300

    py_cat_and_dog_years_Solution_new

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,28 @@
 from app.main import get_human_age
 
 
-def test_equal_right_result():
-    args = (
-        {"cat_age": 15, "dog_age": 15, "human_age": [1, 1]},
-        {"cat_age": 24, "dog_age": 24, "human_age": [2, 2]},
-        {"cat_age": 28, "dog_age": 29, "human_age": [3, 3]},
-        {"cat_age": 70, "dog_age": 70, "human_age": [13, 11]},
-        {"cat_age": 100, "dog_age": 100, "human_age": [21, 17]}
-    )
-    for i in range(len(args)):
-        assert get_human_age(
-            args[i]["cat_age"], args[i]["dog_age"]) == args[i]["human_age"]
+def test_zero_ages():
+    ages = {"cat_age": 14, "dog_age": 14, "human_age": [0, 0]}
+    assert get_human_age(
+        ages["cat_age"], ages["dog_age"]) == ages["human_age"]
+
+
+def test_first_ages():
+    ages = {"cat_age": 15, "dog_age": 15, "human_age": [1, 1]}
+    assert get_human_age(
+        ages["cat_age"], ages["dog_age"]) == ages["human_age"]
+
+
+def test_second_ages():
+    ages = {"cat_age": 24, "dog_age": 24, "human_age": [2, 2]}
+    assert get_human_age(
+        ages["cat_age"], ages["dog_age"]) == ages["human_age"]
+
+
+def test_more_ages():
+    ages = {"cat_age": 28, "dog_age": 29, "human_age": [3, 3]}
+    assert get_human_age(
+        ages["cat_age"], ages["dog_age"]) == ages["human_age"]
 
 
 def test_result_should_is_list():
