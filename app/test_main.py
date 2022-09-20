d401632e11c7c99fd26fdf5ebf7de858671d29c7
Vlad Magdenko <vladearth7@gmail.com>
2022-09-20 18:15:03 +0300

    'Solution1'

--- app/test_main.py
+++ app/test_main.py
@@ -1,26 +1,21 @@
 from app.main import get_human_age
+import pytest
 
 
-def test_should_return_correct_result_if_ages_bigger_then_14():
-    cat_age, dog_age = 28, 28
-    assert get_human_age(cat_age, dog_age) == [3, 2]
-
-
-def test_should_return_correct_result_if_cat_age_smaller_then_15():
-    cat_age, dog_age = 11, 15
-    assert get_human_age(cat_age, dog_age) == [0, 1]
-
-
-def test_should_return_correct_result_if_dog_age_smaller_then_15():
-    cat_age, dog_age = 15, 10
-    assert get_human_age(cat_age, dog_age) == [1, 0]
-
-
-def test_should_return_zeros_if_ages_are_negative():
-    cat_age, dog_age = -1, -12
-    assert get_human_age(cat_age, dog_age) == [0, 0]
-
-
-def test_should_return_correct_result_with_big_numbers():
-    cat_age, dog_age = 340, 280
-    assert get_human_age(cat_age, dog_age) == [81, 53]
+class Test:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, result",
+        [
+            (28, 28, [3, 2]),
+            (11, 15, [0, 1]),
+            (15, 10, [1, 0]),
+            (-1, -12, [0, 0]),
+            (340, 280, [81, 53])
+        ]
+    )
+    def test_get_human_age(self,
+                           cat_age,
+                           dog_age,
+                           result
+                           ):
+        assert get_human_age(cat_age, dog_age) == result
