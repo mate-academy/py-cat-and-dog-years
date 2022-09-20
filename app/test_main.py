71f22b4eceecda59c9a503e2efd0805b3d7dfbc2
Vadim Kosnovskiy <kvmltkv@gmail.com>
2022-09-20 11:29:34 +0300

    py_cat_and_dog_years_Solution3

--- app/test_main.py
+++ app/test_main.py
@@ -2,27 +2,19 @@ from app.main import get_human_age
 
 
 def test_zero_ages():
-    ages = {"cat_age": 14, "dog_age": 14, "human_age": [0, 0]}
-    assert get_human_age(
-        ages["cat_age"], ages["dog_age"]) == ages["human_age"]
+    assert get_human_age(14, 14) == [0, 0]
 
 
 def test_first_ages():
-    ages = {"cat_age": 15, "dog_age": 15, "human_age": [1, 1]}
-    assert get_human_age(
-        ages["cat_age"], ages["dog_age"]) == ages["human_age"]
+    assert get_human_age(15, 15) == [1, 1]
 
 
 def test_second_ages():
-    ages = {"cat_age": 24, "dog_age": 24, "human_age": [2, 2]}
-    assert get_human_age(
-        ages["cat_age"], ages["dog_age"]) == ages["human_age"]
+    assert get_human_age(24, 24) == [2, 2]
 
 
 def test_more_ages():
-    ages = {"cat_age": 28, "dog_age": 29, "human_age": [3, 3]}
-    assert get_human_age(
-        ages["cat_age"], ages["dog_age"]) == ages["human_age"]
+    assert get_human_age(28, 29) == [3, 3]
 
 
 def test_result_should_is_list():
