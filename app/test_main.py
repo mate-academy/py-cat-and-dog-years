a9c6be01be87e0bf437230da29b6ac8d69ff4571
Oleksandr Panasiuk <provizor.307@gmail.com>
2022-09-23 10:27:03 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_add_zeros_when_cat_and_dog_age_is_lower_15():
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_one_when_cat_and_dog_age_equal_15_and_lower_24():
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_return_two_when_cat_and_dog_age_equal_24_and_lower_28():
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_should_return_age_when_cat_and_dog_age_equal_28():
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_should_return_age_when_cat_and_dog_age_equal_100():
+    assert get_human_age(100, 100) == [21, 17]
