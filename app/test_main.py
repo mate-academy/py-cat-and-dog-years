429daa146abd028ba0531dd4184f4a7052b7944b
Egor Voloshyn <103663723+AriohBelskij@users.noreply.github.com>
2022-09-23 09:47:22 +0300

    Update test annotation

--- app/test_main.py
+++ app/test_main.py
@@ -10,43 +10,42 @@ class TestGetHumanAge:
                 0,
                 0,
                 [0, 0],
-                id="Test years when cat and dogs years"
-                   " equal 0"),
+                id="0, 0 -> should return 0"
+                   " when cat and dog 0"),
 
             pytest.param(
                 14,
                 14,
                 [0, 0],
-                id="Test correct counting cat and dog years"
-                   " if years < 15"),
+                id="14, 14 -> should return 0"
+                   " when cat and dog years less than 15"),
 
             pytest.param(
                 15,
                 15,
                 [1, 1],
-                id="Test first 15 dog and cat years"
-                   " give 1 human year"),
+                id="15, 15 -> should return 1"
+                   " when cat and dog years equal 15"),
 
             pytest.param(
                 27,
                 27,
                 [2, 2],
-                id="Finally test when cat and dogs years"
-                   " must be equal"),
+                id="27, 27 -> should return 2,"
+                   " when cat and dog years 27"),
 
             pytest.param(
                 28,
                 28,
                 [3, 2],
-                id="Test when cat years must be more"
-                   " then dogs years in range 28 year."),
+                id=" should return 3 for cat and 2"
+                   " for dog when years equal to 28."),
 
             pytest.param(
                 100,
                 100,
                 [21, 17],
-                id="Finally test in range, when the cat"
-                   " years go forward overtaking dogs years")])
+                id="100, 100 -> should work correct with big numbers")])
     def test_human_age_correctly(self,
                                  cat_years: int,
                                  dog_years: int,
