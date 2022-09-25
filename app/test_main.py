8ab0ec50a7085f881fac778d24fcc6c10b03b44e
Tetyana Pavlyuk <tetyana.pas@gmail.com>
2022-09-25 16:56:14 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,50 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, human_age",
+        [
+            pytest.param(
+                0, 0, [0, 0],
+                id="should convert min value"
+            ),
+            pytest.param(
+                14, 14, [0, 0],
+                id="should convert max for zero years"
+            ),
+            pytest.param(
+                15, 15, [1, 1],
+                id="should convert min for one year"
+            ),
+            pytest.param(
+                23, 23, [1, 1],
+                id="should convert max for one year"
+            ),
+            pytest.param(
+                24, 24, [2, 2],
+                id="should convert min for two years"
+            ),
+            pytest.param(
+                27, 28, [2, 2],
+                id="should convert max for two years"
+            ),
+            pytest.param(
+                28, 29, [3, 3],
+                id="should convert min for three years"
+            ),
+            pytest.param(
+                100, 100, [21, 17],
+                id="should convert max value"
+            )
+        ]
+    )
+    def test_get_human_age(
+            self,
+            cat_age,
+            dog_age,
+            human_age
+    ):
+        result = get_human_age(cat_age, dog_age)
+        assert result == human_age
