7ccf6434072837690c998847757559228f01e1c9
Kyryll Katash <kyryllkatash@gmail.com>
2022-09-26 20:28:18 +0300

    Solution for this task x2

--- app/test_main.py
+++ app/test_main.py
@@ -10,7 +10,7 @@ def test_returns_one_when_age_is_fifteen():
 
 
 def test_returns_one_when_age_is_bigger_than_15_and_lower_than_24():
-    assert get_human_age(22, 22) == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
 
 
 def test_func_return_2_when_age_is_24():
@@ -23,6 +23,10 @@ def test_returns_2_when_upper_24_and_lower_28():
 
 def test_should_return_different_values_when_age_is_28():
     assert get_human_age(28, 28) == [3, 2]
+
+
+def test_for_age_is_29():
+    assert get_human_age(29, 29) == [3, 3]
 
 
 def test_return_different_values_when_age_is_above_28():
