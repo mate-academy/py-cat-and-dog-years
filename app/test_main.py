3dfe52ff3d24156b541025e9316a60dfa2aa6c45
Artem <Artemvyhovanec@gmail.com>
2022-09-27 16:03:21 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,41 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected",
+    [
+        (
+            28,
+            28,
+            [3, 2]
+        ),
+        (
+            0,
+            0,
+            [0, 0]
+        ),
+        (
+            15,
+            15,
+            [1, 1]
+        ),
+        (
+            100,
+            100,
+            [21, 17]
+        )
+    ]
+)
+def test_function_correct_working(cat_age, dog_age, expected):
+    assert get_human_age(cat_age, dog_age) == expected
+
+
+def test_should_raise_error_when_arguments_not_int():
+    with pytest.raises(TypeError):
+        get_human_age("one", [2, 3, 4])
+
+
+def test_should_raise_error_when_not_2_arguments_passed_to_function():
+    with pytest.raises(TypeError):
+        get_human_age(24)
