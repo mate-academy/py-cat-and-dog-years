6c005d6f79fba56b7d3e675611fe53e60d7916cd
Soroka Igor <copokaigor@gmail.com>
2022-09-28 13:55:13 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_zero_ages():
+    result1 = get_human_age(15, 15)
+    assert result1 == [1, 1]
+
+
+def test_first_ages():
+    result2 = get_human_age(24, 24)
+    assert result2 == [2, 2]
+
+
+def test_second_ages():
+    result = get_human_age(28, 29)
+    assert result == [3, 3]
