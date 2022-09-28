85ff07e8543af190a590ee7b5a11ae0b61443fca
Soroka Igor <copokaigor@gmail.com>
2022-09-28 16:53:34 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -2,7 +2,7 @@ from app.main import get_human_age
 
 
 def test_zero_ages():
-    assert get_human_age(15, 15) == [1, 1]
+    assert get_human_age(16, 14) == [1, 0]
 
 
 def test_first_ages():
@@ -10,7 +10,7 @@ def test_first_ages():
 
 
 def test_second_ages():
-    assert get_human_age(28, 29) == [3, 3]
+    assert get_human_age(28, 28) == [3, 2]
 
 
 def test_third_ages():
