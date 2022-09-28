870f15e0fae2ce7c9296b8eeac81898694acaf1f
Soroka Igor <copokaigor@gmail.com>
2022-09-28 16:42:21 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -2,15 +2,16 @@ from app.main import get_human_age
 
 
 def test_zero_ages():
-    result1 = get_human_age(15, 15)
-    assert result1 == [1, 1]
+    assert get_human_age(15, 15) == [1, 1]
 
 
 def test_first_ages():
-    result2 = get_human_age(24, 24)
-    assert result2 == [2, 2]
+    assert get_human_age(24, 24) == [2, 2]
 
 
 def test_second_ages():
-    result = get_human_age(28, 29)
-    assert result == [3, 3]
+    assert get_human_age(28, 29) == [3, 3]
+
+
+def test_third_ages():
+    assert get_human_age(100, 100) == [21, 17]
