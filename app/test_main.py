5183ae7486ceb3c49f00779421f53d00bea2141b
Soroka Igor <copokaigor@gmail.com>
2022-09-29 09:58:11 +0300

    Solution1

--- app/test_main.py
+++ app/test_main.py
@@ -9,8 +9,16 @@ def test_first_ages():
     assert get_human_age(24, 24) == [2, 2]
 
 
+def test_first_ages_before_more_year():
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_first_and_next_age_before_extra():
+    assert get_human_age(27, 28) == [2, 2]
+
+
 def test_second_ages():
-    assert get_human_age(28, 28) == [3, 2]
+    assert get_human_age(28, 29) == [3, 3]
 
 
 def test_third_ages():
