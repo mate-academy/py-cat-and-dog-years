875ca647cd412b6a5a5ab796862ee9926aee6553
Diana <daria.blessed18@gmail.com>
2022-09-29 13:43:27 +0300

    Slution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_add_zeros_if_arguments_zeros():
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_return_zeros_if_age_less_fifteen():
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_one_if_age_equals_fifteen():
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_twenty_four_years_should_convert_into_two_human_years():
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_twenty_eight_and_twenty_nine_should_convert_into_three():
+    assert get_human_age(28, 29) == [3, 3]
