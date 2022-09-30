37bf1a592ac3cdfdd7d9b64436b445d8f7d1f2a0
Diana <daria.blessed18@gmail.com>
2022-09-30 17:37:31 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -17,9 +17,13 @@ def test_twenty_four_years_should_convert_into_two_human_years():
     assert get_human_age(24, 24) == [2, 2]
 
 
+def test_hundred_should_convert_into_twenty_one_and_seventeen():
+    assert get_human_age(100, 100) == [21, 17]
+
+
 def test_twenty_eight_and_twenty_nine_should_convert_into_three():
     assert get_human_age(28, 29) == [3, 3]
 
 
-def test_hundred_should_convert_into_twenty_one_and_seventeen():
-    assert get_human_age(100, 100) == [21, 17]
+def test_fifty_should_convert_into_eight_and_seven():
+    assert get_human_age(50, 50) == [8, 7]
