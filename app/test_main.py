847770ae44b9f4fc2c1bbf0002ee1b87a0759d4a
Maks Danylchuk <maxitosm00@gmail.com>
2022-09-30 16:29:43 +0300

    Ready

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_0_if_age_less_than_15():
+    goals = get_human_age(0, 14)
+
+    assert goals == [0, 0]
+
+
+def test_should_return_1_if_age_more_than_14_and_less_24():
+    goals = get_human_age(15, 23)
+
+    assert goals == [1, 1]
+
+
+def test_should_return_2_if_age_more_than_23_and_less_28():
+    goals = get_human_age(24, 27)
+
+    assert goals == [2, 2]
+
+
+def test_should_return_3_if_age_more_than_27_for_cat_and_28_for_dog():
+    goals = get_human_age(28, 29)
+
+    assert goals == [3, 3]
