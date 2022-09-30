d8f45b0049836dee0046043d2fd92bdc1d265c9f
Maks Danylchuk <maxitosm00@gmail.com>
2022-09-30 20:07:37 +0300

    ready

--- app/test_main.py
+++ app/test_main.py
@@ -23,3 +23,23 @@ def test_should_return_3_if_age_more_than_27_for_cat_and_28_for_dog():
     goals = get_human_age(28, 29)
 
     assert goals == [3, 3]
+
+
+def test_every_4_years_after_28_for_cat_should_add_a_year():
+    goals = get_human_age(32, 29)
+
+    assert goals == [4, 3]
+
+
+def test_every_5_years_after_29_for_dog_should_add_a_year():
+    goals = get_human_age(28, 39)
+
+    assert goals == [3, 5]
+
+
+def test_big_numbers_of_years():
+    goals = get_human_age(100, 100)
+
+    assert goals == [21, 17]
+
+
