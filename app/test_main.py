8c93d2298cfe01e6a7db5ee1d6db043711945d2b
Rostyslav Nikulin <rostik.nikss@gmail.com>
2022-10-02 23:11:43 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,27 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_zeros_when_ages_equal_0():
+    assert [0, 0] == get_human_age(0, 0)
+
+
+def test_should_return_zeros_when_ages_less_than_15():
+    assert [0, 0] == get_human_age(14, 2)
+
+
+def test_should_return_correct_output_for_different_ages():
+    assert [1, 2] == get_human_age(23, 24)
+
+
+def test_should_return_different_result_for_28_pets_ages():
+    assert [3, 2] == get_human_age(28, 28)
+
+
+def test_for_extra_ages():
+    assert [21, 17] == get_human_age(100, 100)
+
+
+def test_should_return_zeros_for_ages_less_than_0():
+    assert [0, 0] == get_human_age(-213, -523423)
+
+
