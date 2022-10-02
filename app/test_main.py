2e222fb4032628e3efb4463d47a311ba581b799c
vadymsabadyr <Sabadyrvv94@gmail.com>
2022-10-02 20:34:02 +0300

    change id

--- app/test_main.py
+++ app/test_main.py
@@ -11,50 +11,50 @@ class TestHumanAge:
                 0,
                 0,
                 [0, 0],
-                id="should return [0, 0]"
+                id="should return list with zero ages."
             ),
             pytest.param(
                 14,
                 14,
                 [0, 0],
-                id="should return [0, 0]"
+                id="list with zero ages, cat_age and dog_age < 15."
             ),
             pytest.param(
                 15,
                 15,
                 [1, 1],
-                id="should return [1, 1]"
+                id="list with ages, cat_age and dog_age == 15"
 
             ),
             pytest.param(
                 23,
                 23,
                 [1, 1],
-                id="should return [1, 1]"
+                id="list with ages, cat_age and dog_age > 15"
             ),
             pytest.param(
                 24,
                 24,
                 [2, 2],
-                id="should return [2, 2]"
+                id="list with ages, cat_age and dog_age == 24"
             ),
             pytest.param(
                 27,
                 27,
                 [2, 2],
-                id="should return [2, 2]"
+                id="list with ages, cat_age and dog_age > 24"
             ),
             pytest.param(
                 28,
                 28,
                 [3, 2],
-                id="should return [3, 2]"
+                id="list with ages, cat_age == 28  dog_age > 24"
             ),
             pytest.param(
                 100,
                 100,
                 [21, 17],
-                id="should return [21, 17]"
+                id="list with ages, cat_age > 28  dog_age > 29]"
             )
         ]
     )
