bfb773d39c902cea154235a5db5b327fc58adb75
Zakhar Podporinov <zp162162@gmail.com>
2022-10-03 18:17:53 +0300

    Complete!

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,10 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_expected_value():
+    assert get_human_age(0, 0) == [0, 0]
+    assert get_human_age(23, 23) == [1, 1]
+    assert get_human_age(28, 14) == [3, 0]
+    assert get_human_age(14, 28) == [0, 2]
+    assert get_human_age(28, 28) == [3, 2]
+    assert get_human_age(100, 100) == [21, 17]
