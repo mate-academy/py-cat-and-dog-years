f68d9c7ff316f2d56e75193012717268e58c3358
Dima Ursatii <Dimon4ir@gmail.com>
2022-10-04 21:02:51 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -10,14 +10,3 @@ def convert_to_human(animal_age, first_year, second_year, each_year):
     if animal_age < first_year + second_year:
         return 1
     return 2 + (animal_age - first_year - second_year) // each_year
-
-
-
-print(get_human_age(0, 0)) == [0, 0]
-print(get_human_age(14, 14)) == [0, 0]
-print(get_human_age(15, 15)) == [1, 1]
-print(get_human_age(23, 23)) == [1, 1]
-print(get_human_age(24, 24)) == [2, 2]
-print(get_human_age(27, 27)) == [2, 2]
-print(get_human_age(28, 28)) == [3, 2]
-print(get_human_age(100, 100)) == [21, 17]
