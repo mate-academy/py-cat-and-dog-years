0b382317af6359190442334338d6056347f34ec4
Alexander <alexzubkoff123@gmail.com>
2022-10-04 20:47:06 +0300

    Writed test for converting into human cat and dog years

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,11 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_get_human_age_years_should_convert_into_1():
+    assert get_human_age(15, 15) == [1, 1]
+
+def test_get_human_age_years_should_convert_into_2():
+    assert get_human_age(24, 24) == [2, 2]
+
+def test_get_human_age_years_should_convert_into_3():
+    assert get_human_age(28, 29) == [3, 3]
