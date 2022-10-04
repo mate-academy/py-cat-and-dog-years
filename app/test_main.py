daf9310c6fc3e2d2e7ec9b0a91b519e8ae8a57fe
Yaroslav Stetsko <yar.stetsko@gmail.com>
2022-10-04 17:43:55 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,14 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_proper_age_if_animal_years_are_more_than_14():
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_should_return_proper_age_if_animal_years_are_more_than_23():
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_should_return_proper_age_if_cat_age_28_dog_age_29():
+    assert get_human_age(28, 29) == [3, 3]
+
