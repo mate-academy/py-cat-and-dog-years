84f6060d33b7c01a95b78b65e7f3100cdb2cbbe7
Roman Kolmakov <cerne313@gmail.com>
2022-10-05 21:10:00 +0300

    changed single to double quotes

--- app/test_main.py
+++ app/test_main.py
@@ -4,55 +4,55 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    'cat_age, dog_age, expected',
+    "cat_age, dog_age, expected",
     [
         pytest.param(
             0,
             0,
             [0, 0],
-            id='should return [0, 0] if ages are 0'
+            id="should return [0, 0] if ages are 0"
         ),
         pytest.param(
             -1,
             -1,
             [0, 0],
-            id='should return [0, 0] if ages are negative'
+            id="should return [0, 0] if ages are negative"
         ),
         pytest.param(
             14,
             14,
             [0, 0],
-            id='should return [0, 0] if ages < 15'
+            id="should return [0, 0] if ages < 15"
         ),
         pytest.param(
             15,
             15,
             [1, 1],
-            id='should return [1, 1] if ages are 15'
+            id="should return [1, 1] if ages are 15"
         ),
         pytest.param(
             23,
             23,
             [1, 1],
-            id='should return [1, 1] if ages between 15 and 24'
+            id="should return [1, 1] if ages between 15 and 24"
         ),
         pytest.param(
             24,
             24,
             [2, 2],
-            id='should return [2, 2] if ages are 24'
+            id="should return [2, 2] if ages are 24"
         ),
         pytest.param(
             28,
             28,
             [3, 2],
-            id='should return [3,2] if ages are 28'
+            id="should return [3,2] if ages are 28"
         ),
         pytest.param(
             100,
             100,
             [21, 17],
-            id='should return [21, 17] if ages are 100'
+            id="should return [21, 17] if ages are 100"
         )
     ]
 
