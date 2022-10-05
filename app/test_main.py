0697a51200ddaa2f7caf1c3d490a2466f5c37c2f
Dmytro Petliovanyi <petliovany@gmail.com>
2022-10-05 17:27:07 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,42 @@
+import pytest
+
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_years,dog_years,human_age",
+    [
+        (
+            0, 0, [0, 0]
+        ),
+        (
+            14, 14, [0, 0]
+        ),
+        (
+            15, 15, [1, 1]
+        ),
+        (
+            23, 23, [1, 1]
+        ),
+        (
+            24, 24, [2, 2]
+        ),
+        (
+            27, 27, [2, 2]
+        ),
+        (
+            28, 28, [3, 2]
+        ),
+        (
+            100, 100, [21, 17]
+        )
+    ]
+)
+def test_convert_correctly(cat_years: int,
+                           dog_years: int,
+                           human_age: list) -> None:
+
+    result = get_human_age(cat_years, dog_years)
+
+    assert result == human_age
