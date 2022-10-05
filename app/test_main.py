0b8c6f972ac53451568ee5ef992461eed35fbfed
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-10-06 00:22:09 +0300

    Type annotations added

--- app/test_main.py
+++ app/test_main.py
@@ -1,9 +1,9 @@
 from app.main import get_human_age
 
 
-def test_should_return_zero_when_animal_age_less_than_15():
+def test_should_return_zero_when_animal_age_less_than_15() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
 
-def test_should_return_correct_human_age():
+def test_should_return_correct_human_age() -> None:
     assert get_human_age(28, 28) == [3, 2]
