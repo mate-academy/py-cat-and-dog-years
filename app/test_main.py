83a02612574bf4e5a9a69844cd20f4981ad2ba1b
Alexander <grygoruk123@gmail.com>
2022-10-05 12:21:44 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,24 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_both_ages_equal_to_0():
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_less_than_first_human_year():
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_different_values_for_same_age():
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_first_human_year():
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_big_ages():
+    assert get_human_age(100, 100) == [21, 17]
+
+
+
