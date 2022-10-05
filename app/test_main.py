0e9e07bb469dbcf5e35c0bbc0e3b3db2945e3bb7
Dima Ursatii <Dimon4ir@gmail.com>
2022-10-05 14:05:24 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -6,11 +6,26 @@ class TestDogAge:
     @pytest.mark.parametrize(
         "cat, dog, human",
         [
-            (0, 0, [0, 0]),
-            (15, 15, [1, 1]),
-            (24, 24, [2, 2]),
-            (28, 28, [3, 2]),
-            (100, 100, [21, 17]),
+            pytest.param(
+                0, 0, [0, 0],
+                id="Human must be equal to zero if cat/dog 0 - 14]"
+            ),
+            pytest.param(
+                15, 15, [1, 1],
+                id="Human must be equal to [1, 1] if cat/dog 15"
+            ),
+            pytest.param(
+                24, 24, [2, 2],
+                id="Human must be to [2, 2] if cat/dog 24"
+            ),
+            pytest.param(
+                28, 28, [3, 2],
+                id="Human must be [3, 2] when cat/dog 28"
+            ),
+            pytest.param(
+                100, 100, [21, 17],
+                id="Human must be [21, 17] when cat/dog 100"
+            ),
         ]
     )
     def test_dog_age(self, cat, dog, human):
