0f2572d96202b9f385c05db0756d4bd474dcd8b7
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-10-06 00:20:18 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,9 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_zero_when_animal_age_less_than_15():
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_correct_human_age():
+    assert get_human_age(28, 28) == [3, 2]
