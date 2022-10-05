5e61d618423a1048b899241d1c59b35bd7e60ee2
Katrin Lazarenko <katrin903@gmail.com>
2022-10-05 12:34:55 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,23 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,human_age",
+    [
+        pytest.param(0, 0, [0, 0], id="test 0 cat/dog years"),
+        pytest.param(14, 14, [0, 0], id="test under 15 cat/dog years"),
+        pytest.param(15, 15, [1, 1], id="test 15 cat/dog years"),
+        pytest.param(23, 23, [1, 1], id="test above 15 cat/dog years"),
+        pytest.param(24, 24, [2, 2], id="test every 9 cat/dog years"),
+        pytest.param(27, 27, [2, 2], id="test after 9 before 4/5 cat/dog years"),
+        pytest.param(28, 28, [3, 2], id="test every 4/5 cat/dog years"),
+        pytest.param(100, 100, [21, 17], id="test repeated 4/5 cat/dog years")
+    ]
+)
+def test_get_human_age(cat_age, dog_age, human_age):
+    assert get_human_age(cat_age, dog_age) == human_age
+
+
+if __name__ == '__main__':
+    pytest.main()
