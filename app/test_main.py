ece0f0bb2b900bf8296b03eaa7b85351e5bb4b06
Igor Kosianenko <funkcionalkos@gmail.com>
2022-10-07 21:11:40 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -2,39 +2,20 @@ from app.main import get_human_age
 import pytest
 
 
-class TestDogAge:
+class TestDogCatAge:
     @pytest.mark.parametrize(
-        "age,result",
+        "age_cat,age_dog,result",
         [
-            (0, 0),
-            (14, 0),
-            (15, 1),
-            (23, 1),
-            (24, 2),
-            (27, 2),
-            (29, 3),
-            (100, 17)
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 29, [3, 3]),
+            (100, 100, [21, 17])
         ]
     )
-    def test_for_dog(self, age: int, result: int) -> None:
+    def test_for_dog(self, age_dog, age_cat, result):
 
-        assert get_human_age(0, age)[1] == result
-
-
-class TestCatAge:
-    @pytest.mark.parametrize(
-        "age,result",
-        [
-            (0, 0),
-            (14, 0),
-            (15, 1),
-            (23, 1),
-            (24, 2),
-            (27, 2),
-            (28, 3),
-            (100, 21)
-        ]
-    )
-    def test_for_dog(self, age: int, result: int) -> None:
-
-        assert get_human_age(age, 0)[0] == result
+        assert get_human_age(age_cat, age_dog) == result
