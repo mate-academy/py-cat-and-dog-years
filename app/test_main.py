0c87948337a3e9c4c375870ebf1a63a137d7cf63
Misha Doroshenko <doroshenko.misha73@gmail.com>
2022-10-09 15:57:09 +0300

    Fixed solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,9 +1,29 @@
 from app.main import get_human_age
 
 
-def test_should_return_zero_when_animal_age_less_than_15() -> None:
+def test_zero() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_age_equals_14() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
 
-def test_should_return_correct_human_age() -> None:
+def test_age_equals_15() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_age_equals_23() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_age_equals_24() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_age_equals_28() -> None:
     assert get_human_age(28, 28) == [3, 2]
+
+
+def test_age_equals_100() -> None:
+    assert get_human_age(100, 100) == [21, 17]
