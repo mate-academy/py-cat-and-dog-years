0cf0cc93ef3aa813334672e424ee47225914756b
Bohdan2001007 <bogdan1991007@gmail.com>
2022-10-10 12:19:37 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,33 @@
 from app.main import get_human_age
 
 
-def if_ages_are_zero() -> None:
+def test_zero_ages() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def if_ages_are_both_14() -> None:
+def test_age_lower_than_first_year() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
 
-def if_ages_are_both_15() -> None:
+def test_if_ages_are_both_15() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def if_ages_are_both_23() -> None:
+def test_if_ages_are_both_23() -> None:
     assert get_human_age(23, 23) == [1, 1]
 
 
-def if_ages_are_both_24() -> None:
+def test_if_ages_are_both_24() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
 
-def if_ages_are_both_27() -> None:
-    assert get_human_age(27, 27) == [2, 2]
+def test_if_ages_are_both_26() -> None:
+    assert get_human_age(26, 26) == [2, 2]
 
 
-def if_ages_are_both_28() -> None:
+def test_if_ages_are_both_28() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
 
-def if_ages_are_both_100() -> None:
+def test_if_ages_are_both_100() -> None:
     assert get_human_age(100, 100) == [21, 17]
