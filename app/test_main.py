a574455805a95dada2e4133218daecc86ee99bfb
IvanGLS <gladiolus0707@gmail.com>
2022-10-11 14:38:39 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,6 @@
 from app.main import get_human_age
 
-# write your code here
+
+# Brevity is the soul of wit
+def test_check_all_counted_parameters_cat_and_dog() -> None:
+    assert get_human_age(28, 29) == [3, 3]
