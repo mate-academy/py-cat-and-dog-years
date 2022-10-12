bdda9668de35e36ffdf0305131e9313938018df8
IvanGLS <gladiolus0707@gmail.com>
2022-10-12 08:50:59 +0300

    'Done'

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,13 @@
 from app.main import get_human_age
 
 
-# Brevity is the soul of wit
 def test_check_all_counted_parameters_cat_and_dog() -> None:
     assert get_human_age(28, 29) == [3, 3]
+
+
+def test_if_zero_income() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_different_values_for_cat_and_dog() -> None:
+    assert get_human_age(28, 28) == [3, 2]
