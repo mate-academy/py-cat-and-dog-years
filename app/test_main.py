80c1ef89b3f75b1f5ce2f7deb87d61041f83e0e0
IvanGLS <gladiolus0707@gmail.com>
2022-10-12 11:23:41 +0300

    'Added'

--- app/test_main.py
+++ app/test_main.py
@@ -11,3 +11,11 @@ def test_if_zero_income() -> None:
 
 def test_different_values_for_cat_and_dog() -> None:
     assert get_human_age(28, 28) == [3, 2]
+
+
+def test_not_enough_age() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_one_year_age() -> None:
+    assert get_human_age(15, 15) == [1, 1]
