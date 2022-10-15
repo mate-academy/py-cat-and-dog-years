eafa4262079787ff5b05fbb8b5405993129cd28f
Dmytro Petliovanyi <petliovany@gmail.com>
2022-10-15 13:44:43 +0300

    Solution with message

--- app/test_main.py
+++ app/test_main.py
@@ -37,4 +37,6 @@ def test_convert_correctly(cat_years: int,
                            dog_years: int,
                            human_age: list) -> None:
 
-    assert get_human_age(cat_years, dog_years) == human_age
+    assert get_human_age(cat_years, dog_years) == human_age, \
+        f"Should return {human_age}, when cat age is " \
+        f"{cat_years} and dog age is {dog_years}"
