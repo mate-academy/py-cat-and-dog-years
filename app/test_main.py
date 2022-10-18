ee514cd8c2bb60d77d169b459ea52f3ae0d9536a
Oleksandr Mamchur <irex1988@gmail.com>
2022-10-18 17:51:44 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_expected_animal_ages() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_when_cat_age_is_less_than_0() -> None:
+    assert get_human_age(-10, 100) == [0, 17]
+
+
+def test_when_dog_age_is_less_than_0() -> None:
+    assert get_human_age(100, -1) == [21, 0]
+
+
+def test_should_raise_error_if_incorrect_data_type() -> None:
+    with pytest.raises(TypeError):
+        assert get_human_age("sf", [])
+
