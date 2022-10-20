b9f570147a1289ac4c42f30a17afcedcafa39ffb
Oleksandr Mamchur <irex1988@gmail.com>
2022-10-20 12:42:06 +0300

    'added_2_more_tests'

--- app/test_main.py
+++ app/test_main.py
@@ -14,11 +14,19 @@ def test_when_dog_age_is_less_than_0() -> None:
     assert get_human_age(23, -1) == [1, 0]
 
 
-def test_should_return_0_when_both_ages_is__0() -> None:
+def test_should_return_0_when_both_ages_are_0() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_when_function_resive_realy_large_numbers() -> None:
+def test_should_return_zeros_when_ages_are_less_than_15() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_double_one_when_ages_are_15() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_when_function_receive_large_numbers() -> None:
     assert get_human_age(100, 100) == [21, 17]
 
 
