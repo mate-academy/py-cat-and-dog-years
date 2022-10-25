0461097f05d7f0f90ec54ebee3f2be57762c5471
Andrey Losev <andry07070712@gmail.comless.gitconfiggit config --global user.email andry07070712@gmail.com>
2022-10-25 11:25:32 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,14 @@
 from app.main import get_human_age
 
 
+def test_with_negative_number_should_return_0() -> None:
+    assert get_human_age(-1, -1) == [0, 0]
+
+
+def test_0_cat_and_dog_years_convert_into_0() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
 def test_14_cat_and_dog_years_convert_into_0() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
@@ -23,3 +31,7 @@ def test_27_and_28_cat_and_dog_years_convert_into_2() -> None:
 
 def test_28_and_29_cat_and_dog_years_convert_into_3() -> None:
     assert get_human_age(28, 29) == [3, 3]
+
+
+def test_500_cat_and_dog_years_is_float_convert_into_121_and_97() -> None:
+    assert get_human_age(500, 500) == [121, 97]
