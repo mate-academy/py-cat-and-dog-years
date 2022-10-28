6309640d09cdce40c7c7ad63b7601783070f5433
Mykola Zakharov <kolyazakharov94@gmail.com>
2022-10-28 21:30:47 +0300

    for check

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,24 @@
-
-
 from app.main import get_human_age
 
-
-def test_should_return_zeros_when_input_is_negative() -> None:
-    assert get_human_age(-13, -2) == [0, 0]
+def test_should_return_one_when_input_equal_15() -> None:
+    assert get_human_age(15, 15) == [1, 1]
 
 
 def test_should_return_zeros_when_input_less_than_15() -> None:
-    assert get_human_age(10, 10) == [0, 0]
+    assert get_human_age(14, 14) == [0, 0]
 
 
 def test_should_return_1_when_input_less_than_24() -> None:
-    assert get_human_age(16, 18) == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_return_2_when_input_equal_24() -> None:
+    assert  get_human_age(24, 24) == [2, 2]
 
 
 def test_should_return_expected_value_when_input_more_than_24() -> None:
-    assert get_human_age(26, 27) == [2, 2]
+    assert get_human_age(27, 28) == [2, 2]
+
+def test_shoul_retern_expected_value_when_input_more_27() -> None:
+    assert  get_human_age(28, 28) == [3, 2]
+
