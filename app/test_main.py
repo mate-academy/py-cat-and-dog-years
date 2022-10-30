b59262dc4d671af9f7d8d4a9c44c98d014ccdf44
Mykola Zakharov <kolyazakharov94@gmail.com>
2022-10-30 10:52:07 +0200

    solution1

--- app/test_main.py
+++ app/test_main.py
@@ -1,24 +1,31 @@
+import pytest
 from app.main import get_human_age
 
-def test_should_return_one_when_input_equal_15() -> None:
-    assert get_human_age(15, 15) == [1, 1]
+
+def test_should_raise_error_when_input_not_int() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("16", 15)
 
 
-def test_should_return_zeros_when_input_less_than_15() -> None:
-    assert get_human_age(14, 14) == [0, 0]
+def test_should_return_zeros_when_input_is_negative() -> None:
+    assert get_human_age(-5, -12) == [0, 0]
 
 
-def test_should_return_1_when_input_less_than_24() -> None:
-    assert get_human_age(23, 23) == [1, 1]
+def test_should_return_zeros_when_age_is_less_than_15() -> None:
+    assert get_human_age(1, 14) == [0, 0]
 
 
-def test_should_return_2_when_input_equal_24() -> None:
-    assert  get_human_age(24, 24) == [2, 2]
+def test_should_return_ones_when_age_is_more_than_15() -> None:
+    assert get_human_age(16, 19) == [1, 1]
 
 
-def test_should_return_expected_value_when_input_more_than_24() -> None:
-    assert get_human_age(27, 28) == [2, 2]
+def test_should_return_extra_human_year_to_cat_after_age_of_28() -> None:
+    assert get_human_age(28, 28) == [3, 2]
 
-def test_shoul_retern_expected_value_when_input_more_27() -> None:
-    assert  get_human_age(28, 28) == [3, 2]
 
+def test_should_return_extra_human_year_to_dog_after_age_of_29() -> None:
+    assert get_human_age(28, 29) == [3, 3]
+
+
+def test_should_return_value_when_ages_are_too_big() -> None:
+    assert get_human_age(150, 150) == [33, 27]
