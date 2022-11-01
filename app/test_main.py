4548cae956c4f339c1b5c133e17fffe5e6f7a5bd
Denys Tereshchuk <terrya@ukr.net>
2022-11-01 15:34:42 +0400

    flake8 check

--- app/test_main.py
+++ app/test_main.py
@@ -13,7 +13,7 @@ def test_should_return_1_human_years_when_animals_age_are_15() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_should_return_1_human_years_when_animals_age_between_15_and_23() -> None:
+def test_should_return_1_when_animals_age_between_15_and_23() -> None:
     assert get_human_age(23, 23) == [1, 1]
 
 
@@ -21,7 +21,7 @@ def test_should_return_2_human_years_when_animals_age_are_24() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
 
-def test_should_return_2_human_years_when_animals_years_are_27_28_cat_dog() -> None:
+def test_should_return_2_when_animals_years_are_27_28_cat_dog() -> None:
     assert get_human_age(27, 28) == [2, 2]
 
 
