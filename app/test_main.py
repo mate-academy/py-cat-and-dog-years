e7a8c6b1045d769637abf734ebfb60116d96ff10
Oleksandr Kolomiiets <jumanje82@gmail.com>
2022-11-01 11:25:04 +0200

    Solution_#1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize("cat_age, dog_age, expected_result",  [
+                                                    (-10, 23, [0, 1]),
+                                                    (23, -1, [1, 0]),
+                                                    (14, 14, [0, 0]),
+                                                    (15, 15, [1, 1]),
+                                                    (23, 23, [1, 1]),
+                                                    (24, 24, [2, 2]),
+                                                    (27, 28, [2, 2]),
+                                                    (28, 29, [3, 3]),
+                                                    (100, 100, [21, 17]),
+                                                    ])
+def test_should_return_different_ages(cat_age, dog_age, expected_result):
+    assert get_human_age(cat_age, dog_age) == expected_result
+
+
+@pytest.mark.parametrize("cat_age, dog_age, expected_exception",  [
+                                                    ("28", 29, TypeError),
+                                                    (28, "29", TypeError)])
+def test_should_return_type_error(cat_age, dog_age, expected_exception):
+    with pytest.raises(TypeError):
+        assert get_human_age(cat_age, dog_age)
