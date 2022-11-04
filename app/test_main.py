2ab456f4862335165b719162222d6906a6ab933d
Taras Martyniuk <bythewaters800@gmail.com>
2022-11-04 14:07:27 +0100

    Sol2

--- app/test_main.py
+++ app/test_main.py
@@ -26,6 +26,5 @@ from app.main import get_human_age
 def test_should_return_age_convert_to_human(cat_age: int,
                                             dog_age: int,
                                             cat_dog_human_age: list) -> None:
-    age = get_human_age(cat_age, dog_age)
 
-    assert age == cat_dog_human_age
+    assert get_human_age(cat_age, dog_age) == cat_dog_human_age
