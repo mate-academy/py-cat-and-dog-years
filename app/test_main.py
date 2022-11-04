5f077a2cf0dfd2fde85107a5b9a7bd23bc5c6b74
Dede <specialized8393@gmail.com>
2022-11-05 01:18:24 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,28 +2,32 @@ import pytest
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize(
-    "cat_age,dog_age,human_age",
-    [
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (27, 27, [2, 2]),
-        (27, 28, [2, 2]),
-        (28, 28, [3, 2]),
-        (28, 29, [3, 3]),
-    ],
-    ids=[
-        "Up to 15 cat/dog years give 0 human year",
-        "First 15 cat/dog years give 1 human year",
-        "24 cat/dog years should be equal 1 human years",
-        "Every 4 next cat years give 1 extra human year",
-        "Every 5 next dog years give 1 extra human year.",
-        "28 cat years should be equal 3 human years",
-        "29 dog years should be equal 3 human years",
-    ]
-)
-def test_get_human_age(cat_age: int, dog_age: int, human_age: list) -> None:
-    assert (
-            get_human_age(cat_age, dog_age) == human_age
-    ), f"Cat years: {cat_age} and Dog years: {dog_age} should be equal to human years: {human_age}"
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,human_age",
+        [
+            pytest.param(14, 14, [0, 0], id="Up to 15 cat/dog years give 0 human year"),
+            pytest.param(15, 15, [1, 1], id="First 15 cat/dog years give 1 human year"),
+            pytest.param(23, 23, [1, 1], id="24 cat/dog years should be equal 1 human years"),
+            pytest.param(27, 27, [2, 2], id="Every 4 next cat years give 1 extra human year"),
+            pytest.param(27, 28, [2, 2], id="Every 5 next dog years give 1 extra human year"),
+            pytest.param(28, 28, [3, 2], id="28 cat years should be equal 3 human years"),
+            pytest.param(28, 29, [3, 3], id="29 dog years should be equal 3 human years"),
+        ],
+    )
+    def test_get_human_age(self, cat_age: int, dog_age: int, human_age: list) -> None:
+        assert (
+                get_human_age(cat_age, dog_age) == human_age
+        ), f"Cat years: {cat_age} " \
+           f"and Dog years: {dog_age} " \
+           f"should be equal to human years: {human_age}"
+
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,key_error",
+        [
+            pytest.param("14", 14, TypeError, id="The function receives an incorrect type of data"),
+        ]
+    )
+    def test_should_raise_error_when_input_not_int(self, cat_age, dog_age, key_error) -> None:
+        with pytest.raises(key_error):
+            get_human_age(cat_age, dog_age)
