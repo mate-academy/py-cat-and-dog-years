160a39b4d1f1911b9d291439ae1aa695352e05f4
Andrew.H <john.riderr9@gmail.com>
2022-11-08 08:08:18 +0200

    added checking range and types

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 from app.main import get_human_age
+import pytest
 
 
 def test_zero_values() -> None:
@@ -25,5 +26,15 @@ def test_should_convert_into_two_human_ages2() -> None:
     assert get_human_age(27, 28) == [2, 2], "test convert to [2, 2] human ages"
 
 
-def test_should_convert_into_three_human_agees() -> None:
-    assert get_human_age(28, 29) == [3, 3], "test convert to [3, 3] human ages"
+def test_negative_values() -> None:
+    with pytest.raises(ValueError):
+        get_human_age(-1, -1)
+
+
+def test_large_values() -> None:
+    assert get_human_age(1000000000, 1000000000) == [249999996, 199999997]
+
+
+def test_incorrect_type_values() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("6", "12")
