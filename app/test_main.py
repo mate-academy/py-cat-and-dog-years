4407664b0a8be9c91d48e243bffac5753eec31d6
Mykhailo Sotnikov <sotnikov.mykhail@gmail.com>
2022-11-23 16:55:19 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,10 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_years_should_be_integers() -> None:
+    result = get_human_age(50, 50)
+    assert isinstance(result[0], int) and isinstance(result[1], int)
+
+
+def test_if_result_values_are_correct() -> None:
+    assert get_human_age(88, 88) == [18, 14]
