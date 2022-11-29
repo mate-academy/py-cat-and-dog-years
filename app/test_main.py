320f30447a5796f8a2a57638b97f50b666ec7647
Mykyta Moshchenko <mykytamoshchenko@outlook.com>
2022-11-29 14:20:19 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,38 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+class TestGetAgeHuman:
+
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_result",
+        [
+            pytest.param(
+                28,
+                28,
+                [3, 2],
+                id="test function calculates age correctly"
+            ),
+            pytest.param(
+                13,
+                12,
+                [0, 0],
+                id="test function return [0, 0] when animals ages "
+                   "are less than human's year"
+            ),
+            pytest.param(
+                18,
+                12,
+                [1, 0],
+                id="test function return 0 for animal which age "
+                   "is less than human's year"
+            ),
+        ]
+    )
+    def test_function_calculations(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_result: list
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_result
