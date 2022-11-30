6d1cecb260651dcc36ff7c136ba20947af46d296
Mykyta Moshchenko <mykytamoshchenko@outlook.com>
2022-11-30 14:56:49 +0100

    Solution with test-names chenges

--- app/test_main.py
+++ app/test_main.py
@@ -1,27 +1,29 @@
 from app.main import get_human_age
 
 
-def test_get_human_age_return_zero_if_animals_age_is_zero() -> None:
+def test_human_age_is_zero_if_animals_age_is_zero() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_14_or_less_cat_or_dog_years_should_convert_into_0_human_age() -> None:
+def test_if_animal_age_is_less_than_human_year() -> None:
     assert get_human_age(14, 14) == [0, 0]
     assert get_human_age(12, 13) == [0, 0]
 
 
-def test_15_cat_or_dog_years_should_convert_into_1_human_age() -> None:
+def test_if_animal_age_is_equal_to_one_human_year() -> None:
     assert get_human_age(15, 15) == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
 
 
-def test_27_cat_and_28_dog_years_should_convert_into_2_human_age() -> None:
+def test_animals_ages_equal_to_two_human_age() -> None:
+    assert get_human_age(24, 24) == [2, 2]
     assert get_human_age(27, 28) == [2, 2]
 
 
-def test_28_cat_and_29_dog_years_should_convert_into_2_human_age() -> None:
+def test_animals_ages_equal_to_three_human_age() -> None:
     assert get_human_age(28, 29) == [3, 3]
 
 
-def test_get_human_age_calculates_correctly() -> None:
-    assert get_human_age(28, 28) == [3, 2]
+def test_human_age_if_animals_age_more_than_three_human_years() -> None:
+    assert get_human_age(32, 34) == [4, 4]
     assert get_human_age(100, 100) == [21, 17]
