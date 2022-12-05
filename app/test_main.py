57bb90471bc33acd475341e51b23d5035fdf9b7d
Maksym Maksimov <maksymmaksimov88@gmail.com>
2022-12-05 16:48:03 +0800

    Fixed flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
 from app.main import get_human_age
 
 
-def test_animal_age_is_negative_number():
+def test_animal_age_is_negative_number() -> None:
     assert get_human_age(-100, -25) == [0, 0]
 
 
-def test_animal_age_is_zero_or_less_then_one_human_year():
+def test_animal_age_is_zero_or_less_then_one_human_year() -> None:
     assert get_human_age(0, 14) == [0, 0]
 
 
-def test_animal_age_equal_to_one_human_year():
+def test_animal_age_equal_to_one_human_year() -> None:
     assert get_human_age(23, 15) == [1, 1]
 
 
-def test_animal_age_equal_to_two_human_years():
+def test_animal_age_equal_to_two_human_years() -> None:
     assert get_human_age(24, 26) == [2, 2]
 
 
-def test_different_rules_for_cat_dog_when_animal_age_more_then_two_human_years():
+def test_different_rules_when_animal_age_more_then_two_human_years() -> None:
     assert get_human_age(99, 99) == [20, 17]
