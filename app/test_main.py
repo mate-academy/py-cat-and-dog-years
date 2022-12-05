9fcedf7b792af0ee9afb0ee0723b951539150b92
soobig666 <darkvader.ukr.net@gmail.com>
2022-12-05 18:37:51 +0200

    Solution_v2

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Any
 from app.main import get_human_age
 
 # write your code here
@@ -7,6 +8,18 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age,dog_age,expected_result",
     [
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="dog_and_cat_should_be_zero_if_age_is_0"
+        ),
+        pytest.param(
+            -1,
+            -2,
+            [0, 0],
+            id="dog_and_cat_should_be_zero_if_age_is_negative"
+        ),
         pytest.param(
             14,
             13,
@@ -30,13 +43,24 @@ from app.main import get_human_age
             29,
             [2, 3],
             id="dog_every_fife_years_after_first_fifteen_and_second_9"
-        )
+        ),
+        pytest.param(
+            1000,
+            1000,
+            [246, 197],
+            id="should_correctly_calculate_if_large_numbers"
+        ),
     ]
 )
-def test_convert_animal_age_to_human_age(
+def test_right_convert_animal_age_to_human_age(
         cat_age: int,
         dog_age: int,
         expected_result: list
 ) -> None:
 
     assert get_human_age(cat_age, dog_age) == expected_result
+
+
+def test_type_must_be_integer() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("cat_age", 11.1)
