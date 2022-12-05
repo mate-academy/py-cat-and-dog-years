599ecabdd5013e12d32b4f2120c676c3d6284e1d
Maksym Maksimov <maksymmaksimov88@gmail.com>
2022-12-05 16:45:28 +0800

    Solution Maksym Maksimov

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_animal_age_is_negative_number():
+    assert get_human_age(-100, -25) == [0, 0]
+
+
+def test_animal_age_is_zero_or_less_then_one_human_year():
+    assert get_human_age(0, 14) == [0, 0]
+
+
+def test_animal_age_equal_to_one_human_year():
+    assert get_human_age(23, 15) == [1, 1]
+
+
+def test_animal_age_equal_to_two_human_years():
+    assert get_human_age(24, 26) == [2, 2]
+
+
+def test_different_rules_for_cat_dog_when_animal_age_more_then_two_human_years():
+    assert get_human_age(99, 99) == [20, 17]
