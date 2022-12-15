5ec0742892b1f944c32a17eb93f94b72b9f8ea6d
Іван Березянський <mamanadarin@gmail.com>
2022-12-15 20:26:37 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_when_input_is_0() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_when_input_is_negative() -> None:
+    assert get_human_age(-14, -15) == [0, 0]
+
+
+def test_should_convert_to_0_if_age_less_then_15() -> None:
+    assert get_human_age(7, 7) == [0, 0]
+
+
+def test_should_convert_to_1_if_age_15() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_should_convert_to_1_if_age_less_then_24() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_convert_to_2_if_age_24() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_should_convert_to_2_if_age_less_then_28_and_29() -> None:
+    assert get_human_age(27, 28) == [2, 2]
+
+
+def test_should_convert_to_3_if_age_more_then_27_and_28() -> None:
+    assert get_human_age(28, 29) == [3, 3]
