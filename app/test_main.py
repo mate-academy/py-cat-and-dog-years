5ce59d9068606201fc8ed065c9adab6d3bfaa2b2
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-19 11:54:01 +0200

    flake8 2

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,22 @@
 from app.main import get_human_age
+
 import pytest
 
 
 class TestGetHumanAge:
 
-    @pytest.mark.parametrize(
-                             "cat_age, dog_age, result",
-                             [
-                                (0, 0, [0, 0]),
+    @pytest.mark.parametrize("cat_age, dog_age, result",
+                             [  (0, 0, [0, 0]),
                                 (14, 14, [0, 0]),
                                 (15, 15, [1, 1]),
                                 (24, 24, [2, 2]),
                                 (-1, -1, [0, 0])
-                             ]
-                            )
+                             ])
                              
-    def test_get_human_age_value_is_int(self, cat_age, dog_age, result) -> None:
+    def test_get_human_age_value_is_int(self,
+                                        cat_age: str,
+                                        dog_age,
+                                        result: list) -> None:
         assert get_human_age(cat_age, dog_age) == result
 
     def test_get_human_age_value_is_not_int(self) -> None:
