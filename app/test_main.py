ba16e6cb1b392e3c6665863b4960b531e1d864ab
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-19 12:02:31 +0200

    flake8 3

--- app/test_main.py
+++ app/test_main.py
@@ -6,13 +6,12 @@ import pytest
 class TestGetHumanAge:
 
     @pytest.mark.parametrize("cat_age, dog_age, result",
-                             [  (0, 0, [0, 0]),
-                                (14, 14, [0, 0]),
-                                (15, 15, [1, 1]),
-                                (24, 24, [2, 2]),
-                                (-1, -1, [0, 0])
-                             ])
-                             
+                             [(0, 0, [0, 0]),
+                              (14, 14, [0, 0]),
+                              (15, 15, [1, 1]),
+                              (24, 24, [2, 2]),
+                              (-1, -1, [0, 0])
+                              ])
     def test_get_human_age_value_is_int(self,
                                         cat_age: str,
                                         dog_age,
