713d36386103bc5339c0dece6ce83bc8561b5d0e
Tetiana Kozakova <kleotan901@gmail.com>
2022-12-19 10:13:36 +0100

    soÃlution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,9 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_convert_into_1_human_age() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_convert_into_2_human_age() -> None:
+    assert get_human_age(24, 24) == [2, 2]
