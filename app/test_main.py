4ae39009dfa54195f8a04e30a99998a24419d9b8
Musakovskiy Olexandr <magazynuniversam@gmail.com>
2022-12-20 10:54:57 +0200

    changed by auto format pep8 add param

--- app/test_main.py
+++ app/test_main.py
@@ -6,12 +6,18 @@ import pytest
 class TestGetHumanAge:
 
     @pytest.mark.parametrize("cat_age, dog_age, result",
-                             [(0, 0, [0, 0]),
-                              (14, 14, [0, 0]),
-                              (15, 15, [1, 1]),
-                              (24, 24, [2, 2]),
-                              (-1, -1, [0, 0])
-                              ])
+                             [
+                                 pytest.param(0, 0, [0, 0],
+                                              id="return list of zero if age = 0"),
+                                 pytest.param(14, 14, [0, 0],
+                                              id="return list of zero if age < 15"),
+                                 pytest.param(15, 15, [1, 1],
+                                              id="return [1, 1] if 15<age<24"),
+                                 pytest.param(24, 24, [2, 2],
+                                              id="return [2, 2] if 24<age<28"),
+                                 pytest.param(-1, -1, [0, 0],
+                                              id="return list of zero if age<0 ")
+                             ])
     def test_get_human_age_value_is_int(self,
                                         cat_age: int,
                                         dog_age: int,
