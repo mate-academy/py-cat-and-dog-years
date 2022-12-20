b6d0a47031796b32bbc1f8266b0b576044e88c61
Artem Motrunich <motrunich2013@gmail.com>
2022-12-20 20:16:19 +0100

    Solution_v2

--- app/test_main.py
+++ app/test_main.py
@@ -6,11 +6,23 @@ class TestGetHumanAge:
     @pytest.mark.parametrize(
         "cat_age, dog_age, result_convert_to_human_age",
         [
-            (14, 14, [0, 0]),
-            (23, 23, [1, 1]),
-            (24, 24, [2, 2]),
-            (28, 29, [3, 3]),
-            (100, 100, [21, 17])
+            pytest.param(0, 0, [0, 0],
+                         id="when input is 0"),
+            pytest.param(-14, -15, [0, 0],
+                         id="when input is negative"),
+            pytest.param(14, 14, [0, 0],
+                         id="should convert to 0 if age less then 15"),
+            pytest.param(15, 15, [1, 1],
+                         id="should convert to 1 if age 15"),
+            pytest.param(23, 23, [1, 1],
+                         id="should convert to 1 if age less then 24"),
+            pytest.param(24, 24, [2, 2],
+                         id="should convert to 2 if age 24"),
+            pytest.param(27, 28, [2, 2],
+                         id="should convert to 2 if age less then 27 and 28"),
+            pytest.param(28, 29, [3, 3],
+                         id="should convert to 3 if age more then 28 and 29"),
+
         ]
     )
     def test_calculate_human_age(
