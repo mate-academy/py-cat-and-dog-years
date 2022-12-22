fdc535dbd5551563f007cbec53a80d303198c13c
Derkach Oleksandr <derkach03553@gmail.com>
2022-12-22 12:23:59 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,19 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "example_input, expected",
+    [
+        (get_human_age(0, 0), [0, 0]),
+        (get_human_age(14, 14), [0, 0]),
+        (get_human_age(15, 15), [1, 1]),
+        (get_human_age(23, 23), [1, 1]),
+        (get_human_age(24, 24), [2, 2]),
+        (get_human_age(27, 27), [2, 2]),
+        (get_human_age(28, 28), [3, 2]),
+        (get_human_age(100, 100), [21, 17])
+    ]
+)
+def test_get_human_age(example_input, expected):
+    assert example_input == expected
