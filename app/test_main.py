586c54a730536293e0feba7ceb74099cdecda282
Yevhen Martyniuk <evgenijmartynuk07@gmail.com>
2022-12-25 13:10:18 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_be_add_zero_when_age_0() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_convert_age_into_human_age() -> None:
+    assert get_human_age(100, 100) == [21, 17]
+
+
+def test_should_convert_age_into_one_human_age() -> None:
+    assert get_human_age(23, 23) == [1, 1]
