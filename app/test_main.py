6f2a5867631c6c2405da932534af275d88c3f603
Viacheslav Yasynetskyi <slawa1594@gmail.com>
2023-01-02 22:23:15 +0000

    ÂSolution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_cat_age_and_dog_age_is_0():
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_return_1_if_cat_age_and_dog_age_less_than_24():
+    assert get_human_age(20, 20) == [1, 1]
+
+
+def test_should_return_integer_if_cat_age_and_dog_age_more_than_24():
+    assert get_human_age(28, 28) == [3, 2]
