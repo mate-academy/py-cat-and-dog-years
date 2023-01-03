402db00c5cd4e1ecfc1dbfde589431c1432b9f80
Vitalii Panochko <vpapplic@gmail.com>
2023-01-03 13:36:28 +0200

    tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+def test_check_that_it_is_not_changed_with_previous_value() -> None:
+    get_human_age(15, 15)
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_resive_data_out_of_normal_range() -> None:
+    assert get_human_age(-1, 0) == [0, 0]
+
+
+def test_receives_an_incorrect_type_of_data() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("15", 12.5)
