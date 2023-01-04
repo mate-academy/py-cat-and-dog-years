752d3cd52846d87b8712f573b6b9de1fcc742f0f
Maryna Prokhorenko <maryna.prokhorenko11@gmail.com>
2023-01-04 11:56:45 +0200

    Resolve remarks

--- app/test_main.py
+++ app/test_main.py
@@ -2,35 +2,28 @@ from app.main import get_human_age
 
 
 def test_should_return_zero_human_age() -> None:
-    goal = get_human_age(14, 14)
-    assert goal == [0, 0]
+    assert get_human_age(14, 14) == [0, 0]
 
 
 def test_first_year_animal_life() -> None:
-    goal = get_human_age(15, 15)
-    assert goal == [1, 1]
+    assert get_human_age(15, 15) == [1, 1]
 
 
 def test_first_year_animal_life_ceil_value() -> None:
-    goal = get_human_age(23, 23)
-    assert goal == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
 
 
 def test_second_year_animal_life() -> None:
-    goal = get_human_age(24, 24)
-    assert goal == [2, 2]
+    assert get_human_age(24, 24) == [2, 2]
 
 
 def test_second_year_animal_life_ceil_value() -> None:
-    goal = get_human_age(27, 27)
-    assert goal == [2, 2]
+    assert get_human_age(27, 27) == [2, 2]
 
 
 def test_lasr_each_year_animal_life_for_cat() -> None:
-    goal = get_human_age(28, 28)
-    assert goal == [3, 2]
+    assert get_human_age(28, 28) == [3, 2]
 
 
 def test_lasr_each_year_animal_life_for_dog() -> None:
-    goal = get_human_age(29, 29)
-    assert goal == [3, 3]
+    assert get_human_age(29, 29) == [3, 3]
