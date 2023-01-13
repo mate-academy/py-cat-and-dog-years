a81222ab037badb700af96ec33f8a7bb3ad7f60c
Artur Leleiko <artur_98@ukr.net>
2023-01-13 08:28:47 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_zero_when_age_is_zero_or_negative() -> None:
+    assert get_human_age(-1, 0) == [0, 0]
+
+
+def test_should_return_zero_when_age_is_less_than_15() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_1_when_age_is_less_than_24() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_return_correct_value_when_age_is_less_than_29() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_should_return_correct_value_when_age_is_100() -> None:
+    assert get_human_age(100, 100) == [21, 17]
