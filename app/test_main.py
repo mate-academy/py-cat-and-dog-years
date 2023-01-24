548d5eff05f20c1fa0a820a77b360f2daa8d85df
Yehor Kornev <dmekkindom00@gmail.com>
2023-01-24 17:57:00 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,35 @@
 from app.main import get_human_age
+import pytest
 
 
-def test_14_cat_dog_years_should_convert_into_0_human_age() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_15_cat_dog_years_should_convert_into_1_human_age() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_23_cat_dog_years_should_convert_into_1_human_age() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_24_cat_dog_years_should_convert_into_2_human_age() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_27_28_cat_dog_years_should_convert_into_2_human_age() -> None:
-    assert get_human_age(27, 28) == [2, 2]
-
-
-def test_28_29_cat_dog_years_should_convert_into_3_human_age() -> None:
-    assert get_human_age(28, 29) == [3, 3]
+class TestConvertAge:
+    @pytest.mark.parametrize(
+        "animals_age,into_human_age",
+        [
+            ((-2, -2), [0, 0]),
+            ((0, 0), [0, 0]),
+            ((14, 14), [0, 0]),
+            ((15, 15), [1, 1]),
+            ((23, 23), [1, 1]),
+            ((24, 24), [2, 2]),
+            ((27, 28), [2, 2]),
+            ((28, 29), [3, 3])
+        ],
+        ids=[
+            "takes a negative value cat/dog"
+            " years should convert into 0 human age",
+            "0 cat/dog years should convert into 0 human age",
+            "14 cat/dog years should convert into 0 human age",
+            "15 cat/dog years should convert into 1 human age",
+            "23 cat/dog years should convert into 1 human age",
+            "24 cat/dog years should convert into 2 human age",
+            "27/28 cat/dog years should convert into 2 human age",
+            "28/29 cat/dog years should convert into 3 human age"
+        ]
+    )
+    def test_cat_dog_years_should_convert_into_human_age(
+            self,
+            animals_age: tuple,
+            into_human_age: list
+    ) -> None:
+        assert get_human_age(*animals_age) == into_human_age
