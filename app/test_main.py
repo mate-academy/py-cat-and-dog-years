fc84d79eb7529056d5f7407676f1ed9bc2072887
Alina Boichenko <ilub1108@gmail.com>
2023-01-25 09:45:01 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,65 @@
+import pytest
+from typing import Any
 from app.main import get_human_age
 
 
-def test_cat_and_dog_age_equal_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_age",
+        [
+            pytest.param(
+                -1, -2, [0, 0],
+                id="years should be positive integers."
+            ),
+            pytest.param(
+                0, 0, [0, 0],
+                id="years should convert into 0 human age."
+            ),
+            pytest.param(
+                14, 14, [0, 0],
+                id="years should convert into 0 human age."
+            ),
+            pytest.param(
+                15, 15, [1, 1],
+                id="years should convert into 1 human age."
+            ),
+            pytest.param(
+                23, 23, [1, 1],
+                id="years should convert into 1 human age."
+            ),
+            pytest.param(
+                24, 24, [2, 2],
+                id="years should convert into 2 human age."
+            ),
+            pytest.param(
+                28, 29, [3, 3],
+                id="years should convert into 3 human age."
+            )
+        ]
+    )
+    def test_modify_age_correctly(
+        self,
+        cat_age: int,
+        dog_age: int,
+        expected_age: list
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_age
 
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_error",
+        [
+            pytest.param(
+                2.1, [2], TypeError,
+                id="should raise error when age cat/dog is not integer"
+            )
+        ]
+    )
+    def test_raising_error_correctly(
+        self,
+        cat_age: Any,
+        dog_age: Any,
+        expected_error: Any
+    ) -> None:
 
-def test_cat_and_dog_age_less_than_one_human_year() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_cat_and_dogs_age_equal_one_human_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_cat_and_dogs_age_less_than_two_human_years() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_cat_and_dogs_age_equal_two_human_years() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_cat_and_dogs_age_equal_three_human_years() -> None:
-    assert get_human_age(28, 29) == [3, 3]
+        with pytest.raises(expected_error):
+            get_human_age(cat_age, dog_age)
