3aefb050dc1d8b684a401e4ae6b645348c6ff8e4
Tymur Semelinskyi <t.semelinsky@gmail.com>
2023-01-25 22:25:30 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_cat_and_dog_years_under_15_should_equal_0_human_years() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_15_cat_and_dog_years_should_equal_1_human_year() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_cat_and_dog_years_under_24_should_equal_1_human_year() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_24_cat_and_dog_years_should_equal_2_human_years() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_cat_and_dog_years_under_28_and_29_should_equal_2_human_year() -> None:
+    assert get_human_age(27, 28) == [2, 2]
+
+
+def test_24_cat_and_29_dog_years_should_equal_3_human_years() -> None:
+    assert get_human_age(28, 29) == [3, 3]
