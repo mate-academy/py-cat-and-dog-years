8ed28a9874efb8862a241205dbcc8e8c693ed41a
Taras Savchyn <savik1992@gmail.com>
2023-02-01 16:01:35 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,5 +5,5 @@ def test_get_human_age_convert_to_four() -> None:
     assert get_human_age(32, 34) == [4, 4]
 
 
-def test_get_human_age_convert_different():
+def test_get_human_age_convert_different() -> None:
     assert get_human_age(14, 31) == [0, 1]
