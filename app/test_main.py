5a9e0ed860b44d40ebfcd4b4c32c37a3cae2e867
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-15 13:34:24 +0200

    1 commit

--- app/test_main.py
+++ app/test_main.py
@@ -3,11 +3,13 @@ import pytest
 from app.main import get_human_age
 
 
-def test_normal():
+def test_normal() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
 
-def test_type_error():
+def test_type_error() -> None:
     with pytest.raises(TypeError):
         get_human_age("28", "28")
 
+# def test_float_input():
+#     assert get_human_age(28.1, 28) == [3, 2]
