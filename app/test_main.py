b1eb281e52ff97e14dc74acbd0a2ec8a316ac045
Vadym Bihun <bihunva@gmail.com>
2023-02-15 21:23:33 +0200

    Added a type checking test

--- app/test_main.py
+++ app/test_main.py
@@ -16,8 +16,7 @@ from app.main import get_human_age
 def test_get_human_age(cat_age: int, dog_age: int, human_age: list) -> None:
     assert get_human_age(cat_age, dog_age) == human_age
 
-def test_errors():
+
+def test_should_raise_error_if_age_not_a_number() -> None:
     with pytest.raises(TypeError):
-        get_human_age("28", "28")
-
-
+        get_human_age("1", [1])
