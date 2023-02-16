bccbf5a95a348513d7b6e03063fe0b601e8d8409
bohdanlysuchenko <lysachenko111@ukr.net>
2023-02-16 13:06:16 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,37 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat,dog,result",
+    [
+        (0, 0, [0, 0]),
+        (-27, 15, [0, 1]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ]
+)
+def test_correct_result(cat: int, dog: int, result: list) -> None:
+    assert (
+            get_human_age(cat, dog) == result
+    ), f"Your function should return correct age for cat and dog {cat}, {dog} != {result}"
+
+
+@pytest.mark.parametrize(
+    "cat,dog",
+    [
+        ("dog", "cat"),
+        ([], ()),
+        (14, "12"),
+        (17.5, 26)
+    ]
+)
+def test_age_not_integer(cat: str, dog: str) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat, dog)
