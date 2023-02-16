f4d928f6b3bd0cb603a97b4f2be0970332c6ffa9
Ruslan Yanyshyn <ruslan.yanyshyn@yahoo.com>
2023-02-16 09:58:22 -0600

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,29 +1,47 @@
+from typing import Any
+
+import pytest
+
 from app.main import get_human_age
 
 
-def test_get_human_age_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+    [
+        ("15", 0, TypeError),
+        (15, "15", TypeError)
+
+    ]
+)
+def test_should_raise_type_error(
+        cat_age: Any,
+        dog_age: Any,
+        expected_error: TypeError
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
 
 
-def test_get_human_age_is_14() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_get_human_age_is_15() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_get_human_age_is_24() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_get_human_age_is_28() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_get_human_age_is_29() -> None:
-    assert get_human_age(29, 29) == [3, 3]
-
-
-def test_get_human_age_is_101() -> None:
-    assert get_human_age(101, 101) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_human_age",
+    [
+        (0, 0, [0, 0]),
+        (-15, -15, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (101, 101, [21, 17])
+    ]
+)
+def test_should_return_correct_value_for_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected_human_age: list
+) -> None:
+    assert get_human_age(
+        cat_age,
+        dog_age
+    ) == expected_human_age
