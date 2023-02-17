e5698b820d4088f8f5e93d9665efd3a43e1bf0a9
Vadym Bihun <bihunva@gmail.com>
2023-02-17 20:53:49 +0200

    Added 'ids' and more data

--- app/test_main.py
+++ app/test_main.py
@@ -9,8 +9,23 @@ from app.main import get_human_age
         (-1, -1, [0, 0]),
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
         (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
         (100, 100, [21, 17]),
+    ],
+    ids=[
+        "-1, -1 should return [0, 0]",
+        "0, 0 should return [0, 0]",
+        "14, 14 should return [0, 0]",
+        "15, 15 should return [1, 1]",
+        "23, 23 should return [1, 1]",
+        "24, 24 should return [2, 2]",
+        "27, 27 should return [2, 2]",
+        "28, 28 should return [3, 2]",
+        "100, 100 should return [21, 17]",
     ]
 )
 def test_get_human_age(cat_age: int, dog_age: int, human_age: list) -> None:
