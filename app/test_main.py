2d533402794e1111e5eef4adfb33420ab6ca06d7
Paul Savchuk <nekrjkeee@gmail.com>
2023-02-18 03:25:42 +0200

    Solution type(False) is not int

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,50 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestHumanCatDogYears:
+    @pytest.mark.parametrize(
+        "cat_years,dog_years,human_years",
+        [
+            (14, 14.22, [0, 0]),
+            (1, 1, [0, 0]),
+            (0, 0, [0, 0]),
+            (-3, -12, [0, 0]),
+            (15, 15, [1, 1]),
+            (23.9, 23.9, [1, 1]),
+            (24.0, 24.96, [2, 2]),
+            (27.96, 28.0, [2, 2]),
+            (28.0, 29.0, [3, 3]),
+            (10000, 10000, [2496, 1997]),
+        ],
+        ids=[
+            "`human_years` should equal `catdog_till_15` max",
+            "`human_years` should equal `catdog_till_15` min",
+            "should correctly work with zeros",
+            "should correctly work with negative numbers",
+            "`human_years` should equal `catdog_15_24` min",
+            "`human_years` should equal `catdog_15_24` max",
+            "`human_years` should equal `catdog_24` min",
+            "`human_years` should equal `catdog_24` max",
+            "`human_years` should equal `catdog_24+` min",
+            "`human_years` should equal `catdog_24+` big_num"
+        ]
+    )
+    def test_results_should_equal_correct_period(
+            self,
+            cat_years,
+            dog_years,
+            human_years
+    ):
+        assert get_human_age(cat_years, dog_years) == human_years
+
+
+class TestErrorTypes:
+    def test_type_error_missing_1_positional_argument(self) -> None:
+        with pytest.raises(TypeError, match=r".* missing 1 *."):
+            get_human_age(1)
+
+    def test_type_error_expected_2_ints(self) -> None:
+        with pytest.raises(TypeError):
+            get_human_age(1, False)
