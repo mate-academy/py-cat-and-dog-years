2faeb70a135c19f27e8ba950819b5c145f84b1f3
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-18 10:25:22 +0200

    add large numbers test

--- app/test_main.py
+++ app/test_main.py
@@ -8,12 +8,15 @@ def test_should_return_list_of_integers() -> None:
             and isinstance(get_human_age(27, 27)[1], int))
 
 
-def test_should_return_zeros_when_take_zeros() -> None:
+def test_should_raise_error_when_take_not_int() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("15.0", "15.0")
+
+
+def test_receive_data_out_of_normal_range() -> None:
     assert get_human_age(0, 0) == [0, 0]
-
-
-def test_should_return_zero_when_take_negative() -> None:
     assert get_human_age(-10, -10) == [0, 0]
+    assert get_human_age(100000, 100000) == [24996, 19997]
 
 
 def test_should_return_zeros_when_age_lower_than_15() -> None:
@@ -30,8 +33,3 @@ def test_should_return_two_when_age_equal_24() -> None:
 
 def test_should_return_three_when_age_equal_28_and_29() -> None:
     assert get_human_age(28, 29) == [3, 3]
-
-
-def test_should_raise_error_when_take_not_int() -> None:
-    with pytest.raises(TypeError):
-        get_human_age("15.0", "15.0")
