452c860a83552bd75c1904de0a54d06e576828c9
Dmitriy Kuzmin <kuzminds@hotmail.com>
2023-02-19 17:50:38 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_calculate_14_cat_or_dog_years_correctly() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_calculate_23_cat_or_dog_years_correctly() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_calculate_28_cat_and_29_dog_years_correctly() -> None:
+    assert get_human_age(28, 29) == [2, 3]
