047eb3617b1232d18c9772756b64917964723a15
usrofgh <mailforgimail@gmail.com>
2023-02-19 21:07:22 +0200

    exceptions testing was added

--- app/test_main.py
+++ app/test_main.py
@@ -15,6 +15,7 @@ class TestConvertAnimalAgeToHuman:
             (27, 27, [2, 2]),
             (28, 28, [3, 2]),
             (100, 100, [21, 17]),
+            (-1, -1, [0, 0]),
         ],
     )
     def test_converting_age(
@@ -23,5 +24,28 @@ class TestConvertAnimalAgeToHuman:
             dog_age: int,
             expected_ages: list
     ) -> None:
-
         assert get_human_age(cat_age, dog_age) == expected_ages
+
+
+class TestExpectionsConvertAnimalAgeToHuman:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_error",
+        [
+            ("18", 15, TypeError),
+            (18, "15", TypeError),
+            ("18", "14", TypeError)
+        ],
+        ids=[
+            "cat_age should be int value",
+            "dog_age should be int value",
+            "cat_age and dog_age should be int values",
+        ],
+    )
+    def test_raising_error_correctly(
+            self,
+            cat_age,
+            dog_age,
+            expected_error
+    ) -> None:
+        with pytest.raises(expected_error):
+            get_human_age(cat_age, dog_age)
