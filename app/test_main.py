2742633499934f25b7698564453c476e14b0e301
Eugene <eugene.lot77@gmail.com>
2023-02-21 17:18:14 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_different_between_the_same_animals_age() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_negative_values() -> None:
+    assert get_human_age(-1, -1) == [0, 0]
+
+
+def test_large_values() -> None:
+    assert get_human_age(100, 100) == [21, 17]
