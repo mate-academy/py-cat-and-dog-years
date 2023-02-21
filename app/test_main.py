f9da71d0ea7f5217708444df6b1b37b904c612d1
Ada Kravchenko <ada.t.krav@gmail.com>
2023-02-21 22:00:07 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_returns_list() -> None:
+    assert type(get_human_age(1, 1)) is list
+
+
+def test_not_positive_years() -> None:
+    assert get_human_age(-1, 0) == [0, 0]
+
+
+def test_0_human_year() -> None:
+    assert get_human_age(13, 14) == [0, 0]
+
+
+def test_1_human_year() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_2_human_years() -> None:
+    assert get_human_age(24, 25) == [2, 2]
+
+
+def test_more_human_years() -> None:
+    assert get_human_age(29, 35) == [3, 4]
