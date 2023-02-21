4eb4c9a4f5215a02fc16833403bda030b589fb1b
Roman Senyshyn <senjaskorp@gmail.com>
2023-02-21 17:15:23 +0200

    Fixed ids.

--- app/test_main.py
+++ app/test_main.py
@@ -6,22 +6,16 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, result",
     [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
-        (28, 28, [3, 2]),
-        (-2, 28, [0, 2]),
-        (-2, -5, [0, 0]),
-        (-2, -28, [0, 0]),
-        (0, -28, [0, 0]),
-        (0, 0, [0, 0]),
-        (100, 100, [21, 17]),
+        pytest.param(0, 0, [0, 0], id="zero_test"),
+        pytest.param(3, 3, [0, 0], id="young_age_test"),
+        pytest.param(100, 85, [21, 14], id="old_age_test"),
+        pytest.param(-2, 28, [0, 2], id="minus_cat_age_test"),
+        pytest.param(-2, -5, [0, 0], id="minus_cat_and_dog_age_test"),
+        pytest.param(0, -28, [0, 0], id="cat_zero_and_minus_dog_age_test"),
+
     ]
 )
-def test_get_human_age_with_negative_numbers_and_zero(
+def test_get_human_age(
         cat_age: int,
         dog_age: int,
         result: list[int]
