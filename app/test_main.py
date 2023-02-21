e54a762d435b90e9b20203f5a944eb7417cf27f8
Safonov Volodymyr <34244814+SafonovVladimir@users.noreply.github.com>
2023-02-21 08:58:19 +0200

    add @pytest.mark.parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -2,34 +2,26 @@ import pytest
 from app.main import get_human_age
 
 
-def test_should_return_list_of_integers() -> None:
-    assert (isinstance(get_human_age(27, 27), list)
-            and isinstance(get_human_age(27, 27)[0], int)
-            and isinstance(get_human_age(27, 27)[1], int))
+@pytest.mark.parametrize(
+    "cat_age, dog_age, human_age",
+    [
+        (0, 0, [0, 0]),
+        (-10, -10, [0, 0]),
+        (100000, 100000, [24996, 19997]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (24, 24, [2, 2]),
+        (28, 29, [3, 3]),
+    ]
+)
+def test_different_data_range(
+        cat_age: int,
+        dog_age: int,
+        human_age: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == human_age
 
 
 def test_should_raise_error_when_take_not_int() -> None:
     with pytest.raises(TypeError):
         get_human_age("15.0", "15.0")
-
-
-def test_receive_data_out_of_normal_range() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(-10, -10) == [0, 0]
-    assert get_human_age(100000, 100000) == [24996, 19997]
-
-
-def test_should_return_zeros_when_age_lower_than_15() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_should_return_ones_when_age_equal_15() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_should_return_two_when_age_equal_24() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_should_return_three_when_age_equal_28_and_29() -> None:
-    assert get_human_age(28, 29) == [3, 3]
