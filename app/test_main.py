a3d9fa8dbed598114ec650329e9595a6723ef430
Max Bilokur <bilmaxim@gmail.com>
2023-02-21 23:25:09 +0200

    added tests for abnormal cases and types

--- app/test_main.py
+++ app/test_main.py
@@ -6,6 +6,7 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age,dog_age,expected",
     [
+        (-4, -5, [0, 0]),
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
@@ -13,8 +14,9 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
+        (28, 29, [3, 3]),
         (100, 100, [21, 17]),
-        (150, 150, [33, 27])
+        (1000, 2000, [246, 397])
     ]
 )
 def test_get_different_results_same_input(
@@ -25,3 +27,8 @@ def test_get_different_results_same_input(
     assert (
         get_human_age(cat_age, dog_age) == expected
     ), f"{cat_age} for cat and {dog_age} for dog should return {expected}"
+
+
+def test_get_error_if_not_int() -> None:
+    with pytest.raises(TypeError):
+        get_human_age(12, "foo")
