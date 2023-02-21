fa88a418908612caad0d988c37ad43bd4b56e18c
Dmitriy Kuzmin <kuzminds@hotmail.com>
2023-02-21 21:37:53 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+import pytest
 from app.main import get_human_age
 
 
@@ -10,4 +11,21 @@ def test_should_calculate_23_cat_or_dog_years_correctly() -> None:
 
 
 def test_should_calculate_28_cat_and_29_dog_years_correctly() -> None:
-    assert get_human_age(28, 29) == [2, 3]
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_negative_input() -> None:
+    assert get_human_age(-1, -5) == [0, 0]
+
+
+def test_input_equals_0() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_float_input() -> None:
+    assert get_human_age(50.2, 180.0) == [8.0, 33.0]
+
+
+def test_input_of_wrong_type() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("cat", {})
