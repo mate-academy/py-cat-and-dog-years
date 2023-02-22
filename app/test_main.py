a0b634c884004d78be6d73f7ff1fd14e8a5b3131
Ruslan <noezyboy1@gmail.com>
2023-02-22 13:41:53 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -5,16 +5,16 @@ def test_get_human_age() -> None:
 
     assert get_human_age(0, 0) == [0, 0]
 
-    assert get_human_age(14, 14) == [0, 0]
+    assert get_human_age(13, 13) == [0, 0]
 
     assert get_human_age(15, 15) == [1, 1]
 
     assert get_human_age(23, 23) == [1, 1]
 
-    assert get_human_age(24, 24) == [2, 2]
+    assert get_human_age(30, 30) == [2, 2]
 
     assert get_human_age(27, 27) == [2, 2]
 
     assert get_human_age(28, 20) == [3, 2]
 
-    assert get_human_age(100, 100) == [21, 17]
+    assert get_human_age(200, 200) == [21, 17]
