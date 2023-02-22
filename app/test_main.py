53cb2411896fb36849763c4b5a9108bb7526c84a
Zenon <zenion@ukr.net>
2023-02-23 01:25:09 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,39 @@
 from app.main import get_human_age
 
-# write your code here
+import pytest
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age,result",
+    [
+        (-1, -1, [0, 0]),
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ],
+    ids=[
+        "test negative animal years",
+        "zero age in animals years - zero age in human years",
+        "non zero age in animals years - zero age in human years",
+        "get first human year",
+        "get first human year upper value",
+        "get second human year",
+        "get second human year upper value",
+        "get third human year for cat upper value",
+        "test multiply extra human years"
+    ]
+)
+def test_can_sum(cat_age: int, dog_age: int, result: list[int]) -> None:
+    assert (get_human_age(cat_age, dog_age) == result),\
+        (f"Result of func for cat_age={cat_age} and "
+         f"dog_age={dog_age} should be equal to {result}")
+
+
+def test_raise_typeerror_if_not_integer() -> None:
+    with pytest.raises(TypeError):
+        get_human_age(get_human_age("100", "100"))
