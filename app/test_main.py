30d7c49d9394c01f09bd24819f32e95944802aea
oksana-feshchenko <foksanv@gmail.com>
2023-02-22 18:34:17 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -26,7 +26,34 @@ from app.main import get_human_age
         pytest.param(
             100, 100, [21, 17], id="test when arguments 100 returns [21,17]"
         ),
+        pytest.param(
+            -6, -3, [0, 0], id="test when negative arguments returns [21,17]"
+        ),
+        pytest.param(
+            123456,
+            234567,
+            [30860, 46910],
+            id="test when with large numbers returns [21,17]",
+        ),
     ],
 )
 def test_get_human_age(cat_years: int, dog_years: int, expected: list) -> bool:
     assert get_human_age(cat_years, dog_years) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_years, dog_years, expected_error",
+    [
+        pytest.param(
+            "dog",
+            "cat",
+            TypeError,
+            id="test should raise error when arguments are string",
+        )
+    ],
+)
+def test_correct_error_get_human_age(
+    cat_years: int, dog_years: int, expected_error: list
+) -> bool:
+    with pytest.raises(expected_error):
+        get_human_age(cat_years, dog_years)
