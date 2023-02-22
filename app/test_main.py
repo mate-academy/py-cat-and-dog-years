e3ddce44b46d0647dcc0b593981a6d7c4d61a82d
Alessa <yvoangel@gmail.com>
2023-02-22 22:24:10 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -8,27 +8,34 @@ class TestGetHumanAge:
         "cat_age,dog_age,expected",
         [
             (-1, -1, [0, 0]),
-            (14, 1, [0, 0]),
-            (23, 15, [1, 1]),
-            (28, 28, [3, 2]),
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 28, [2, 2]),
+            (28, 29, [3, 3]),
             (100, 100, [21, 17]),
-            (35.5, 50.1, [4, 7]),
+            (35.5, 50.1, [4, 7])
         ],
         ids=[
-            "Test if age is negative.",
-            "Test if pets from 1 to 14 have less than 1 human's year",
-            "Test if pets age from 15 to 23 are equal to 1 human's year",
-            "Test if pet's age after 24 calculating correctly",
+            "Test if age is negative",
+            "Test if 14 yo pet's have less than 1 human's year",
+            "Test if 15 yo pet's are equal to 1 human's year",
+            "Test if 23 yo pet's have less than 2 human's year",
+            "Test if 24 yo pet's already have 2 human year's",
+            "Test max age limit's for pets before condition changes",
+            "Test min age limit's for pets after condition changes",
             "Test if function work with large number's correctly",
-            "Test if function will work with float age",
+            "Test if function will work with float age"
         ],
     )
     def test_get_human_age(
-        self, cat_age: int, dog_age: int, expected: list
+            self, cat_age: int, dog_age: int, expected: list
     ) -> None:
         assert get_human_age(cat_age, dog_age) == expected
 
 
-def test_wrong_data_type_str() -> None:
-    with pytest.raises(TypeError):
-        get_human_age("1", 1)
+class TestWrongData:
+    def test_wrong_data_type(self) -> None:
+        with pytest.raises(TypeError):
+            get_human_age("1", 1)
