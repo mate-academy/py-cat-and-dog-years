3952218c1d4356b5250838bcc9aba7d6a64b7bf2
Misha Vovchok <mivvaw@gmail.com>
2023-02-22 21:45:25 +0200

    solutionis

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+def test_raises_type_error() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("kitten", 5)
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, human_age",
+    [
+        (-1, 0, [0, 0]),
+        (13, 14, [0, 0]),
+        (20, 22, [1, 1]),
+        (24, 25, [2, 2]),
+        (30, 36, [3, 4]),
+    ],
+    ids=[
+        "not positive numbers should return [0, 0]",
+        "test zero human years",
+        "test one human year",
+        "test two human years",
+        "test different human years for cat and dog",
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, human_age: list) -> None:
+    assert get_human_age(cat_age, dog_age) == human_age
