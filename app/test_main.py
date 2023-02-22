b62929b3f88fe553b531687fc9d972c7626a6370
da Da <danildacenko@gmail.com>
2023-02-22 18:58:07 +0200

    Solution 6

--- app/test_main.py
+++ app/test_main.py
@@ -14,6 +14,8 @@ from app.main import get_human_age
         pytest.param(27, 28, [2, 2], id="cat 27, dog 28 years old"),
         pytest.param(14, 14, [0, 0], id="both animals 14 years old"),
         pytest.param(28, 29, [3, 3], id="cat 28, dog 29 years old"),
+        pytest.param(-1, -1, [0, 0], id="both animals have negative ages"),
+        pytest.param(100, 200, [21, 37], id="cat 100, dog 200 years old"),
     ],
 )
 def test_get_human_age(
