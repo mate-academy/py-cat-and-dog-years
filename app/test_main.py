acb9f43c934f974188d2a0e0783033b905acfccf
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-02-22 19:55:53 +0200

    Solution add parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -55,3 +55,32 @@ def test_negative_age(cat_age: int,
                       dog_age: int,
                       expected_years: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected_years
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_years",
+    [
+        (
+            1.2, 8.5, [0, 0]
+        ),
+        (
+           5.1, 6.1, [0, 0]
+        ),
+        (
+            24.9, 25.6, [2, 2]
+        ),
+        (
+            27.1, 27.2, [2, 2]
+        ),
+        (
+            28.1, 28.7, [3, 2]
+        ),
+        (
+            100.3, 100.9, [21, 17]
+        )
+    ]
+)
+def test_float_numbers_age(cat_age: int,
+                           dog_age: int,
+                           expected_years: list) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_years
