c8e638ead364a01a58d1eb49df3ec46fcaef186e
Teliks <hapyto371@gmail.com>
2023-02-22 16:39:06 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,26 +1,40 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_zero_years_in_both() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, human_age",
+    [
+        (-1, -1, [0, 0]),
+        (0, 0, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ],
+    ids=[
+        "-1, -1 should return [0, 0]",
+        "0, 0, should return [0, 0]",
+        "15, 15, should return [1, 1]",
+        "23, 23, should return [1, 1]",
+        "24, 24, should return [2, 2]",
+        "27, 27, should return [2, 2]",
+        "28, 28, should return [3, 2]",
+        "100, 100, should return [21, 17]"
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, human_age: int) -> None:
+    assert get_human_age(cat_age, dog_age) == human_age
 
 
-def test_fifteen_years_in_cat() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_23_years_in_both() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_24_years_in_both() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_27_years_in_both() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_28_years_in_both() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
+@pytest.mark.parametrize("cat_age, dog_age, human_age", [
+    ("1", 1, TypeError),
+    (1, "1", TypeError),
+    ("1", "1", TypeError),
+])
+def test_get_human_age_errors(cat_age, dog_age, human_age):
+    with pytest.raises(human_age):
+        get_human_age(cat_age, dog_age)
