1098c61babbd8a8f9b3e03591ac764d942003fe6
name <ksyusha0kubay@gmail.com>
2023-02-22 16:11:20 +0200

    'Solution_with_mark_parametrize'

--- app/test_main.py
+++ app/test_main.py
@@ -1,41 +1,52 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_should_return_list() -> None:
-    years = get_human_age(15, 15)
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat, dog, result",
+        [
+            pytest.param(
+                -1, -2,
+                [0, 0],
+                id="test not positive human age"
+            ),
+            pytest.param(
+                0, 0,
+                [0, 0],
+                id="test zero human age"
+            ),
+            pytest.param(
+                10, 14,
+                [0, 0],
+                id="test less than one human age"
+            ),
+            pytest.param(
+                15, 16,
+                [1, 1],
+                id="test one human age"
+            ),
+            pytest.param(
+                25, 26,
+                [2, 2],
+                id="test two human age"
+            ),
+            pytest.param(
+                18, 28,
+                [1, 2],
+                id="test different human age"
+            ),
+            pytest.param(
+                100, 100,
+                [21, 17],
+                id="test should return expected years"
+            ),
+        ]
+    )
+    def test_modify_correctly(self, cat: int, dog: int, result: list) -> None:
+        assert get_human_age(cat, dog) == result
 
-    assert isinstance(years, list)
-
-
-def test_should_return_list_of_given_length() -> None:
-    years = get_human_age(11, 10)
-
-    assert len(years) == 2
-
-
-def test_not_positive_human_age() -> None:
-    assert get_human_age(-1, -2) == [0, 0]
-
-
-def test_zero_human_age() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_less_than_one_human_age() -> None:
-    assert get_human_age(10, 14) == [0, 0]
-
-
-def test_one_human_age() -> None:
-    assert get_human_age(15, 16) == [1, 1]
-
-
-def test_two_human_age() -> None:
-    assert get_human_age(25, 26) == [2, 2]
-
-
-def test_different_human_age() -> None:
-    assert get_human_age(18, 28) == [1, 2]
-
-
-def test_should_return_expected_years() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+#
+# def test_input_correctly() -> None:
+#     with pytest.raises(TypeError):
+#         get_human_age("3", [8])
