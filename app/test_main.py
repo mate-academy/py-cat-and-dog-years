21608d9be32804aba85d140baf902d42bf20cefa
da Da <danildacenko@gmail.com>
2023-02-22 13:50:52 +0200

    Solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -6,8 +6,10 @@ from app.main import get_human_age
     "cat_age, dog_age, expected_result",
     [
         pytest.param(1, 1, [0, 0], id="both animals younger than 15 years"),
+        pytest.param(25, 25, [2, 2], id="both animals older than 15 + 9 years"),
         pytest.param(15, 15, [1, 1], id="both animals 15 years old"),
         pytest.param(23, 23, [1, 1], id="both animals 23 years old"),
+        pytest.param(24, 24, [2, 2], id="both animals 24 years old"),
         pytest.param(27, 28, [2, 2], id="cat 27, dog 28 years old"),
         pytest.param(14, 14, [0, 0], id="both animals 14 years old"),
         pytest.param(28, 29, [3, 3], id="cat 28, dog 29 years old"),
