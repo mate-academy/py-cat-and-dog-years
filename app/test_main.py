2b229be72e9bc684b65fcd7c73c28d4012376741
Anna Boichenko <ann.boychenko199@gmail.com>
2023-02-22 21:29:15 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -8,7 +8,7 @@ from app.main import get_human_age
     [
         pytest.param(0, 14, [0, 0], id="animals' age is less then 15"),
         pytest.param(
-            15, 23, [1, 1], id="animals' age more then 15 less then 24"
+            15, 23, [1, 1], id="animals' age is more then 15 less then 24"
         ),
         pytest.param(28, 23, [3, 1], id="cat's age is more then 24"),
         pytest.param(15, 29, [1, 3], id="dog's age is more then 24"),
