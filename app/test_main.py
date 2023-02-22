6370085236e95439b23c55f502cbb65ffb958ec7
Eugene <eugene.lot77@gmail.com>
2023-02-22 19:47:35 +0200

    Made with mark.parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,53 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_different_between_the_same_animals_age() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_negative_values() -> None:
-    assert get_human_age(-1, -1) == [0, 0]
-
-
-def test_large_values() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="test zero human age when cat and dog ages is 0"
+        ),
+        pytest.param(
+            1,
+            1,
+            [0, 0],
+            id="test zeros when cat and dog ages is 1"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="test zeros when cat and dog ages is 14"
+        ),
+        pytest.param(
+            23,
+            23,
+            [1, 1],
+            id="test human age 1 for animal ages 23"
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="test different human ages when animal ages is the same"
+        ),
+        pytest.param(
+            -1,
+            -1,
+            [0, 0],
+            id="test negative values in animal ages"
+        ),
+        pytest.param(
+            100,
+            100,
+            [21, 17],
+            id="test large values is animal ages"
+        ),
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
