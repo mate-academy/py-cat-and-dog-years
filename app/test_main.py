99828735592802846f1e939c1f81ba826c9d98ee
ramsty <ramsty27@gmail.com>
2023-02-23 17:42:23 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,23 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_both_ages_equals_to_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize("input_age, expected_output", [
+    ((-1, -1), [0, 0]),
+    ((0, 0), [0, 0]),
+    ((14, 14), [0, 0]),
+    ((15, 15), [1, 1]),
+    ((23, 23), [1, 1]),
+    ((24, 24), [2, 2]),
+    ((28, 28), [3, 2]),
+    ((100, 100), [21, 17])
+])
+def test_get_human_age(input_age, expected_output):
+    assert get_human_age(*input_age) == expected_output
 
 
-def test_age_less_15() -> None:
-    assert get_human_age(14, 14) == [0, 0]
+def test_raise_error_when_incorect_value() -> None:
+    with pytest.raises(TypeError):
+        get_human_age(2, "2")
 
-
-def test_age_less_24() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_age_28() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_age_more_then_29() -> None:
-    assert get_human_age(100, 100) == [21, 17]
