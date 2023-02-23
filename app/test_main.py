b259f98914fe7bde0e73657329bdd299614ca837
Serhii Leonenko <104218550+Serhii-Leonenko@users.noreply.github.com>
2023-02-23 14:36:45 +0200

    dog and cat
    Signed-off-by: Olexandr <maxbovling23432@gmail.com>

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,9 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_an_array() -> bool:
+    assert type(get_human_age(10, 3)) == list
+
+
+def test_main_get_human_age() -> list:
+    assert get_human_age(28, 28) == [3, 2]
