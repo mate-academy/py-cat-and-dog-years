b400f541550b1e31829c0eeca3047c0b57b99c61
Ruslan <noezyboy1@gmail.com>
2023-02-23 15:19:54 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,31 @@
-
+import pytest
 from app.main import get_human_age
 
 
-def test_get_human_age() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize(
+    "animal_age, expected_age",
+    [
+        (0, [0, 0]),
+        (14, [0, 0]),
+        (15, [1, 1]),
+        (23, [1, 1]),
+        (24, [2, 2]),
+        (27, [2, 2]),
+        (28, [3, 2]),
+        (100, [21, 17]),
+        (-5, [0, 0]),
+        (100000, [24996, 19997]),
+    ]
+)
+def test_get_human_age(animal_age, expected_age) -> None:
+    assert get_human_age(animal_age, animal_age) == expected_age
 
-    assert get_human_age(14, 14) == [0, 0]
 
-    assert get_human_age(28, 20) == [3, 2]
-
-    assert get_human_age(100, 100) == [21, 17]
-
-    assert get_human_age(-5, 0) == [0, 0]
-
-    assert get_human_age(100000, 0) == [2129, 0]
-
-    try:
-        get_human_age(2.5, 5)
-    except TypeError as e:
-        assert str(e) == "Animal age should be an integer"
-
-    try:
-        get_human_age("cat", "dog")
-    except TypeError as e:
-        assert str(e) == "Animal age should be an integer"
-
-    try:
+def test_get_human_age_with_invalid_input() -> None:
+    with pytest.raises(TypeError, match="Animal age should be an integer"):
+        get_human_age(2.5, "cat")
+    with pytest.raises(TypeError, match="Animal age should be an integer"):
+        get_human_age("dog", 5)
+    with pytest.raises(TypeError, match="Animal age should be an integer"):
         get_human_age(5, "dog")
-    except TypeError as e:
-        assert str(e) == "Animal age should be an integer"
+
