f35b451f02f4c5c1f3320f8535075250c1309326
Roman Rega <romanrega1994@gmail.com>
2023-02-23 16:56:45 +0200

    Solution4

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+
 from app.main import get_human_age
 
 
@@ -9,12 +10,10 @@ from app.main import get_human_age
         pytest.param(14, 14, [0, 0], id="test with arguments 14"),
         pytest.param(16, 14, [1, 0], id="test with different arguments"),
         pytest.param(23, 23, [1, 1], id="test with arguments equal 23"),
-        pytest.param(28, 28, [3, 2], id="test with arguments equal 28"),
+        pytest.param(28, 29, [3, 3], id="test with arguments equal 28"),
         pytest.param(100, 100, [21, 17], id="test with arguments equal 100"),
         pytest.param(-5, -5, [0, 0], id="test with negative arguments"),
-        pytest.param(-5, 5, [0, 0], id="test with first negative argument"),
-        pytest.param(5, -5, [0, 0], id="test with second negative argument"),
-        pytest.param(2.5, 3, [0, 0], id="test with bool argument"),
+        pytest.param(2.5, 3, [0, 0], id="test with float argument"),
 
     ]
 )
