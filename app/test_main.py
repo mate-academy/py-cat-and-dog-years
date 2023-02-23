cd627a4d76896c8ab12fc6177e4d62dcc5af5f75
Ruslan <noezyboy1@gmail.com>
2023-02-23 16:41:20 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -13,19 +13,47 @@ from app.main import get_human_age
         (24, [2, 2]),
         (27, [2, 2]),
         (28, [3, 2]),
+        (29, [3, 3]),
         (100, [21, 17]),
         (-5, [0, 0]),
         (100000, [24996, 19997]),
+    ],
+    ids=[
+        "animal_age_0",
+        "animal_age_less_than_15",
+        "animal_age_equal_to_15",
+        "animal_age_between_16_and_24",
+        "animal_age_equal_to_24",
+        "animal_age_between_25_and_28",
+        "animal_age_equal_to_28",
+        "animal_age_equal_to_29",
+        "animal_age_greater_than_28",
+        "negative_animal_age",
+        "very_large_animal_age"
     ]
 )
 def test_get_human_age(animal_age: int, expected_age: List[int]) -> None:
     assert get_human_age(animal_age, animal_age) == expected_age
 
 
-def test_get_human_age_with_invalid_input() -> None:
-    with pytest.raises(TypeError, match="Animal age should be an integer"):
-        get_human_age(2.5, "cat")
-    with pytest.raises(TypeError, match="Animal age should be an integer"):
-        get_human_age("dog", 5)
-    with pytest.raises(TypeError, match="Animal age should be an integer"):
-        get_human_age(5, "dog")
+@pytest.mark.parametrize(
+    "animal_age, animal_type, expected_error",
+    [
+        (2.5, "cat", "Animal age should be an integer"),
+        ("dog", 5, "Animal age should be an integer"),
+        (5, "dog", None),
+    ],
+    ids=[
+        "invalid_animal_age_type_float",
+        "invalid_animal_age_type_str",
+        "valid_input"
+    ]
+)
+def test_get_human_age_with_invalid_input(animal_age: int,
+                                          animal_type: str,
+                                          expected_error: str) -> None:
+    if expected_error:
+        with pytest.raises(TypeError, match=expected_error):
+            get_human_age(animal_age, animal_type)
+    else:
+        assert get_human_age(animal_age, animal_type) == [0, 0]
