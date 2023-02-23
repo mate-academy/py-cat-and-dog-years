d877ce81a9f836d5d3e907b42b61363b742e1fc4
Vitalii Shulepa <vitaliy.shulep@gmail.com>
2023-02-23 20:49:15 +0200

    Solution add parametrize, fixed flake add id's

--- app/test_main.py
+++ app/test_main.py
@@ -5,34 +5,66 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected_years",
     [
-        (
-            0, 0, [0, 0]
+        pytest.param(
+            0, 0, [0, 0],
+            id="should return 0, 0"
         ),
-        (
-            14, 14, [0, 0]
+        pytest.param(
+            14, 14, [0, 0],
+            id="should return 0, 0"
         ),
-        (
-            15, 15, [1, 1]
+        pytest.param(
+            15, 15, [1, 1],
+            id="should return 1, 1"
         ),
-        (
-            23, 23, [1, 1]
+        pytest.param(
+            23, 23, [1, 1],
+            id="should return 1, 1"
         ),
-        (
-            24, 24, [2, 2]
+        pytest.param(
+            24, 24, [2, 2],
+            id="should return 2, 2"
         ),
-        (
-            27, 27, [2, 2]
+        pytest.param(
+            27, 27, [2, 2],
+            id="should return 2, 2"
         ),
-        (
-            28, 28, [3, 2]
+        pytest.param(
+            28, 28, [3, 2],
+            id="should return 3, 2"
         ),
-        (
-            100, 100, [21, 17]
+        pytest.param(
+            100, 100, [21, 17],
+            id="should return 21, 17"
+        ),
+        pytest.param(
+            1.2, 8.5, [0, 0],
+            id="should return 0, 0"
+        ),
+        pytest.param(
+            5.1, 6.1, [0, 0],
+            id="should return 0, 0"
+        ),
+        pytest.param(
+            24.9, 25.6, [2, 2],
+            id="should return 2, 2"
+        ),
+        pytest.param(
+            27.1, 27.2, [2, 2],
+            id="should return 2, 2"
+        ),
+        pytest.param(
+            28.1, 28.7, [3, 2],
+            id="should return 3, 2"
+        ),
+        pytest.param(
+            100.3, 100.9, [21, 17],
+            id="should return 21, 17"
         )
     ]
 )
-def test_get_correct_human_age(cat_age: int,
-                               dog_age: int,
+def test_get_correct_human_age(cat_age: (int, float),
+                               dog_age: (int, float),
                                expected_years: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected_years
 
@@ -40,47 +72,21 @@ def test_get_correct_human_age(cat_age: int,
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected_years",
     [
-        (
-            -1, 8, [0, 0]
+        pytest.param(
+            -1, 8, [0, 0],
+            id="should return 0, 0"
         ),
-        (
-            2, -10, [0, 0]
+        pytest.param(
+            2, -10, [0, 0],
+            id="should return 0, 0"
         ),
-        (
-            -2, -25, [0, 0]
+        pytest.param(
+            -2, -25, [0, 0],
+            id="should return 0, 0"
         )
     ]
 )
 def test_negative_age(cat_age: int,
                       dog_age: int,
                       expected_years: list) -> None:
-    assert get_human_age(cat_age, dog_age) == expected_years
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected_years",
-    [
-        (
-            1.2, 8.5, [0, 0]
-        ),
-        (
-            5.1, 6.1, [0, 0]
-        ),
-        (
-            24.9, 25.6, [2, 2]
-        ),
-        (
-            27.1, 27.2, [2, 2]
-        ),
-        (
-            28.1, 28.7, [3, 2]
-        ),
-        (
-            100.3, 100.9, [21, 17]
-        )
-    ]
-)
-def test_float_numbers_age(cat_age: int,
-                           dog_age: int,
-                           expected_years: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected_years
