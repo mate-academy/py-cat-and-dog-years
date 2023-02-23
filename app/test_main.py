b6d0fc02177612346ea50b8d8ceae91e4a712c54
Mykola Savchyn <kola.savchyn@gmail.com>
2023-02-23 12:47:04 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,42 @@
+import pytest
+from typing import Any
 from app.main import get_human_age
 
 
-def test_age_is_zero():
-    assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, human_age",
+    [
+        (-1, -1, [0, 0]),
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 23, [1, 1]),
+        (25, 25, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ],
+    ids=[
+        "-1, -1 should return [0, 0]",
+        "0, 0, should return [0, 0]",
+        "14, 14, should return [0, 0]",
+        "15, 23, should return [1, 1]",
+        "25, 25, should return [2, 2]",
+        "28, 28, should return [3, 2]",
+        "100, 100, should return [21, 17]"
+    ]
+)
+def test_human_age(cat_age: int, dog_age: int, human_age:int) -> None:
+    assert get_human_age(cat_age, dog_age) == human_age
 
 
-def test_less_than_15_years():
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_first_15_years():
-    assert get_human_age(15, 23) == [1, 1]
-
-
-def test_next_9_years():
-    assert get_human_age(25, 25) == [2, 2]
-
-
-def test_every_next_5_years():
-    assert get_human_age(28, 28) == [3, 2]
+@pytest.mark.parametrize("cat_age, dog_age, human_age", [
+    ("one", 1, TypeError),
+    (1, "one", TypeError),
+    ("one", "one", TypeError),
+])
+def test_get_human_age_errors(
+        cat_age: int,
+        dog_age: int,
+        human_age: Any
+) -> None:
+    with pytest.raises(human_age):
+        get_human_age(cat_age, dog_age)
