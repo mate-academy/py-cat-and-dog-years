e0792fdf619af58924bb11fa73af58c492f0b879
Kateryna Kushnir <katerynakushnir3@gmail.com>
2023-02-23 19:36:25 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,26 +1,6 @@
 import pytest
-from app.main import get_human_age, convert_to_human
 
-
-@pytest.mark.parametrize(
-    "animal_age,first_year,second_year,each_year,human_age", [
-        (0, 10, 5, 1, 0),
-        (20, 1, 1, 1, 20),
-        (123, 15, 9, 5, 21),
-        (-1, 15, 9, 5, 0)
-    ]
-)
-def test_if_convert_to_human_returns_correct_value(
-    animal_age: int,
-    first_year: int,
-    second_year: int,
-    each_year: int,
-    human_age: int
-) -> None:
-    assert (convert_to_human(
-        animal_age, first_year, second_year, each_year) == human_age
-    ), (f"Animal age({animal_age})"
-        f" in human years should be equal to {human_age}")
+from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
@@ -31,6 +11,14 @@ def test_if_convert_to_human_returns_correct_value(
         (27, 27, [2, 2]),
         (100, 100, [21, 17]),
         (-67, -3, [0, 0])
+    ],
+    ids=[
+        "age_equals_zero",
+        "age_equals_14_15",
+        "age_equals_24_23",
+        "age_equals_27_27",
+        "age_equals_100_100",
+        "age_equals_negative_value"
     ]
 )
 def test_if_get_human_age_returns_correct_value(
@@ -46,8 +34,3 @@ def test_if_get_human_age_returns_correct_value(
 def test_type_error_get_human_age() -> None:
     with pytest.raises(TypeError):
         get_human_age("2", "3")
-
-
-def test_type_error_convent_to_human() -> None:
-    with pytest.raises(TypeError):
-        convert_to_human("2", [3], False, {"each_year": 2})
