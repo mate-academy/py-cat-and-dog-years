974670d4e3b51b2608aa85287e758cc659f3f534
Taras Panasiuk <107722931+tarasavage@users.noreply.github.com>
2023-02-25 21:43:57 +0200

    fixed flake8

--- app/test_main.py
+++ app/test_main.py
@@ -8,15 +8,26 @@ class TestGetHumanAge:
     @pytest.mark.parametrize(
         "cat_age,dog_age,expected_ages",
         [
-            pytest.param(0, 0, [0, 0], id="zero ages should equal [0, 0]"),
-            pytest.param(12, 13, [0, 0], id="less than 15 ages should equal [0, 0]"),
-            pytest.param(15, 15, [1, 1], id="both 15 ages should equal [1, 1]"),
-            pytest.param(23, 23, [1, 1], id="less than 24 ages should equal [1, 1]"),
-            pytest.param(24, 24, [2, 2], id="both 24 ages should equal [2, 2]"),
-            pytest.param(27, 27, [2, 2], id="less than 28 ages should equal [2, 2]"),
-            pytest.param(28, 28, [3, 2], id="both 28 ages should equal [3, 2]"),
-            pytest.param(-120, -20, [0, 0], id="negative ages should return [0, 0]"),
-            pytest.param(100, 100, [21, 17], id="both 100 ages should return [21, 17]"),
+            pytest.param(0, 0, [0, 0]),
+            pytest.param(12, 13, [0, 0]),
+            pytest.param(15, 15, [1, 1]),
+            pytest.param(23, 23, [1, 1]),
+            pytest.param(24, 24, [2, 2]),
+            pytest.param(27, 27, [2, 2]),
+            pytest.param(28, 28, [3, 2]),
+            pytest.param(-120, -20, [0, 0]),
+            pytest.param(100, 100, [21, 17]),
+        ],
+        ids=[
+            "zero ages equal [0, 0]",
+            "less than 15 ages equal [0, 0]",
+            "both 15 ages equal [1, 1]",
+            "less than 24 ages equal [1, 1]",
+            "both 24 ages equal [2, 2]",
+            "less than 28 ages equal [2, 2]",
+            "both 28 ages equal [3, 2]",
+            "negative ages return [0, 0]",
+            "both 100 ages return [21, 17]"
         ]
     )
     def test_get_human_age(
@@ -29,9 +40,14 @@ class TestGetHumanAge:
     @pytest.mark.parametrize(
         "cat_age,dog_age,expected_error",
         [
-            pytest.param(1, "2", TypeError, id="wrong type of age param raises exception"),
-            pytest.param([2], 2, TypeError, id="wrong type of age param raises exception"),
-            pytest.param([2], "2", TypeError, id="wrong type of age param raises exception")
+            pytest.param(1, "2", TypeError),
+            pytest.param([2], 2, TypeError),
+            pytest.param([2], "2", TypeError)
+        ],
+        ids=[
+            "wrong type of age param raises exception",
+            "wrong type of age param raises exception",
+            "wrong type of age param raises exception"
         ]
     )
     def test_get_human_age_wrong_types(
