f9f79f5362be33660cb1071e79f537d3b58debb7
Yurii Kuchmenko <kuazakotl1@gmail.com>
2023-02-26 02:31:01 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,70 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age",
+    [
+        (0, 0),
+        (14, 14),
+    ]
+)
+def test_first_15_years_give_1_human_year(
+        cat_age: int, dog_age: int
+) -> None:
+    assert get_human_age(cat_age, dog_age) == [0, 0]
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age",
+    [
+        (15, 15),
+        (23, 23)
+    ]
+)
+def test_next_9_years_give_1_more_human_year(
+        cat_age: int, dog_age: int
+) -> None:
+    assert get_human_age(cat_age, dog_age) == [1, 1]
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ]
+)
+def test_every_4_and_5_years_give_an_extra_human_year(
+        cat_age: int, dog_age: int, result: list[int]
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        (0, 0, [0, 0]),
+        (-1, -1, [0, 0]),
+        (1000, 10000, [246, 1997])
+    ]
+)
+def test_input_out_of_range(
+        cat_age: int, dog_age: int, result: list[int]
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age",
+    [
+        ("string", 0),
+        ([5], {5})
+    ]
+)
+def test_type_error_exception(cat_age: int, dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
