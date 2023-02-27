5cce4bbc6cbafef8d2a5024de9cb5bbecb0c53fb
VladyslavCherkashyn <Sparrowq2@gmail.com>
2023-02-27 16:48:27 +0200

    fixed test for all boundaries for both(cat and dog), implemented test with lage values, implemented test for incorrect data types

--- app/test_main.py
+++ app/test_main.py
@@ -14,14 +14,26 @@ class TestGetHumanAge:
             ),
             pytest.param(
                 15,
+                15,
+                [1, 1],
+                id="function should return 1 for 15 >= arguments"
+            ),
+            pytest.param(
+                23,
                 23,
                 [1, 1],
-                id="function should return 1 for 15 >= arguments <= 23"
+                id="function should return 1 for arguments <= 23"
             ),
             pytest.param(
                 24,
+                24,
+                [2, 2],
+                id="boundary condition"
+            ),
+            pytest.param(
                 29,
-                [2, 3],
+                29,
+                [3, 3],
                 id="upper boundary condition"
             ),
             pytest.param(
@@ -35,6 +47,17 @@ class TestGetHumanAge:
                 30.0,
                 [4.0, 3.0],
                 id="float data"
+            ),
+            pytest.param(
+                0,
+                0,
+                [0, 0]
+            ),
+            pytest.param(
+                1000,
+                1000,
+                [246, 197],
+                id="large values"
             )
         ]
     )
@@ -45,3 +68,22 @@ class TestGetHumanAge:
             expected_result: list
     ) -> None:
         assert get_human_age(cat_age, dog_age) == expected_result
+
+    @pytest.mark.parametrize(
+        "value, expected_result",
+        [
+            pytest.param(
+                ("string", list, [], tuple, (), {}, dict, frozenset),
+                TypeError,
+                id="Inappropriate type of data"
+            ),
+        ],
+    )
+    def test_inappropriate_data_type(
+            self,
+            value: list,
+            expected_result: Exception
+    ) -> None:
+        with pytest.raises(TypeError):
+            get_human_age(3, value)
+            get_human_age(value, 4)
