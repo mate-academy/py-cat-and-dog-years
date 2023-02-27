c0b74da572d7aa05de15616454e1d7ce18c7d174
Yulia <Mala>
2023-02-27 15:19:58 +0200

    boundary values added

--- app/test_main.py
+++ app/test_main.py
@@ -5,18 +5,35 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_years,dog_years,result",
     [
-        pytest.param(15, 15, [1, 1], id="exactly one year"),
-        pytest.param(24, 24, [2, 2], id="exactly two years"),
-        pytest.param(28, 29, [3, 3], id="exactly three years"),
-        pytest.param(32, 34, [4, 4], id="exactly four years"),
-        pytest.param(0, 0, [0, 0], id="zero years"),
-        pytest.param(7, 14, [0, 0], id="zero years - we discard the reminder"),
-        pytest.param(15, 20, [1, 1], id="1 year, we discard the reminder"),
-        pytest.param(27, 25, [2, 2], id="2 years, we discard the reminder"),
-        pytest.param(29, 33, [3, 3], id="3 years, we discard the reminder"),
-        pytest.param(33, 35, [4, 4], id="4 years, we discard the reminder"),
-        pytest.param(127, 100, [27, 17], id="big years value"),
-        pytest.param(-120, -1, [0, 0], id="negative years"),
+        pytest.param(15, 15, [1, 1],
+                     id="this values give us exactly one human year"),
+        pytest.param(24, 24, [2, 2],
+                     id="this values give us exactly two human years"),
+        pytest.param(28, 29, [3, 3],
+                     id="this values give us exactly three human years"),
+        pytest.param(32, 34, [4, 4],
+                     id="this values give us exactly four human years"),
+        pytest.param(0, 0, [0, 0],
+                     id="zero animal years == zero human years"),
+        pytest.param(14, 14, [0, 0],
+                     id=("boundary values give us zero years, "
+                         " we discard the reminder")),
+        pytest.param(23, 23, [1, 1],
+                     id=("boundary values give us 1 year, "
+                         "we discard the reminder")),
+        pytest.param(27, 28, [2, 2],
+                     id=("boundary values give us  2 years, "
+                         "we discard the reminder")),
+        pytest.param(31, 33, [3, 3],
+                     id=("boundary values give us  3 years, "
+                         "we discard the reminder")),
+        pytest.param(33, 35, [4, 4],
+                     id=("boundary values give us 4 years, "
+                         "we discard the reminder")),
+        pytest.param(127, 100, [27, 17],
+                     id="big years value"),
+        pytest.param(-120, -1, [0, 0],
+                     id="negative years values give us zero human years"),
         pytest.param(1234567890, 987654321,
                      [308641968, 197530861], id="large numbers"),
 
