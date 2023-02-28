6195f1212ac51f71807d651fc32476f869e114be
Anna Blahitka <anettaabl@gmail.com>
2023-02-28 13:11:27 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,48 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,human_age",
+        [
+            (
+                0,
+                0,
+                [0, 0]
+            ),
+            (
+                12,
+                14,
+                [0, 0]
+            ),
+            (
+                24,
+                27,
+                [2, 2]
+            ),
+            (
+                28,
+                15,
+                [3, 1]
+            ),
+            (
+                54,
+                97,
+                [9, 16]
+            ),
+            (
+                23,
+                23,
+                [1, 1]
+            )
+        ]
+    )
+    def test_to_count_a_human_age(
+        self,
+        cat_age: int,
+        dog_age: int,
+        human_age: list
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == human_age
