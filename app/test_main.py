f94806a2fad3be826cf0330c959e876bc2460f78
Arsen Makovei <makoveyarsen@gmail.com>
2023-03-06 22:01:03 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,human_age",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ],
+    ids=[
+        "pet age equals 0",
+        "pet age is less than 1 human year",
+        "pet age equals to 1 human year ",
+        "pet age is less than 2 human year ",
+        "pet age equals to 2 human years ",
+        "pet age is less than 3 human years ",
+        "cat age equals to 3 human years",
+        "pet age equals 100 years",
+    ]
+)
+def test_can_sum(cat_age: int, dog_age: int, human_age: list) -> None:
+    assert (get_human_age(cat_age, dog_age) == human_age)
