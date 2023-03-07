3378a5b7df7f9ba5f2f835c39ca0445ce188b673
Arsen Makovei <makoveyarsen@gmail.com>
2023-03-07 17:17:41 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -23,8 +23,33 @@ from app.main import get_human_age
         "pet age equals to 2 human years ",
         "pet age is less than 3 human years ",
         "cat age equals to 3 human years",
-        "pet age equals 100 years",
+        "pet age equals 100 years"
     ]
 )
-def test_can_sum(cat_age: int, dog_age: int, human_age: list) -> None:
+def test_should_convert_correctly(
+        cat_age: int,
+        dog_age: int,
+        human_age: list
+) -> None:
     assert (get_human_age(cat_age, dog_age) == human_age)
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age",
+    [
+        ("50", 50),
+        (100, [100]),
+        ({"age": 15}, 15),
+        (40, (40,))
+
+    ],
+    ids=[
+        "pet age must be integer not str",
+        "pet age must be integer not list",
+        "pet age must be integer not dict",
+        "pet age must be integer not tuple"
+    ]
+)
+def test_parameters_should_be_int(cat_age: int, dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
