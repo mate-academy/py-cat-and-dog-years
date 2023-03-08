fea7234ad83485d8b693bdb6bfd19f011a3850f3
Aleksandr <lotrikys@gmail.com>
2023-03-08 16:09:46 +0200

    Fix solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,21 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_cat_dog_years_less_than_first_years() -> None:
-    assert get_human_age(10, 10) == [0, 0]
-
-
-def test_cat_dog_years_equals_first_years() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_cat_dog_years_more_than_first_years() -> None:
-    assert get_human_age(28, 28) == [3, 2]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        (0, 0, [0, 0]),
+        (10, 10, [0, 0]),
+        (15, 15, [1, 1]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ]
+)
+def test_get_human_age(
+        cat_age,
+        dog_age,
+        result
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result
