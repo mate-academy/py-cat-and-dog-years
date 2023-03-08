c5a6fe34a0f1b7af9937238fc7645dd3d38e726b
Oleksandr Hryshchenko <oleksandr.grk@gmail.com>
2023-03-08 21:21:48 +0200

    add type annotations

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,25 @@
 from app.main import get_human_age
 
 
-def test_should_return_zeroes_when_integers_are_zeroes():
+def test_should_return_zeroes_when_integers_are_zeroes() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_should_return_zeroes_when_integers_less_first_year():
+def test_should_return_zeroes_when_integers_less_first_year() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
 
-def test_should_return_one_when_integers_less_second_year():
+def test_should_return_one_when_integers_less_second_year() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_should_return_one_when_integers_less_third_year():
+def test_should_return_one_when_integers_less_third_year() -> None:
     assert get_human_age(27, 27) == [2, 2]
 
 
-def test_should_return_validates_years_for_each_animal():
+def test_should_return_validates_years_for_each_animal() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
 
-def test_should_check_huge_numbers():
+def test_should_check_huge_numbers() -> None:
     assert get_human_age(100, 100) == [21, 17]
