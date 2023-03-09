bb445e30de16b0bd669d39c44dae8cdbf865de56
Aleksandr <lotrikys@gmail.com>
2023-03-09 10:53:23 +0200

    Add 'ids'. Add more incorrect types for second test

--- app/test_main.py
+++ app/test_main.py
@@ -11,6 +11,13 @@ from app.main import get_human_age
         (15, 15, [1, 1]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17])
+    ],
+    ids=[
+        "Check zeros",
+        "10 cat and dog years return 0 human age",
+        "15 cat and dog years return 1 human age",
+        "28 cat and dog years return 3 and 2 human age",
+        "Check large cat and dog years"
     ]
 )
 def test_get_human_age(
@@ -21,6 +28,19 @@ def test_get_human_age(
     assert get_human_age(cat_age, dog_age) == result
 
 
-def test_function_raises_error() -> None:
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("ten", 10),
+        (10, [10]),
+        (object, 10)
+    ],
+    ids=[
+        "String in cat age should raise TypeError",
+        "List in dog age should raise TypeError",
+        "Object in cat age should raise TypeError"
+    ]
+)
+def test_function_raises_error(cat_age: int, dog_age: int,) -> None:
     with pytest.raises(TypeError):
-        get_human_age("10", 10)
+        get_human_age(cat_age, dog_age)
