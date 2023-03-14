56a2a3d154003475d4727091cca1af05d61610d9
Kirill Melanich <kirillmelanich@gmail.com>
2023-03-14 18:30:23 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,50 +1,40 @@
+import pytest
+
 from app.main import get_human_age
 
 
-# write your code here
-def test_if_the_age_is_0() -> None:
-    age = get_human_age(0, 0)
-
-    assert age == [0, 0]
-
-
-def test_if_age_less_then_15_then_convert_to_0() -> None:
-    age = get_human_age(14, 14)
-
-    assert age == [0, 0]
-
-
-def test_if_age_more_then_15_then_convert_to_1() -> None:
-    age = get_human_age(15, 15)
-
-    assert age == [1, 1]
-
-
-def test_if_age_less_then_24_then_convert_to_1() -> None:
-    age = get_human_age(23, 23)
-
-    assert age == [1, 1]
-
-
-def test_if_age_more_then_24_then_convert_to_2() -> None:
-    age = get_human_age(24, 24)
-
-    assert age == [2, 2]
-
-
-def test_if_age_less_then_28_and_29_then_convert_to_2() -> None:
-    age = get_human_age(27, 28)
-
-    assert age == [2, 2]
-
-
-def test_if_age_more_then_28_and_29_then_convert_to_3() -> None:
-    age = get_human_age(28, 29)
-
-    assert age == [3, 3]
-
-
-def test_if_the_age_is_negative() -> None:
-    age = get_human_age(-15, -15)
-
-    assert age == [0, 0]
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_result",
+        [
+            (-1, -1, [0, 0]),
+            (0, 0, [0, 0]),
+            (13, 13, [0, 0]),
+            (15, 15, [1, 1]),
+            (22, 22, [1, 1]),
+            (25, 25, [2, 2]),
+            (28, 28, [3, 2]),
+            (29, 29, [3, 3]),
+            (111, 111, [23, 19]),
+            (12345, 12345, [3082, 2466]),
+        ],
+        ids=[
+            "Should return '[0, 0]' if cat's and dog's age < 0",
+            "Should return '[0, 0]' if cat's and dog's age == 0",
+            "Should return '[0, 0]' if cat's and dog's age == 13",
+            "Should return '[1, 1]' if cat's and dog's age == 15",
+            "Should return '[2, 2]' if cat's and dog's age == 22",
+            "Should return '[2, 2]' if cat's and dog's age == 25",
+            "Should return '[3, 2]' if cat's and dog's age == 28",
+            "Should return '[3, 3]' if cat's and dog's age == 29",
+            "Should return '[23, 19]' if cat's and dog's age == 111",
+            "Should return '[3082, 2466]' if cat's and dog's age == 12345"
+        ]
+    )
+    def test_of_correct_years_converting(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_result: list[int]
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_result
