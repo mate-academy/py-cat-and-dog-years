71cb3556ae640d236fa4635fdc0d27882c8c9bbf
Iryna Ishchenko <irynaira@gmail.com>
2023-04-03 18:12:13 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -13,14 +13,14 @@ from app.main import get_human_age
 def test_if_result_has_right_format(cat_years: int, dog_years: int) -> None:
     result = get_human_age(cat_years, dog_years)
     assert (
-            isinstance(result, list)
+        isinstance(result, list)
     ), "result should be list"
     assert (
         len(result) == 2
     ), "result list should have 2 items"
     assert (
         isinstance(result[0], int) and isinstance(result[1], int)
-    ),  "items of result should be int"
+    ), "items of result should be int"
 
 
 @pytest.mark.parametrize(
@@ -61,7 +61,7 @@ def test_if_dog_has_zero_years(cat_years: int, dog_years: int) -> None:
 )
 def test_if_cat_has_one_years(cat_years: int, dog_years: int) -> None:
     assert (
-            get_human_age(cat_years, dog_years)[0] == 1
+        get_human_age(cat_years, dog_years)[0] == 1
     ), f"cat age should be 1 if parameter is {cat_years}"
 
 
@@ -75,7 +75,7 @@ def test_if_cat_has_one_years(cat_years: int, dog_years: int) -> None:
 )
 def test_if_dog_has_one_years(cat_years: int, dog_years: int) -> None:
     assert (
-            get_human_age(cat_years, dog_years)[1] == 1
+        get_human_age(cat_years, dog_years)[1] == 1
     ), f"dog age should be 1 if parameter is {dog_years}"
 
 
@@ -89,8 +89,9 @@ def test_if_dog_has_one_years(cat_years: int, dog_years: int) -> None:
 )
 def test_if_cat_has_two_or_more_years(cat_years: int, dog_years: int) -> None:
     assert (
-            get_human_age(cat_years, dog_years)[0] == 2 + (cat_years - 24) // 4
-    ), f"cat age should be {2 + (cat_years - 24) // 4} if parameter is {cat_years}"
+        get_human_age(cat_years, dog_years)[0] == 2 + (cat_years - 24) // 4
+    ), f"cat age should be {2 + (cat_years - 24) // 4}" \
+       f" if parameter is {cat_years}"
 
 
 @pytest.mark.parametrize(
@@ -103,5 +104,6 @@ def test_if_cat_has_two_or_more_years(cat_years: int, dog_years: int) -> None:
 )
 def test_if_dog_has_two_or_more_years(cat_years: int, dog_years: int) -> None:
     assert (
-            get_human_age(cat_years, dog_years)[1] == 2 + (dog_years - 24) // 5
-    ), f"dog age should be {2 + (dog_years - 24) // 5} if parameter is {dog_years}"
+        get_human_age(cat_years, dog_years)[1] == 2 + (dog_years - 24) // 5
+    ), f"dog age should be {2 + (dog_years - 24) // 5}" \
+       f" if parameter is {dog_years}"
