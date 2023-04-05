7e20e5c45601225381d956029cbf77ac0492657d
Iryna Ishchenko <irynaira@gmail.com>
2023-04-05 20:47:01 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -16,94 +16,26 @@ def test_if_result_has_right_format(cat_years: int, dog_years: int) -> None:
         isinstance(result, list)
     ), "result should be list"
     assert (
-        len(result) == 2
+            len(result) == 2
     ), "result list should have 2 items"
     assert (
-        isinstance(result[0], int) and isinstance(result[1], int)
+            isinstance(result[0], int) and isinstance(result[1], int)
     ), "items of result should be int"
 
 
 @pytest.mark.parametrize(
-    "cat_years,dog_years",
+    "cat_years,dog_years,expected_result",
     [
-        (i, j)
-        for i in range(0, 15)
-        for j in [0, 7, 15, 22, 24, 26, 31]
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (100, 100, [21, 17]),
     ]
 )
-def test_if_cat_has_zero_years(cat_years: int, dog_years: int) -> None:
+def test_if_cat_has_years(cat_years: int, dog_years: int, expected_result: list) -> None:
     assert (
-        get_human_age(cat_years, dog_years)[0] == 0
-    ), f"cat age should be 0 if parameter is {cat_years}"
-
-
-@pytest.mark.parametrize(
-    "cat_years,dog_years",
-    [
-        (i, j)
-        for i in [0, 7, 15, 22, 24, 26, 31]
-        for j in range(0, 15)
-    ]
-)
-def test_if_dog_has_zero_years(cat_years: int, dog_years: int) -> None:
-    assert (
-        get_human_age(cat_years, dog_years)[1] == 0
-    ), f"dog age should be 0 if parameter is {dog_years}"
-
-
-@pytest.mark.parametrize(
-    "cat_years,dog_years",
-    [
-        (i, j)
-        for i in range(15, 24)
-        for j in [0, 7, 15, 22, 24, 26, 31]
-    ]
-)
-def test_if_cat_has_one_years(cat_years: int, dog_years: int) -> None:
-    assert (
-        get_human_age(cat_years, dog_years)[0] == 1
-    ), f"cat age should be 1 if parameter is {cat_years}"
-
-
-@pytest.mark.parametrize(
-    "cat_years,dog_years",
-    [
-        (i, j)
-        for i in [0, 7, 15, 22, 24, 26, 31]
-        for j in range(15, 24)
-    ]
-)
-def test_if_dog_has_one_years(cat_years: int, dog_years: int) -> None:
-    assert (
-        get_human_age(cat_years, dog_years)[1] == 1
-    ), f"dog age should be 1 if parameter is {dog_years}"
-
-
-@pytest.mark.parametrize(
-    "cat_years,dog_years",
-    [
-        (i, j)
-        for i in range(24, 100)
-        for j in [0, 7, 15, 22, 24, 26, 31]
-    ]
-)
-def test_if_cat_has_two_or_more_years(cat_years: int, dog_years: int) -> None:
-    assert (
-        get_human_age(cat_years, dog_years)[0] == 2 + (cat_years - 24) // 4
-    ), f"cat age should be {2 + (cat_years - 24) // 4}" \
-       f" if parameter is {cat_years}"
-
-
-@pytest.mark.parametrize(
-    "cat_years,dog_years",
-    [
-        (i, j)
-        for i in [0, 7, 15, 22, 24, 26, 31]
-        for j in range(24, 100)
-    ]
-)
-def test_if_dog_has_two_or_more_years(cat_years: int, dog_years: int) -> None:
-    assert (
-        get_human_age(cat_years, dog_years)[1] == 2 + (dog_years - 24) // 5
-    ), f"dog age should be {2 + (dog_years - 24) // 5}" \
-       f" if parameter is {dog_years}"
+        get_human_age(cat_years, dog_years) == expected_result
+    ), f"cat, dog age should be {expected_result[0]}, {expected_result[1]} " \
+       f"if parameter is {cat_years}, {dog_years}"
