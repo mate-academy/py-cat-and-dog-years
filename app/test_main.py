0ed7638657ffaa23b23757e8dc4861b430d5130f
Mykola <nickkozlov90@gmail.com>
2023-04-08 14:19:34 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,58 @@
+import pytest
 from app.main import get_human_age
+from typing import Type
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 28, [2, 2]),
+        (28, 29, [3, 3]),
+        (100, 100, [21, 17])
+    ],
+    ids=[
+        "cat_and_dog_years_are_zeros_values",
+        "cat_and_dog_years_less_than_1_human_year",
+        "edge_entry_values_for_1_human_years",
+        "edge_exit_values_for_1_human_years",
+        "edge_entry_values_for_2_human_years",
+        "edge_exit_values_for_2_human_years",
+        "edge_entry_values_for_3_human_years",
+        "cat_and_dog_years_more_than_3_human_years"
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, result: list[int]) -> None:
+    assert (
+        get_human_age(cat_age, dog_age) == result
+    ), f"Conversion of {cat_age} and {dog_age} should be equal to {result}"
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+    [
+        (-1, 70, ValueError),
+        (1000, 10, ValueError),
+        ("20", 50, TypeError),
+        (20, [50], TypeError),
+        ((20, 50), {20: 50}, TypeError)
+    ],
+    ids=[
+        "raise_value_error_if_years_are_negative",
+        "raise_value_error_if_years_are_too_large",
+        "raise_type_error_if_years_is_string",
+        "raise_type_error_if_years_is_list",
+        "raise_type_error_if_years_is_tuple_or_dictionary"
+    ]
+)
+def test_raises_error_correctly(
+        cat_age: int,
+        dog_age: int,
+        expected_error: Type[Exception]
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
