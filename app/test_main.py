48cf9430b4ab0d097617dc61a9a2c638bf164620
Dmytro Shvets <dirolius@gmail.com>
2023-04-10 12:28:01 +0300

    Tests done

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,38 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "input_value_cat,input_value_dog,expected_value",
+        [
+            (
+                0,
+                0,
+                [0, 0]
+            ),
+            (
+                100,
+                100,
+                [21, 17]
+            ),
+            (
+                23,
+                23,
+                [1, 1]
+            ),
+            (
+                -2,
+                -3,
+                [0, 0]
+            )
+        ])
+    def test_convert_to_human_correctly(self,
+                                        input_value_cat: int,
+                                        input_value_dog: int,
+                                        expected_value: list[int]) -> None:
+        assert get_human_age(input_value_cat,
+                             input_value_dog) == expected_value
+
+    with pytest.raises(TypeError):
+        get_human_age("12", 22)
