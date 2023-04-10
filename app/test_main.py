f99d3c9e02e9aa5e0f38f80bd18a1e899ef746d7
“Ruslan <callogan217@gmail.com>
2023-04-11 00:00:51 +0300

    Improvements

--- app/test_main.py
+++ app/test_main.py
@@ -5,6 +5,11 @@ def test_should_return_zero_when_input_zero_years() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
+def test_should_return_zero_when_input_negative_values() -> None:
+    assert get_human_age(-5, 1) == [0, 0]
+    assert get_human_age(5, -1) == [0, 0]
+
+
 def test_should_convert_animal_years_younger_than_1_human() -> None:
     assert get_human_age(12, 14) == [0, 0]
 
@@ -23,3 +28,10 @@ def test_should_convert_extra_cat_years() -> None:
 
 def test_should_convert_extra_dog_years() -> None:
     assert get_human_age(16, 29) == [1, 3]
+
+
+def test_incorrect_type_of_data() -> None:
+    try:
+        get_human_age("28", [28])
+    except TypeError as e:
+        assert "'<' not supported between instances of 'str' and 'int'"
