d5ae6b346417c06fa4f272e8743d4ff23535fa13
leetwinoff <leetwinoff92@gmail.com>
2023-04-11 20:23:26 -0500

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,41 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_should_return_zero_when_pass_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,output",
+    [
+        (14, 8, [0, 0]),
+        (15, 15, [1, 1]),
+        (15, 14, [1, 0]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        pytest.param(0, 0, [0, 0], id="should work with zeros"),
+        pytest.param(-2, -5, [0, 0], id="should work with negative numbers"),
+        pytest.param(2.0, 3.2, [0, 0], id="should work with float numbers"),
+        pytest.param(824, 1024, [202, 202], id="should work with big numbers")
+    ]
+)
+def test_should_return_age_of_animals_in_human_years(
+    cat_age: int,
+    dog_age: int,
+    output: int
+) -> None:
+    assert get_human_age(cat_age, dog_age) == output
 
 
-def test_should_return_zero_when_age_pass_negative() -> None:
-    assert get_human_age(-10, 0) == [0, 0]
-    assert get_human_age(1, -5) == [0, 0]
-
-
-def test_should_cconvert_animal_years_less_than_human() -> None:
-    assert get_human_age(14, 8) == [0, 0]
-
-
-def test_should_convert_animal_years_to_human() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(15, 14) == [1, 0]
-
-
-def test_should_convert_animal_years_equal_to_two_human() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_should_convert_cat_years_equal_to_three_human() -> None:
-    assert get_human_age(28, 25) == [3, 2]
-
-
-def test_should_convert_dog_years_equal_to_three_human() -> None:
-    assert get_human_age(27, 29) == [2, 3]
+@pytest.mark.parametrize(
+    "cat_age,dog_age",
+    [
+        ("14", "15"),
+        (23, "25"),
+        ("36", 25)
+    ]
+)
+def test_should_raise_correct_error_when_incorrect_type(
+        cat_age: int,
+        dog_age: int
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
