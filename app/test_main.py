a33b6916940c53ffc7701162438b8dbc6983c02f
Oleksandr Yanchuk <sashajanchuck13@gmail.com>
2023-04-12 12:20:08 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+import pytest
 from app.main import get_human_age
 
 
@@ -11,8 +12,13 @@ def test_should_return_list_of_given_length() -> None:
     assert len(goals) == 2
 
 
-def test_should_return_zero_if_given_zero_age() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+def test_should_zero_ages_if_given_negative_ages() -> None:
+    assert get_human_age(-1, -1) == [0, 0]
+
+
+def test_should_raise_key_error_if_given_incorrect_input_type() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("-1", "-1")
 
 
 def test_14_cat_dog_years_should_convert_into_0_human_age() -> None:
