f5f54e45d38469d38f2a8c889484fb66ca5a04e9
Oleksandr Yanchuk <sashajanchuck13@gmail.com>
2023-04-12 11:18:57 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,39 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_list() -> None:
+    goals = get_human_age(0, 0)
+    assert isinstance(goals, list)
+
+
+def test_should_return_list_of_given_length() -> None:
+    goals = get_human_age(0, 0)
+    assert len(goals) == 2
+
+
+def test_should_return_zero_if_given_zero_age() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_14_cat_dog_years_should_convert_into_0_human_age() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_15_cat_dog_years_should_convert_into_1_human_age() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_24_cat_dog_years_should_convert_into_2_human_age() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_23_cat_dog_years_should_convert_into_1_human_age() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_28_29_cat_dog_years_should_convert_into_3_human_age() -> None:
+    assert get_human_age(28, 29) == [3, 3]
+
+
+def test_27_28_cat_dog_years_should_convert_into_2_human_age() -> None:
+    assert get_human_age(27, 28) == [2, 2]
