1420aeeca9aecbd7af189221533378b3160697de
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-16 17:23:20 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_zeros_when_animals_ages_less_then_15() -> None:
+    goal1 = get_human_age(0, 0)
+    goal2 = get_human_age(14, 14)
+
+    assert goal1 == goal2 == [0, 0]
+
+
+def test_should_return_1_when_animals_ages_more_than_14_and_has_no_extra_life() -> None:
+    goal1 = get_human_age(15, 15)
+    goal2 = get_human_age(23, 23)
+
+    assert goal1 == goal2 == [1, 1]
+
+
+def test_should_return_many_animals_extra_life() -> None:
+    goals = get_human_age(100, 100)
+
+    assert goals == [21, 17]
