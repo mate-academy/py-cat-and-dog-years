955c47bed593707b866021aaf98f4089a228dc7e
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-18 21:26:12 +0300

    added more tests

--- app/test_main.py
+++ app/test_main.py
@@ -2,20 +2,26 @@ from app.main import get_human_age
 
 
 def test_should_return_zeros_when_animals_ages_less_then_15() -> None:
-    goal1 = get_human_age(0, 0)
-    goal2 = get_human_age(14, 14)
+    negative_values = get_human_age(-1, -2)
+    zero_values = get_human_age(0, 0)
+    max_positive = get_human_age(14, 14)
 
-    assert goal1 == goal2 == [0, 0]
+    assert negative_values == zero_values == max_positive == [0, 0]
 
 
 def test_should_return_1_when_ages_more_than_14_and_no_extra_life() -> None:
-    goal1 = get_human_age(15, 15)
-    goal2 = get_human_age(23, 23)
-
-    assert goal1 == goal2 == [1, 1]
+    assert get_human_age(15, 15) == get_human_age(23, 23) == [1, 1]
 
 
 def test_should_return_many_animals_extra_life() -> None:
-    goals = get_human_age(100, 100)
+    assert get_human_age(100, 100) == [21, 17]
 
-    assert goals == [21, 17]
+
+def test_for_incorrect_input_types() -> None:
+    try:
+        get_human_age(None, None)
+        get_human_age("text", "0")
+        get_human_age([1], [0])
+        get_human_age({1: 1}, {0: 0})
+    except TypeError:
+        pass
