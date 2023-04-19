fd6b65610e3d5af9c9ee82834adc22c4ba346c5c
Volodymyr Borovets <vborovets0@gmail.com>
2023-04-19 23:28:18 +0300

    Use pytest.mark.parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,37 +1,32 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_should_check_when_cat_and_dog_year_is_equal_0():
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_should_check_when_cat_and_dog_year_is_equal_14():
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_should_check_when_cat_and_dog_year_is_equal_15():
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_should_check_when_cat_and_dog_year_is_equal_23():
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_should_check_when_cat_and_dog_year_is_equal_24():
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_should_check_when_cat_and_dog_year_is_equal_27():
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_should_check_when_cat_year_is_equal_28():
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_should_check_when_dog_year_is_equal_29():
-    assert get_human_age(29, 29) == [3, 3]
-
-
-def test_should_check_when_cat_and_dog_year_is_equal_100():
-    assert get_human_age(100, 100) == [21, 17]
+class TestMain:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, human_age",
+        [
+            pytest.param(-1, -1, [0, 0],
+                         id="should return 0 when cat and dog age is less than 15"),
+            pytest.param(0, 0, [0, 0],
+                         id="should return 0 when cat and dog age is less than 15"),
+            pytest.param(14, 14, [0, 0],
+                         id="should return 0 when cat and dog age is less than 15"),
+            pytest.param(15, 15, [1, 1],
+                         id="should return 1 when cat and dog age is less than 24"),
+            pytest.param(23, 23, [1, 1],
+                         id="should return 1 when cat and dog age is less than 24"),
+            pytest.param(24, 24, [2, 2],
+                         id="should return 1 when cat and dog age is less than 24"),
+            pytest.param(27, 27, [2, 2],
+                         id="should return 2 when cat age is less than 28 and dog age is less than 29"),
+            pytest.param(28, 28, [3, 2],
+                         id="should return 3 when cat age is 28 or bigger"),
+            pytest.param(29, 29, [3, 3],
+                         id="should return 3 when dog age is 29 or bigger"),
+            pytest.param(100, 100, [21, 17],
+                         id="should return 21 and 17 when cat and dog age is equal 100"),
+        ]
+    )
+    def test_should_return_correct_age(self, cat_age, dog_age, human_age):
+        assert get_human_age(cat_age, dog_age) == human_age
