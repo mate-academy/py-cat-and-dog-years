6db1bca77336e46fec828b3b476163cc665d9a9e
Vitalii Shkrobatskyi <vetali5700@gmail.com>
2023-04-20 21:22:17 +0300

    Solution with pytest.parametrize and more data test

--- app/test_main.py
+++ app/test_main.py
@@ -1,27 +1,101 @@
+import pytest
+
+from typing import Any
+
 from app.main import get_human_age
 
 
-def test_should_return_zeros_when_animals_ages_less_then_15() -> None:
-    negative_values = get_human_age(-1, -2)
-    zero_values = get_human_age(0, 0)
-    max_positive = get_human_age(14, 14)
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_result",
+        [
+            pytest.param(
+                -1,
+                -2,
+                [0, 0],
+                id="should return zeros when animals ages less then 0"
+            ),
+            pytest.param(
+                0,
+                0,
+                [0, 0],
+                id="should return zeros when animals ages equal zeros"
+            ),
+            pytest.param(
+                14,
+                14,
+                [0, 0],
+                id="should return zeros when animals ages equal 14"
+            ),
+            pytest.param(
+                15,
+                15,
+                [1, 1],
+                id="should return 1 when ages more then 14 and no extra life"
+            ),
+            pytest.param(
+                23,
+                23,
+                [1, 1],
+                id="should return 1 when ages more then 14 and no extra life"
+            ),
+            pytest.param(
+                24,
+                24,
+                [2, 2],
+                id="should return 2 when ages more then 23 (one extra life)"
+            ),
+            pytest.param(
+                28,
+                28,
+                [3, 2],
+                id="should return [3, 2] when different one extra life numbers"
+            ),
+            pytest.param(
+                100,
+                100,
+                [21, 17],
+                id="should return many animals extra life"
+            )
+        ]
+    )
+    def test_should_return_some_values(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_result: list
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_result
 
-    assert negative_values == zero_values == max_positive == [0, 0]
-
-
-def test_should_return_1_when_ages_more_than_14_and_no_extra_life() -> None:
-    assert get_human_age(15, 15) == get_human_age(23, 23) == [1, 1]
-
-
-def test_should_return_many_animals_extra_life() -> None:
-    assert get_human_age(100, 100) == [21, 17]
-
-
-def test_for_incorrect_input_types() -> None:
-    try:
-        get_human_age(None, None)
-        get_human_age("text", "0")
-        get_human_age([1], [0])
-        get_human_age({1: 1}, {0: 0})
-    except TypeError:
-        pass
+    @pytest.mark.parametrize(
+        "cat_age,dog_age",
+        [
+            pytest.param(
+                None,
+                None,
+                id="should return TypeError when animals ages is None"
+            ),
+            pytest.param(
+                "",
+                "",
+                id="should return TypeError when animals ages is str"
+            ),
+            pytest.param(
+                [0],
+                [0],
+                id="should return TypeError when animals ages is list"
+            ),
+            pytest.param(
+                {0: 0},
+                {0: 0},
+                id="should return TypeError when animals ages is dict"
+            )
+        ]
+    )
+    def test_should_return_incorrect_input_types_error(
+            self,
+            cat_age: Any,
+            dog_age: Any
+    ) -> None:
+        with pytest.raises(TypeError):
+            get_human_age(cat_age, dog_age)
