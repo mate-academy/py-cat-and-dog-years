e59dc0f7d2a0acd633bb4afe668705cb398d1422
Volodymyr <elohimwz@gmail.com>
2023-04-26 09:46:55 +0300

    'Solution#catDogYear'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_human_age_shouldbe_zero_if_animals_are_under_fifteen() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_human_age_shouldbe_one_if_animals_are_under_twenty_four() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_be_the_same_for_different_years() -> None:
+    assert get_human_age(44, 44) == [7, 6]
