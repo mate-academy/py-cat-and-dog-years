5e087a6dd50cdf755a674da9ff6f1b77d7b7a405
Alim Selemetov <alimselemetow@gmail.com>
2023-05-08 18:11:52 +0300

    add 2 tests

--- app/test_main.py
+++ app/test_main.py
@@ -63,3 +63,35 @@ class TestCatAndDogYears:
             result: list
     ) -> None:
         assert get_human_age(cat_age, dog_age) == result
+
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_error",
+        [
+            pytest.param(
+                -1,
+                2,
+                ValueError,
+                id="Cat_age and Dog_age should be equal to positive values"
+            ),
+            pytest.param(
+                2,
+                -7,
+                ValueError,
+                id="Cat_age and Dog_age should be equal to positive values"
+            ),
+            pytest.param(
+                "23",
+                "hello",
+                TypeError,
+                id="Cat_age and Dog_age should have type int not str"
+            )
+        ]
+    )
+    def test_raising_errors(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_error: ValueError | TypeError
+    ) -> None:
+        with pytest.raises(expected_error):
+            get_human_age(cat_age, dog_age)
