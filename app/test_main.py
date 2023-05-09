8c7fabfb7eaf8609bfeaf45d6103f4e0e2b8e8db
Munkoor <eee1lvlboss@gmail.com>
2023-05-09 13:37:58 +0300

    used a parametraze

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,27 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_less_than_one_human_year() -> None:
-    assert get_human_age(14, 14) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,human_result",
+    [
+        pytest.param(14, 14, [0, 0],
+                     id="Check when cat's and dog's "
+                        "age less than one human year"),
+        pytest.param(15, 15, [1, 1],
+                     id="Check when cat's and dog's"
+                        " age is equal to one human year"),
+        pytest.param(24, 24, [2, 2],
+                     id="Check when cat's and dog's"
+                        " age is equal to two human years"),
+        pytest.param(28, 28, [3, 2],
+                     id="Check one extra human year for cat"),
+        pytest.param(100, 100, [21, 17],
+                     id="Check extra human years for all animals")
 
-
-def test_one_human_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_two_human_years() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_extra_human_years_for_cats() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_extra_human_years_for_all_animals() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+    ]
+)
+def test_check_animal_age_for_human(cat_age: int, dog_age: int,
+                                    human_result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == human_result
