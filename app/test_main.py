586aef81a85b133930d41e31e796044e9dc41489
Dmitry <dimior1992@gmail.com>
2023-05-11 12:27:58 +0300

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,61 @@
+import pytest
+from typing import Any
 from app.main import get_human_age
 
 
-def test_if_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_result",
+        [
+            pytest.param(
+                -1,
+                -3,
+                [0, 0],
+                id="should return zero if negative age",
+            ),
 
+            pytest.param(
+                0,
+                0,
+                [0, 0],
+                id="should return zero if age is zero",
+            ),
 
-def test_if_less_than_year() -> None:
-    assert get_human_age(14, 14) == [0, 0]
+            pytest.param(
+                14,
+                14,
+                [0, 0],
+                id="should return zero if less than one human year",
+            ),
 
+            pytest.param(
+                15,
+                15,
+                [1, 1],
+                id="should return 1 year if animal years are 15",
+            ),
 
-def test_if_more_than_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
+            pytest.param(
+                28,
+                28,
+                [3, 2],
+                id="should return correct year if human years are different",
+            ),
+        ]
+    )
+    def test_return_correct_age(
+            self, cat_age: int, dog_age: int, expected_result: list
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_result
 
-
-def test_if_less_than_year_and_more_than_year() -> None:
-    assert get_human_age(14, 15) == [0, 1]
-
-
-def test_if_different_years() -> None:
-    assert get_human_age(28, 28) == [3, 2]
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, error",
+        [
+            pytest.param([], [], TypeError, id="Wrong type"),
+        ]
+    )
+    def test_raise_typeerror(
+            self, cat_age: Any, dog_age: Any, error: type[TypeError]
+    ) -> None:
+        with pytest.raises(error):
+            get_human_age(cat_age, dog_age)
