8bcf2e3da2609cdf1d5030c12c9b737f97cbf460
Anton Piddubnyi <antonpiddubnyii@gmail.com>
2023-05-13 14:41:13 -0600

    used parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,102 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_with_age_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+class TestDifferentAges:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_value",
+        [
+            pytest.param(
+                0,
+                0,
+                [0, 0],
+                id="Function should work correct with when arguments are equal 0"
+            ),
+            pytest.param(
+                0,
+                15,
+                [0, 1],
+                id="Function should work correct if cat age is equal 0"
+            ),
 
+            pytest.param(
+                15,
+                0,
+                [1, 0],
+                id="Function should work correct if dog age is equal 0"
+            ),
 
-def test_with_age_less_than_first_year() -> None:
-    assert get_human_age(14, 14) == [0, 0]
+            pytest.param(
+                -7,
+                -5,
+                [0, 0],
+                id="Test should work correct with negative arguments"
+            ),
 
+            pytest.param(
+                100,
+                100,
+                [21, 17],
+                id="Test should work correct with normal arguments"
+            ),
 
-def test_with_age_equal_to_first_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
+            pytest.param(
+                28,
+                28,
+                [3, 2],
+                id="Test should work correct with normal arguments"
+            ),
 
+            pytest.param(
+                27,
+                27,
+                [2, 2],
+                id="Test should work correct with normal arguments"
+            )
+        ])
+    def test_different_ages(
+            self,
+            cat_age,
+            dog_age,
+            expected_value
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_value
 
-def test_with_age_less_than_two() -> None:
-    assert get_human_age(23, 23) == [1, 1]
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, error",
+        [
+            pytest.param(
+                "1",
+                1,
+                TypeError,
+                id="Cat value should be int. Not string!"
+            ),
+            pytest.param(
+                1,
+                "1",
+                TypeError,
+                id="Dog value should be int. Not string!"
+            ),
 
+            pytest.param(
+                [1],
+                1,
+                TypeError,
+                id="Cat value should be int. Not list!"
+            ),
 
-def test_with_age_equal_to_two() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_with_age_less_than_three() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_with_cat_age_equal_to_three() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_with_both_ages_equal_to_100() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+            pytest.param(
+                1,
+                [1],
+                TypeError,
+                id="Dog value should be int. Not list!"
+            ),
+        ])
+    def test_converting_animal_age_to_himan_expected_error(
+            self,
+            cat_age: int,
+            dog_age: int,
+            error: TypeError
+    ) -> None:
+        with pytest.raises(error):
+            get_human_age(cat_age, dog_age)
