2afbdb79237d1fb4d4adb47b932a22686d838494
admin <admin@Yaroslavs-MacBook-Pro.local>
2023-05-18 17:00:52 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -14,13 +14,15 @@ from app.main import get_human_age
     pytest.param(24, 24, [2, 2]),
     pytest.param(28, 29, [3, 3])
 ])
-def test_return_human_age_correctly(cat_age, dog_age, expected):
+def test_return_human_age_correctly(cat_age: int, dog_age: int,
+                                    expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
 
 @pytest.mark.parametrize("cat_age, dog_age, expected_error", [
     pytest.param("20", 20.1, TypeError, id="all attributes should be integer")
 ])
-def test_raise_correctly_errors(cat_age, dog_age, expected_error):
+def test_raise_correctly_errors(cat_age: int, dog_age: int,
+                                expected_error: type[Exception]) -> None:
     with pytest.raises(expected_error):
         get_human_age(cat_age, dog_age)
