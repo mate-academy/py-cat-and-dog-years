5ecb382d4d3fbc09b29422e629582c230db1a127
Vitalii2906 <fahivchik1@gmail.com>
2023-05-20 09:52:27 +0300

    olution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,5 @@
+import pytest
+
 from app.main import get_human_age
 
 
@@ -14,3 +16,20 @@ def test_when_cat_and_dog_have_1_human_year() -> None:
 def test_cat_and_dog_are_more_than_2_years_old() -> None:
     assert get_human_age(28, 28) == [3, 2]
     assert get_human_age(100, 100) == [21, 17]
+
+
+@pytest.mark.parametrize("cat_age, dog_age, expected", [
+    (2, 3, [24, 15]),
+    (0, 0, [0, 0]),
+    (1, 0, [15, 0]),
+    ("2", 3, ValueError),
+    (2, "3", ValueError),
+    (1.5, 3, ValueError),
+    (2, 3.5, ValueError),
+])
+def test_get_human_age(cat_age, dog_age, expected):
+    if isinstance(expected, list):
+        assert get_human_age(cat_age, dog_age) == expected
+    else:
+        with pytest.raises(expected):
+            get_human_age(cat_age, dog_age)
