2d466736113c5288dc9732595d664800699e85b5
Vitalii2906 <fahivchik1@gmail.com>
2023-05-20 19:08:19 +0300

    Solution6

--- app/test_main.py
+++ app/test_main.py
@@ -42,7 +42,19 @@ def test_cat_and_dog_are_more_than_2_years_old(
     (1.5, 3, ValueError),
     (2, 3.5, ValueError),
 ])
-def test_get_human_age(
+def test_cat_and_dog_age_are_not_string_or_float(
+        cat_age: int,
+        dog_age: int,
+        expected: list or type) -> None:
+    if isinstance(expected, list):
+        assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize("cat_age, dog_age, expected", [
+    (-1, 3, ValueError),
+    (3, -1, ValueError),
+])
+def test_cat_and_dog_age_are_not_negative_values(
         cat_age: int,
         dog_age: int,
         expected: list or type) -> None:
