a16c62a2be3378a9052c6a61a37e36caa2b8570d
Oksana Makieieva <oksana.zhornyk@gmail.com>
2023-05-22 12:15:17 +0200

    py-cat-and-dogs-v3

--- app/test_main.py
+++ app/test_main.py
@@ -19,6 +19,7 @@ from app.main import get_human_age
         (-15, -15, [0, 0]),
         (28, -28, [3, 0]),
         (14.5, 23.2, [0, 1]),
+        (30, True, [3, 0])
     ],
     ids=[
         "function should return 0 if the age of the animals is 0",
@@ -34,7 +35,26 @@ from app.main import get_human_age
         "function should return 0 if input less then 0",
         "function should return 0 if input less then 0",
         "function should work even if input is float type",
+        "function should return 0 if input is bool"
     ]
 )
 def test_get_human_age(cat_years: int, dog_years: int, result: list) -> None:
     assert get_human_age(cat_years, dog_years) == result
+
+
+@pytest.mark.parametrize(
+    "cat_years,dog_years",
+    [
+        ("12", 15),
+        (23, [10, 20]),
+        (None, 15),
+        ({"cat": 15}, {"dog": 18}),
+        (23, {23})
+    ]
+)
+def test_should_return_error_if_input_has_wrong_type(
+        cat_years: str | int | dict[str, int] | None,
+        dog_years: int | list[int] | dict[str, int] | set[int]
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_years, dog_years)
