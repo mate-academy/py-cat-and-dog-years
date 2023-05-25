a2f2f2e84f8f0282af65e408fc64f2687041152e
Vladyslav <vladyslavkhokhlov@gmail.com>
2023-05-25 10:36:53 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,26 +5,69 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, goals",
     [
-        pytest.param(-1, -1, [0, 0], id="should return first 15 dog and cat years in human year"),
-        pytest.param(14, 14, [0, 0], id="should return first 15 dog and cat years in human year"),
-        pytest.param(28, 29, [3, 3], id="should return every 5 next dog year in human year"),
-        pytest.param(15, 15, [1, 1], id="should return the next 9 dog and cat years in human year"),
-        pytest.param(28, 28, [3, 2], id="should return every 4 next cat year in human year")
-    ]
+        pytest.param(
+            -1,
+            -1,
+            [0, 0],
+            id="should return first 15 dog and cat years in human year"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="should return first 15 dog and cat years in human year"
+        ),
+        pytest.param(
+            28,
+            29,
+            [3, 3],
+            id="should return every 5 next dog year in human year"
+        ),
+        pytest.param(
+            15,
+            15,
+            [1, 1],
+            id="should return the next 9 dog and cat years in human year",
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="should return every 4 next cat year in human year"
+        ),
+    ],
 )
-def test_cat_and_dog_years_in_human_years_correctly(cat_age: int, dog_age: int, goals: list) -> None:
+def test_cat_and_dog_years_in_human_years_correctly(
+    cat_age: int, dog_age: int, goals: list
+) -> None:
     assert get_human_age(cat_age, dog_age) == goals
 
 
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected_error",
     [
-        pytest.param('1', 1, TypeError, id="should raise error when cat age is a string"),
-        pytest.param([1], 1, TypeError, id="should raise error when cat age is a list"),
-        pytest.param(1, [1], TypeError, id="should raise error when dog age is a list"),
-        pytest.param(1, '1', TypeError, id="should raise error when dog age is a string")
-    ]
+        pytest.param(
+            "1",
+            1,
+            TypeError,
+            id="should raise error when cat age is a string"
+        ),
+        pytest.param([1],
+                     1,
+                     TypeError,
+                     id="should raise error when cat age is a list"),
+        pytest.param(1,
+                     [1],
+                     TypeError,
+                     id="should raise error when dog age is a list"),
+        pytest.param(1,
+                     "1",
+                     TypeError,
+                     id="should raise error when dog age is a string"),
+    ],
 )
-def test_raising_errors_correctly(cat_age: int, dog_age: int, expected_error) -> None:
-    with pytest.raises(expected_error):
+def test_raising_errors_correctly(cat_age: int,
+                                  dog_age: int,
+                                  expected_error: Exception) -> None:
+    with pytest.raises(TypeError):
         assert get_human_age(cat_age, dog_age)
