9b85011896f4289baa6867500e15bebbed6772c1
Andrii Kuzmenkov <kuzmenkov.andr@gmail.com>
2023-05-29 18:23:50 +0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -9,32 +9,32 @@ class TestGetHumanAge:
         "cat_age,dog_age,expected_result",
         [
             pytest.param(
-                14, 14, [0, 0], id="should return 0 if pets age unger first year threshold"
+                14, 14, [0, 0], id="should return 0 if pets age below 15"
             ),
             pytest.param(
-                15, 15, [1, 1], id="should return 1 if pets age at first year threshold"
+                15, 15, [1, 1], id="should return 1 if pets age is 15"
             ),
             pytest.param(
-                23, 23, [1, 1], id="should return 1 if pets age unger second year threshold"
+                23, 23, [1, 1], id="should return 1 if pets age below 24"
             ),
             pytest.param(
-                24, 24, [2, 2], id="should return 2 if pets age at second year threshold"
+                24, 24, [2, 2], id="should return 2 if pets age is 24"
             ),
             pytest.param(
-                27, 28, [2, 2], id="should return 2 if pets age unger third year threshold"
+                27, 28, [2, 2], id="should return 2 if cat age below 28 and dog age below 29"
             ),
             pytest.param(
-                28, 29, [3, 3], id="should return 3 if pets age at third year threshold"
+                28, 29, [3, 3], id="should return 3 if cat age is 28 and dog age is 29"
             ),
             pytest.param(
-                34, 45, [4, 6], id="should return correct age if pets age above third year threshold"
+                34, 45, [4, 6], id="should return correct age if cat age above 28 and dog age above 29"
             ),
         ]
     )
     def test_get_human_age_correctly(
             self,
-            cat_age,
-            dog_age,
-            expected_result
+            cat_age: int,
+            dog_age: int,
+            expected_result: list
     ) -> None:
         assert get_human_age(cat_age, dog_age) == expected_result
