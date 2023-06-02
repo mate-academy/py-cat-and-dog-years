f8c17065e8f80f515e8a2591385b535053754be4
Dmytro Kolodii <dimakolodiy.ua@gmail.com>
2023-06-02 23:04:13 +0300

    Just better solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,38 +1,50 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_age_below_15_should_be_zero() -> None:
-    assert (
-        get_human_age(0, 0) == [0, 0]
-    ), "Only first 15 dog/cat years give 1 human year"
-    assert (
-        get_human_age(14, 14) == [0, 0]
-    ), "Only first 15 dog/cat years give 1 human year"
-
-
-def test_from_15_years_to_24() -> None:
-    assert (
-        get_human_age(15, 15) == [1, 1]
-    ), "Only next 9 dog/cat years after 15 give 1 more year"
-    assert (
-        get_human_age(23, 23) == [1, 1]
-    ), "Only next 9 dog/cat years after 15 give 1 more year"
-    assert (
-        get_human_age(24, 24) == [2, 2]
-    ), "Only next 9 dog/cat years after 15 give 1 more year"
-
-
-def test_after_24_years() -> None:
-    assert (
-        get_human_age(27, 27) == [2, 2]
-    ), "Every 5 next dog(4 for cat) years after 24 give 1 extra human year"
-    assert (
-        get_human_age(28, 28) == [3, 2]
-    ), "Every 5 next dog(4 for cat) years after 24 give 1 extra human year"
-    assert (
-        get_human_age(28, 29) == [3, 3]
-    ), "Every 5 next dog(4 for cat) years after 24 give 1 extra human year"
-
-    assert (
-        get_human_age(100, 100) == [21, 17]
-    ), "Every 5 next dog(4 for cat) years after 24 give 1 extra human year"
+# write your code here
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        pytest.param(
+            -1,
+            -1,
+            [0, 0],
+            id="Should return 0 if animals have negative number"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="Only first 15 dog/cat years give 1 human year"
+        ),
+        pytest.param(
+            23,
+            23,
+            [1, 1],
+            id="Only first 15 dog/cat years give 1 human year"
+        ),
+        pytest.param(
+            24,
+            24,
+            [2, 2],
+            id="Only next 9 dog/cat years after 15 give 1 more year"
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="Every 4 years for cat after 24 give 1 extra human year"
+        ),
+        pytest.param(
+            28,
+            29,
+            [3, 3],
+            id="Every 5 years for dog years after 24 give 1 extra human year"
+        ),
+    ],
+)
+def test_cat_and_dog_years_in_human_years_correctly(
+        cat_age: int, dog_age: int, result: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result
