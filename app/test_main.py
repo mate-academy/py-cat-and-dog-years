ff0546b631b3bdf89240581deea5a9508274a69b
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-06-02 22:50:49 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -9,15 +9,15 @@ from main import get_human_age
                      "human year"),
      pytest.param(28, 29, [3, 3],
                   id="should return every 5 next dog and cat years in "
-                    "human years"),
+                     "human years"),
      pytest.param(27, 28, [2, 2],
                   id="should return every 5 next dog and cat years in "
-                    "human years"),
+                     "human years"),
      pytest.param(23, 23, [1, 1],
                   id="should return next 9 dog and cat years in human year"),
      pytest.param(24, 24, [2, 2],
                   id="should return every 5 next dog and cat years in "
-                    "human years"),
+                     "human years"),
      pytest.param(15, 15, [1, 1],
                   id="should return next 9 dog and cat years in human year")])
 def test_cat_and_dog_years_in_human_years_correctly(
