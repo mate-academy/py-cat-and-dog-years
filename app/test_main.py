e2bf730376e863e074926a2a6f19ad1c9a07565a
Serhii <85514036+volodinsy@users.noreply.github.com>
2023-06-03 21:24:35 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -19,16 +19,13 @@ from app.main import get_human_age
                   id="should return every 5 next dog and cat years in "
                      "human years"),
      pytest.param(15, 15, [1, 1],
-                  id="should return next 9 dog and cat years in human year")])
+                  id="should return next 9 dog and cat years in human year"),
+     pytest.param(-1, 1, 0, id="should return 0 in human year")])
 def test_cat_and_dog_years_in_human_years_correctly(
         cat_age: int,
         dog_age: int,
         expected_result: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected_result
-
-    @pytest.mark.parametrize(
-        "cat_age, dog_age, expected_error",
-        [pytest.param(-1, 1, 0, id="should return 0 in human year")])
 
 
 @pytest.mark.parametrize(
