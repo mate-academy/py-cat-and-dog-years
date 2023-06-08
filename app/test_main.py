b256803c6373c94588177dccb3d8f73984b893d7
Soloveyk1n <swain.volt@gmail.com>
2023-06-08 21:21:18 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,37 +1,57 @@
 from app.main import get_human_age
 
-
-def test_should_equal_to_zero() -> None:
-    cat_to_human = 0
-    dog_to_human = 0
-    assert get_human_age(cat_to_human, dog_to_human) == [0, 0]
+import pytest
 
 
-def test_one_human_year_should_equal_below_15_year_of_both() -> None:
-    cat_to_human = 14
-    dog_to_human = 14
-    assert get_human_age(cat_to_human, dog_to_human) == [0, 0]
+@pytest.mark.parametrize(
+    "initial_age, expect_age",
+    [
+        pytest.param(
+            [0, 0],
+            [0, 0],
+            id="should equal to zero"
+        ),
+        pytest.param(
+            [14, 14],
+            [0, 0],
+            id="one human year should equal below 15 year of both"
+        ),
+        pytest.param(
+            [15, 15],
+            [1, 1],
+            id="one human year should equal 15 year of both"
+        ),
+        pytest.param(
+            [23, 23],
+            [1, 1],
+            id="one human year should equal below 24 year of both"
+        ),
+        pytest.param(
+            [24, 24],
+            [2, 2],
+            id="second human year should add 9 years of both"
+        ),
+        pytest.param(
+            [28, 29],
+            [3, 3],
+            id="next human years should add 4 cat years and 5 dog years"
+        )
+    ]
+)
+def test_age_correctly(initial_age: list, expect_age: list) -> None:
+    assert get_human_age(initial_age[0], initial_age[1]) == expect_age
 
 
-def test_one_human_year_should_equal_15_year_of_both() -> None:
-    cat_to_human = 15
-    dog_to_human = 15
-    assert get_human_age(cat_to_human, dog_to_human) == [1, 1]
-
-
-def test_one_human_year_should_equal_below_24_year_of_both() -> None:
-    cat_to_human = 23
-    dog_to_human = 23
-    assert get_human_age(cat_to_human, dog_to_human) == [1, 1]
-
-
-def test_second_human_year_should_add_9_years_of_both() -> None:
-    cat_to_human = 24
-    dog_to_human = 24
-    assert get_human_age(cat_to_human, dog_to_human) == [2, 2]
-
-
-def test_next_human_years_should_add_4_cat_years_and_5_dog_years() -> None:
-    cat_to_human = 28
-    dog_to_human = 29
-    assert get_human_age(cat_to_human, dog_to_human) == [3, 3]
+@pytest.mark.parametrize(
+    "initial_age, expect_error",
+    [
+        pytest.param(
+            "abs",
+            TypeError,
+            id="should raise error when parameter is not int"
+        ),
+    ]
+)
+def test_raising_error(initial_age: list, expect_error: any) -> None:
+    with pytest.raises(expect_error):
+        assert get_human_age(initial_age)
