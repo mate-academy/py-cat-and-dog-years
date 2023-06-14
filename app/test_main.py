839c2b8b7726391937ae7c6f97c7bd27e5893616
YOUR_NAME <YOUR_EMAIL>
2023-06-14 18:16:11 +0300

    Cat and Dog age(added check for negative and very large value):

--- app/test_main.py
+++ app/test_main.py
@@ -13,7 +13,8 @@ from app.main import get_human_age
         pytest.param(100, 100, [21, 17], id="both ages greater than 28"),
         pytest.param(15, 0, [1, 0], id="cat equal to 15, dog below 15"),
         pytest.param(0, 15, [0, 1], id="cat below 15, dog age to 15"),
-        pytest.param(10**10, 10**20, [2499999996, 19999999999999999997], id="big numbers"),
+        pytest.param(10**10, 10**20, [2499999996, 19999999999999999997],
+                     id="big numbers"),
         pytest.param(25, -50, [2, 0], id="negative dog age"),
         pytest.param(-2, 35, [0, 4], id="negative cat age"),
         pytest.param(-2, -1, [0, 0], id="both negative age"),
@@ -21,4 +22,3 @@ from app.main import get_human_age
 )
 def test_get_human_age(cat_age: int, dog_age: int, expected: int) -> None:
     assert get_human_age(cat_age, dog_age) == expected
-
