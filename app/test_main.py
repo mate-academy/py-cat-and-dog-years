d55907a1804eb3cdea532ceeacf1b29470e5c78d
Tarasidze <dtar84@ukr.net>
2023-06-14 12:30:39 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,49 @@
+import pytest
+
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestCatAndDogYears:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, exp_result",
+        [
+            pytest.param(0, 0, [0, 0],
+                         id="Should return zero values"),
+            pytest.param(14, 14, [0, 0],
+                         id="Values less than cat/gog one year"),
+            pytest.param(23, 23, [1, 1],
+                         id="Values less than cat/gog two year"),
+            pytest.param(27, 27, [2, 2],
+                         id="Values less than cat/gog three year"),
+            pytest.param(28, 28, [3, 2],
+                         id="Values cat has three year and dog four"),
+            pytest.param(28, 28, [3, 2],
+                         id="Values cat has three year and dog four"),
+            pytest.param(100, 100, [21, 17],
+                         id="Cat and dog have more than three year"),
+        ]
+    )
+    def test_get_human_age(self,
+                           cat_age: int,
+                           dog_age: int,
+                           exp_result: list) -> None:
+        assert (
+            get_human_age(cat_age, dog_age) == exp_result
+        ), f"Cat:{cat_age} and dog:{cat_age} should return {exp_result}"
+
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_error",
+        [
+            pytest.param("",
+                         17,
+                         TypeError,
+                         id="Should return error then type incorrect")
+        ]
+    )
+    def test_raising_error(self,
+                           cat_age: int,
+                           dog_age: int,
+                           expected_error: type[Exception]) -> None:
+        with pytest.raises(expected_error):
+            get_human_age(cat_age, dog_age)
