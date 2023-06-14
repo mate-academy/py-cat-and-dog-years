93d5234f7a1fad9b4d4a720089d180a96446c838
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-06-14 13:11:55 +0300

    My tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,34 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize("value,result",
+                         [((14, 14), [0, 0]),
+                          ((15, 15), [1, 1]),
+                          ((24, 24), [2, 2]),
+                          ((28, 28), [3, 2]),
+                          ((100, 100), [21, 17])])
+def test_usual_case(value: tuple, result: list) -> None:
+    assert get_human_age(*value) == result
+
+
+@pytest.mark.parametrize("value",
+                         [(0, 0),
+                          (-1, -1),
+                          (0, 0.2),
+                          (0, True)])
+def test_data_out_of_normal_range(value: tuple) -> None:
+    assert get_human_age(*value) == [0, 0]
+
+
+@pytest.mark.parametrize("value",
+                         [(1, "hh"),
+                          (1, []),
+                          (2, {}),
+                          (1, ())])
+def test_wrong_type(value: tuple) -> None:
+    try:
+        get_human_age(*value)
+    except TypeError:
+        pass
