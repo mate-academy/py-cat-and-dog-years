a28a5832c1dd19fba058672c84e9723a68d6d0af
Alex Yatsun <starcrafter4444@gmail.com>
2023-06-14 16:11:31 +0300

    Improvement

--- app/test_main.py
+++ app/test_main.py
@@ -8,8 +8,12 @@ from app.main import get_human_age
     "cat_age,dog_age,expected_human_age",
     [
         pytest.param(
+            -3, -4, [0, 0],
+            id="dog and cat ages lesser than 0 should return 0 human years"
+        ),
+        pytest.param(
             0, 0, [0, 0],
-            id="dog and cat ages equal to 0 should return 0 human years"
+            id="first dog and cat ages should return 0 human years"
         ),
         pytest.param(
             14, 14, [0, 0],
@@ -38,8 +42,16 @@ from app.main import get_human_age
         pytest.param(
             100, 100, [21, 17],
             id="dog and cat ages after 23 should return calculated human years"
+        ),
+        pytest.param(
+            999999999999999999999999999, 9999999999999999999999999,
+            [249999999999999999999999995, 1999999999999999999999997],
+            id="dog and cat ages after 23 should return calculated human years"
         )
     ]
 )
-def test_convert_dog_and_cat_ages_in_human_age_correctly(cat_age,dog_age,expected_human_age):
+def test_convert_dog_and_cat_ages_in_human_age(
+        cat_age: int, dog_age: int, expected_human_age: list[int]
+) -> None:
+    print(cat_age)
     assert get_human_age(cat_age, dog_age) == expected_human_age
