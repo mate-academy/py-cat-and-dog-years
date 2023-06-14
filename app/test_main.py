0753742cf6fe2208810584d840e9f205c2ff6452
Denys Seliverstov <catsperr322@gmail.com>
2023-06-14 15:41:39 +0300

    Fixed

--- app/test_main.py
+++ app/test_main.py
@@ -23,60 +23,39 @@ def test_raise_error_if_value_types_are_incorrect(
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected_result",
     [
-        (2, 8, [0, 0]),
-        (10, 12, [0, 0]),
-        (14, 13, [0, 0])
+        pytest.param(
+            2,
+            8,
+            [0, 0],
+            id="age should be zero when ages below 15"
+        ),
+        pytest.param(
+            16,
+            19,
+            [1, 1],
+            id="age should be one when ages between 15 and 23"
+        ),
+        pytest.param(
+            24,
+            27,
+            [2, 2],
+            id="age should be one when ages between 23 and 28"
+        ),
+        pytest.param(
+            -1,
+            -10,
+            [0, 0],
+            id="age should be zero when ages is negative numbers"
+        ),
+        pytest.param(
+            14,
+            35,
+            [0, 4],
+            id="age in other cases"
+        )
     ]
 )
-def test_human_age_should_be_zero_when_ages_below_15(
-        cat_age: int,
-        dog_age: int,
-        expected_result: list
-) -> None:
-    assert get_human_age(cat_age, dog_age) == expected_result
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected_result",
-    [
-        (16, 19, [1, 1]),
-        (21, 22, [1, 1]),
-        (17, 18, [1, 1])
-    ]
-)
-def test_human_age_should_be_one_when_ages_below_23_and_above_15(
-        cat_age: int,
-        dog_age: int,
-        expected_result: list
-) -> None:
-    assert get_human_age(cat_age, dog_age) == expected_result
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected_result",
-    [
-        (24, 26, [2, 2]),
-        (26, 24, [2, 2]),
-        (25, 25, [2, 2])
-    ]
-)
-def test_human_age_when_ages_above_23_and_below_28(
-        cat_age: int,
-        dog_age: int,
-        expected_result: list
-) -> None:
-    assert get_human_age(cat_age, dog_age) == expected_result
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected_result",
-    [
-        (28, 28, [3, 2]),
-        (100, 100, [21, 17]),
-        (14, 35, [0, 4])
-    ]
-)
-def test_human_age_in_another_cases(
+def test_age_converts_correctly(
         cat_age: int,
         dog_age: int,
         expected_result: list
