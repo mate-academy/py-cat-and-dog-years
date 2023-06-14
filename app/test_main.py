eaa7c3de85f1ea2f36c821da9974562caa3d6358
Oleksii Kolii <igravkompik@gmail.com>
2023-06-14 14:00:10 +0300

    fixed flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -10,7 +10,10 @@ class TestCatsDogsClass:
         (5, 5, [0, 0]),
         (14, 14, [0, 0])
     ])
-    def test_zero_when_age_less_14(self, cat: int, dog: int, expected: list) -> None:
+    def test_zero_when_age_less_14(self,
+                                   cat: int,
+                                   dog: int,
+                                   expected: list) -> None:
         assert get_human_age(cat, dog) == expected
 
     @pytest.mark.parametrize("cat,dog,expected", [
@@ -18,7 +21,10 @@ class TestCatsDogsClass:
         (17, 17, [1, 1]),
         (23, 23, [1, 1])
     ])
-    def test_age_more_than_15(self, cat: int, dog: int, expected: list) -> None:
+    def test_age_more_than_15(self,
+                              cat: int,
+                              dog: int,
+                              expected: list) -> None:
         assert get_human_age(cat, dog) == expected
 
     @pytest.mark.parametrize("cat,dog,expected", [
@@ -33,5 +39,8 @@ class TestCatsDogsClass:
         (28, 28, [3, 2]),
         (100, 100, [21, 17])
     ])
-    def test_age_dog_and_cat_should_be_different(self, cat: int, dog: int, expected: list) -> None:
+    def test_age_dog_and_cat_should_be_different(self,
+                                                 cat: int,
+                                                 dog: int,
+                                                 expected: list) -> None:
         assert get_human_age(cat, dog) == expected
