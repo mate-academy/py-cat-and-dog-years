c6ab452b2610d757b4cfeb8782992c8ee1d73ea1
YOUR_NAME <YOUR_EMAIL>
2023-06-15 09:28:04 +0300

    Cat and Dog age(add: test_raising_errors_correctly, new "id" descriptions):

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 import pytest
 from app.main import get_human_age
 
@@ -5,20 +7,38 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
-        pytest.param(0, 0, [0, 0], id="both ages zero"),
-        pytest.param(14, 14, [0, 0], id="both ages below 15"),
-        pytest.param(23, 23, [1, 1], id="both ages between 15 and 24"),
-        pytest.param(27, 27, [2, 2], id="both ages between 24 and 28"),
-        pytest.param(28, 28, [3, 2], id="cat age greater than dog age"),
-        pytest.param(100, 100, [21, 17], id="both ages greater than 28"),
-        pytest.param(15, 0, [1, 0], id="cat equal to 15, dog below 15"),
-        pytest.param(0, 15, [0, 1], id="cat below 15, dog age to 15"),
-        pytest.param(10**10, 10**20, [2499999996, 19999999999999999997],
-                     id="big numbers"),
-        pytest.param(25, -50, [2, 0], id="negative dog age"),
-        pytest.param(-2, 35, [0, 4], id="negative cat age"),
-        pytest.param(-2, -1, [0, 0], id="both negative age"),
+        pytest.param(0, 0, [0, 0], id="should return 0 when age is 0"),
+        pytest.param(14, 14, [0, 0], id="should return 0 when age < 15"),
+        pytest.param(23, 23, [1, 1], id="should return 1 when 15 <= age < 24"),
+        pytest.param(27, 27, [2, 2], id="should return 2 when 24 <= age < 28"),
+        pytest.param(28, 28, [3, 2], id="should return different result"),
+        pytest.param(100.5, 100.5, [21, 17], id="should return round result"),
+        pytest.param(-5, -15, [0, 0], id="should return 0 when age < 0"),
     ],
 )
 def test_get_human_age(cat_age: int, dog_age: int, expected: int) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        pytest.param(
+            "10", "20",
+            TypeError,
+            id="should raise error when age is str"
+        ),
+        pytest.param(
+            {"Cat": 25}, {"Dog": 10},
+            TypeError,
+            id="should raise error when age is dict"
+        )
+    ]
+)
+def test_raising_errors_correctly(
+        cat_age: int,
+        dog_age: int,
+        expected: Type[Exception]
+) -> None:
+    with pytest.raises(expected):
+        get_human_age(cat_age, dog_age)
