73486ba74f8c953ada4377f6db1f8345dd504903
kovaliskoveronika <veronika.kovalisko.knm.2020@lpnu.ua>
2023-06-19 16:29:22 +0300

    deleted unnecessary test and check if cat_age is also not int

--- app/test_main.py
+++ app/test_main.py
@@ -9,43 +9,32 @@ from app.main import get_human_age
                              (15, 15, [1, 1]),
                              (24, 24, [2, 2]),
                              (28, 28, [3, 2]),
-                             (100, 100, [21, 17])
-                         ],
-                         ids=[
-                             "both age less than 15, expect 0 years",
-                             "both age equal 15, expect 1 ears",
-                             "both age equal 15 + 9, expect 2 years",
-                             "both age equal 15 + 9 + 4, expect 3 cat, dog 2",
-                             "both age equal 100, expect 21 cat, 17 dog"
-                         ])
-def test_usual_case(cat_age: int, dog_age: int, result: list) -> None:
-    assert get_human_age(cat_age, dog_age) == result
-
-
-@pytest.mark.parametrize("cat_age, dog_age, expected_result",
-                         [
+                             (100, 100, [21, 17]),
                              (0, 0, [0, 0]),
                              (-1, -1, [0, 0]),
                              (0, 0.2, [0, 0]),
                              (0, True, [0, 0])
                          ],
                          ids=[
+                             "both age less than 15, expect 0 years",
+                             "both age equal 15, expect 1 ears",
+                             "both age equal 15 + 9, expect 2 years",
+                             "both age equal 15 + 9 + 4, expect 3 cat, dog 2",
+                             "both age equal 100, expect 21 cat, 17 dog",
                              "age equal zeros",
                              "age is negative number",
                              "one of age is float",
                              "one of age is bool"
                          ])
-def test_data_out_of_normal_range(cat_age: int,
-                                  dog_age: int,
-                                  expected_result: list) -> None:
-    assert get_human_age(cat_age, dog_age) == [0, 0]
+def test_usual_case(cat_age: int, dog_age: int, result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result
 
 
 @pytest.mark.parametrize("cat_age, dog_age, error",
                          [
                              (1, "hh", TypeError),
                              (1, [], TypeError),
-                             (2, {}, TypeError),
+                             ({}, 2, TypeError),
                              (1, (), TypeError)
                          ],
                          ids=[
@@ -54,7 +43,7 @@ def test_data_out_of_normal_range(cat_age: int,
                              "wrong type dict",
                              "wrong type set"
                          ])
-def test_wrong_type(cat_age: int, dog_age: any, error: any) -> None:
-    if not isinstance(dog_age, int):
+def test_wrong_type(cat_age: any, dog_age: any, error: any) -> None:
+    if not isinstance(dog_age, int) or not isinstance(cat_age, int):
         with pytest.raises(error):
             get_human_age(cat_age, dog_age)
