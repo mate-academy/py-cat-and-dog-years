9cce21ac052e921ab657aa3afe2dab88b1a0bdc2
Vlad Dukh <vladdukh15@gmail.com>
2023-06-21 07:46:41 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,3 @@
-from __future__ import annotations
-
 import pytest
 from app.main import get_human_age
 
@@ -17,36 +15,36 @@ class TestCatDogYears:
             (27, 27, [2, 2]),
             (28, 28, [3, 2]),
             (100, 100, [21, 17]),
+        ],
+        ids=[
+            "Negative age",
+            "Zero age",
+            "Same age",
+            "Small age",
+            "Medium age",
+            "Medium age (next)",
+            "Big age",
+            "Big age (next)",
+            "Very big age"
         ]
     )
     def test_human_age_is_correct(
-            self,
-            cat_age: int,
+            self, cat_age: int,
             dog_age: int,
-            result: list
-    ) -> None:
-
+            result: list) -> None:
         assert get_human_age(cat_age, dog_age) == result
 
     @pytest.mark.parametrize(
         "cat_age,dog_age,expected_error",
         [
-            (
-                "one",
-                "twenty",
-                TypeError),
-            (
-                [1],
-                [10],
-                TypeError
-            )
+            ("one", "twenty", TypeError),
+            ([1], [10], TypeError)
         ]
     )
     def test_raising_errors_correctly(
             self,
             cat_age: int,
             dog_age: int,
-            expected_error: type[Exception]
-    ) -> None:
+            expected_error: Exception) -> None:
         with pytest.raises(expected_error):
             get_human_age(cat_age, dog_age)
