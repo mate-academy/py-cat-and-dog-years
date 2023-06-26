bb55cda7b025f2c95fda34eb3a41c2d1ecf31f47
Vlad <vladislav.zhyla@gmail.com>
2023-06-26 18:11:11 +0300

    1. Added check for input in not normal range 2. fixed check for incorrect input

--- app/test_main.py
+++ app/test_main.py
@@ -22,5 +22,23 @@ def test_get_human_age(
 
 
 def test_get_human_age_with_incorrect_input() -> None:
-    with pytest.raises(TypeError):
+    with pytest.raises(TypeError) as error:
         get_human_age("bob", False)
+    assert error.type == TypeError
+
+
+@pytest.mark.parametrize(
+    "cat_years,dog_years,result",
+    [
+        (-100, 20, [0, 1]),
+        (20, -100, [1, 0]),
+        (0, 0, [0, 0]),
+        (999999, 999999, [249995, 199997]),
+    ]
+)
+def test_get_human_age_in_big_range(
+        cat_years: int,
+        dog_years: int,
+        result: int
+) -> None:
+    assert get_human_age(cat_years, dog_years) == result
