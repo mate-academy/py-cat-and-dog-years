31f2d380a133023767a46debd2adf32b788bbc88
Nazar Lytvyniuk <nazarltvnk@icloud.com>
2023-06-26 17:20:39 +0200

    Add more tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,6 @@
 import pytest
 
+from typing import Any
 from app.main import get_human_age
 
 
@@ -14,7 +15,10 @@ from app.main import get_human_age
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
         (29, 29, [3, 3]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
+        (32, 56, [4, 8]),
+        (-2, 78, [0, 12]),
+        (46, -7, [7, 0])
     ]
 )
 def test_should_return_correct_ages(
@@ -24,3 +28,22 @@ def test_should_return_correct_ages(
 ) -> None:
 
     assert get_human_age(cat_age, dog_age) == expected_human_ages
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ([], 12),
+        ("cat", 4),
+        (6, []),
+        (5, "dog")
+    ]
+)
+def test_should_raise_type_error(
+        cat_age: Any,
+        dog_age: Any
+) -> None:
+
+    with pytest.raises(Exception) as error:
+        get_human_age(cat_age, dog_age)
+    assert error.type == TypeError
