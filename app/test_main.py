ada49f754f7bdd4adf1de8db80074692acfe1117
Vitaliy Perehrest <vit.perehrest@gmail.com>
2023-06-27 18:18:04 +0300

    test_cat_and_dog_years

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,11 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_a_list_with_the_same_age_of_cat_and_dog() -> None:
+    goals = get_human_age(27, 27)
+    assert goals == [2, 2]
+
+
+def test_should_return_a_list_with_the_different_age_of_cat_and_dog() -> None:
+    goals = get_human_age(100, 100)
+    assert goals == [21, 17]
