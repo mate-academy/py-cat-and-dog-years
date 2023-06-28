61719653c1c689dacaa636bd669410f6fcda97e2
stepband <vladshvik@gmail.com>
2023-06-28 21:22:18 +0300

    created main structure for tests, and a couple of them

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,27 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+class TestRightValue:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_list",
+        [
+            pytest.param(
+                0, 0, [0, 0], id="test should return zero"
+            ),
+            pytest.param(
+                10, 10, [0, 0], id="ages below 15 years"
+            ),
+            pytest.param(
+                15, 15, [1, 1], id="cat and dog minimum age for firs year"
+            ),
+            pytest.param(
+                23, 23, [1, 1], id="cat and dog age are equal for second year"
+            )
+        ]
+    )
+    def test_check_value_result(self, cat_age, dog_age, expected_list):
+        assert get_human_age(cat_age, dog_age) == expected_list
+
+
+
