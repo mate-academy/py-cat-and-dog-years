c9a68c4a13df96d24b871e63d76d5f5d103179c2
Vitaliy Perehrest <vit.perehrest@gmail.com>
2023-06-29 15:12:31 +0300

    test_cat_and_dog_years(fix)

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,29 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_should_return_a_list_with_the_same_age_of_cat_and_dog() -> None:
-    goals = get_human_age(27, 27)
-    assert goals == [2, 2]
+@pytest.mark.parametrize("cat_age, dog_age, expected_cat_human_age", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (23, 23, [1, 1]),
+    (24, 24, [2, 2]),
+    (27, 27, [2, 2]),
+    (28, 28, [3, 2]),
+    (100, 100, [21, 17])
+])
+def test_get_human_age(cat_age, dog_age, expected_cat_human_age) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_cat_human_age
 
 
-def test_should_return_a_list_with_the_different_age_of_cat_and_dog() -> None:
-    goals = get_human_age(100, 100)
-    assert goals == [21, 17]
+@pytest.mark.parametrize("cat_age, dog_age", [
+    (17, "19"),
+    ("3", 10),
+    ([5], 22),
+    (9, {"age": 7})
+])
+def test_should_raise_error_if_cat_and_dog_age_is_not_correct_type(cat_age, dog_age) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
+
+
