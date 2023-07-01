0a9556ad4081e323a86a1172ecf6e511a8985c64
Alek Ilchenko <yaichenko87@gmail.com>
2023-07-01 20:09:11 +0100

    Fixed flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,33 @@
 from app.main import get_human_age
 
 
-def test_dog_zero_cat_zero():
+def test_dog_zero_cat_zero() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_zero_years_end():
+def test_zero_years_end() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
 
-def test_first_years_begin():
+def test_first_years_begin() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
-def test_first_years_end():
+
+def test_first_years_end() -> None:
     assert get_human_age(23, 23) == [1, 1]
 
-def test_second_years_begin():
+
+def test_second_years_begin() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
 
-def test_second_years_end():
+def test_second_years_end() -> None:
     assert get_human_age(27, 27) == [2, 2]
 
 
-def test_cat_thirt_dog_end_of_second():
+def test_cat_thirt_dog_end_of_second() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
 
-def test_correct_calculation():
+def test_correct_calculation() -> None:
     assert get_human_age(100, 100) == [21, 17]
