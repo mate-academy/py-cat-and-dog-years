f15927b6b52a04ea188d5b615bb459ddd0659fd5
Yaroslav Lysiuk <quiet.klirik@gmail.com>
2023-07-05 15:57:43 +0300

    Changes

--- app/test_main.py
+++ app/test_main.py
@@ -6,28 +6,36 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age,dog_age,expected_result",
     [
-        pytest.param(
-            2, 14, [0, 0],
-            id="should return 0 if pet age is less than 15"
-        ),
-        pytest.param(
-            23, 15, [1, 1],
-            id="should return 1 if pet age is >= 15 and < 24"
-        ),
-        pytest.param(
-            24, 24, [2, 2],
-            id="should return 2 when pet is reach 24 years old"
-        ),
-        pytest.param(
-            112, 52, [24, 7],
-            id=("should raise to 1 for every extra years "
-                "(4 years for cat and 5 for dog)")
-        ),
-        pytest.param(
-            14.3, 23.13, [0, 1],
-            id="should round down the float ages"
-        ),
+        (2, 14, [0, 0]),
+        (23, 15, [1, 1]),
+        (24, 24, [2, 2]),
+        (112, 52, [24, 7]),
+        (14.3, 23.13, [0, 1]),
+        (0, 0, [0, 0]),
+        (-12, -3, [0, 0]),
     ]
 )
-def test_ages(cat_age: int, dog_age: int, expected_result: list) -> None:
+def test_values(
+        cat_age: int,
+        dog_age: int,
+        expected_result: list
+) -> None:
     assert get_human_age(cat_age, dog_age) == expected_result
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+    [
+        ("1", 1, TypeError),
+        (0, dict(), TypeError),
+        ((12,), 13, TypeError),
+        (33, complex(3, 12), TypeError)
+    ]
+)
+def test_exceptions(
+        cat_age: int,
+        dog_age: int,
+        expected_error: Exception
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
