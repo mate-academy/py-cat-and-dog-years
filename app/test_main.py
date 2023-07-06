c4a790a8761f0c79c0798dd7119a8dbede3a44f7
Alex Matiash <matiash.jr@gmail.com>
2023-07-06 10:41:17 +0300

    'Solution2'

--- app/test_main.py
+++ app/test_main.py
@@ -1,27 +1,31 @@
+import pytest
 from app.main import get_human_age
 
 
-class TestGetHumanAge:
-    def test_no_arguments(self) -> None:
-        assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize("cat_age, dog_age, expected_cat_human_age", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (23, 23, [1, 1]),
+    (24, 24, [2, 2]),
+    (27, 27, [2, 2]),
+    (28, 28, [3, 2]),
+    (100, 100, [21, 17])
+])
+def test_get_human_age(cat_age: int, dog_age: int,
+                       expected_cat_human_age: list
+                       ) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_cat_human_age
 
-    def test_less_one_year(self) -> None:
-        assert get_human_age(14, 14) == [0, 0]
 
-    def test_one_year(self) -> None:
-        assert get_human_age(15, 15) == [1, 1]
-
-    def test_second_years(self) -> None:
-        assert get_human_age(23, 23) == [1, 1]
-
-    def test_two_years(self) -> None:
-        assert get_human_age(24, 24) == [2, 2]
-
-    def test_three_years(self) -> None:
-        assert get_human_age(28, 29) == [3, 3]
-
-    def test_cat_three_dog_end_of_second(self) -> None:
-        assert get_human_age(28, 28) == [3, 2]
-
-    def test_old_cat_and_dog(self) -> None:
-        assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize("cat_age, dog_age", [
+    (18, "21"),
+    ("7", 11),
+    ([5], 21),
+    (9, {"age": 6})
+])
+def test_incorrect_type(
+        cat_age: int,
+        dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
