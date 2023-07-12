3acd2141e62f80ada5b89aad9988bcf9dcf09012
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-07-12 15:41:06 +0300

    Revert "Implemented Cat-and-dog-years task"
    This reverts commit aedb510b14f6efcfc6a31f57d5cc9cc48140dba8.

--- app/test_main.py
+++ app/test_main.py
@@ -1,28 +1,3 @@
-import pytest
 from app.main import get_human_age
 
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected_human_age",
-    [
-        (0, 14, [0, 0]),
-        (15, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 28, [2, 2]),
-        (28, 29, [3, 3]),
-    ],
-    ids=[
-        "0 cat age and 14 dog age converted into 0 human years",
-        "15 cat age and 23 dog age converted into 1 human years",
-        "24 cat and dog age converted into 2 human years",
-        "27 cat age and 28 dog age converted into 2 human years",
-        "28 cat age and 29 dog age converted into 3 human years"
-    ]
-
-)
-def test_get_human_age(
-        cat_age: int,
-        dog_age: int,
-        expected_human_age: list[int]
-) -> None:
-    assert get_human_age(cat_age, dog_age) == expected_human_age
+# write your code here
