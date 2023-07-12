a23bd4b252265e6e2496f310bc0482ce4bc2fbeb
Oleksiy Lyashenko <oleksij.lyashenko@gmail.com>
2023-07-12 20:17:42 +0300

    - added tests with zero, negative numbers, huge numbers

--- app/test_main.py
+++ app/test_main.py
@@ -8,6 +8,21 @@ class TestGetHumanAge:
         "cat_age,dog_age,human_age",
         [
             pytest.param(
+                -5,
+                15,
+                [0, 1],
+                id="negative cat/dog age turn into to 0"
+            ),
+            pytest.param(
+                0,
+                0,
+                [0, 0],
+                id=(
+                    "from 0 to 14 cat/dog years "
+                    "should convert into 0 human age."
+                )
+            ),
+            pytest.param(
                 14,
                 14,
                 [0, 0],
@@ -32,18 +47,6 @@ class TestGetHumanAge:
                 id="24 cat/dog years should convert into 2 human age."
             ),
             pytest.param(
-                24,
-                24,
-                [2, 2],
-                id="24 cat/dog years should convert into 2 human age."
-            ),
-            pytest.param(
-                27,
-                28,
-                [2, 2],
-                id="27/28 cat/dog years should convert into 2 human age."
-            ),
-            pytest.param(
                 27,
                 28,
                 [2, 2],
@@ -55,6 +58,16 @@ class TestGetHumanAge:
                 [3, 3],
                 id="28/29 cat/dog years should convert into 3 human age."
             ),
+            pytest.param(
+                100,
+                100,
+                [21, 17]
+            ),
+            pytest.param(
+                155,
+                208,
+                [34, 38]
+            )
         ]
     )
     def test_finds_human_age_correctly(
