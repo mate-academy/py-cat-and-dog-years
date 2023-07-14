6cdfeb0f655bbc6c1e0d9abfa438cd3bc7edf94f
Oleksii Kiva <oleksii.kiva@gmail.com>
2023-07-14 14:50:51 +0300

    Added tests for negative numbers v.1

--- app/test_main.py
+++ app/test_main.py
@@ -10,13 +10,19 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (27, 28, [2, 2]),
         (28, 29, [3, 3]),
+        (-28, 14, [0, 0]),
+        (14, -29, [0, 0]),
+        (-28, -29, [0, 0])
     ],
     ids=[
         "0 cat age and 14 dog age converted into 0 human years",
         "15 cat age and 23 dog age converted into 1 human years",
         "24 cat and dog age converted into 2 human years",
         "27 cat age and 28 dog age converted into 2 human years",
-        "28 cat age and 29 dog age converted into 3 human years"
+        "28 cat age and 29 dog age converted into 3 human years",
+        "negative cat age are return 0 human years",
+        "negative dog age are return 0 human years",
+        "negative cat and dog age are return 0 human years"
     ]
 
 )
