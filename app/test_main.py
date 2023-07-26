98f18d4ae8e83c7a6e745ebb7b199beea4b005f7
Yegor Turenko <ytferma@gmail.com>
2023-07-26 14:20:53 +0300

    v1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_convert_less_than_15_years_to_zero() -> None:
+
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_convert_next_9_years_to_1() -> None:
+
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_return_same_value_up_to_28_years_old() -> None:
+    assert get_human_age(27, 27)[0] == get_human_age(27, 27)[1]
+
+
+def test_should_return_different_value_after_next_4_years() -> None:
+
+    assert get_human_age(28, 28)[0] != get_human_age(28, 28)[1]
