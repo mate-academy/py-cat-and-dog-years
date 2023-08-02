88171de54e0b24599f9c53be630b0b89d9cd9026
H1kki <vetalkuz102@gmail.com>
2023-08-02 22:58:50 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,41 +1,38 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_both_zero_age() -> None:
-    cat_age, dog_age = 0, 0
-    assert get_human_age(cat_age, dog_age) == [0, 0]
+@pytest.mark.parametrize("cat_age, dog_age, expected", [
+    pytest.param(0, 0, [0, 0], id="both_zero_age"),
+    pytest.param(14, 14, [0, 0], id="both_under_15_years"),
+    pytest.param(15, 15, [1, 1], id="both_15_years"),
+    pytest.param(23, 23, [1, 1], id="both_just_over_15_years"),
+    pytest.param(24, 24, [2, 2], id="both_exactly_1_more_year"),
+    pytest.param(27, 27, [2, 2], id="both_just_over_1_more_year"),
+    pytest.param(28, 28, [3, 2], id="cat_1_more_year_than_dog"),
+    pytest.param(100, 100, [21, 17], id="both_just_over_2_more_years"),
+])
+def test_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
 
 
-def test_both_under_15_years() -> None:
-    cat_age, dog_age = 14, 14
-    assert get_human_age(cat_age, dog_age) == [0, 0]
+def test_negative_values() -> None:
+    with pytest.raises(ValueError):
+        get_human_age(-5, 10)
 
 
-def test_both_15_years() -> None:
-    cat_age, dog_age = 15, 15
-    assert get_human_age(cat_age, dog_age) == [1, 1]
+def test_zero_values() -> None:
+    assert get_human_age(0, 0) == [0, 0]
 
 
-def test_both_just_over_15_years() -> None:
-    cat_age, dog_age = 23, 23
-    assert get_human_age(cat_age, dog_age) == [1, 1]
+def test_large_values() -> None:
+    assert get_human_age(2 ** 31 - 1, 2 ** 31 - 1) == [1073741858, 858993062]
 
 
-def test_both_exactly_1_more_year() -> None:
-    cat_age, dog_age = 24, 24
-    assert get_human_age(cat_age, dog_age) == [2, 2]
-
-
-def test_both_just_over_1_more_year() -> None:
-    cat_age, dog_age = 27, 27
-    assert get_human_age(cat_age, dog_age) == [2, 2]
-
-
-def test_cat_1_more_year_than_dog() -> None:
-    cat_age, dog_age = 28, 24
-    assert get_human_age(cat_age, dog_age) == [3, 2]
-
-
-def test_both_just_over_2_more_years() -> None:
-    cat_age, dog_age = 100, 100
-    assert get_human_age(cat_age, dog_age) == [21, 17]
+def test_incorrect_type() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("12", 14)
