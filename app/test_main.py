0e814e3be617716494ad7d212c88578401b74c6e
Nazarii Schcur <nazae530@gmail.com>
2023-08-04 12:09:05 +0300

    use parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -3,40 +3,50 @@ import pytest
 from app.main import get_human_age
 
 
-def test_get_human_age() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(23, 23) == [1, 1]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(27, 27) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(100, 100) == [21, 17]
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_human_age",
+        [
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
+            (100, 100, [21, 17])
+        ],
+        ids=[
+            "should return correct value for 0 years old",
+            "should return correct value for 14 years old",
+            "should return correct value for 15 years old",
+            "should return correct value for 23 years old",
+            "should return correct value for 24 years old",
+            "should return correct value for 27 years old",
+            "should return correct value for 28 years old",
+            "should return correct value for 100 years old"
+        ]
+    )
+    def test_convert_to_human_age_correctly(
+            self, cat_age: int, dog_age: int, expected_human_age: list
+    ) -> None:
 
+        assert get_human_age(cat_age, dog_age) == expected_human_age
 
-def test_get_human_age_negative_numbers() -> None:
-    assert get_human_age(-5, 10) == [0, 0]
-    assert get_human_age(10, -5) == [0, 0]
-    assert get_human_age(-5, -5) == [0, 0]
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_error",
+        [
+            ("", "", TypeError),
+            ([], [], TypeError)
+        ],
+        ids=[
+            "should raise an error if parameters are strings",
+            "should raise an error if parameters are lists"
+        ]
+    )
+    def test_should_rise_a_value_error(
+            self, cat_age: int, dog_age: int, expected_error: Exception
+    ) -> None:
 
-
-def test_get_human_age_invalid_data_types() -> None:
-    # Test when one age is provided as a string
-    with pytest.raises(TypeError):
-        get_human_age("10", 15)
-    with pytest.raises(TypeError):
-        get_human_age(10, "15")
-
-    # Test when both ages are provided as strings
-    with pytest.raises(TypeError):
-        get_human_age("10", "15")
-
-    # Test when one age is None
-    with pytest.raises(TypeError):
-        get_human_age(None, 15)
-    with pytest.raises(TypeError):
-        get_human_age(10, None)
-
-    # Test when both ages are None
-    with pytest.raises(TypeError):
-        get_human_age(None, None)
+        with pytest.raises(expected_error):
+            get_human_age(cat_age, dog_age)
