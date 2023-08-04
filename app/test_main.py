7bf9750da77fc9670eb4599dddbffaad865514ed
Nazarii Schcur <nazae530@gmail.com>
2023-08-04 11:26:40 +0300

    add tests for negative numbers and invalid data types + fix flake8

--- app/test_main.py
+++ app/test_main.py
@@ -13,12 +13,14 @@ def test_get_human_age() -> None:
     assert get_human_age(28, 28) == [3, 2]
     assert get_human_age(100, 100) == [21, 17]
 
-def test_get_human_age_negative_numbers():
+
+def test_get_human_age_negative_numbers() -> None:
     assert get_human_age(-5, 10) == [0, 1]
     assert get_human_age(10, -5) == [1, 0]
     assert get_human_age(-5, -5) == [0, 0]
 
-def test_get_human_age_invalid_data_types():
+
+def test_get_human_age_invalid_data_types() -> None:
     with raises(TypeError):
         get_human_age("10", 15)
     with raises(TypeError):
