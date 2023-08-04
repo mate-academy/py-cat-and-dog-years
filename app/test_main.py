af5999bf045052efc96c3d3b8c14e71f09ffa015
Nazarii Schcur <nazae530@gmail.com>
2023-08-04 11:24:34 +0300

    add tests for negative numbers and invalid data types

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,5 @@
+from _pytest.python_api import raises
+
 from app.main import get_human_age
 
 
@@ -10,3 +12,22 @@ def test_get_human_age() -> None:
     assert get_human_age(27, 27) == [2, 2]
     assert get_human_age(28, 28) == [3, 2]
     assert get_human_age(100, 100) == [21, 17]
+
+def test_get_human_age_negative_numbers():
+    assert get_human_age(-5, 10) == [0, 1]
+    assert get_human_age(10, -5) == [1, 0]
+    assert get_human_age(-5, -5) == [0, 0]
+
+def test_get_human_age_invalid_data_types():
+    with raises(TypeError):
+        get_human_age("10", 15)
+    with raises(TypeError):
+        get_human_age(10, "15")
+    with raises(TypeError):
+        get_human_age("10", "15")
+    with raises(TypeError):
+        get_human_age(10.5, 15)
+    with raises(TypeError):
+        get_human_age(10, 15.5)
+    with raises(TypeError):
+        get_human_age(10.5, 15.5)
