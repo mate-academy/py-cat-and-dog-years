ab7c1b013d7efdec3d9c44b811d37f8991493b6c
Alexander Izmashkin <alexandr.izmashkin@gmail.com>
2023-08-06 17:53:07 +0300

    Solution/Alex/2

--- app/test_main.py
+++ app/test_main.py
@@ -14,7 +14,10 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
+        (-1, 1, [0, 0]),
+        (1, -1, [0, 0]),
+        (-1, -1, [0, 0])
     ]
 )
 def test_get_human_age(
@@ -23,3 +26,19 @@ def test_get_human_age(
         human_age: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == human_age
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age",
+    [
+        ("1", 1),
+        (1, "1"),
+        ("1", "1")
+    ]
+)
+def test_get_human_age_raises_exception(
+        cat_age: int,
+        dog_age: int
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
