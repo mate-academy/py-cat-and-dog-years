d37e046c69f040728c36b9b64802115276746f5b
Artem Putsenko <artempucenko3@gmail.com>
2023-08-07 18:21:50 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,28 +1,43 @@
-import json
-from decimal import Decimal
+from typing import Type
+
+from app.main import get_human_age
+import pytest
 
 
-def calculate_profit(file_name: str) -> None:
-    matecoin_account = 0
-    earned_money = 0
-    with open(file_name, "r") as file:
-        trades = json.load(file)
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_result",
+    [(0, 0, [0, 0]),
+     (-1, 0, [0, 0]),
+     (14, 14, [0, 0]),
+     (14, -14, [0, 0]),
+     (15, 15, [1, 1]),
+     (-15, 15, [0, 1]),
+     (23, 23, [1, 1]),
+     (24, 24, [2, 2]),
+     (27, 27, [2, 2]),
+     (28, 28, [3, 2]),
+     (100, 100, [21, 17]),
+     (1000, 1000, [246, 197])]
+)
+def test_if_return_is_correct(
+        cat_age: int,
+        dog_age: int,
+        expected_result: list) -> None:
+    assert expected_result == get_human_age(cat_age, dog_age)
 
-    for trade in trades:
-        if trade["bought"] is not None:
-            earned_money += (Decimal(trade["bought"])
-                             * Decimal(trade["matecoin_price"]))
-            matecoin_account += Decimal(trade["bought"])
 
-        if trade["sold"] is not None:
-            earned_money -= (Decimal(trade["sold"])
-                             * Decimal(trade["matecoin_price"]))
-            matecoin_account -= Decimal(trade["sold"])
-
-    profit = {
-        "earned_money": str(earned_money * (-1)),
-        "matecoin_account": str(matecoin_account)
-    }
-
-    with open("profit.json", "w") as file:
-        json.dump(profit, file, indent=2)
+# write your code here
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+    [([], 0, TypeError),
+     ([-1], 0, TypeError),
+     (14, {14}, TypeError),
+     (14, (14, 23), TypeError),
+     ([15, 15], 0, TypeError)]
+)
+def test_raise_errors_correctly(
+        cat_age: int,
+        dog_age: int,
+        expected_error: Type[BaseException]) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
