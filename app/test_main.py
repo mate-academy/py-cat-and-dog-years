0654c5c4c6f93d0967e149afcb87a26541eec1a2
Bogdan Dovgan <dovganbv94@gmail.com>
2023-08-08 17:24:53 +0300

    Solution fix

--- app/test_main.py
+++ app/test_main.py
@@ -13,12 +13,29 @@ class TestGetHumanAge:
             (24, 24, [2, 2]),
             (27, 27, [2, 2]),
             (28, 28, [3, 2]),
-            (100, 100, [21, 17])
+            (100, 100, [21, 17]),
+            (-5, -5, [0, 0]),
         ]
     )
     def test_age_animal_to_human(self,
                                  cat_age: int,
                                  dog_age: int,
-                                 expected_age: int
+                                 expected_age: list[int]
                                  ) -> None:
         assert get_human_age(cat_age, dog_age) == expected_age
+
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_exception",
+        [
+            ("2", 2, TypeError),
+            (1, "1", TypeError),
+        ]
+    )
+    def test_incorrect_age(self,
+                           cat_age: int,
+                           dog_age: int,
+                           expected_exception: Exception
+                           ) -> None:
+
+        with pytest.raises(expected_exception):
+            get_human_age(cat_age, dog_age)
