0ab545ac7dd428d26eee0da9efae7c6320a0cdf4
jinjo <vitaly.tyagunov@gmail.com>
2023-08-12 20:58:31 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_convert_into_zero() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_convert_into_zero_when_age_less_zero() -> None:
+    assert get_human_age(-1, -1) == [0, 0]
+
+
+def test_should_convert_into_1() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_convert_into_2() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
