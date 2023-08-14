4425309334832eacdb666dec69a4a9c938843054
Anastasia <anastasiiya.p@gmail.com>
2023-08-14 15:24:46 +0300

    Fixed

--- app/test_main.py
+++ app/test_main.py
@@ -1,34 +1,59 @@
+import pytest
 from app.main import get_human_age
 
 
 def test_should_return_list_of_int() -> None:
     result = get_human_age(15, 15)
-    assert (
-        isinstance(result, list)
-        and all(isinstance(element, int) for element in result)
+    assert all(isinstance(val, int) for val in result)
+
+
+class TestCheckNumberCases:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_result",
+        [
+            # test_get_human_age_should_return_zeros_for_zero_ages
+            (
+                0,
+                0,
+                [0, 0]
+            ),
+            # test_get_human_age_should_return_zeros_if_less_than_15
+            (
+                14,
+                14,
+                [0, 0]
+            ),
+            # test_get_human_age_should_check_if_more_than_14_less_than_24
+            (
+                15,
+                15,
+                [1, 1]
+            ),
+            # test_get_human_age_should_check_if_more_than_23_less_than_28
+            (
+                24,
+                24,
+                [2, 2]
+            ),
+            # test_get_human_age_should_check_difference_between_cat_dog_ages
+            (
+                28,
+                28,
+                [3, 2]
+            ),
+            # test_get_human_age_should_check_big_numbers
+            (
+                100,
+                100,
+                [21, 17]
+            )
+        ]
     )
-
-
-def test_get_human_age_should_return_zeros_for_zero_ages() -> None:
-    result = get_human_age(0, 0)
-    assert result == [0, 0]
-
-
-def test_get_human_age_should_return_zeros_if_less_than_15() -> None:
-    result = get_human_age(14, 14)
-    assert result == [0, 0]
-
-
-def test_get_human_age_should_check_if_more_than_14_less_than_24() -> None:
-    result = get_human_age(15, 15)
-    assert result == [1, 1]
-
-
-def test_get_human_age_should_check_if_more_than_23_less_than_28() -> None:
-    result = get_human_age(24, 24)
-    assert result == [2, 2]
-
-
-def test_get_human_age_should_check_difference_between_cat_dog_ages() -> None:
-    result = get_human_age(28, 28)
-    assert result == [3, 2]
+    def test_should_return_correct_values(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_result: list[int]
+    ) -> None:
+        result = get_human_age(cat_age, dog_age)
+        assert result == expected_result
