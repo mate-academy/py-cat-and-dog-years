9d50ae6e7cb65a1b6420d1f59c2795a91a3378dc
jinjo <vitaly.tyagunov@gmail.com>
2023-08-14 17:41:17 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -8,7 +8,8 @@ from app.main import get_human_age
     (15, 15, [1, 1]),
     (24, 24, [2, 2]),
     (27, 27, [2, 2]),
-    (28, 28, [3, 2])
+    (28, 28, [3, 2]),
+    (-1, -3, [0, 0])
 ])
 def test_get_human_age(cat_age: int, dog_age: int,
                        expected_result: list[int]) -> None:
@@ -19,7 +20,10 @@ def test_get_human_age(cat_age: int, dog_age: int,
 @pytest.mark.parametrize("cat_age, dog_age, expected_exception", [
     ("1", "2", TypeError),
     ([1], [2], TypeError),
-    ({1}, {2}, TypeError)
+    ({1}, {2}, TypeError),
+    (1.2, 2.3, TypeError),
+    (True, True, TypeError)
+
 ])
 def test_get_human_age_on_exceptions(cat_age: int, dog_age: int,
                                      expected_exception: Exception) -> None:
