0d447b7c598da370a77a861a6cea26f01175237c
Vladyslav Shepilov <shepsterya@gmail.com>
2023-08-16 23:53:52 +0300

    added error tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pytest
 
 from app.main import get_human_age
@@ -6,7 +8,12 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age,dog_age,expected",
     [
-        pytest.param(0, 0, [0, 0], id="Zero"),
+        pytest.param(0, 0, [0, 0], id="Zero number"),
+        pytest.param(-1, -1, [0, 0], id="Negative number"),
+        pytest.param(14.5, 14.5, [0, 0], id="Float numbers"),
+        # pytest.param("15", "15", TypeError),
+        # pytest.param({"14": 14}, {"14": 14}, [0, 0]),
+        # pytest.param([15], [15], [0, 0]),
         pytest.param(14, 14, [0, 0], id="higher bound 0"),
         pytest.param(15, 15, [1, 1], id="lower bound 1 year"),
         pytest.param(23, 23, [1, 1], id="higher bound 1 year"),
@@ -21,3 +28,19 @@ def test_get_human_age(cat_age: int,
                        expected: list) -> None:
 
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age",
+    (
+        [
+            pytest.param([15], [15], id="List"),
+            pytest.param("15", "15", id="String"),
+            pytest.param({"14": 14}, {"14": 14}, id="Dictionary")
+        ]
+    )
+)
+def test_get_human_age_raise_error(cat_age: Any, dog_age: Any) -> None:
+
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
