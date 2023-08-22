8abfbb229605ffe05669e7691bd459a17ab69ffc
Bohdan Kolomiiets <kb907406@gmail.com>
2023-08-22 13:29:20 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        pytest.param(14, 14, [0, 0], id="Cat/Dog age less than fifteen should be zeros human years"),
+        pytest.param(23, 23, [1, 1], id="Cat/Dog age should convert to one human year"),
+        pytest.param(24, 24, [2, 2], id="Cat/Dog age convert to second human year"),
+        pytest.param(28, 29, [3, 3], id="Cat/Dog check third rule of converting"),
+        pytest.param(100, 100, [21, 17], id="Cat/Dog check longevity"),
+    ],
+)
+def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
+
+    assert get_human_age(cat_age, dog_age) == expected
