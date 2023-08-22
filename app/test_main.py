fcce1fc035742b6830322438457c88921cdfb022
Roman Podolets <romanpyu@gmail.com>
2023-08-22 21:38:02 +0300

    Added tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,42 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_result",
+        [
+            pytest.param(
+                14,
+                14,
+                [0, 0],
+                id="test cat and dog age below first year"
+            ),
+            pytest.param(
+                15,
+                15,
+                [1, 1],
+                id="test cat and dog age equal to first year"
+            ),
+            pytest.param(
+                23,
+                23,
+                [1, 1],
+                id="test cat and dog second year"
+            ),
+            pytest.param(
+                32,
+                32,
+                [4, 3],
+                id="test cat and dog extra years"
+            )
+        ]
+    )
+    def test_should_return_correct_values(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_result: list[int]
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_result
