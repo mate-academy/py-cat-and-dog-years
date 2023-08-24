2dc4b6dfe003376f8ea812e4a2a3db7a6232c329
Yaroslav Skrypchenko <yarik2300@gmail.com>
2023-08-24 22:22:39 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,42 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age, expected_result",
+        [
+            pytest.param(
+                -44,
+                -3,
+                [0, 0],
+                id="should return 0 for both cat and dog ages under zero",
+            ),
+            pytest.param(
+                14, 14, [0, 0], id="check age under 1 for both cat and dog"
+            ),
+            pytest.param(
+                15, 23, [1, 1], id="check age equals 1 for both cat and dog"
+            ),
+            pytest.param(
+                27, 24, [2, 2], id="check age equals 2 for both cat and dog"
+            ),
+            pytest.param(
+                28, 29, [3, 3], id="check age equals 3 for both cat and dog"
+            ),
+            pytest.param(
+                314,
+                314,
+                [74, 60],
+                id="check high age conversion for both cat and dog",
+            ),
+        ],
+    )
+    def test_should_return_correctly_converted_age(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_result: list[int]
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_result
