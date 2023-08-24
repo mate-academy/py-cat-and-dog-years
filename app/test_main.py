bf23c6a008b127a1aa93ced1cd0f29c32e436e52
Vasya Turok <turokvasil0@gmail.com>
2023-08-24 23:05:05 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_zero_for_both() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_one_for_both() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_end_of_first_year() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_return_two_for_both() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_should_return_two_for_both_end_of_second_year() -> None:
+    assert get_human_age(27, 28) == [2, 2]
+
+
+def test_should_return_three_for_both() -> None:
+    assert get_human_age(28, 29) == [3, 3]
