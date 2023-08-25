566086eba2a1f4778c5e04727248a9c264c2e73b
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-08-25 16:04:14 +0300

    py-cat-and-dog-years

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        (27, 27, [2, 2]),
+        (0, 0, [0, 0]),
+        (28, 28, [3, 2]),
+        (22, 12, [1, 0])
+    ]
+)
+def test_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        result: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result
