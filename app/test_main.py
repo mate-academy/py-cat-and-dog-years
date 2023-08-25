2aece5ca9ae82101e2003db518483bea99e8795c
Vasya Turok <turokvasil0@gmail.com>
2023-08-25 14:01:05 +0300

    Solution with parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,18 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_should_return_zero_for_both() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_should_return_one_for_both() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_end_of_first_year() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_should_return_two_for_both() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_should_return_two_for_both_end_of_second_year() -> None:
-    assert get_human_age(27, 28) == [2, 2]
-
-
-def test_should_return_three_for_both() -> None:
-    assert get_human_age(28, 29) == [3, 3]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        pytest.param(14, 14, [0, 0], id="zero for both"),
+        pytest.param(15, 15, [1, 1], id="one for both"),
+        pytest.param(23, 23, [1, 1], id="end of first year"),
+        pytest.param(24, 24, [2, 2], id="two for both"),
+        pytest.param(27, 28, [2, 2], id="end of second year"),
+        pytest.param(28, 29, [3, 3], id="three for both")
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result
