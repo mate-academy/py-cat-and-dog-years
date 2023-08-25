c0a101e94be900ecc32824a1a96be193d1a9d347
Vasya Turok <turokvasil0@gmail.com>
2023-08-25 14:39:31 +0300

    Solution with edits

--- app/test_main.py
+++ app/test_main.py
@@ -11,7 +11,10 @@ from app.main import get_human_age
         pytest.param(23, 23, [1, 1], id="end of first year"),
         pytest.param(24, 24, [2, 2], id="two for both"),
         pytest.param(27, 28, [2, 2], id="end of second year"),
-        pytest.param(28, 29, [3, 3], id="three for both")
+        pytest.param(28, 29, [3, 3], id="three for both"),
+        pytest.param(-2, 29, [0, 3], id="zero for first"),
+        pytest.param(100, 29, [21, 3], id="many years")
+
     ]
 )
 def test_get_human_age(cat_age: int, dog_age: int, result: list) -> None:
