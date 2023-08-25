91b5e0583b08f32256b2fb0a5b39778e0ccfebc3
oreny <orenycdota@gmail.com>
2023-08-25 12:18:57 +0300

    Added type annotation to tests

--- app/test_main.py
+++ app/test_main.py
@@ -20,23 +20,21 @@ from app.main import get_human_age
         pytest.param(
             100, 100, [21, 17], id="Test if big age is calculated correctly"
         ),
-        pytest.param(
-            -5, -5, [0, 0], id="Negative data should return 0"
-        ),
+        pytest.param(-5, -5, [0, 0], id="Negative data should return 0"),
     ],
 )
 def test_get_human_age(
-        cat_age: int, dog_age: int, expected_human_age: list[int]
-):
+    cat_age: int, dog_age: int, expected_human_age: list[int]
+) -> None:
     goals = get_human_age(cat_age, dog_age)
     assert goals == expected_human_age
 
 
-def test_type_error_if_string_is_given():
+def test_type_error_if_string_is_given() -> None:
     with pytest.raises(TypeError):
         get_human_age("18", "four")
 
 
-def test_type_error_if_not_enough_values():
+def test_type_error_if_not_enough_values() -> None:
     with pytest.raises(TypeError):
         get_human_age(1)
