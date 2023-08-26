4349cc660f25c16da9df1f3c141c7d8d6b841e07
anna-lybid <anna.lybid@gmail.com>
2023-08-26 12:42:30 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,40 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        pytest.param(
+            0, 0, [0, 0],
+            id="should check zero age"
+        ),
+        pytest.param(
+            14, 14, [0, 0],
+            id="should check age under 1 human year"
+        ),
+        pytest.param(
+            23, 23, [1, 1],
+            id="should check animal age under 2 human years"
+        ),
+        pytest.param(
+            28, 28, [3, 2],
+            id="should check animal age above 2 human years"
+        ),
+        pytest.param(
+            -1, -9, [0, 0],
+            id="should check negative ages"
+        ),
+        pytest.param(
+            100, 100, [21, 17],
+            id="should check large ages"
+        )
+    ]
+)
+def test_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
