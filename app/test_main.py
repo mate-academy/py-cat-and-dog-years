cd98dcda8a83fbe3cabf6f8bc48034c88a96ff5b
Viktoriia Zaichenko <vikazaibuba@gmail.com>
2023-08-28 20:26:31 +0200

    update

--- app/test_main.py
+++ app/test_main.py
@@ -1,16 +1,43 @@
 import pytest
+
 from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (27, 2, [2, 0]),
-        (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        pytest.param(
+            0, 0, [0, 0],
+            id="zero age should return list with zeros"
+        ),
+        pytest.param(
+            -5, -1, [0, 0],
+            id="negative age should return list with zeros"
+        ),
+        pytest.param(
+            14, 14, [0, 0],
+            id="age less then 15 should return list with zeros"
+        ),
+        pytest.param(
+            15, 15, [1, 1],
+            id="the age of both animals coincides with 1 human year"
+        ),
+        pytest.param(
+            27, 2, [2, 0],
+            id="only one of animal age is less than 1 human year"
+        ),
+        pytest.param(
+            28, 28, [3, 2],
+            id="the same age for animals can return different human age"
+        ),
+        pytest.param(
+            100, 100, [21, 17],
+            id="check the big ages"
+        ),
+        pytest.param(
+            1024, 1024, [252, 202],
+            id="check the big ages"
+        )
     ]
 )
 def test_correct_getting_human_age(
