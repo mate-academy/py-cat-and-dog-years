8db62de721bfe3fd234b3786482ead9c2a7ce116
Yeva <yevabrookman@gmail.com>
2023-08-28 21:16:58 +0300

    added cases and deleted unnecessary tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,6 @@
 import pytest
 
-from app.main import get_human_age, convert_to_human
+from app.main import get_human_age
 
 
 class TestGetHumanAge:
@@ -8,17 +8,33 @@ class TestGetHumanAge:
         "cat_age, dog_age, expected_result",
         [
             pytest.param(
+                -1, -1, [0, 0], id="negative age give 0 human age"
+            ),
+            pytest.param(
+                0, 0, [0, 0], id="should return zero"
+            ),
+            pytest.param(
                 14, 14, [0, 0], id="check cat_dog less than first year"
             ),
             pytest.param(
-                15, 15, [1, 1], id="check cat_dog first year"),
-            pytest.param(
-                23, 24, [1, 2], id="check cat first and dog second year"
+                15, 15, [1, 1], id="check cat_dog first year"
             ),
             pytest.param(
-                28, 29, [3, 3], id="check cat_dog third year"),
+                23, 23, [1, 1], id="check cat/dog first year"
+            ),
             pytest.param(
-                50, 50, [8, 7], id="check cat_dog extra years"),
+                24, 24, [1, 1], id="check cat/dog second year"
+            ),
+            pytest.param(
+                27, 28, [2, 2], id="check cat_dog second year"),
+            pytest.param(
+                28, 28, [3, 2], id="check second cat and third dog year"
+            ),
+            pytest.param(
+                28, 29, [3, 3], id="check cat/dog third year"
+            ),
+            pytest.param(
+                100, 100, [21, 17], id="check cat_dog extra years"),
         ],
     )
     def test_get_human_age(
@@ -27,28 +43,3 @@ class TestGetHumanAge:
         result = get_human_age(cat_age, dog_age)
 
         assert result == expected_result
-
-
-class TestConvertToHuman:
-    @pytest.mark.parametrize(
-        "animal_age, first_year, second_year, each_year, expected_result",
-        [
-            pytest.param(14, 15, 9, 4, 0, id="check age less than first year"),
-            pytest.param(15, 15, 9, 4, 1, id="check first year"),
-            pytest.param(24, 15, 9, 4, 2, id="check second year"),
-            pytest.param(50, 15, 9, 4, 8, id="check extra years"),
-        ],
-    )
-    def test_check_convert_correctly(
-        self,
-        animal_age: int,
-        first_year: int,
-        second_year: int,
-        each_year: int,
-        expected_result: int,
-    ) -> None:
-        result = convert_to_human(
-            animal_age, first_year, second_year, each_year
-        )
-
-        assert expected_result == result
