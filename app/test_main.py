617b5d02190662e204b4010bb25f10a85c823407
Oleh Bohatov <obohatov@gmail.com>
2023-08-28 22:11:14 +0200

    Modified solution

--- app/test_main.py
+++ app/test_main.py
@@ -49,15 +49,35 @@ class TestGetHumanAge:
                 28,
                 28,
                 [3, 2],
+                id="pet's age should convert into 3 for cat and 2 for dog "
+                   "when ages are 28"
+            ),
+            pytest.param(
+                28,
+                29,
+                [3, 3],
+                id="pet's age should correct convert into 3 human years "
+                   "when more or equal 28(29) for cat(dog)"
+            ),
+            pytest.param(
+                28,
+                28,
+                [3, 2],
                 id="pet's age should convert into 3 for cat and 2 for dog"
             ),
             pytest.param(
-                100,
-                100,
-                [21, 17],
+                1000,
+                1000,
+                [246, 197],
                 id=("cat/dog years should correct convert into human age "
                     "when used large pet's ages")
             ),
+            pytest.param(
+                -15,
+                -15,
+                [0, 0],
+                id="should return 0 when ages are negative numbers"
+            )
         ]
     )
     def test_age_conversion(
@@ -67,3 +87,7 @@ class TestGetHumanAge:
             result: list
     ) -> None:
         assert get_human_age(cat_age, dog_age) == result
+
+    def test_incorrect_data(self) -> None:
+        with pytest.raises(TypeError):
+            get_human_age("15", 15)
