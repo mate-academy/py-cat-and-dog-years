c44e5858f39e71625a5b79c9b0afda8e719b73e9
Andrii Kurta <andriykurta97@gmail,com>
2023-08-28 20:57:49 +0200

    Solution after Serhii advices

--- app/test_main.py
+++ app/test_main.py
@@ -5,13 +5,60 @@ from app.main import get_human_age
 class TestConvertAge:
     @pytest.mark.parametrize(
         "age_for_dog, age_for_cat, expected_age_dog_cat", [
-            (-5, -5, [0, 0]),
-            (0, 0, [0, 0]),
-            (14, 14, [0, 0]),
-            (15, 15, [1, 1]),
-            (28, 28, [3, 2]),
-            (50, 50, [8, 7]),
-            (100, 100, [21, 17]),
+            pytest.param(
+                -10,
+                -10,
+                [0, 0],
+                id="Function should convert -10 to 0 for dog or cat"
+            ),
+            pytest.param(
+                0,
+                0,
+                [0, 0],
+                id="Function should convert 0 to 0 for dog or cat"
+            ),
+            pytest.param(
+                14,
+                14,
+                [0, 0],
+                id="Function should convert 14 to 0 for dog or cat"
+            ),
+            pytest.param(
+                15,
+                15,
+                [1, 1],
+                id="Function should convert 15 to 1 for dog or cat"
+            ),
+            pytest.param(
+                23,
+                23,
+                [1, 1],
+                id="Function should convert 23 to 1 for dog or cat"
+            ),
+            pytest.param(
+                24,
+                24,
+                [2, 2],
+                id="Function should convert 24 to 2 for dog or cat"
+            ),
+            pytest.param(
+                27,
+                27,
+                [2, 2],
+                id="Function should convert 27 to 2 for dog or cat"
+            ),
+            pytest.param(
+                28,
+                28,
+                [3, 2],
+                id="Function should convert 28 to 3 for dog and 2 for cat"
+            ),
+            pytest.param(
+                100,
+                100,
+                [21, 17],
+                id="Function should convert 100 to 21 for dog and 17 for cat"
+            ),
         ]
     )
     def test_dof_and_cat_age(
@@ -21,3 +68,42 @@ class TestConvertAge:
             expected_age_dog_cat: list[int]
     ) -> None:
         assert get_human_age(age_for_dog, age_for_cat) == expected_age_dog_cat
+
+
+class TestCatAndDogDataType:
+    @pytest.mark.parametrize(
+        "age_for_dog, age_for_cat, expected_exception", [
+            pytest.param(
+                0,
+                "0",
+                TypeError,
+                id="You can use only 'int' type for age"
+            ),
+            pytest.param(
+                0,
+                [0],
+                TypeError,
+                id="You can use only 'int' type for age"
+            ),
+            pytest.param(
+                0,
+                {0},
+                TypeError,
+                id="You can use only 'int' type for age"
+            ),
+            pytest.param(
+                0,
+                (0,),
+                TypeError,
+                id="You can use only 'int' type for age"
+            ),
+        ]
+    )
+    def test_cat_and_dog_data_type(
+            self,
+            age_for_dog: int,
+            age_for_cat: int,
+            expected_exception: Exception,
+    ) -> None:
+        with pytest.raises(expected_exception):
+            get_human_age(age_for_dog, age_for_cat)
