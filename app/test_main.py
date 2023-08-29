3e41f7da7b5913e49142facbdbc2a5f8729ee221
WoyDiam <freedom.channel2003@gmail.com>
2023-08-29 19:42:55 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,22 +5,24 @@ from app.main import get_human_age
 
 class Tests:
     @pytest.mark.parametrize(
-        "cat_years,dog_years,result",
+        "years,result",
         [
             pytest.param(
-                14,
                 14,
                 [0, 0],
                 id="When dog/cat years < 15, function should be return 0"
             ),
             pytest.param(
                 23,
-                23,
                 [1, 1],
                 id="23 cat/dog years should convert into 1 human age."
             ),
             pytest.param(
-                100,
+                28,
+                [3, 2],
+                id="First change should be at 28 y. for dog/cat"
+            ),
+            pytest.param(
                 100,
                 [21, 17],
                 id="Result should be calculated according to formula"
@@ -28,5 +30,5 @@ class Tests:
         ]
     )
     def test_function(self,
-                      cat_years: int, dog_years: int, result: list) -> None:
-        assert get_human_age(cat_years, dog_years) == result
+                      years: int, result: list) -> None:
+        assert get_human_age(years, years) == result
