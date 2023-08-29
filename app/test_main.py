b7a308d7746ede786650792dfe742fecbf729c31
OlegatorLE <oleh.travin@gmail.com>
2023-08-29 18:49:25 +0300

    added new tests

--- app/test_main.py
+++ app/test_main.py
@@ -4,6 +4,12 @@ from app.main import get_human_age
 
 @pytest.mark.parametrize("cat_age, dog_age, expected", [
     pytest.param(
+        0,
+        0,
+        [0, 0],
+        id="zeros gives zeros"
+    ),
+    pytest.param(
         14,
         14,
         [0, 0],
@@ -22,10 +28,16 @@ from app.main import get_human_age
         id="Both animals under 24 years should give 1 human age"
     ),
     pytest.param(
+        24,
+        24,
+        [2, 2],
+        id="Both animals older 24 years should give 2 human age"
+    ),
+    pytest.param(
         27,
         27,
         [2, 2],
-        id="Both animals under 28 years should give 2 human age"
+        id="Both animals older 27 years should give 2 human age"
     ),
     pytest.param(
         28,
@@ -34,22 +46,28 @@ from app.main import get_human_age
         id="Cat 28, dog 28 should give 3 and 2 human years respectively"
     ),
     pytest.param(
+        29,
+        29,
+        [3, 3],
+        id="Cat 29, dog 29 should give 3 human age"
+    ),
+    pytest.param(
         99,
         99,
         [20, 17],
         id="Both animals 99 years should give 20 and 17 human age respectively"
     ),
     pytest.param(
+        100,
+        100,
+        [21, 17],
+        id="Both animals 100 years should give 21 and 17 human age respectively"
+    ),
+    pytest.param(
         -1,
         -1,
         [0, 0],
         id="negative age gives 0 human age"
-    ),
-    pytest.param(
-        0,
-        0,
-        [0, 0],
-        id="zeros gives zeros"
     ),
 ])
 def test_get_human_age(
