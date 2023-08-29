e3e5d8f19373e38b90cc39567679cd6db47672a3
Oleksandr Moroz <oleksandrmoroz2003@gmail.com>
2023-08-29 19:03:51 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,24 +6,38 @@ from app.main import get_human_age
 
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected", [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
-        (28, 28, [3, 2]),
-        (100, 100, [21, 17])
-    ],
-    ids=[
-        "0 cat/dog years should be 0 human age",
-        "under 15 cat/dog years should be 0 human age",
-        "15 cat/dog years give 1 human year",
-        "under 24 cat/dog years should be 1 human age",
-        "24 cat/dog years give 2 human year",
-        "under 28 cat year and under 29 dog year should be 2 human age",
-        "28 cat years and 28 dog year give 3/2 human years",
-        "100 cat/dog years give 21/17 human years"
+        pytest.param(
+            0, 0, [0, 0],
+            id="0 cat/dog years should be 0 human age"
+        ),
+        pytest.param(
+            14, 14, [0, 0],
+            id="under 15 cat/dog years should be 0 human age"
+        ),
+        pytest.param(
+            15, 15, [1, 1],
+            id="15 cat/dog years give 1 human year"
+        ),
+        pytest.param(
+            23, 23, [1, 1],
+            id="under 24 cat/dog years should be 1 human age"
+        ),
+        pytest.param(
+            24, 24, [2, 2],
+            id="24 cat/dog years give 2 human year"
+        ),
+        pytest.param(
+            27, 27, [2, 2],
+            id="under 28 cat year and under 29 dog year should be 2 human age"
+        ),
+        pytest.param(
+            28, 28, [3, 2],
+            id="28 cat years and 28 dog year give 3/2 human years"
+        ),
+        pytest.param(
+            100, 100, [21, 17],
+            id="100 cat/dog years give 21/17 human years"
+        )
     ]
 )
 def test_get_human_age(
@@ -36,16 +50,22 @@ def test_get_human_age(
 
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected_error", [
-        ("12", 1, TypeError),
-        ([], 1, TypeError),
-        ({}, 1, TypeError),
-        ((), 1, TypeError)
-    ],
-    ids=[
-        "str value instead of int",
-        "list value instead of int",
-        "dict value instead of int",
-        "tuple value instead of int",
+        pytest.param(
+            "12", 1, TypeError,
+            id="str value instead of int"
+        ),
+        pytest.param(
+            [], 1, TypeError,
+            id="list value instead of int"
+        ),
+        pytest.param(
+            {}, 1, TypeError,
+            id="dict value instead of int"
+        ),
+        pytest.param(
+            (), 1, TypeError,
+            id="tuple value instead of int"
+        )
     ]
 )
 def test_incorrect_value(
