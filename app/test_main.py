d38681177ee6b67a44f5131b50456aa197d4af5d
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-08-30 16:56:32 +0300

    py-cat-and-dog-years

--- app/test_main.py
+++ app/test_main.py
@@ -6,10 +6,34 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, result",
     [
-        (27, 27, [2, 2]),
-        (0, 0, [0, 0]),
-        (28, 28, [3, 2]),
-        (22, 12, [1, 0])
+        pytest.param(
+            0, 0, [0, 0],
+            id="0 cat/dog years convert into 0 human years"
+        ),
+        pytest.param(
+            14, 14, [0, 0],
+            id="14 cat/dog years convert into 0 human years"
+        ),
+        pytest.param(
+            15, 15, [1, 1],
+            id="15 cat/dog years convert into 1 human years"
+        ),
+        pytest.param(
+            23, 23, [1, 1],
+            id="23 cat/dog years convert into 1 human years"
+        ),
+        pytest.param(
+            24, 24, [2, 2],
+            id="24 cat/dog years convert into 2 human years"
+        ),
+        pytest.param(
+            27, 28, [2, 2],
+            id="27/28 cat/dog years convert into 2 human years"
+        ),
+        pytest.param(
+            28, 29, [3, 3],
+            id="28/29 cat/dog years convert into 3 human years"
+        )
     ]
 )
 def test_get_human_age(
