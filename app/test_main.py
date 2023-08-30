ad0cfb6b40c5e8a3451df23e43066e04c2cb4f11
anna-lybid <anna.lybid@gmail.com>
2023-08-30 15:20:42 +0300

    Added type tests

--- app/test_main.py
+++ app/test_main.py
@@ -15,11 +15,19 @@ from app.main import get_human_age
             id="should check age under 1 human year"
         ),
         pytest.param(
+            15, 15, [1, 1],
+            id="should check animal age right in 1 human year"
+        ),
+        pytest.param(
             23, 23, [1, 1],
-            id="should check animal age under 2 human years"
+            id="should check animal age above 1 human year"
         ),
         pytest.param(
             28, 28, [3, 2],
+            id="should check animal age right in 2 human years"
+        ),
+        pytest.param(
+            32, 40, [4, 5],
             id="should check animal age above 2 human years"
         ),
         pytest.param(
@@ -38,3 +46,20 @@ def test_get_human_age(
         expected: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+def test_get_human_age_with_incorrect_type() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("cat", 17)
+
+    with pytest.raises(TypeError):
+        get_human_age(25, "dog")
+
+    with pytest.raises(TypeError):
+        get_human_age("cat", "dog")
+
+    with pytest.raises(TypeError):
+        get_human_age(4)
+
+    with pytest.raises(TypeError):
+        get_human_age(17, None)
