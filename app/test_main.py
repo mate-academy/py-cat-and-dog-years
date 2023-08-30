27181e4c077e4d8a5b7a94cde197b7076f61d1b2
Yaroslav Mazurkevych <yaroslavmazurkevych@gmail.com>
2023-08-30 20:16:43 +0300

    py-cat-and-dog-years

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Any
 
 from app.main import get_human_age
 
@@ -33,6 +34,10 @@ from app.main import get_human_age
         pytest.param(
             28, 29, [3, 3],
             id="28/29 cat/dog years convert into 3 human years"
+        ),
+        pytest.param(
+            -1, -1, [0, 0],
+            id="-1/-1 cat/dog years convert into 0 human years"
         )
     ]
 )
@@ -42,3 +47,20 @@ def test_get_human_age(
         result: list
 ) -> None:
     assert get_human_age(cat_age, dog_age) == result
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_error",
+    [
+        pytest.param(
+            "0", "0", TypeError,
+            id="invalid type should raise TypeError"
+        )
+    ]
+)
+def raising_error(
+        cat_age: int,
+        dog_age: int,
+        expected_error: Any):
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
