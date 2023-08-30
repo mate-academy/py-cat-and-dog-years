1c2929c45e8b7f3f1a9d4369fe6741fb4df080aa
Tymur Yermolenko <wonnnakoo@gmail.com>
2023-08-30 19:39:29 +0300

    add new cases and descriptive ids

--- app/test_main.py
+++ app/test_main.py
@@ -6,15 +6,76 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (27, 27, [2, 2]),
-        (28, 28, [3, 2]),
-        (100, 100, [21, 17])
-
-    ]
+        pytest.param(
+            -1,
+            -1,
+            [0, 0],
+            id="should return '[0, 0]' when ages are negative"
+        ),
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="should return '[0, 0]' when ages are '0'"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="should return '[0, 0]' when ages are less then '15'",
+        ),
+        pytest.param(
+            15,
+            15,
+            [1, 1],
+            id="should return '[1, 1]' when ages are '15'"
+        ),
+        pytest.param(
+            23,
+            23,
+            [1, 1],
+            id="should return '[1, 1]' when ages are '23'"
+        ),
+        pytest.param(
+            24,
+            24,
+            [2, 2],
+            id="should return '[2, 2]' when ages are '24'"
+        ),
+        pytest.param(
+            27,
+            27,
+            [2, 2],
+            id="should return '[2, 2]' when ages are '27'"
+        ),
+        pytest.param(
+            27,
+            28,
+            [2, 2],
+            id="should return '[2, 2]' when cat age are ' 27' "
+               "and dog age are '28'",
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="should return '[3, 2]' when ages are '28'"
+        ),
+        pytest.param(
+            28,
+            29,
+            [3, 3],
+            id="should return '[3, 3]' when cat age are '28' "
+               "and dog age are '29'",
+        ),
+        pytest.param(
+            100,
+            100,
+            [21, 17],
+            id="should return '[21, 17]' when cat age are '100' "
+               "and dog age are '100'",
+        ),
+    ],
 )
 def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
