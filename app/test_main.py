c520c00bf1e4577be0b583f8dc4180392342ecc0
Mikhail <tiunovmixs@gmail.com>
2023-08-31 12:45:00 +0300

    Implemented Cat and Dog years

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+class TestYearsCatDog:
+    @pytest.mark.parametrize("cat_age, dog_age, expected", [
+        (0, 0, [0, 0]),
+        (15, 15, [1, 1]),
+        (28, 28, [3, 2]),
+        (32, 32, [4, 3]),
+        (36, 36, [5, 4]),
+        (40, 40, [6, 5]),
+        (44, 44, [7, 6]),
+        (100, 100, [21, 17]),
+    ])
+    def test_get_human_age(self, cat_age, dog_age, expected) -> None:
+        assert get_human_age(cat_age, dog_age) == expected
