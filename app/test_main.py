74717d76d068bd5352171437cf5abe485f0015c0
Max Katkalov <makskatkalov@gmail.com>
2023-09-05 00:46:09 +0300

    another tests written

--- app/test_main.py
+++ app/test_main.py
@@ -3,44 +3,50 @@ import pytest
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize(
-    "cat_age, dog_age, result",
-    (
-        pytest.param(0, 0, [0, 0], id="animal ages are zeros"),
-        pytest.param(
-            13, 6, [0, 0], id="any num lower than 15 should return 0"
-        ),
-        pytest.param(
-            15, 15, [1, 1], id="check for 15 dog/cat ages is 1 for human"
-        ),
-        pytest.param(
-            24, 24, [2, 2], id="check that next 9 years calculated correctly"
-        ),
-        pytest.param(23, 23, [1, 1], id="23 is still 1 human year"),
-        pytest.param(
-            29, 30, [3, 3], id="check that next 4/5 years calculated correctly"
-        ),
-        pytest.param(
-            100, 100, [21, 17], id="correct calculating for big ages"
-        ),
-    ),
-)
-def test_correct_work_of_get_human_age(
-    cat_age: int, dog_age: int, result: list[int]
-) -> None:
-    assert get_human_age(cat_age, dog_age) == result
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_result",
+        [
+            pytest.param(
+                -1, -1, [0, 0], id="negative age gives 0 human age"
+            ),
+            pytest.param(
+                0, 0, [0, 0], id="should return zero"
+            ),
+            pytest.param(
+                2, 2, [0, 0], id="check cat or dog first year"
+            ),
+            pytest.param(
+                14, 14, [0, 0], id="check cat_dog less than first year"
+            ),
+            pytest.param(
+                15, 15, [1, 1], id="check cat_dog first year"
+            ),
+            pytest.param(
+                23, 23, [1, 1], id="check cat/dog first year"
+            ),
+            pytest.param(
+                24, 24, [2, 2], id="check cat/dog second year"
+            ),
+            pytest.param(
+                27, 28, [2, 2], id="check cat_dog second year"),
+            pytest.param(
+                28, 28, [3, 2], id="check second cat and third dog year"
+            ),
+            pytest.param(
+                28, 29, [3, 3], id="check cat/dog third year"
+            ),
+            pytest.param(
+                100, 100, [21, 17], id="check cat_dog extra years"),
+        ],
+    )
+    def test_get_human_age(
+        self, cat_age: int, dog_age: int, expected_result: list
+    ) -> None:
+        result = get_human_age(cat_age, dog_age)
 
+        assert result == expected_result
 
-@pytest.mark.parametrize(
-    "unnormal_value",
-    (
-        pytest.param("15", id="for str"),
-        pytest.param({24}, id="for set"),
-        pytest.param({15: 15}, id="for dict"),
-    ),
-)
-def test_correct_exceptions_for_get_human_age(
-    unnormal_value: str | dict | set,
-) -> None:
-    with pytest.raises(TypeError):
-        assert get_human_age(unnormal_value, unnormal_value) == TypeError
+    def test_if_raises_error(self) -> None:
+        with pytest.raises(TypeError):
+            get_human_age("10", 2)
