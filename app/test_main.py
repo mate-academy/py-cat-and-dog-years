d7c59888e41f575170acf36a109d3cd06857e616
Artem <hartem472@gmail.com>
2023-09-11 20:09:48 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,19 @@
 from app.main import get_human_age
 
 
-def test_get_human_age_0():
+def test_get_human_age_0() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_get_human_age_15():
+def test_get_human_age_15() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_get_human_age_28():
+def test_get_human_age_28() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
 
-def test_get_human_age_100():
+def test_get_human_age_100() -> None:
     assert get_human_age(100, 100) == [21, 17]
 
 
@@ -26,7 +26,6 @@ def test_no_one_or_all_values() -> None:
 
 def test_another_type() -> None:
     try:
-        assert get_human_age('13', '10') == [0, 0]
+        assert get_human_age("13", "10") == [0, 0]
     except TypeError:
-        print('False type')
-
+        print("False type")
