9b7ae3741baf1f9c814173eaa4158ecf9240c330
Artem <hartem472@gmail.com>
2023-09-11 21:13:11 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,23 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_get_human_age_0() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+test_cases = [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (23, 23, [1, 1]),
+    (24, 24, [2, 2]),
+    (27, 27, [2, 2]),
+    (28, 28, [3, 2]),
+    (100, 100, [21, 17])
+]
 
 
-def test_get_human_age_15() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_get_human_age_28() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_get_human_age_100() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize("cat_age, dog_age, expected_result", test_cases)
+def test_get_human_age(cat_age, dog_age, expected_result):
+    assert get_human_age(cat_age, dog_age) == expected_result
 
 
 def test_no_one_or_all_values() -> None:
