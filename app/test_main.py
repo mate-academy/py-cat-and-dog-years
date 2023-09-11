78867ce8dcc15fe643f61a3dbcaed0e268185253
Artem <hartem472@gmail.com>
2023-09-11 18:16:03 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_zeros() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_default_return() -> None:
+    assert get_human_age(21, 15) == [1, 1]
+
+
+def test_age_28() -> None:
+    assert get_human_age(28, 28) == [3, 2]
