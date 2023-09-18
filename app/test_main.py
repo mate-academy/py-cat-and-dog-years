1767c3170bb8f44e9c489b9dce6dc72178c6e1bd
Maksim Stahovskij <maksss196@gmail.com>
2023-09-18 07:37:32 +0300

    py-cat-dog-years was added pytest.mark.parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+import pytest
 from app.main import get_human_age
 
 
@@ -6,19 +7,14 @@ def test_check_output_value_not_equal_previous_value() -> None:
             != get_human_age(cat_age=24, dog_age=24))
 
 
-def test_check_resive_data_minus_value_or_zero_value() -> None:
-    assert get_human_age(cat_age=0, dog_age=-16) == [0, 0]
-
-
-def test_check_get_human_age_return_list() -> None:
-    assert isinstance(get_human_age(cat_age=10, dog_age=10), list)
-
-
-def test_check_age_if_cat_dog_age_equal_tw_eight() -> None:
-    assert get_human_age(cat_age=28, dog_age=28) == [3, 2]
-
-
-def test_check_correct_type_of_value() -> None:
-    data_type = get_human_age(cat_age=10, dog_age=10)
-    assert isinstance(data_type[0], int)
-    assert isinstance(data_type[1], int)
+@pytest.mark.parametrize("cat_age, dog_age, expected_result", [
+    (0, 16, [0, 1]),
+    (27, 28, [2, 2]),
+    (28, 28, [3, 2]),
+])
+def test_check_data_minus_type_age_value(
+        cat_age: int,
+        dog_age: int,
+        expected_result: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_result
