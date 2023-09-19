d2f03a67526e330c0e0aa39e07036e21b515df2d
Nadiia <nadiia.developer777@gmail.com>
2023-09-19 10:51:08 +0300

    Solution-py-cat-and-dog-years

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,12 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_first_year_second_year_each_year_cat_each_year_dog() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+    assert get_human_age(14, 14) == [0, 0]
+    assert get_human_age(15, 15) == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
+    assert get_human_age(24, 24) == [2, 2]
+    assert get_human_age(27, 27) == [2, 2]
+    assert get_human_age(28, 28) == [3, 2]
+    assert get_human_age(100, 100) == [21, 17]
