f84bab275db4d0b99737f8715d7b117cc424c6f8
Nadiia <nadiia.developer777@gmail.com>
2023-09-19 15:02:21 +0300

    Solution-three-py-cat-and-dog-years

--- app/test_main.py
+++ app/test_main.py
@@ -3,7 +3,12 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize("cat_age, dog_age, expected", [
+    (2, 3, [0, 0]),
+    (17, 20, [1, 1]),
+    (-1, -5, [0, 0]),
     (0, 0, [0, 0]),
+    (1000, 2000, [246, 397]),
+    ("cat", "dog", TypeError),
     (14, 14, [0, 0]),
     (15, 15, [1, 1]),
     (23, 23, [1, 1]),
@@ -11,6 +16,11 @@ from app.main import get_human_age
     (27, 27, [2, 2]),
     (28, 28, [3, 2]),
     (100, 100, [21, 17])
+
 ])
 def test_get_human_age(cat_age: int, dog_age: int, expected: int) -> None:
-    assert get_human_age(cat_age, dog_age) == expected
+    if expected == TypeError:
+        with pytest.raises(TypeError):
+            get_human_age(cat_age, dog_age)
+    else:
+        assert get_human_age(cat_age, dog_age) == expected
