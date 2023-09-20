1b04503996dfb02be27227117f93e86f52028a3c
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-20 18:20:59 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,7 @@
 from app.main import get_human_age
 
 
-def test_cat_age_conversion():
+def test_cat_age_conversion() -> None:
 
     assert get_human_age(5, 10) == [0, 0]
 
@@ -10,7 +10,7 @@ def test_cat_age_conversion():
     assert get_human_age(30, 40) == [3, 5]
 
 
-def test_dog_age_conversion():
+def test_dog_age_conversion() -> None:
     assert get_human_age(10, 5) == [0, 0]
 
     assert get_human_age(20, 21) == [1, 1]
