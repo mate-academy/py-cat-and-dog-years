e975beeff31b9ba275d81149c517f9fc742406f2
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-20 19:07:43 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,18 +1,14 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_cat_age_conversion() -> None:
-
-    assert get_human_age(5, 10) == [0, 0]
-
-    assert get_human_age(15, 20) == [1, 1]
-
-    assert get_human_age(30, 40) == [3, 5]
-
-
-def test_dog_age_conversion() -> None:
-    assert get_human_age(10, 5) == [0, 0]
-
-    assert get_human_age(20, 21) == [1, 1]
-
-    assert get_human_age(30, 35) == [3, 4]
+@pytest.mark.parametrize("cat_age, dog_age, expected", [
+    (5, 10, [0, 0]),
+    (15, 20, [1, 1]),
+    (30, 40, [3, 5]),
+    (10, 5, [0, 0]),
+    (20, 21, [1, 1]),
+    (30, 35, [3, 4]),
+])
+def test_age_conversion(cat_age, dog_age, expected):
+    assert get_human_age(cat_age, dog_age) == expected
