4f8149a641444e2cef2197824354da336b62533f
Oleksandr Suligan <suligan.alexander@gmail.com>
2023-09-22 15:25:54 +0200

    Solution-added correct value type test

--- app/test_main.py
+++ app/test_main.py
@@ -21,3 +21,19 @@ def test_worked_human_age(
         animal_age: list,
         human_age: list) -> None:
     assert get_human_age(*animal_age) == human_age
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        pytest.param(
+            "six",
+            [1, 2]
+        )
+    ]
+)
+def test_get_human_age_correct_value_type(
+        cat_age: int, dog_age: int
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
