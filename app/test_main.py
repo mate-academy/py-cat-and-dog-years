9cf7becef4d5fd35cc351cd87d7a3a5060bc7e88
Vitalii <137533215+VitaliyZhabskyi@users.noreply.github.com>
2023-09-24 16:41:04 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -10,5 +10,17 @@ from app.main import get_human_age
     (27, 28, [2, 2]),
     (28, 29, [3, 3]),
 ])
-def test_age_conversion(cat_age: int, dog_age: int, expected: int) -> None:
+def test_age_conversion(cat_age, dog_age, expected):
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize("cat_age, dog_age", [
+    ("cat", 15),
+    (23, "dog"),
+    ("cat", "dog"),
+    (23.5, 15),
+    (23, 15.5),
+])
+def test_invalid_data_types(cat_age, dog_age):
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
