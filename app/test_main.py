569d423bf051f317c2a92ed2e3f6add1cb5f15b4
Hlova Oleh <hlovaoleh0@gmail.com>
2023-09-25 23:03:34 +0300

    implemented tests to py-cat-and-dog-years task

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,42 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,result",
+        [
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
+            (100, 100, [21, 17])
+        ]
+    )
+    def test_get_human_age_with_correct_values(
+            self,
+            cat_age: int,
+            dog_age: int,
+            result: list
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == result
+
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_error",
+        [
+            ("value", 12, TypeError),
+            (13, "value", TypeError)
+        ]
+    )
+    def test_get_human_age_with_correct_types_of_value(
+            self,
+            cat_age,
+            dog_age,
+            expected_error
+    ) -> None:
+        with pytest.raises(expected_error):
+            get_human_age(cat_age, dog_age)
