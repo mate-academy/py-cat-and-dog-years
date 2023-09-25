aa95f94b485d87a41a823101f4e34305bc32808d
Даня Пономаренко <you@example.com>
2023-09-25 11:11:40 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,6 @@
 from app.main import get_human_age
 
+
 def test_convert_zero() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
@@ -7,11 +8,14 @@ def test_convert_zero() -> None:
 def test_exta_add_first() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
+
 def test_second_add() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
+
 def test_second_add_fifteen() -> None:
     assert get_human_age(15, 15) == [1, 1]
+
 
 def test_thirty_add() -> None:
     assert get_human_age(24, 24) == [2, 2]
@@ -19,6 +23,3 @@ def test_thirty_add() -> None:
 
 def test_hundred() -> None:
     assert get_human_age(100, 100) == [21, 17]
-
-# def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-#     assert split_integer(10, 2) == [5, 5]
