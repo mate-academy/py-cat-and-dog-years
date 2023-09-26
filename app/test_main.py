d425a85ba53dbe895eef500051ebc817d94729d6
Nikolay Krasulin <krasulinphotograph@example.com>
2023-09-26 15:11:05 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,6 +3,7 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize("cat_age, dog_age, expected_result", [
+
     (0, 0, [0, 0]),
     (14, 0, [0, 0]),
     (15, 0, [1, 0]),
@@ -25,14 +26,18 @@ from app.main import get_human_age
     (27, 27, [2, 2]),
     (28, 28, [3, 2]),
     (100, 100, [21, 17]),
+    ("14", 14, TypeError),
+    (14, "14", TypeError),
+    ("value", 1, TypeError),
+    (1, "value", TypeError),
 ])
 def test_get_human_age(
         cat_age: int,
         dog_age: int,
         expected_result: tuple
 ) -> None:
-    if expected_result == TypeError:
+    if isinstance(expected_result, list):
+        assert get_human_age(cat_age, dog_age) == expected_result
+    elif expected_result == TypeError:
         with pytest.raises(expected_result):
             get_human_age(cat_age, dog_age)
-    else:
-        assert get_human_age(cat_age, dog_age) == expected_result
