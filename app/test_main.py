85b04c956d7c46e703ad3167a8d824f70a31b92a
Nikolay Krasulin <krasulinphotograph@example.com>
2023-09-26 13:14:49 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,35 +1,30 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_get_human_age_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_get_human_age_cat_only() -> None:
-    assert get_human_age(14, 0) == [0, 0]
-    assert get_human_age(15, 0) == [1, 0]
-    assert get_human_age(23, 0) == [1, 0]
-    assert get_human_age(24, 0) == [2, 0]
-    assert get_human_age(27, 0) == [2, 0]
-    assert get_human_age(28, 0) == [3, 0]
-    assert get_human_age(100, 0) == [21, 0]
-
-
-def test_get_human_age_dog_only() -> None:
-    assert get_human_age(0, 14) == [0, 0]
-    assert get_human_age(0, 15) == [0, 1]
-    assert get_human_age(0, 23) == [0, 1]
-    assert get_human_age(0, 24) == [0, 2]
-    assert get_human_age(0, 27) == [0, 2]
-    assert get_human_age(0, 28) == [0, 2]
-    assert get_human_age(0, 100) == [0, 17]
-
-
-def test_get_human_age_both() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(23, 23) == [1, 1]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(27, 27) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize("cat_age, dog_age, expected_result", [
+    (0, 0, [0, 0]),
+    (14, 0, [0, 0]),
+    (15, 0, [1, 0]),
+    (23, 0, [1, 0]),
+    (24, 0, [2, 0]),
+    (27, 0, [2, 0]),
+    (28, 0, [3, 0]),
+    (100, 0, [21, 0]),
+    (0, 14, [0, 0]),
+    (0, 15, [0, 1]),
+    (0, 23, [0, 1]),
+    (0, 24, [0, 2]),
+    (0, 27, [0, 2]),
+    (0, 28, [0, 2]),
+    (0, 100, [0, 17]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (23, 23, [1, 1]),
+    (24, 24, [2, 2]),
+    (27, 27, [2, 2]),
+    (28, 28, [3, 2]),
+    (100, 100, [21, 17]),
+])
+def test_get_human_age(cat_age, dog_age, expected_result) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_result
