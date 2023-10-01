f2d109fb18710ba8d1ecab5b2f7c191a07aa5845
Andriy_Shumskyy <andshumskyy@gmail.com>
2023-10-01 10:09:59 +0300

    solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,12 +1,32 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_poss_results() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(23, 23) == [1, 1]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(27, 27) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize("cat_age, dog_age, expected_output", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (23, 23, [1, 1]),
+    (24, 24, [2, 2]),
+    (27, 27, [2, 2]),
+    (28, 28, [3, 2]),
+    (100, 100, [21, 17]),
+])
+def test_get_human_age(cat_age, dog_age, expected_output):
+    actual_output = get_human_age(cat_age, dog_age)
+    assert actual_output == expected_output
+
+
+def test_get_human_age_with_negative_cat_age():
+    actual_output = get_human_age(-1, 0)
+    assert actual_output == [0, 0]
+
+
+def test_get_human_age_with_zero_cat_age():
+    actual_output = get_human_age(0, 0)
+    assert actual_output == [0, 0]
+
+
+def test_get_human_age_with_string_cat_age():
+    with pytest.raises(TypeError):
+        get_human_age("15", 0)
