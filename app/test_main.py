7db95059f531919aff07ec005989c25301988421
asdadaversa <peikqudakeus@gmail.com>
2023-10-06 14:09:01 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,21 +4,37 @@ from app.main import get_human_age
 
 class TestGetHumanAgeClass:
     @pytest.mark.parametrize(
-        "cat_age, dog_age, expected",
+        "cat_age, dog_age, value",
         [
+            (-1, 1, [0, 0]),
             (14, 14, [0, 0]),
             (15, 15, [1, 1]),
             (23, 23, [1, 1]),
             (24, 24, [2, 2]),
             (27, 28, [2, 2]),
-            (28, 29, [3, 3])
+            (28, 29, [3, 3]),
+            (1000, 1000, [246, 197])
         ]
     )
     def test_human_age_tester(
             self,
             cat_age: int,
             dog_age: int,
-            expected: list
+            value: list
     ) -> None:
 
-        assert get_human_age(cat_age, dog_age) == expected
+        assert get_human_age(cat_age, dog_age) == value
+
+    @pytest.mark.parametrize(
+        "cat_age, dog_age",
+        [
+            ("k", 12),
+            (12, "k"),
+            ("k", "k"),
+            ({"k"}, {"k"}),
+            (["k"], ["k"])
+        ]
+    )
+    def test_coorect_value(self, cat_age: int, dog_age: int) -> None:
+        with pytest.raises(TypeError):
+            get_human_age(cat_age, dog_age)
