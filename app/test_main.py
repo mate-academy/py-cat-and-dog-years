23895e2999ef08aaea8bf18d9fb23a0785e21614
Максим <www.depaton@gmail.com>
2023-10-11 00:25:43 +0300

    changes

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,33 @@
 from app.main import get_human_age
+import pytest
 
 
-def test_animal_age_is_less_or_equal_14() -> None:
-    assert get_human_age(14, 14) == [0, 0]
+@pytest.mark.parametrize("cat_age, dog_age, output", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (27, 27, [2, 2]),
+    (28, 28, [3, 2]),
+    (100, 100, [21, 17]),
+    (-10, -10, [0, 0])
+])
+def test_animal_age_with_input_data(
+        cat_age: int,
+        dog_age: int,
+        output: int
+) -> None:
+    assert get_human_age(cat_age, dog_age) == output
 
 
-def test_animal_age_is_less_or_equal_23() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_animal_is_less_or_equal_27() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_animal_is_bigger_equal_27() -> None:
-    assert get_human_age(28, 28) == [3, 2]
+@pytest.mark.parametrize("cat_age, dog_age, error", [
+    ("10", "10", TypeError),
+    ([10], [10], TypeError),
+    ({10: ""}, {10: ""}, TypeError),
+])
+def test_animal_age_with_incorrect_data_type(
+        cat_age: str or list or dict,
+        dog_age: str or list or dict,
+        error: Exception
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
