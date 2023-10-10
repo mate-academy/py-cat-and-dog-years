6b8ab26e645a3c8fbefa17b15b656f7159a94338
Alex <moleksii@gmail.com>
2023-10-10 21:07:14 +0300

    Fixed Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,35 +2,28 @@ from app.main import get_human_age
 
 
 def test_check_human_age_when_animal_age_less_than_15() -> None:
-    result = get_human_age(14, 14)
-    assert result == [0, 0]
+    assert get_human_age(14, 14) == [0, 0]
 
 
 def test_check_human_age_when_animal_age_equal_15() -> None:
-    result = get_human_age(15, 15)
-    assert result == [1, 1]
+    assert get_human_age(15, 15) == [1, 1]
 
 
 def test_check_human_age_when_animal_age_less_than_24() -> None:
-    result = get_human_age(23, 23)
-    assert result == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
 
 
 def test_check_human_age_when_animal_age_equal_24() -> None:
-    result = get_human_age(24, 24)
-    assert result == [2, 2]
+    assert get_human_age(24, 24) == [2, 2]
 
 
 def test_check_human_age_when_animal_age_equal_27() -> None:
-    result = get_human_age(27, 27)
-    assert result == [2, 2]
+    assert get_human_age(27, 27) == [2, 2]
 
 
 def test_check_human_age_when_cat_and_dog_age_equal_28() -> None:
-    result = get_human_age(28, 28)
-    assert result == [3, 2]
+    assert get_human_age(28, 28) == [3, 2]
 
 
 def test_check_human_age_when_cat_and_dog_age_equal_100() -> None:
-    result = get_human_age(100, 100)
-    assert result == [21, 17]
+    assert get_human_age(100, 100) == [21, 17]
