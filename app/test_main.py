56339ac437c82f41934962180d135f6aa048c32b
Andrey Britov <abritov82@gmail.com>
2023-10-12 14:06:55 +0300

    develop

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
-from app.main import get_human_age
+from app.main import get_human_age,convert_to_human
 
-# write your code here
+
+def test_should_check_arguments_function_less_than_15():
+    assert get_human_age(0,0) == [0, 0]
+
+
+def test_should_check_arguments_function_between_15_and_24():
+    assert get_human_age(15, 17) == [1, 1]
+
+
+def test_should_check_arguments_function_more_24():
+    assert get_human_age(28, 28) == [3, 2]
