4d7296e99ce86d51048ed4eb103ac432963906cf
Максим <www.depaton@gmail.com>
2023-10-13 00:11:15 +0300

    new changes

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,5 @@
-from app.main import get_human_age
 import pytest
+from app.main import get_human_age
 
 
 @pytest.mark.parametrize("cat_age, dog_age, output", [
@@ -10,12 +10,14 @@ import pytest
     (28, 28, [3, 2]),
     (28, 20, [3, 1]),
     (100, 100, [21, 17]),
-    (-10, -10, [0, 0])
+    (-10, -10, [0, 0]),
+    (5, 25, [0, 2]),
+    (20, 1, [1, 0])
 ])
 def test_animal_age_with_input_data(
         cat_age: int,
         dog_age: int,
-        output: int
+        output: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == output
 
@@ -26,8 +28,8 @@ def test_animal_age_with_input_data(
     ({10: ""}, {10: ""}, TypeError),
 ])
 def test_animal_age_with_incorrect_data_type(
-        cat_age: str or list or dict,
-        dog_age: str or list or dict,
+        cat_age: any,
+        dog_age: any,
         error: Exception
 ) -> None:
     with pytest.raises(TypeError):
