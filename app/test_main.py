46bc648e93fc6fa8dc8a1fbf6d51cdf8acbfc36e
Andrey Britov <abritov82@gmail.com>
2023-10-12 16:44:04 +0300

    Solusion_2

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,43 @@
-from app.main import get_human_age,convert_to_human
+from typing import Type
+from app.main import get_human_age
+import pytest
 
 
-def test_should_check_arguments_function_less_than_15():
-    assert get_human_age(0,0) == [0, 0]
+class TestGetHumanAge:
 
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_result",
+        [
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
+            (100, 100, [21, 17])
+        ],
+    )
+    def test_should_check_return_human_age(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_result: list
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_result
 
-def test_should_check_arguments_function_between_15_and_24():
-    assert get_human_age(15, 17) == [1, 1]
-
-
-def test_should_check_arguments_function_more_24():
-    assert get_human_age(28, 28) == [3, 2]
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_result",
+        [
+            ("str", 27, TypeError),
+            (27, "str", TypeError)
+        ],
+    )
+    def test_check_type_arguments_get_human_age(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_result: Type[BaseException]
+    ) -> None:
+        with pytest.raises(expected_result):
+            get_human_age(cat_age, dog_age)
