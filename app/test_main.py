d14c955bef0bf94ec90888cb69f58d55678a3c84
Vladyslav Sabo <lebwa1337@gmail.com>
2023-10-13 20:31:39 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,5 @@
-import pytest
 from typing import Any
+import pytest
 from app.main import get_human_age
 
 
@@ -21,15 +21,24 @@ def test_check_for_receiving_incorrect_type_of_data(
 @pytest.mark.parametrize(
     "cat_age,dog_age,expected_result",
     [
-        pytest.param(-1, -1, [0, 0], id="test receives negative number"),
-        pytest.param(-5, -7, [0, 0], id="test receives negative number"),
-        pytest.param(0, 0, [0, 0], id="test receives zero"),
-        pytest.param(23, 23, [1, 1], id="test receives normal range number"),
-        pytest.param(28, 28, [3, 2], id="test receives normal range number"),
-        pytest.param(15, 15, [1, 1], id="test receives normal range number"),
-        pytest.param(100, 100, [21, 17], id="test receives large number"),
-        pytest.param(500, 300, [121, 57], id="test receives large number"),
-        pytest.param(1002, 600, [246, 117], id="test receives large number")
+        pytest.param(-1, 23, [0, 1],
+                     id="test receives negative number for cat"),
+        pytest.param(15, -7, [1, 0],
+                     id="test receives negative number for dog"),
+        pytest.param(0, 0, [0, 0],
+                     id="test receives zeros"),
+        pytest.param(23, 23, [1, 1],
+                     id="test receives normal range numbers"),
+        pytest.param(28, 28, [3, 2],
+                     id="test receives normal range numbers"),
+        pytest.param(15, 15, [1, 1],
+                     id="test receives normal range numbers"),
+        pytest.param(100, 100, [21, 17],
+                     id="test receives large numbers"),
+        pytest.param(500, 300, [121, 57],
+                     id="test receives large numbers"),
+        pytest.param(1002, 600, [246, 117],
+                     id="test receives large numbers")
     ]
 )
 def test_check_for_receiving_different_range_of_numbers_data(
