1d814a8e7de330302d8931949be6b9ba88f4101e
SerhiiBudko <140949363+SerhiiBudko@users.noreply.github.com>
2023-10-15 13:24:54 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -8,7 +8,7 @@ import pytest
         pytest.param(0, 0, [0, 0], id="Age of human = 0"),
         pytest.param(15, 15, [1, 1], id="Age of human = 1"),
         pytest.param(28, 28, [3, 2], id="Age of human = 3, 2 years"),
-        pytest.param(100, 100, [21, 17], id="Age of human = 21 and 17 years old"),
+        pytest.param(100, 100, [21, 17], id="Age of human = 21 and 17y.o"),
     ]
 )
 def test_get_human_age(cat_age: int, dog_age: int, expected_age: list) -> None:
