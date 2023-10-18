0e2303c575e9b303fee8ef8fdfb5d08afd239de1
SerhiiBudko <140949363+SerhiiBudko@users.noreply.github.com>
2023-10-18 19:41:15 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,6 @@
-from app.main import get_human_age
 from typing import Any
 import pytest
+from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
@@ -15,6 +15,10 @@ import pytest
         pytest.param("cat age", 20, None, id="Cat age isn`t int!"),
         pytest.param(15, "dog age", None, id="Dog age isn`t int!"),
         pytest.param("cat age", "dog age", None, id="Both ages are not int!"),
+        pytest.param({"cat": 10}, {"dog": 12}, None, id="Dictionary input"),
+        pytest.param((5, 10), (3, 8), None, id="Tuple input"),
+        pytest.param({1, 2, 3}, {4, 5, 6}, None, id="Set input"),
+        pytest.param([8, 9], [10, 11, 12], None, id="List input"),
     ]
 )
 def test_get_human_age(
