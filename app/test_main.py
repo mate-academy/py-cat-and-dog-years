5a489aca93b84e3938169862420843433529b859
ValeriiaShvanska <ladyvaleriya13@gmail.com>
2023-10-18 18:15:32 +0300

    Check type

--- app/test_main.py
+++ app/test_main.py
@@ -5,6 +5,7 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age,dog_age,expected",
     [
+        (-2, -2, [0, 0]),
         (0, 0, [0, 0]),
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
@@ -18,3 +19,17 @@ from app.main import get_human_age
 def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert (get_human_age(cat_age, dog_age) == expected
             ), f"Result should be equal to {expected}"
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age",
+    [
+        ("2", "3"),
+        ([1], [2]),
+        ((15,), 15),
+        ({13}, 1),
+    ]
+)
+def test_check_incorrect_types(cat_age: int, dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
