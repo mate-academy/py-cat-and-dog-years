0c4d34c81b8f153f4d1d865d2d35a87a1d027783
Denys <den.prislipskyi@gmail.com>
2023-10-18 20:10:00 +0200

    test human age

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_control_age_should_be_0() -> None:
+    old = get_human_age(14, 14)
+    assert old == [0, 0]
+
+
+def test_should_be_more_0() -> None:
+    old = get_human_age(15, 15)
+    assert old == [1, 1]
+
+
+def test_control_should_be_2() -> None:
+    old = get_human_age(24, 24)
+    assert old == [2, 2]
+
+
+def test_control_age() -> None:
+    old = get_human_age(28, 28)
+    assert old == [3, 2]
