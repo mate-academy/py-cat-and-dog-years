f8a778917c88444e6c927baf37aa11e549c1fd0e
SerhiiBudko <140949363+SerhiiBudko@users.noreply.github.com>
2023-10-18 16:54:39 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -10,23 +10,15 @@ import pytest
         pytest.param(28, 28, [3, 2], id="Age of human = 3, 2 years"),
         pytest.param(100, 100, [21, 17], id="Age of human = 21 and 17y.o"),
         pytest.param(-1, 15, [0, 1], id="Age of human = 1"),
-        pytest.param(28, 39, [3, 5], id="Age of human = 3 and 5y.o")
+        pytest.param(28, 39, [3, 5], id="Age of human = 3 and 5y.o"),
+        pytest.param("cat age", 20, None, id="Cat age isn`t int!"),
+        pytest.param(15, "dog age", None, id="Dog age isn`t int!"),
+        pytest.param("cat age", "dog age", None, id="Both ages are not int!"),
     ]
 )
 def test_get_human_age(cat_age: int, dog_age: int, expected_age: list) -> None:
-    assert get_human_age(cat_age, dog_age) == expected_age
-
-
-def test_get_human_age_for_unexpected_data_type_cat_age() -> None:
-    with pytest.raises(TypeError):
-        get_human_age("cat age", 20)
-
-
-def test_get_human_age_for_unexpected_data_type_dog_age() -> None:
-    with pytest.raises(TypeError):
-        get_human_age(15, "dog age")
-
-
-def test_get_human_age_for_unexpected_data_type() -> None:
-    with pytest.raises(TypeError):
-        get_human_age("cat age", "dog age")
+    if expected_age is not None:
+        assert get_human_age(cat_age, dog_age) == expected_age
+    else:
+        with pytest.raises(TypeError):
+            get_human_age(cat_age, dog_age)
