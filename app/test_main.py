cc621e8cfa23768c291d01e2eaabc9644f3ffd46
Lana Chernenko <svitlaana.chernenko.s@gmail.com>
2023-10-18 18:49:59 +0100

    Fixed code

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+from typing import Type
 import pytest
 
 from app.main import get_human_age
@@ -6,21 +7,16 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, human_age_of_cat_and_dog",
     [
-        pytest.param(0, 0, [0, 0],
-                     id="test should return zeroes if parameters zeroes"),
-        pytest.param(14, 14, [0, 0],
-                     id="test should return zeroes if "
-                        "parameters are less than first year"),
-        pytest.param(15, 15, [1, 1],
-                     id="test should return [1, 1] if parameters "
-                        "are less than sum 2 years"),
-        pytest.param(23, 23, [1, 1],
-                     id="test should round down results"),
-        pytest.param(28, 28, [3, 2],
-                     id="test may return different results "
-                        "for different animals"),
-        pytest.param(100, 100, [21, 17],
-                     id="test should work with big numbers")
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (-100, -100, [0, 0]),
+        (100000, 100000, [24996, 19997]),
+
     ]
 )
 def test_correct_human_age(
@@ -31,3 +27,18 @@ def test_correct_human_age(
     assert (get_human_age(cat_age, dog_age) == human_age_of_cat_and_dog), \
         (f"Human age of {cat_age} cats years and {dog_age} dogs years "
          f"should be equal to {human_age_of_cat_and_dog}")
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        (1, "age", TypeError),
+        (["age"], 1, TypeError),
+        (1, {"dog": 1}, TypeError),
+    ]
+)
+def test_should_raise_error_if_wrong_data_type_provided(
+    cat_age: int, dog_age: int, result: Type[Exception]
+) -> None:
+    with pytest.raises(result):
+        get_human_age(cat_age, dog_age)
