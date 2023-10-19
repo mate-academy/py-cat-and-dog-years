0aa79f4aad2b8a50fd2a837bfbf3a8d0631a32b4
Denis Komandyr <sabsdaid@gmail.com>
2023-10-20 01:32:28 +0300

    Cat and dog years

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,49 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="zeros should equal zeros"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="values less than 15 don't count"
+        ),
+        pytest.param(
+            23,
+            23,
+            [1, 1],
+            id="values from 15 to 24 should count as 1"
+        ),
+        pytest.param(
+            27,
+            27,
+            [2, 2],
+            id="values from 24 to 28 count as 2 for both"
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="for cat age 28 counts as 3, when for dog it still 2"
+        ),
+        pytest.param(
+            100,
+            100,
+            [21, 17],
+            id=("for cats after 24, counts as 1 every 4 years "
+                "when for dogs every 5 years")
+        )
+    ]
+)
+def test_can_sum(cat_age: int, dog_age: int, result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result
