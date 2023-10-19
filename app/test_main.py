191be5c87f9acbd8e85e264012bc94fe466fda67
Serhii <martinenko.sergiy@gmail.com>
2023-10-19 16:37:35 +0300

    negatives and incorrect types

--- app/test_main.py
+++ app/test_main.py
@@ -11,12 +11,25 @@ from app.main import get_human_age
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
         (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
+        (-10, -10, [0, 0]),
     ]
 )
 def test_counting_dog_ang_cat_years(cat_age: int,
                                     dog_age: int,
                                     expected_list: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected_list
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ([], []),
+        ("x", "b"),
+        ({}, {})
+    ]
+)
+def test_unsupported_types_error(cat_age: int, dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
