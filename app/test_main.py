f8b71f3b8a8c2bba9bb646479730b7d539238442
Katherine-Greg <katiasimpson17@gmail.com>
2023-10-19 17:00:43 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ],
+    ids=[
+        "should return 0 when cat and dog age is 0",
+        "should return 0 when cat and dog age less then 15",
+        "first 15 cat and dog years give 1 human age",
+        "should return 1 cat years and 1 dog years",
+        "24 cat and dog years should equal 2 human years",
+        "should return 2 cat years and 2 dog years",
+        "should return 3 cat years and 2 dog years",
+        "should return 21 cat years and 17 dog years"
+    ]
+)
+def test_if_first_15_cat_years_give_1_human(cat_age: int, dog_age: int, expected: list) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
