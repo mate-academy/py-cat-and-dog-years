0b0e96b2e246b6e8fe1ec820a7f9df884b61e75d
Kostia <kostia3507@gmail.com>
2023-10-19 18:51:44 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,35 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_human_age",
+    [
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="should return zeroes when cat_age and dog_age are zeroes"
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="should return right values"
+        ),
+        pytest.param(
+            -1,
+            -1,
+            [0, 0],
+            id="should return zeroes when (cat_age and dog age) < 0 "
+        )
+    ]
+)
+def test_cat_and_dogs_years_works_correctly(
+        cat_age: int,
+        dog_age: int,
+        expected_human_age: list
+) -> None:
+    human_age = get_human_age(cat_age, dog_age)
+    assert expected_human_age == human_age
