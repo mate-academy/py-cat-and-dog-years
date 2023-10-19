9edbbf4ece94516049560a0e8883d514e611316b
Сергей <kovalenkoserhii33@gmail.com>
2023-10-19 22:46:56 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_zero_years() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_less_then_human_year() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_one_human_year() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_a_few_year() -> None:
+    assert get_human_age(28, 28) == [3, 2]
