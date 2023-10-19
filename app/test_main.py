0b905b0e9f3d4c3fd2524465e7a608dfa9dfbb0f
Severyn Krok <sevkrok@gmail.com>
2023-10-19 17:28:49 +0300

    Implement py-cat-and-dog-years tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,24 @@
 from app.main import get_human_age
 
-# write your code here
+
+class TestGetHumanAge:
+    def test_if_pets_age_zero_or_less(self) -> None:
+        assert get_human_age(0, 0) == [0, 0]
+
+    def test_if_pets_age_less_15(self) -> None:
+        assert get_human_age(14, 14) == [0, 0]
+
+    def test_if_pets_age_15_years(self) -> None:
+        assert get_human_age(15, 15) == [1, 1]
+
+    def test_if_pets_age_15_and_24_years(self) -> None:
+        assert get_human_age(23, 23) == [1, 1]
+        assert get_human_age(24, 24) == [2, 2]
+
+    def test_if_pets_age_between_25_and_27_years(self) -> None:
+        assert get_human_age(25, 25) == [2, 2]
+        assert get_human_age(27, 27) == [2, 2]
+
+    def test_if_pets_age_over_27_years(self) -> None:
+        assert get_human_age(28, 28) == [3, 2]
+        assert get_human_age(100, 100) == [21, 17]
