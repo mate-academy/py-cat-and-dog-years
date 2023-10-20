11c46f77639b85d7d981156626056c24a9d7415a
Andrii Silenko <andrewformicr@gmail.com>
2023-10-20 08:51:03 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_age_under_first_year_convert():
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_age_more_than_first_year_convert_and_less_than_second_year():
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_age_more_than_second_year_and_less_than_third_year():
+    assert get_human_age(25, 25) == [2, 2]
+
+
+def test_age_more_than_third_year():
+    assert get_human_age(40, 50) == [6, 7]
