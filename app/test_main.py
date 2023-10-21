6a3d168e745a87032aef481b8f28f1482d7273fe
TROT <seotrotpol@gmail.com>
2023-10-21 22:03:27 +0300

    "Solution2"

--- app/test_main.py
+++ app/test_main.py
@@ -11,7 +11,8 @@ from app.main import get_human_age
         (23, 23, [1, 1]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
+        (-1, -1, [0, 0])
     ],
     ids=[
         "the result is null if the value is null",
@@ -19,8 +20,19 @@ from app.main import get_human_age
         "if the value is from 15 to 24, the result is 1",
         "if the value is from 25 to 28, the result is 2",
         "for cat age 28 counts as 3, when for dog it still 2",
-        "for cat age 100 counts as 21, when for dog it still 17"
+        "for cat age 100 counts as 21, when for dog it still 17",
+        "if function resive data out of normal range, such as negative numbers"
     ]
 )
 def test_get_human_age(cat_age: int, dog_age: int, result: list) -> None:
     assert get_human_age(cat_age, dog_age) == result
+
+
+def test_get_human_age_function_incorrect_type_for_cat() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("1", 1)
+
+
+def test_get_human_age_function_incorrect_type_for_dog() -> None:
+    with pytest.raises(TypeError):
+        get_human_age(1, "1")
