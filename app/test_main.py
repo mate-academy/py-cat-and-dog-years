be47b2956f5eb8b370fd76d4cc5a4002abe55c8b
Vladyslav Shatrovskyi <shatrovskyi.vlad@gmail.com>
2023-10-21 10:03:28 +0300

    Fixed names for tests

--- app/test_main.py
+++ app/test_main.py
@@ -18,15 +18,14 @@ def test_human_age_by_cat_and_dog_age(cat_age: int,
                                       dog_age: int,
                                       result: list) -> None:
 
-    assert (get_human_age(cat_age, dog_age) == result
-            ), f"should convert into {result} human age"
+    assert get_human_age(cat_age, dog_age) == result
 
 
-def test_type_of_cat_arg() -> None:
+def test_incorrect_type_for_cat_arg_in_get_human_age_function() -> None:
     with pytest.raises(TypeError):
         get_human_age("28", 28)
 
 
-def test_type_of_dog_arg() -> None:
+def test_incorrect_type_for_dog_arg_in_get_human_age_function() -> None:
     with pytest.raises(TypeError):
         get_human_age(28, "28")
