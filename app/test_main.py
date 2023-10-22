fb9384d9d3092bd551735c67ebe6992124cb1100
Eugene Rabenko <qwer09846@gmail.com>
2023-10-22 13:07:32 +0300

    Corrected

--- app/test_main.py
+++ app/test_main.py
@@ -4,67 +4,47 @@ import pytest
 from app.main import get_human_age
 
 
-def test_should_return_zero_if_age_less_than_15() -> None:
-    assert (get_human_age(14, 14) == [0, 0]
-            ), "Should return zero if age less than 15"
-
-
 @pytest.mark.parametrize(
     "cat_age, dog_age, result",
     [
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1])
+        pytest.param(
+            0, 0, [0, 0],
+            id="Age of 0 should be equal to zero"
+        ),
+        pytest.param(
+            14, 14, [0, 0],
+            id="Age of 14 should be equal to zero"
+        ),
+        pytest.param(
+            15, 15, [1, 1],
+            id="Age of 15 should be equal to 1"
+        ),
+        pytest.param(
+            23, 23, [1, 1],
+            id="Age of 23 should be equal to 1"
+        ),
+        pytest.param(
+            24, 24, [2, 2],
+            id="Age of 24 should be equal to 2"
+        ),
+        pytest.param(
+            27, 27, [2, 2],
+            id="Age of 27 should be equal to 3"
+        ),
+        pytest.param(
+            100, 100, [21, 17],
+            id="Age of 100 should return correct values"
+        ),
+        pytest.param(
+            -1, -1, [0, 0],
+            id="Negative age should return zero"
+        )
     ]
 )
-def test_should_return_one_if_age_in_15_23(
+def test_should_return_correct_values(
         cat_age: int, dog_age: int, result: list
 ) -> None:
-    assert (
-        get_human_age(cat_age, dog_age) == result
-    ), "Should return one if age in (15-23)"
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, result",
-    [
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2])
-    ]
-)
-def test_should_return_two_if_age_in_24_27(
-        cat_age: int, dog_age: int, result: list
-) -> None:
-    assert (
-        get_human_age(cat_age, dog_age) == result
-    ), "Should return two if age in (24-27)"
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, result",
-    [
-        (100, 100, [21, 17]),
-    ]
-)
-def test_should_correctly_work_with_large_numbers(
-        cat_age: int, dog_age: int, result: list
-) -> None:
-    assert (
-        get_human_age(cat_age, dog_age) == result
-    ), "Should correctly work with large numbers"
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, result",
-    [
-        (-1, -1, [0, 0])
-    ]
-)
-def test_should_return_zeros_if_age_is_less_than_zero(
-        cat_age: int, dog_age: int, result: list
-) -> None:
-    assert (
-        get_human_age(cat_age, dog_age) == result
-    ), "Should return zero if age < 0"
+    assert (get_human_age(cat_age, dog_age) == result)
 
 
 @pytest.mark.parametrize(
