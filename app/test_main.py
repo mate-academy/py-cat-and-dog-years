b174589c717dc105a4a959c0449c5ab7dc6fd521
Severyn Krok <sevkrok@gmail.com>
2023-10-24 17:16:58 +0300

    Rewrite with `.parametrize`

--- app/test_main.py
+++ app/test_main.py
@@ -4,30 +4,40 @@ from app.main import get_human_age
 
 
 class TestGetHumanAge:
-    def test_should_raise_error(self) -> None:
+    @staticmethod
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected",
+        [
+            (-1, -2, [0, 0]),
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
+            (100, 100, [21, 17])
+        ]
+    )
+    def test_pets_age_convert_to_human(
+            cat_age: int,
+            dog_age: int,
+            expected: list
+    ) -> None:
+        assert (get_human_age(cat_age, dog_age)) == expected
+
+    @staticmethod
+    @pytest.mark.parametrize(
+        "cat_age, dog_age",
+        [
+            ([1], 0),
+            (0, [1]),
+            (0, {1}),
+            ({0}, 1),
+            (0, "1"),
+            ("1", 0),
+        ]
+    )
+    def test_pets_age_invalid_type(cat_age: int, dog_age: int) -> None:
         with pytest.raises(TypeError):
-            get_human_age("4", [1, 4])
-
-    def test_if_pets_age_less_than_zero(self) -> None:
-        assert get_human_age(-1, -2) == [0, 0]
-
-    def test_if_pets_age_zero(self) -> None:
-        assert get_human_age(0, 0) == [0, 0]
-
-    def test_if_pets_age_less_15(self) -> None:
-        assert get_human_age(14, 14) == [0, 0]
-
-    def test_if_pets_age_15_years(self) -> None:
-        assert get_human_age(15, 15) == [1, 1]
-
-    def test_if_pets_age_15_and_24_years(self) -> None:
-        assert get_human_age(23, 23) == [1, 1]
-        assert get_human_age(24, 24) == [2, 2]
-
-    def test_if_pets_age_between_25_and_27_years(self) -> None:
-        assert get_human_age(25, 25) == [2, 2]
-        assert get_human_age(27, 27) == [2, 2]
-
-    def test_if_pets_age_over_27_years(self) -> None:
-        assert get_human_age(28, 28) == [3, 2]
-        assert get_human_age(100, 100) == [21, 17]
+            get_human_age(cat_age, dog_age)
