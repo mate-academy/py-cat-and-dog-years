a699d9ba5d7c3162fd7b02c9585850fa639b9e1d
Ivan <mozharivan17@gmail.com>
2023-10-30 03:53:57 +0200

    Fixed solution

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
-    cat_to_human = convert_to_human(cat_age, 15, 9, 4)
-    dog_to_human = convert_to_human(dog_age, 15, 9, 5)
-    return [cat_to_human, dog_to_human]
+    if (isinstance(cat_age, int) and isinstance(dog_age, int)
+            and cat_age > 0 and dog_age > 0):
+        cat_to_human = convert_to_human(cat_age, 15, 9, 4)
+        dog_to_human = convert_to_human(dog_age, 15, 9, 5)
+        return [cat_to_human, dog_to_human]
+    else:
+        raise ValueError("Animal age should be positive integer")
 
 
 def convert_to_human(
--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,62 @@
 import pytest
+
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize("cat_age, dog_age, expected", [
-    (0, 0, [0, 0]),
-    (14, 14, [0, 0]),
-    (15, 15, [1, 1]),
-    (23, 23, [1, 1]),
-    (24, 24, [2, 2]),
-    (27, 28, [2, 2]),
-    (28, 29, [3, 3]),
-    (100, 100, [21, 17]),
-])
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        pytest.param(
+            14, 14, [0, 0],
+            id="should add zeros with animal ages less than 15"
+        ),
+        pytest.param(
+            23, 23, [1, 1],
+            id="add one when animal ages less than 24"
+        ),
+        pytest.param(
+            27, 28, [2, 2],
+            id="add one more year when dog is 28, cat is 27"
+        ),
+        pytest.param(
+            28, 29, [3, 3],
+            id="add one more year when dog is  28, cat is 27"
+        )
+    ]
+)
 def test_get_human_age(
         cat_age: int,
         dog_age: int,
         expected: list
 ) -> None:
-    if expected == "Invalid input":
-        with pytest.raises(ValueError):
-            get_human_age(cat_age, dog_age)
-    else:
-        result = get_human_age(cat_age, dog_age)
-        assert result == expected
+    result = get_human_age(cat_age, dog_age)
+    assert result == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_error",
+    [
+        pytest.param(
+            -1, -1,
+            ValueError,
+            id="should raise value error when animal ages less than 0"
+        ),
+        pytest.param(
+            "string", "string",
+            ValueError,
+            id="should raise value error when string is inputted"
+        ),
+        pytest.param(
+            1.1, 1.1,
+            ValueError,
+            id="should raise value error when float is inputted"
+        )
+    ]
+)
+def test_get_human_age_invalid_input(
+        cat_age: int,
+        dog_age: int,
+        expected_error: ValueError
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
