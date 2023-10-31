982bc335e413002ee3d766f9b5fb750336c6be54
Andrii Silenko <andrewformicr@gmail.com>
2023-10-31 06:53:54 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,63 @@
 from app.main import get_human_age
+import pytest
+from typing import Any
 
 
-def test_age_under_first_year_convert():
-    assert get_human_age(14, 14) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_result",
+    [
+        pytest.param(
+            14, 14, [0, 0],
+            id="test zero years human age limit"
+        ),
+        pytest.param(
+            23, 23, [1, 1],
+            id="test one year human age limit"
+        ),
+        pytest.param(
+            27, 27, [2, 2],
+            id="test two years human age limit"
+        ),
+        pytest.param(
+            40, 50, [6, 7],
+            id="test with big ages"
+        ),
+        pytest.param(
+            -2, -2, [0, 0],
+            id="test with negative ages"
+        ),
+
+        pytest.param(
+            0, 0, [0, 0],
+            id="test with ages as zero"
+        )
+    ]
+)
+def test_get_human_age(cat_age: int,
+                       dog_age: int,
+                       expected_result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_result
 
 
-def test_age_more_than_first_year_convert_and_less_than_second_year():
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_age_more_than_second_year_and_less_than_third_year():
-    assert get_human_age(25, 25) == [2, 2]
-
-
-def test_age_more_than_third_year():
-    assert get_human_age(40, 50) == [6, 7]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_result",
+    [
+        pytest.param(
+            "", "", TypeError,
+            id="test with strings as ages"
+        ),
+        pytest.param(
+            [], [], TypeError,
+            id="test with list as ages"
+        ),
+        pytest.param(
+            {}, {}, TypeError,
+            id="test with dict as ages"
+        ),
+    ]
+)
+def test_incorrect_type_ages(cat_age: Any,
+                             dog_age: Any,
+                             expected_result: Exception) -> None:
+    with pytest.raises(expected_result):
+        get_human_age(cat_age, dog_age)
