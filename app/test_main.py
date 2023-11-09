b2e44c07d017949220232dec944b1ab0b5a24f51
Hryhoriy <hryhoriy.bilous@gmail.com>
2023-11-09 17:09:11 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,6 +5,7 @@ import pytest
 @pytest.mark.parametrize(
     "cat_age, dog_age, converted_age",
     [
+        (-1, -1, [0, 0]),
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
@@ -21,3 +22,13 @@ def test_human_age_converted_from_cat_and_dog(
         converted_age: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == converted_age
+
+
+def test_correct_cat_age_type(cat_age: int) -> None:
+    if not isinstance(cat_age, int):
+        assert TypeError, f"{cat_age} must be integer type"
+
+
+def test_correct_dog_age_type(dog_age: int) -> None:
+    if not isinstance(dog_age, int):
+        assert TypeError, f"{dog_age} must be integer type"
