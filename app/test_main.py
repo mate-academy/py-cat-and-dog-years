1f49f4d41d49905a049a6faf530ecb9ec8aab127
Illya Maznitskiy <comercaleuros@gmail.com>
2023-11-11 16:04:04 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -16,7 +16,19 @@ class TestAgeForAnimal:
             (24, 24, [2, 2]),
             (27, 27, [2, 2]),
             (28, 28, [3, 2]),
-            (100, 100, [21, 17])
+            (100, 100, [21, 17]),
+            (-1, 5, [0, 0]),
+            (5, -1, [0, 0]),
+            (0, 5, [0, 0]),
+            (5, 0, [0, 0]),
+            (1000, 2000, [246, 397]),
+            (5, 5, [0, 0]),
+            (2, 7, [0, 0]),
+            (10, 10, [0, 0]),
+            (19, 20, [1, 1]),
+            (23, 25, [1, 2]),
+            (5, 5, [0, 0]),
+            (2, 7, [0, 0])
         ]
     )
     def test_get_age_correctly(
@@ -27,70 +39,6 @@ class TestAgeForAnimal:
     ) -> None:
         assert get_human_age(cat_age, dog_age) == expected
 
-    @pytest.mark.parametrize(
-        "cat_age, dog_age, expected",
-        [
-            (-1, 5, [0, 0]),
-            (5, -1, [0, 0]),
-            (0, 5, [0, 0]),
-            (5, 0, [0, 0]),
-            (1000, 2000, [246, 397])
-        ]
-    )
-    def test_edge_cases(
-            self,
-            cat_age: int,
-            dog_age: int,
-            expected: list
-    ) -> None:
-        assert get_human_age(cat_age, dog_age) == expected
-
-    @pytest.mark.parametrize(
-        "cat_age, dog_age",
-        [
-            (5, 5),
-            (2, 7),
-        ]
-    )
-    def test_normal_cases(
-            self,
-            cat_age: int,
-            dog_age: int,
-    ) -> None:
-        assert get_human_age(cat_age, dog_age) == [0, 0]
-
-    @pytest.mark.parametrize(
-        "cat_age, dog_age, expected",
-        [
-            (10, 10, [0, 0]),
-            (19, 20, [1, 1]),
-            (23, 25, [1, 2]),
-        ]
-    )
-    def test_additional_cases(
-            self,
-            cat_age: int,
-            dog_age: int,
-            expected: list
-    ) -> None:
-        assert get_human_age(cat_age, dog_age) == expected
-
-
-class TestIntegerInput:
-    @pytest.mark.parametrize(
-        "cat_age, dog_age",
-        [
-            (5, 5),
-            (2, 7)
-        ]
-    )
-    def test_integer_inputs(
-            self,
-            cat_age: int,
-            dog_age: int,
-    ) -> None:
-        assert get_human_age(cat_age, dog_age) == [0, 0]
-
 
 class TestErrorCases:
     @pytest.mark.parametrize(
@@ -98,7 +46,6 @@ class TestErrorCases:
         [
             ("A", 5),
             (10, [5, 6]),
-            (True, 3)
         ]
     )
     def test_error_cases(
