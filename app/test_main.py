a3de3445df73ef487d6f63ae03334298c32d3592
Alex <oleksa.shcherbakov@gmail.com>
2023-11-16 16:10:33 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,19 +5,14 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, result",
     [
-        (0 , 0 , [0,0]),
-        (14, 14,  [0,0]),
-        (15, 15, [1, 1]),
-        (24, 24, [2, 2]),
+(0 , 0 , [0,0]),
+    (14, 14,  [0,0]),
+    (15, 15, [1, 1]),
+    (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
     ]
 )
-def test_pets_years(                    cat_age: int,
-                    dog_age: int,
-                    result: list
-                    ) -> None:
-    assert (
-        get_human_age(cat_age, dog_age) == result
-    ), f"Age of cat {cat_age} and dog {dog_age} should be equal to human age {result}"
+def test_pets_years(                    cat_age: int,                    dog_age: int,                     result: list                    ) -> None:
+    assert get_human_age(cat_age, dog_age) == result
