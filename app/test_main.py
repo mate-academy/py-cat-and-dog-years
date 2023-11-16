f0a7e64b91ba5161d3632bee4771ae25ef18f2f1
Alex <oleksa.shcherbakov@gmail.com>
2023-11-16 16:27:51 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,8 +5,8 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, result",
     [
-        (0 , 0 , [0,0]),
-        (14, 14,  [0, 0]),
+        (0 , 0 , [0, 0]),
+        (14, 14, [0, 0]),
         (15, 15, [1, 1]),
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
