930f6e7a6386a0914f49e15c2a848f0e6a79b668
makukh <makukh@gmail.com>
2023-11-17 21:09:58 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,19 +2,6 @@ import pytest
 from app.main import get_human_age
 
 
-# def test_should_raise_error_when_age_not_integer() -> None:
-#     with pytest.raises(TypeError):
-#         get_human_age("five", "0.79")
-
-
-# Test below could be implemented, but main.py doesn't pass it,
-# while function takes negative numbers and returns zeros.
-#
-# def test_should_raise_error_when_age_is_negative() -> None:
-#     with pytest.raises(ValueError):
-#         get_human_age(-1, -17)
-
-
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
