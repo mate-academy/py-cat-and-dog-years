d71690a34b268840820f685bbede5393fb4c9f99
Yuliia Dudych <juliadudych@gmail.com>
2023-11-17 19:43:11 +0200

    Add tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_0_if_animals_are_younger_than_15_years() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_1_if_animals_are_younger_than_24_years() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_return_correct_age_if_animals_older_than_24_years() -> None:
+    assert get_human_age(100, 100) == [21, 17]
