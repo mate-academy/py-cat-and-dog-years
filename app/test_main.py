19b19835fd58b99e841144ece66296ba3551c056
Yuliia Dudych <juliadudych@gmail.com>
2023-11-18 12:34:18 +0200

    Combine test functions into one using parametrize decorator

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,27 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_should_return_0_if_animals_are_younger_than_15_years() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_should_return_1_if_animals_are_younger_than_24_years() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_should_return_correct_age_if_animals_older_than_24_years() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        pytest.param(
+            14, 14, [0, 0],
+            id="should return 0 if animals are younger than 15 years"
+        ),
+        pytest.param(
+            23, 23, [1, 1],
+            id="should return 1 if animals are between 15 and 23 years old"
+        ),
+        pytest.param(
+            100, 100, [21, 17],
+            id="should return correct age if animals older than 24 years"
+        ),
+    ]
+)
+def test_calculates_age_correctly(
+        cat_age: int,
+        dog_age: int,
+        result: int
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result
