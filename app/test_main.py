a02e80ff52583721b3d8ae0f7cfd56f8c51cd1c5
Serhii <serhii.domik@gmail.com>
2023-11-23 19:49:20 +0200

    'Fixed'

--- app/test_main.py
+++ app/test_main.py
@@ -9,7 +9,7 @@ class TestCatAndDogYear:
         [
             pytest.param(
                 0, 0, [0, 0],
-                id="should return list if cat and dog ages equal 0"
+                id="should return list [0, 0] if cat and dog ages equal 0"
             ),
             pytest.param(
                 14, 14, [0, 0],
@@ -45,6 +45,70 @@ class TestCatAndDogYear:
         self,
         cat_age: int,
         dog_age: int,
-        expected: list
+        expected: list[int]
     ) -> None:
         assert get_human_age(cat_age, dog_age) == expected
+
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, excepted_error",
+        [
+            pytest.param(
+                "15", 15,
+                TypeError,
+                id="should raise error when attribute type is str"
+            ),
+            pytest.param(
+                15, "15",
+                TypeError,
+                id="should raise error when attribute type is str"
+            ),
+            pytest.param(
+                None, 15,
+                TypeError,
+                id="should raise error when attribute type is None"
+            ),
+            pytest.param(
+                15, None,
+                TypeError,
+                id="should raise error when attribute type is None"
+            ),
+            pytest.param(
+                (15,), 15,
+                TypeError,
+                id="should raise error when attribute type is tuple"
+            ),
+            pytest.param(
+                15, (15,),
+                TypeError,
+                id="should raise error when attribute type is tuple"
+            ),
+            pytest.param(
+                15, [15],
+                TypeError,
+                id="should raise error when attribute type is list"
+            ),
+            pytest.param(
+                [15], 15,
+                TypeError,
+                id="should raise error when attribute type is list"
+            ),
+            pytest.param(
+                15, {15},
+                TypeError,
+                id="should raise error when attribute type is dict"
+            ),
+            pytest.param(
+                {15}, 15,
+                TypeError,
+                id="should raise error when attribute type is dict"
+            ),
+        ]
+    )
+    def test_get_human_age_invalid_input(
+            self,
+            dog_age: int,
+            cat_age: int,
+            excepted_error: Exception
+    ) -> None:
+        with pytest.raises(excepted_error):
+            get_human_age(cat_age, dog_age)
