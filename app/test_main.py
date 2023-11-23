7ac7b0bb89616888ab616ae602ecfce1cb9c1f04
Serhii <serhii.domik@gmail.com>
2023-11-23 18:23:35 +0200

    'Fixed flake8'

--- app/test_main.py
+++ app/test_main.py
@@ -2,21 +2,22 @@ import pytest
 
 from app.main import get_human_age
 
+
 class TestCatAndDogYear:
     @pytest.mark.parametrize(
         "cat_age,dog_age,expected",
         [
             pytest.param(
                 0, 0, [0, 0],
-                id= "should return list if cat and dog ages equal 0"
+                id="should return list if cat and dog ages equal 0"
             ),
             pytest.param(
                 14, 14, [0, 0],
-                id = "should return list [0, 0] if cat and dog ages equal 14"
+                id="should return list [0, 0] if cat and dog ages equal 14"
             ),
             pytest.param(
                 15, 15, [1, 1],
-                id = "should return list [1, 1] if cat and dog ages equal 15"
+                id="should return list [1, 1] if cat and dog ages equal 15"
             ),
             pytest.param(
                 23, 23, [1, 1],
@@ -42,8 +43,8 @@ class TestCatAndDogYear:
     )
     def test_get_human_age(
         self,
-        cat_age,
-        dog_age,
-        expected
-    ):
+        cat_age: int,
+        dog_age: int,
+        expected: list
+    ) -> None:
         assert get_human_age(cat_age, dog_age) == expected
