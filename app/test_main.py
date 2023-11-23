2a036e4ee0bd65f3ab68620e19c76471a86ad639
Andrii Homeniuk <andreygomenuk@gmail.com>
2023-11-23 15:44:23 +0200

    Added 23, 23 test.

--- app/test_main.py
+++ app/test_main.py
@@ -9,6 +9,7 @@ class TestGetHumanAge:
             (0, 0, 0, 0),
             (14, 14, 0, 0),
             (15, 15, 1, 1),
+            (23, 23, 1, 1),
             (24, 24, 2, 2),
             (28, 29, 3, 3),
             (100, 100, 21, 17)
@@ -17,6 +18,7 @@ class TestGetHumanAge:
             "0 cat/dog years should convert into 0 human age.",
             "14 cat/dog years should convert into 0 human age.",
             "15 cat/dog years should convert into 1 human age.",
+            "23 cat/dog years should convert into 1 human age.",
             "24 cat/dog years should convert into 2 human age.",
             "28/29 cat/dog years should convert into 3/3 human age.",
             "100/100 cat/dog years should convert into 21/17 human age."
