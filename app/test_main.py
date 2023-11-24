e59d20a4102a86c85b3e6eb49ce405ff4d8924b5
Marinel444 <380978907444m@gmail.com>
2023-11-24 12:43:27 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_initial_conditions() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_ages_before_first_threshold() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_first_threshold() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_second_threshold() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_third_threshold() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_larger_ages() -> None:
+    assert get_human_age(100, 100) == [21, 17]
