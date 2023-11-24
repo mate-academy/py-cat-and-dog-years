00b1515a4014d0dba23ca252f6c2477e7b421682
Marinel444 <380978907444m@gmail.com>
2023-11-24 18:00:01 +0200

    add pytest.mark.parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,55 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_initial_conditions() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, result",
+        [
+            pytest.param(
+                0,
+                0,
+                [0, 0],
+                id="initial conditions"
+            ),
+            pytest.param(
+                14,
+                14,
+                [0, 0],
+                id="ages before first threshold"
+            ),
+            pytest.param(
+                15,
+                15,
+                [1, 1],
+                id="first threshold"
+            ),
+            pytest.param(
+                24,
+                24,
+                [2, 2],
+                id="second threshold"
+            ),
+            pytest.param(
+                28,
+                28,
+                [3, 2],
+                id="third threshold"
+            ),
+            pytest.param(
+                100,
+                100,
+                [21, 17],
+                id="larger ages"
+            ),
 
-
-def test_ages_before_first_threshold() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_first_threshold() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_second_threshold() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_third_threshold() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_larger_ages() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+        ]
+    )
+    def test_get_human_age(
+            self,
+            cat_age: int,
+            dog_age: int,
+            result: list
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == result
