d2f0af14e188add3a1d2c817b57aabda3c7803c3
unknown <temakot254@gmail.com>
2023-11-25 12:28:03 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,6 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize("cat_age, dog_age, expected", [
-    # Test cases for positive values
     (0, 0, [0, 0]),
     (14, 14, [0, 0]),
     (15, 15, [1, 1]),
@@ -19,13 +18,11 @@ from app.main import get_human_age
 def test_get_human_age_positive(
         cat_age: int,
         dog_age: int,
-        expected: int
+        expected: list
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
 
 def test_get_human_age_invalid() -> None:
-    # Use pytest.raises to check that the function raises the correct exception
     with pytest.raises(TypeError):
-        # Pass a string as an argument
         get_human_age("cat", "dog")
