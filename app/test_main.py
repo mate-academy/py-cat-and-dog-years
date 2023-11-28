2f023eb335c9ec14a62630a794dfcd4a7e689000
MykolaMazhula <mykm3ua@gmail.com>
2023-11-28 12:54:35 +0100

    Pets -> human

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_ages",
+        [
+            pytest.param(
+                14, 14, [0, 0], id= "test converting first 14 into 0"
+            ),
+            pytest.param(
+                23, 23, [1, 1], id="test converting first 15-23 into 1"
+            ),
+            pytest.param(
+                100, 100, [21, 17], id="test converting age > 24"
+            )
+        ]
+    )
+    def test_convert_age_to_human(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_ages: list[int]
+    ):
+        assert get_human_age(cat_age, dog_age) == expected_ages
+
