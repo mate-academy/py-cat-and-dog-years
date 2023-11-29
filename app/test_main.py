f1427cad1a37b3db53931806f0816dc52f5793aa
Vishnevskyi Dmytro <vishneman@gmail.com>
2023-11-29 13:33:44 +0200

    Solution for "py_cat_and_dog_years"

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,53 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestCalculateLimitValue:
+    @pytest.mark.parametrize(
+        "initial_cat_age, initial_dog_age, expected_result",
+        [
+            pytest.param(
+                0,
+                0,
+                [0, 0],
+                id="When an animals years is zero"
+            ),
+            pytest.param(
+                14,
+                15,
+                [0, 1],
+            ),
+            pytest.param(
+                23,
+                24,
+                [1, 2],
+            ),
+            pytest.param(
+                24,
+                30,
+                [2, 3],
+                id="When an animals years greeter than a human"
+            ),
+            pytest.param(
+                27,
+                28,
+                [2, 2],
+                id="Discard the remainder result"
+            ),
+            pytest.param(
+                28,
+                29,
+                [3, 3],
+                id="Discard the remainder result"
+            )
+        ]
+    )
+    def test_calculate_limit_value(
+            self,
+            initial_cat_age,
+            initial_dog_age,
+            expected_result
+    ) -> None:
+        assert (get_human_age(initial_cat_age, initial_dog_age)) == expected_result
+
