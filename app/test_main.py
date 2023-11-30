cf37a4937b5abd16b4fa53f25af56082a90f34d5
MykolaMazhula <mykm3ua@gmail.com>
2023-11-30 08:29:22 +0100

    Added raising errors check

--- app/test_main.py
+++ app/test_main.py
@@ -17,7 +17,10 @@ class TestGetHumanAge:
             ),
             pytest.param(
                 83, 83, [16, 13], id="test result rounding floor"
-            )
+            ),
+            pytest.param(
+                0, 0, [0, 0], id="test age 0"
+            ),
         ]
     )
     def test_convert_age_to_human(
@@ -27,3 +30,25 @@ class TestGetHumanAge:
             expected_ages: list[int]
     ) -> None:
         assert get_human_age(cat_age, dog_age) == expected_ages
+
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_error",
+        [
+            pytest.param(
+                -10, -42, ValueError,
+                id="raise an error with negative initial data"
+            ),
+            pytest.param(
+                120, 120, ValueError,
+                id="raise an error with unlikely initial data"
+            ),
+        ]
+    )
+    def test_raising_errors(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_error: Exception
+    ) -> None:
+        with pytest.raises(expected_error):
+            get_human_age(cat_age, dog_age)
