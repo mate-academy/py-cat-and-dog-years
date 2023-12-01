de92a2972e1b8fafcab02173a8007f8339d7d80c
Vladyslav Moiseienko <moiseenkov651@gmail.com>
2023-12-01 15:04:28 +0200

    Correction

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,4 @@
 import pytest
-from typing import Type
 
 from app.main import get_human_age
 
@@ -23,21 +22,3 @@ def test_get_human_age(
         expected: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected_error",
-    [
-        pytest.param("15", 15, TypeError, id="String instead of integer"),
-        pytest.param(15, "15", TypeError, id="String instead of integer"),
-        pytest.param(15.5, 15, TypeError, id="Float instead of integer"),
-        pytest.param(15, 15.5, TypeError, id="Float instead of integer"),
-    ]
-)
-def test_get_human_age_wrong_types(
-        cat_age: int,
-        dog_age: int,
-        expected_error: Type[BaseException]
-) -> None:
-    with pytest.raises(expected_error):
-        get_human_age(cat_age, dog_age)
