8009b202623ba04678cc231c85b00f56322bb2ef
Vladyslav Moiseienko <moiseenkov651@gmail.com>
2023-12-01 13:37:05 +0200

    Adding mark.parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,20 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_get_human_age_for_zero_years() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_get_human_age_for_less_than_15_years() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_get_human_age_for_15_years() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_get_human_age_for_less_than_24_years() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_get_human_age_for_24_years() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_get_human_age_for_less_than_28_years() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_get_human_age_for_28_years() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_get_human_age_for_100_years() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        pytest.param(0, 0, [0, 0], id="Zero years"),
+        pytest.param(14, 14, [0, 0], id="Less than 15 years"),
+        pytest.param(15, 15, [1, 1], id="15 years"),
+        pytest.param(23, 23, [1, 1], id="Less than 24 years"),
+        pytest.param(24, 24, [2, 2], id="24 years"),
+        pytest.param(27, 27, [2, 2], id="Less than 28 years"),
+        pytest.param(28, 28, [3, 2], id="28 years"),
+        pytest.param(100, 100, [21, 17], id="100 years")
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
