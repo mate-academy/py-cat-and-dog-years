ee0dc8c478172e075b4484c99a90ae4264c22a14
xboris <boris.tikhonov.21@gmail.com>
2023-12-03 21:03:54 +0200

    append two tests

--- app/test_main.py
+++ app/test_main.py
@@ -53,3 +53,30 @@ class TestGetHumanAge:
                                                    expected_result: int
                                                    ) -> None:
         assert get_human_age(cat_age, dog_age) == expected_result
+
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_result",
+        [
+            (0, 0, [0, 0]),
+            (-1, -1, [0, 0]),
+            (-12, -31, [0, 0]),
+            (12412, 42125, [3099, 8422])
+        ]
+    )
+    def test_input_age_negative_or_zero(self,
+                                        cat_age: int,
+                                        dog_age: int,
+                                        expected_result: list) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_result
+
+    @pytest.mark.parametrize(
+        "cat_age,dog_age",
+        [
+            (12, "124"),
+            ("21", 14),
+            (["dada"], {"dog": "12"})
+        ]
+    )
+    def test_incorrected_input_age(self, cat_age, dog_age):
+        with pytest.raises(TypeError):
+            get_human_age(cat_age, dog_age)
