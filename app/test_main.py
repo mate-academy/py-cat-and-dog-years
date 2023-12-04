2dfcf3c4bd272f47f094f98ab166369eb9ce7fbb
Denysov Viacheslav <wdenisov113@gmail.com>
2023-12-04 13:58:24 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,6 +4,9 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize("cat_age, dog_age, expected_result", [
+    (-1, 3, [0, 0]),      # Test negative cat_age
+    (2, -5, [0, 0]),      # Test negative dog_age
+    (-3, -2, [0, 0]),     # Test negative cat_age and dog_age
     (0, 0, [0, 0]),
     (14, 14, [0, 0]),
     (15, 15, [1, 1]),
@@ -12,14 +15,12 @@ from app.main import get_human_age
     (27, 27, [2, 2]),
     (28, 28, [3, 2]),
     (28, 29, [3, 3]),
-    (100, 100, [21, 17])
+    (100, 100, [21, 17]),
 ])
 def test_get_human_age(
         cat_age: int,
         dog_age: int,
         expected_result: list
 ) -> None:
-    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
-        raise TypeError("Arguments should be int type")
-    assert get_human_age(cat_age, dog_age) == expected_result
-    assert isinstance(get_human_age(cat_age, dog_age), list)
+    result = get_human_age(cat_age, dog_age)
+    assert result == expected_result
