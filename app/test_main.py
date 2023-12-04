ddd5d2fea2bee5a2f49cd69fe223daf2c5cbc1b8
Vlad <mr.darmstadtium@gmail.com>
2023-12-04 19:58:34 +0100

    wrote all code

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,24 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, doge_age, result",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+
+    ]
+)
+def test_get_human_age(
+        cat_age: int,
+        doge_age: int,
+        result: list) -> None:
+    assert (
+        get_human_age(cat_age, doge_age) == result
+    ), (f"Function get_human_age should return {result}, "
+        f"got {get_human_age(cat_age, doge_age)} instead")
