cfca3305fc2aa80e7ecb90282f5e1d0f0894c847
Ryshtun Denys <rishtund@gmail.com>
2023-12-05 12:48:28 +0200

    BDone

--- app/test_main.py
+++ app/test_main.py
@@ -2,24 +2,24 @@ from app.main import get_human_age, convert_to_human
 
 
 class TestGetHumanAge:
-    def test_should_also_take_float_as_argumen(self):
+    def test_should_also_take_float_as_argument(self) -> None:
         result = get_human_age(15.3, 15)
         assert result == [1, 1]
 
-    def test_should_be_equal_to_zero_when_age_less_then_15(self):
+    def test_should_be_equal_to_zero_when_age_less_then_15(self) -> None:
         result = get_human_age(14, 14)
         assert result == [0, 0]
 
-    def test_should_be_equal_to_1_when_age_less_then_24(self):
+    def test_should_be_equal_to_1_when_age_less_then_24(self) -> None:
         result = get_human_age(23, 23)
         assert result == [1, 1]
 
-    def test_should_return_different_values_when_age_28(self):
+    def test_should_return_different_values_when_age_28(self) -> None:
         result = get_human_age(28, 28)
         assert result == [3, 2]
 
 
 class TestConvertToHuman:
-    def test_should_also_take_float_as_argumen(self):
+    def test_should_also_take_float_as_argument(self) -> None:
         result = convert_to_human(17, 15, 9.4, 4.2)
         assert result == 1
