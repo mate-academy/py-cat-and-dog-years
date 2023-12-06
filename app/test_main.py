0cc8c96cf012b58f39b1a8447cc6a8d9130e7939
Ryshtun Denys <rishtund@gmail.com>
2023-12-06 11:35:24 +0200

    CDone

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,48 @@
+import pytest
+
 from app.main import get_human_age, convert_to_human
 
 
 class TestGetHumanAge:
-    def test_should_also_take_float_as_argument(self) -> None:
-        result = get_human_age(15.3, 15)
-        assert result == [1, 1]
+    @pytest.mark.parametrize(
+        "input_cats, input_dogs, expected_result",
+        [
+            pytest.param(
+                15.3,
+                22.9,
+                [1, 1],
+                id="should return correct list if "
+                   "float in input: cat=15.3, dog=22.9"
+            ),
+            pytest.param(
+                14,
+                14,
+                [0, 0],
+                id="should return correct list if cat and dog input: 14"
+            ),
+            pytest.param(
+                23,
+                23,
+                [1, 1],
+                id="should return correct list if cat and dog input: 23"
+            ),
+            pytest.param(
+                28,
+                28,
+                [3, 2],
+                id="should return correct list if cat and dog input: 28"
+            )
+        ]
+    )
+    def test_correct_results(
+            self,
+            input_cats: int,
+            input_dogs: int,
+            expected_result: list[int]
+    ) -> None:
 
-    def test_should_be_equal_to_zero_when_age_less_then_15(self) -> None:
-        result = get_human_age(14, 14)
-        assert result == [0, 0]
-
-    def test_should_be_equal_to_1_when_age_less_then_24(self) -> None:
-        result = get_human_age(23, 23)
-        assert result == [1, 1]
-
-    def test_should_return_different_values_when_age_28(self) -> None:
-        result = get_human_age(28, 28)
-        assert result == [3, 2]
+        result = get_human_age(input_cats, input_dogs)
+        assert result == expected_result
 
 
 class TestConvertToHuman:
