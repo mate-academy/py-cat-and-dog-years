bea11617c0717f7b3d8db19006f421a25f353f0c
Bohdan Horbachov <bgbro.inc@gmail.com>
2023-12-18 21:38:00 +0200

    added tests for convert_to_human func

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize("cat_age, dog_age, expected_result", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (23, 23, [1, 1]),
+    (24, 24, [2, 2]),
+    (27, 27, [2, 2]),
+    (28, 28, [3, 2]),
+    (100, 100, [21, 17]),
+    (-1, 10, [0, 0]),
+    (10, -1, [0, 0]),
+    (0, 10, [0, 0]),
+    (10, 0, [0, 0]),
+    (1000, 1000, [246, 197]),
+    ("cat", 10, TypeError),
+    (10, "dog", TypeError),
+])
+def test_get_human_age(cat_age, dog_age, expected_result):
+    if expected_result == TypeError:
+        with pytest.raises(TypeError):
+            get_human_age(cat_age, dog_age)
+    else:
+        result = get_human_age(cat_age, dog_age)
+        assert result == expected_result
