8bc21fbb28f85ac63d3472827ed48fb5c4d9d519
Bohdan Horbachov <bgbro.inc@gmail.com>
2023-12-21 12:34:50 +0200

    fixed mistakes

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
 import pytest
-from app.main import get_human_age
 from typing import Any
+
+from app.main import get_human_age
 
 
 @pytest.mark.parametrize("cat_age, dog_age, expected_result", [
@@ -12,22 +13,28 @@ from typing import Any
     (27, 27, [2, 2]),
     (28, 28, [3, 2]),
     (100, 100, [21, 17]),
-    (-1, 10, [0, 0]),
-    (10, -1, [0, 0]),
-    (0, 10, [0, 0]),
-    (10, 0, [0, 0]),
-    (1000, 1000, [246, 197]),
-    ("cat", 10, TypeError),
-    (10, "dog", TypeError),
+    (5, 10, [0, 0]),
+    (8, 15, [0, 1]),
+    (20, 10, [1, 0]),
+    (15, 8, [1, 0]),
 ])
-def test_get_human_age(
-        cat_age: int,
-        dog_age: int,
-        expected_result: Any
+def test_get_human_age_with_expected_values(
+    cat_age: int, dog_age: int, expected_result: Any
 ) -> None:
-    if expected_result == TypeError:
-        with pytest.raises(TypeError):
-            get_human_age(cat_age, dog_age)
-    else:
-        result = get_human_age(cat_age, dog_age)
-        assert result == expected_result
+    result = get_human_age(cat_age, dog_age)
+    assert result == expected_result
+
+
+@pytest.mark.parametrize("cat_age, dog_age, expected_error", [
+    ([5], 10, TypeError),
+    (8, {15}, TypeError),
+    ((20,), 10, TypeError),
+    (15, [8], TypeError),
+    ({"cat": 5}, "dog", TypeError),
+    ("cat", {10}, TypeError),
+])
+def test_get_human_age_with_error_inputs(
+    cat_age: Any, dog_age: Any, expected_error: Exception
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
