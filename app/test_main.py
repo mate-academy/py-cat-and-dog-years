e0b0daedbe5049cc27207bcc96db09a7ff7f0f1d
Borys Kirichenko <boryskirichenko99@gmail.com>
2023-12-25 14:35:05 +0100

    flake8 fixed

--- app/test_main.py
+++ app/test_main.py
@@ -9,9 +9,9 @@ def test_should_return_one_year_when_age_less_then_24() -> None:
     assert get_human_age(23, 23) == [1, 1]
 
 
-def test_should_return_two_years_when_age_equal_27_for_cat_28_for_dog() -> None:
+def test_should_return_two_years_when_age_equal_27_and_28() -> None:
     assert get_human_age(27, 28) == [2, 2]
 
 
-def test_should_return_three_years_when_age_equal_28_for_cat_29_for_dog() -> None:
+def test_should_return_three_years_when_age_equal_28_and_29() -> None:
     assert get_human_age(28, 29) == [3, 3]
