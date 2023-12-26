5aa421527efbc519f4ce7169b1fbc69d7fa88a5d
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2023-12-26 17:11:07 +0200

    added tests for inappropriate input types (list, string etc)

--- app/test_main.py
+++ app/test_main.py
@@ -14,6 +14,7 @@ from app.main import get_human_age
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
+        (100, 150, [21, 27]),
         (-10, -10, [0, 0]),
         (-10, -150, [0, 0]),
         (10000, 10000, [2496, 1997]),
@@ -29,12 +30,17 @@ def test_first_15_years(cat_age: int, dog_age: int, result: list) -> None:
     "values,expected_error",
     [
         pytest.param(
-            {"cat_age" : "10", "dog_age": 10},
+            {"cat_age": "10", "dog_age": 10},
             TypeError,
-            id="should return type error!"
+            id="inappropriate data type!"
         ),
+        pytest.param(
+            {"cat_age": [10], "dog_age": [10]},
+            TypeError,
+            id="inappropriate data type!"
+        )
     ]
 )
-def test_incorrect_type() -> None:
+def test_incorrect_type(values: dict, expected_error: Exception) -> None:
     with pytest.raises(expected_error):
         get_human_age(**values)
