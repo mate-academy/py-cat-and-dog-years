bf413fa79092bfc4b7c1b90bb72bb98cbfa69b3b
artem.puhach <artem.puhach@cyberpolice.gov.ua>
2023-12-26 16:43:29 +0200

    added tests for inappropriate input types

--- app/test_main.py
+++ app/test_main.py
@@ -21,6 +21,20 @@ from app.main import get_human_age
 )
 def test_first_15_years(cat_age: int, dog_age: int, result: list) -> None:
     assert (
-        get_human_age(cat_age, dog_age) == result
-    ), "first 15 cat or dog years give 1 human year"
+            get_human_age(cat_age, dog_age) == result
+    ), "function should return appropriate value"
 
+
+@pytest.mark.parametrize(
+    "values,expected_error",
+    [
+        pytest.param(
+            {"cat_age" : "10", "dog_age": 10},
+            TypeError,
+            id="should return type error!"
+        ),
+    ]
+)
+def test_incorrect_type() -> None:
+    with pytest.raises(expected_error):
+        get_human_age(**values)
