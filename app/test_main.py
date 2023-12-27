9d6760cf535cd23450105764b19358a0c1f800da
Юрий <you@exampleabc1234567892777@gmail.com>
2023-12-27 15:17:41 +0200

    'Solution1'

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,21 @@
+import pytest
 from app.main import get_human_age
+from typing import List
 
 
-def test_get_human_age() -> None:
-
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(23, 23) == [1, 1]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(27, 27) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize("cat_age, dog_age, expected_result", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (23, 23, [1, 1]),
+    (24, 24, [2, 2]),
+    (27, 27, [2, 2]),
+    (28, 28, [3, 2]),
+    (100, 100, [21, 17]),
+])
+def test_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected_result: List[int]
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_result
