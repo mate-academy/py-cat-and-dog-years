8f7a65e57e72a778ae3241ec801906abdba96f54
Volodymyr Yablonskyi <wowa2807@gmail.com>
2024-01-06 19:37:58 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
 from app.main import get_human_age
 
-# write your code here
+
+def when_age_equals_zero() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_get_human_age_below_15_years() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_get_human_age_first_15_years() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_get_human_age_next_9_years() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_get_human_age_extra_human_year_cat() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_get_human_age_extra_human_year_dog() -> None:
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_get_human_age_extra_human_year_cat_and_dog() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_get_human_age_large_input() -> None:
+    assert get_human_age(100, 100) == [21, 17]
