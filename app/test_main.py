b58dc967c472ac31eb64fdb52f738241ba9a750c
eLQeR <131271125+eLQeR@users.noreply.github.com>
2024-01-08 15:52:55 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -105,3 +105,16 @@ def test_should_add_one_year_every_five_dog_years(
     assert get_human_age(cat_age, dog_age) == result, (
         "Function should add one year every four cat's years"
     )
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age",
+    [
+        (28, "word"),
+        ("True", 29)
+    ]
+)
+def test_should_take_int_arguments(cat_age: int, dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
+
