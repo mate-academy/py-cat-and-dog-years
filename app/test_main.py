f1e6088ce2406ec028b250f80d653ab0fdd8f72b
Ivan Romanov <i.romanov.dn@gmail.com>
2024-01-09 01:24:42 +0200

    fix solution

--- app/test_main.py
+++ app/test_main.py
@@ -9,13 +9,16 @@ from app.main import get_human_age
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (27, 27, [2, 2]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
+        (-50, 500000000000000000000000000000000000000000,
+         [0, 99999999999999999999999999999999999999997])
     ],
     ids=[
         "zero values",
         "round for first year",
         "second year",
-        "extra year"
+        "extra year",
+        "negative and big value"
     ]
 )
 def test_values_for_get_human_age_func(cat_age: int,
@@ -24,3 +27,8 @@ def test_values_for_get_human_age_func(cat_age: int,
     assert (get_human_age(cat_age, dog_age) == result), \
         (f"Human age for cat_age {cat_age} = {result[0]}, "
          f"for dog age {dog_age} = {result[1]}")
+
+
+def test_wrong_values_for_get_human_age_func() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("cat_age", [0])
