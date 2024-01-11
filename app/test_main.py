8cf95652a7990e74241b46e3e7f7c419af8b32e6
Yura Push <youdeimos1@gmail.com>
2024-01-11 18:22:49 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_zero_human_years():
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_almost_one_human_year():
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_cat_and_dog_one_human_year_old():
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_cat_and_dog_two_human_years_old():
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_cats_get_older_faster():
+    assert get_human_age(28, 28) == [3, 2], \
+        "Takes 4 cat/years and 5 dog/years for 1 human/year after they getting 2 years old."
+
+
+def test_bigger_age_difference_when_getting_older():
+    assert get_human_age(100, 100) == [21, 17]
