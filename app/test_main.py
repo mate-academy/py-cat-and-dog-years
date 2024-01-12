3db0102526d66588e45114063ac70afa41412b77
Olena Kalitsinska <olena.kalitsinka@gmail.com>
2024-01-12 15:22:42 +0200

    'Fixed2'

--- app/test_main.py
+++ app/test_main.py
@@ -2,17 +2,19 @@ from app.main import get_human_age
 import pytest
 
 
-@pytest.mark.parametrize("cat_age,dog_age,human_age",
-                         [
-                             (0, 0, [0, 0]),
-                             (14, 14, [0, 0]),
-                             (15, 15, [1, 1]),
-                             (23, 23, [1, 1]),
-                             (24, 24, [2, 2]),
-                             (27, 27, [2, 2]),
-                             (28, 28, [3, 2]),
-                             (100, 100, [21, 17])
-                         ])
+@pytest.mark.parametrize(
+    "cat_age,dog_age,human_age",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        (-28, -28, [0, 0])
+    ])
 def test_or_list_with_years_is_correct(
         cat_age: int,
         dog_age: int,
