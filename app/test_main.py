5e73dce30856e55d8605271ac55d85f8f248d0b7
Yura Push <youdeimos1@gmail.com>
2024-01-13 19:23:33 +0200

    -used ids parameter in parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -13,26 +13,26 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
-    ]
-)
-def test_check_first_two_years_and_difference_in_aging(
-        cat_age: int,
-        dog_age: int,
-        human_age: list
-) -> None:
-    assert get_human_age(cat_age, dog_age) == human_age
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, human_age",
-    [
+        (100, 100, [21, 17]),
         (-1, 28, [0, 2]),
         (28, -1, [3, 0]),
         (1000, 1060, [246, 209])
+    ],
+    ids=[
+        "zero for cat and dog years",
+        "check border for zero year",
+        "check first possible cat/dog age to human age",
+        "check border for first year",
+        "check lowest possible second year",
+        "check border for second year",
+        "difference in aging",
+        "check if difference gets bigger",
+        "negative number for cat_age",
+        "negative number for dog_age",
+        "enormously big numbers"
     ]
 )
-def test_cat_and_dog_ages_out_of_normal_range(
+def test_check_edging_situations_and_out_of_normal_range_input(
         cat_age: int,
         dog_age: int,
         human_age: list
