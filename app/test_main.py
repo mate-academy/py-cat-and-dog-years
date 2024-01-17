14d3e2e70d76e2566bedb07d3f9d453e24b1ccf3
Nataliia <natali4.kucher55@gmail.com>
2024-01-17 19:53:27 +0100

    Solution_1

--- app/test_main.py
+++ app/test_main.py
@@ -1,37 +1,37 @@
 from app.main import get_human_age
 
 
-def test_check_if_return_list():
+def test_check_if_return_list() -> None:
     age = get_human_age(0, 0)
 
     assert isinstance(age, list)
 
 
-def test_check_year_equil_null():
+def test_check_year_equil_null() -> None:
     age = get_human_age(0, 0)
 
     assert age == [0, 0]
 
 
-def test_check_year_less_than_15():
+def test_check_year_less_than_15() -> None:
     age = get_human_age(14, 14)
 
     assert age == [0, 0]
 
 
-def test_check_year_equil_15():
+def test_check_year_equil_15() -> None:
     age = get_human_age(15, 15)
 
     assert age == [1, 1]
 
 
-def test_check_year_after_9_years_more():
+def test_check_year_after_9_years_more() -> None:
     age = get_human_age(24, 24)
 
     assert age == [2, 2]
 
 
-def test_check_year_after_4_years_more():
+def test_check_year_after_4_years_more() -> None:
     age = get_human_age(28, 28)
 
     assert age == [3, 2]
