e014e071097e6a0b67cd681d2989475d725e4a5a
Vladyslav Pliashechnyk <vlad.pliashechnyk@gmail.com>
2024-01-18 16:30:18 +0200

    Added one more test

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,5 @@
 from app.main import get_human_age
-from pytest import mark
+from pytest import mark, raises, param
 
 
 @mark.parametrize(
@@ -14,6 +14,17 @@ from pytest import mark
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
         (-1, -1, [0, 0]),
+    ],
+    ids=[
+        "edge case: zero ages",
+        "edge case: boundary before conversion changes",
+        "boundary age for conversion to 1 human year",
+        "boundary age just before next conversion",
+        "age at second conversion to human years",
+        "age just before uneven conversion for cat and dog",
+        "age at uneven conversion for cat and dog",
+        "large age numbers",
+        "negative ages"
     ]
 )
 def test_should_return_correct_list(
@@ -22,3 +33,23 @@ def test_should_return_correct_list(
     expected_list: list
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected_list
+
+
+@mark.parametrize(
+    "cat_age, dog_age, expected_exception",
+    [
+        param(
+            "2",
+            [2],
+            TypeError,
+            id="Function should receive integers"
+        ),
+    ]
+)
+def test_raising_errors_correctly(
+        cat_age: str,
+        dog_age: dict,
+        expected_exception: Exception
+) -> None:
+    with raises(TypeError):
+        get_human_age(cat_age, dog_age)
