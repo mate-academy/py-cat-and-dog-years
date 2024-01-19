ae7f4b5ef7a4c23d75d0b6f29a82f5fcd03508e9
Arsen Rudnytskyy <acikz.33@google.com>
2024-01-19 17:08:43 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        pytest.param(-5, 14, [0, 0], id="first 15 animals years"),
+        pytest.param(15, 23, [1, 1], id="next 9 animals years"),
+        pytest.param(24, 27, [2, 2], id="animals ages between 24 and 27"),
+        pytest.param(100, 28, [21, 2], id="animals ages after 28")
+    ]
+)
+def test_get_human_age_with_different_animals_age_ranges(
+        cat_age: int,
+        dog_age: int,
+        result: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result
+
+
+def test_should_raise_error_when_input_data_is_not_an_integer() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("10", 10)
