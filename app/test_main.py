a7dfc5ef43cde2afe7043554ad09f405cfa0e9b4
Shcherbyna Svitlana <sw.shcherbina@gmail.com>
2024-01-19 14:57:12 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,14 +5,21 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, result",
     [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
+        (-5, 14, [0, 0]),
+        (15, 23, [1, 1]),
+        (24, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (28, 30, [3, 3]),
+        (1000, 999, [246, 197])
+    ],
+    ids=[
+        "human age should be 0 if cat or dog age is less than 15",
+        "human age should be 1 if cat or dog age is over 14 but less than 24",
+        "should give 1 more human year if dog or cat years "
+        "are equal or more than 24",
+        "should give 1 extra human year for every 4 cat years over 24",
+        "should give 1 extra human year for every 5 dog years over 24",
+        "should work with big numbers"
     ]
 )
 def test_should_return_correct_results(
