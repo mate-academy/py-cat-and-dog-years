70807f6a384e1a6f92502975db7bec6affe325dc
MaxymChyncha <max.chyncha@gmail.com>
2024-01-22 15:56:38 +0200

    Added description for each test case

--- app/test_main.py
+++ app/test_main.py
@@ -16,6 +16,18 @@ class TestGetHumanAge:
             (28, 28, [3, 2]),
             (100, 100, [21, 17]),
         ],
+        ids=[
+            "Should be zeros if years equal to 0",
+            "Should be zeros if years less than 15",
+            "Should be ones if years equal to 15",
+            "Should be ones if years less than 24",
+            "Should be twos if years equal to 24",
+            "Should be twos if years less than 28",
+            "Should be three for cat years and two for dog years "
+            "if years equal to 28",
+            "Should be 21 for cat years and 17 dog years "
+            "if years equal to 100",
+        ]
     )
     def test_human_age_correctly(
             self,
