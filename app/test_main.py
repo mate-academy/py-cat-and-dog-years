95d7f4c3cd59f467c155df6d064200224b533742
Alisa <alisasklrv@gmail.com>
2024-01-23 19:34:02 +0200

    fixed

--- app/test_main.py
+++ app/test_main.py
@@ -12,7 +12,8 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
+        (-10, -10, [0, 0])
     ]
 )
 def test_converts_age_correctly(
@@ -21,3 +22,10 @@ def test_converts_age_correctly(
         expected_result: list[int]) -> None:
     cat_to_human, dog_to_human = get_human_age(cat_age, dog_age)
     assert [cat_to_human, dog_to_human] == expected_result
+
+
+def test_raises_exception_correctly() -> None:
+    cat_age = "cat"
+    dog_age = "dog"
+    with pytest.raises(ValueError):
+        get_human_age(cat_age, dog_age)
