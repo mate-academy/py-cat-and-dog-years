bea8762f0d7a0cba3c068a77e9ac5b9273a9ba85
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-01-25 15:40:15 +0200

    Solved

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,14 @@
 from app.main import get_human_age
 
-# write your code here
+
+class TestGetHumanAge:
+    def test_return_list_with_zeros_when_less_first_years(self) -> None:
+        assert get_human_age(10, 10) == [0, 0]
+
+    def test_return_list_with_ones_when_less_first_second_years_sum(
+        self
+    ) -> None:
+        assert get_human_age(21, 19) == [1, 1]
+
+    def test_correct_calculating_years(self) -> None:
+        assert get_human_age(32, 46) == [4, 6]
