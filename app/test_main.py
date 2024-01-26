62f748307609391f092b55fec01db84178b84f9a
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-01-26 16:59:34 +0200

    Solved

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,28 @@
 from app.main import get_human_age
 
+import pytest
 
-class TestGetHumanAge:
-    def test_return_list_with_zeros_when_less_first_years(self) -> None:
-        assert get_human_age(10, 10) == [0, 0]
 
-    def test_return_list_with_ones_when_less_first_second_years_sum(
-        self
-    ) -> None:
-        assert get_human_age(21, 19) == [1, 1]
-
-    def test_correct_calculating_years(self) -> None:
-        assert get_human_age(32, 46) == [4, 6]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_human_age",
+    [
+        (0, 0, [0, 0]),
+        (16, 16, [1, 1]),
+        (25, 28, [2, 2]),
+        (14, 14, [0, 0]),
+        (44, 48, [7, 6]),
+    ],
+    ids=[
+        "0/0 cat/dog years should be convert to [0, 0]",
+        "16/16 cat/dog years should be convert to [1, 1]",
+        "25/28 cat/dog years should be convert to [2, 2]",
+        "14/14 cat/dog years should be convert to [0, 0]",
+        "44/48 cat/dog years should be convert to [7, 6]",
+    ],
+)
+def test_get_human_age(
+    cat_age: int,
+    dog_age: int,
+    expected_human_age: list[int, int],
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_human_age
