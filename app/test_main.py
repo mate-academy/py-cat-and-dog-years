f92b2aa0cd5a98d06697641c788777b2832af664
Dmytro Poplinskii <super.shepard2@gmail.com>
2024-01-27 17:00:54 +0200

    Deleted code in main.py to basic, refactored test cases

--- app/test_main.py
+++ app/test_main.py
@@ -11,8 +11,9 @@ import pytest
         (25, 28, [2, 2]),
         (14, 14, [0, 0]),
         (44, 48, [7, 6]),
-        (-10, -2, ValueError),
-        (1005, 1001, ValueError),
+        (-10, -2, [0, 0]),
+        (1005, 1001, [247, 197]),
+        ("dog", "cat", [247, 197]),
     ],
     ids=[
         "0/0 cat/dog years should be convert to [0, 0]",
@@ -20,8 +21,9 @@ import pytest
         "25/28 cat/dog years should be convert to [2, 2]",
         "14/14 cat/dog years should be convert to [0, 0]",
         "44/48 cat/dog years should be convert to [7, 6]",
-        "should return a ValueError with negative numbers",
-        "should return a ValueError with numbers larger 10000",
+        "should return a [0, 0] with negative numbers",
+        "1005/1001 cat/dog years should be convert to [247, 197]",
+        "should return a TypeError with other than 'int' types",
     ],
 )
 def test_get_human_age(
@@ -29,8 +31,8 @@ def test_get_human_age(
     dog_age: int,
     expected_human_age: list[int, int],
 ) -> None:
-    if (cat_age < 0 or cat_age > 1000) or (dog_age < 0 or dog_age > 1000):
-        with pytest.raises(ValueError):
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+        with pytest.raises(TypeError):
             get_human_age(cat_age, dog_age)
     else:
         assert get_human_age(cat_age, dog_age) == expected_human_age
