8b5ec346e8fe2b36fcbc8e80634b7099fb9ee1b4
Artem Chernyshov <achernyshovv1@gmail.com>
2024-01-29 14:10:06 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,17 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_zero_age() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_bellow_first_year() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_first_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_second_year() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_second_year_dog() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_more_years() -> None:
-    assert get_human_age(100, 100) == [21, 17]
-
-
-def test_age_between_first_second() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_age_between_second_third() -> None:
-    assert get_human_age(27, 27) == [2, 2]
+@pytest.mark.parametrize("cat_age, dog_age, expected_result", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (24, 24, [2, 2]),
+    (28, 28, [3, 2]),
+    (100, 100, [21, 17]),
+    (23, 23, [1, 1]),
+    (27, 27, [2, 2])
+])
+def test_get_human_age(cat_age, dog_age, expected_result):
+    assert get_human_age(cat_age, dog_age) == expected_result
