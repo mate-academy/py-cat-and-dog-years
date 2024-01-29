3d04a026e92152911ada0d5726a7e49953c8e4a0
Sikach Illia <55293120+isikach@users.noreply.github.com>
2024-01-29 12:20:33 +0200

    Update test_main.py
    1) Added more examples
    2) Added Type error as raise

--- app/test_main.py
+++ app/test_main.py
@@ -6,10 +6,14 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dogs_age, expected",
     [
-        (-10, 14, [0, 0]),
+        (-5, -7, [0, 0]),
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
-        (1000, 1000, [246, 197])
+        (24, 24, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
     ]
 )
 def test_get_human_age(
@@ -17,3 +21,8 @@ def test_get_human_age(
         dogs_age: int,
         expected: list[int]) -> None:
     assert get_human_age(cat_age, dogs_age) == expected
+
+
+def test_get_human_age_invalid() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("seven", "two")
