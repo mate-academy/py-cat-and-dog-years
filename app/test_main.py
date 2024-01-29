905e6bb173f2f3a3dcdfc129f4214e156071caee
Artem Hrushko <artemgrishko928w@gmail.com>
2024-01-29 15:44:37 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_years_to_convert, dog_years_to_convert, expected_years",
+    [
+        (0, 0, [0, 0]),
+        (24, 24, [2, 2]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1])
+    ],
+    ids=[
+        "should return [0, 0] if both years are 0",
+        "should return [2, 2] for cat and dog both 24 years",
+        "should return [1, 1] for cat and dog both 15 years",
+        "should return [1, 1] for cat and dog both 23 years"
+    ]
+)
+def test_when(
+        cat_years_to_convert: int,
+        dog_years_to_convert: int,
+        expected_years: tuple
+) -> None:
+    assert get_human_age(
+        cat_years_to_convert,
+        dog_years_to_convert
+    ) == expected_years
