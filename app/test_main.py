b780fbce516f482ace4cb2326a133fc523485c39
Artem Chernyshov <achernyshovv1@gmail.com>
2024-01-29 17:54:28 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,20 @@
 import pytest
-
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize("cat_age, dog_age, expected_result", [
-    (0, 0, [0, 0]),
-    (15, 15, [1, 1]),
-    (23, 23, [1, 1]),
-    (24, 24, [2, 2]),
-    (28, 28, [3, 2]),
-    (100, 100, [21, 17]),
-    (-1, -1, [0, 0]),
-    (500, 500, [121, 97])
-])
-def test_get_human_age(
-        cat_age: int,
-        dog_age: int,
-        expected_result: str
-) -> None:
-    assert get_human_age(cat_age, dog_age) == expected_result
+@pytest.mark.parametrize(
+    "cat_years, dog_years, expected, test_id",
+    [
+        (0, 0, [0, 0], "Test for 0 cat/dog years should return [0, 0]"),
+        (14, 14, [0, 0], "Test for 14 cat/dog years should return [0, 0]"),
+        (15, 15, [1, 1], "Test for 15 cat/dog years should return [1, 1]"),
+        (23, 23, [1, 1], "Test for 23 cat/dog years should return [1, 1]"),
+        (24, 24, [2, 2], "Test for 24 cat/dog years should return [2, 2]"),
+        (27, 27, [2, 2], "Test for 27 cat/dog years should return [2, 2]"),
+        (28, 28, [3, 2], "Test for 28 cat/dog years should return [3, 2]"),
+        (100, 100, [21, 17], "Test for 100 cat/dog years should return [21, 17]"),
+        (-1, -1, [0, 0], "Test for negative cat/dog years should return [0, 0]"),
+    ]
+)
+def test_get_human_age(cat_years, dog_years, expected, test_id):
+    assert get_human_age(cat_years, dog_years) == expected, test_id
