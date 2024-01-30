d8d4fd1b0ce7e87b5611818fc4e24fb8de668c8f
Nick Illich <nickillich96@gmail.com>
2024-01-30 19:37:47 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,30 +7,6 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected_result",
     [
-        (5, 10, [0, 0]),
-        (0, 0, [0, 0]),
-        (-1, -5, [0, 0]),
-        (14, 14, [0, 0])
-    ],
-    ids=[
-        "Should return zero when age is too small",
-        "Should return zero if age is zero",
-        "Should return zero if age is negative",
-        "Should return zero when age is too small"
-    ]
-)
-def test_should_return_zero(
-        cat_age: int,
-        dog_age: int,
-        expected_result: List[int]
-) -> None:
-
-    assert get_human_age(cat_age, dog_age) == expected_result
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected_result",
-    [
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
         (150, 150, [33, 27]),
@@ -38,13 +14,25 @@ def test_should_return_zero(
             int(13e29),
             int(7e14),
             [324999999999999985351879819260, 139999999999997]
-        )
+        ),
+        (5, 10, [0, 0]),
+        (0, 0, [0, 0]),
+        (-1, -5, [0, 0]),
+        (14, 14, [0, 0]),
+        (28, 28, [3, 2]),
+        (27, 33, [2, 3])
     ],
     ids=[
         "Should return value without extra years",
         "Should return value without extra years",
         "Should return value with extra years",
-        "Should return correct result for large numbers"
+        "Should return correct result for large numbers",
+        "Should return zero when age is too small",
+        "Should return zero if age is zero",
+        "Should return zero if age is negative",
+        "Should return zero when age is too small",
+        "Should correct value with or without extra years",
+        "Should correct value with or without extra years"
     ]
 )
 def test_should_return_correct_result(
@@ -69,6 +57,6 @@ def test_should_return_correct_result(
         "Should raise TypeError if age is dict",
     ]
 )
-def test_division(cat_age: int, dog_age: int, expected: List[int]) -> None:
+def test_division(cat_age: int, dog_age: int, expected: Exception) -> None:
     with expected:
         get_human_age(cat_age, dog_age)
