02023afa37826b22337f5be9458593ff3e709f36
Artem Hrushko <artemgrishko928w@gmail.com>
2024-02-02 15:10:13 +0200

    Solution2000

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,6 @@
-from app.main import get_human_age
 import pytest
+
+from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
@@ -8,13 +9,23 @@ import pytest
         (0, 0, [0, 0]),
         (24, 24, [2, 2]),
         (15, 15, [1, 1]),
-        (23, 23, [1, 1])
+        (23, 23, [1, 1]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        (14, 14, [0, 0]),
+        (1211213313, 11133131313, [302803324, 2226626259]),
+        (-12, -22, [0, 0])
     ],
     ids=[
         "should return [0, 0] if both years are 0",
         "should return [2, 2] for cat and dog both 24 years",
         "should return [1, 1] for cat and dog both 15 years",
-        "should return [1, 1] for cat and dog both 23 years"
+        "should return [1, 1] for cat and dog both 23 years",
+        "should return [3, 2] for cat and dog both 28 years",
+        "should return [21, 17] for cat and dog both 100 years",
+        "should return [0, 0] for cat and dog age, if age < 15",
+        "should return correct values for big ages",
+        "should return [0, 0], if age < 0"
     ]
 )
 def test_of_converting_animals_years_to_people_years(
@@ -31,11 +42,11 @@ def test_of_converting_animals_years_to_people_years(
 @pytest.mark.parametrize(
     "cat_years,dog_years,expected_error",
     [
-        (0, -1, ValueError),
+        ([1, 2, 3, 4], (22, 22), TypeError),
         ("hi", 13, TypeError)
     ],
     ids=[
-        "all ages numbers should be more than 0",
+        "all ages should have correct type: int",
         "all ages should be type: int"
     ]
 )
