dc3033fed655d0c05fff1b2c1e4a8e1fd1fc84ae
Oleksii Muhyr <1.2.aznch@gmai.com>
2024-02-06 12:13:12 +0200

    implement-py-cat-and-dog-years

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize("cat_age, dog_age, expected", [
+    (15, 15, [1, 1]),
+    (14, 14, [0, 0]),
+    (27, 28, [2, 2]),
+    (24, 24, [2, 2]),
+    (28, 29, [3, 3]),
+    (23, 23, [1, 1])
+]
+)
+def test_to_check_correct_conversion(cat_age: int, dog_age: int,
+                                     expected: list) -> None:
+    result = get_human_age(cat_age, dog_age)
+    assert result == expected
