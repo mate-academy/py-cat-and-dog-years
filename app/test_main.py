6010f0d725cc276f0c0416bae882939c147c54c3
Sofia Zholud <szholuds@gmail.com>
2024-02-06 12:14:17 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -2,24 +2,35 @@ import pytest
 from app.main import get_human_age
 
 
-def test_get_human_age() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(23, 23) == [1, 1]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(27, 27) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(100, 100) == [21, 17]
-    assert get_human_age(-1, 0) == [0, 0]
-    assert get_human_age(0, -1) == [0, 0]
-    assert get_human_age(-1, -1) == [0, 0]
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(1000, 1000) == [246, 197]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        (-1, 0, [0, 0]),
+        (0, -1, [0, 0]),
+        (-1, -1, [0, 0]),
+        (1000, 1000, [246, 197])
+    ],
+)
+def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
 
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("fifteen", 15),
+        (15, "fifteen"),
+        ("fifteen", "fifteen"),
+    ],
+)
+def test_get_human_age_type_error(cat_age: str, dog_age: str) -> None:
     with pytest.raises(TypeError):
-        get_human_age("fifteen", 15)
-    with pytest.raises(TypeError):
-        get_human_age(15, "fifteen")
-    with pytest.raises(TypeError):
-        get_human_age("fifteen", "fifteen")
+        get_human_age(cat_age, dog_age)
