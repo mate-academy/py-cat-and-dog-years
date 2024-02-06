c17a0c3eaa5467ae4d54fd26b1b0d8f4b8907343
sergey <38cerg12021979@jmal.com>
2024-02-06 17:19:21 +0200

    06_02_24_01

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,17 @@
 from app.main import get_human_age
 
 
+def test_should_return_human_age_less_first_year() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_convert_one_human_age() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_should_return_convert_two_human_age() -> None:
+    assert get_human_age(27, 28) == [2, 2]
+
+
 def test_should_return_human_age() -> None:
-    assert get_human_age(28, 28) == [3, 2]
+    assert get_human_age(100, 100) == [21, 17]
