8c517e1d422517a9cb0441fc0b0ddbf707fbff6d
Sofia Zholud <szholuds@gmail.com>
2024-02-06 12:10:01 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+import pytest
 from app.main import get_human_age
 
 
@@ -10,3 +11,15 @@ def test_get_human_age() -> None:
     assert get_human_age(27, 27) == [2, 2]
     assert get_human_age(28, 28) == [3, 2]
     assert get_human_age(100, 100) == [21, 17]
+    assert get_human_age(-1, 0) == [0, 0]
+    assert get_human_age(0, -1) == [0, 0]
+    assert get_human_age(-1, -1) == [0, 0]
+    assert get_human_age(0, 0) == [0, 0]
+    assert get_human_age(1000, 1000) == [246, 197]
+
+    with pytest.raises(TypeError):
+        get_human_age("fifteen", 15)
+    with pytest.raises(TypeError):
+        get_human_age(15, "fifteen")
+    with pytest.raises(TypeError):
+        get_human_age("fifteen", "fifteen")
