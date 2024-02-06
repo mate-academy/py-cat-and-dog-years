63562efc04c7d84b65f9e6d0f6f2b5c949e1e2d5
IvanGuculyak <ivan.guculyak@gmail.com>
2024-02-06 03:10:50 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,20 +4,21 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    ("cat_age", "dog_age", "result"),
+    ("cat_age", "dog_age", "human_age"),
     [
-        (0, 0, 0),
-        (14, 14, 0),
-        (15, 15, 1),
-        (23, 23, 1),
-        (24, 24, 2),
-        (27, 28, 2),
-        (28, 29, 3)
+        (-1, -1, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (28, 28, [3, 2]),
+        (29, 29, [3, 3]),
+        (100, 100, [21, 17])
     ]
 )
 def test_get_human_age(
         cat_age: int,
         dog_age: int,
-        result: int
+        human_age: list
 ) -> None:
-    assert set(get_human_age(cat_age, dog_age)) == {result}
+    assert set(get_human_age(cat_age, dog_age)) == human_age
