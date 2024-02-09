e2c6d8d942d8afd72db11fb8fa75c3b657af58d5
volry <v.v.ryzhuk@gmail.com>
2024-02-09 18:25:09 +0200

    working code

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,8 @@
+import pytest
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize("cat_age, dog_age, expected"
+@pytest.mark.parametrize("cat_age, dog_age, expected",
                          [
     (0, 0, [0, 0]),
     (14, 14, [0, 0]),
