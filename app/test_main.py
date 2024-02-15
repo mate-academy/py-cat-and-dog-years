da8fb49f16f25332bd059761c066e553daaf8c9e
RomanHlodann <roman.hlodann@gmail.com>
2024-02-15 19:07:40 +0200

    added new test

--- app/test_main.py
+++ app/test_main.py
@@ -11,6 +11,7 @@ from app.main import get_human_age
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
         (24, 24, [2, 2]),
+        (27, 28, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
     ]
@@ -21,3 +22,22 @@ def test_calculate_years_correctly(
         result: list
 ) -> None:
     assert get_human_age(cat_years, dog_years) == result
+
+
+@pytest.mark.parametrize(
+    "cat_years,dog_years,error",
+    [
+        (20, -1, ValueError),
+        (-1, -1, ValueError),
+        (-1, 15, ValueError),
+        (20, 0.5, TypeError),
+        ("-1", 20, TypeError),
+        (True, 15, TypeError)
+    ])
+def test_raises_error_if_age_negative(
+        cat_years: int,
+        dog_years: int,
+        error: type(TypeError)
+) -> None:
+    with pytest.raises(error):
+        get_human_age(cat_years, dog_years)
