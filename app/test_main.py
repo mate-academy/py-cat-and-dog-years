1d9ac3f65c8eae8ac7ee1fae128facd2a3486a80
Anna Zadorozhna <annalepiska@gmail.com>
2024-02-20 17:53:42 +0200

    Update tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,4 @@
+from typing import Type
 import pytest
 
 from app.main import get_human_age
@@ -11,6 +12,9 @@ from app.main import get_human_age
         (24, 27, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
+        (0, 0, [0, 0]),
+        (-5, -1, [0, 0]),
+        (10000, 10000, [2496, 1997])
     ],
     ids=[
         "animal age less than 15 should convert to 0",
@@ -18,6 +22,9 @@ from app.main import get_human_age
         "animal age from 24 to 27 should convert to 2",
         "cat age 28 should convert to 3",
         "animal age over 28 should be converted according to requirements",
+        "animal age 0 should convert to 0",
+        "negative animal age should convert to 0",
+        "extra-long animal age should be converted according to requirements"
     ]
 )
 def test_get_human_age(
@@ -26,3 +33,20 @@ def test_get_human_age(
         expected_result: list
 ) -> None:
     assert get_human_age(initial_cat_age, initial_dog_age) == expected_result
+
+
+@pytest.mark.parametrize(
+    "initial_cat_age,initial_dog_age,expected_error",
+    [
+        ("cat", "dog", TypeError),
+        (10, "dog", TypeError),
+        ("cat", 10, TypeError),
+    ]
+)
+def test_get_human_age_errors(
+        initial_cat_age: int,
+        initial_dog_age: int,
+        expected_error: Type[Exception]
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(initial_cat_age, initial_dog_age)
