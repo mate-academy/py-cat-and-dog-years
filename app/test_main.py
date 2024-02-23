0bc5ce07b9ffc5bad527fa8329fbc1c9ffa63f12
Eugen <zond342@gmail.com>
2024-02-23 18:46:56 +0200

    added tests with wrong types

--- app/test_main.py
+++ app/test_main.py
@@ -13,7 +13,21 @@ from app.main import get_human_age
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
+        (-10, -5, [0, 0]),
+        (-76, -53, [0, 0]),
     ])
 def test_get_human_age(cat_age: int, dog_age: int,
                        expected: list[int]) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected", [
+        ("cat", "dog", TypeError),
+        (100, [1, 0, 0], TypeError),
+        ({"cat": 100}, {"dog": 100}, TypeError),
+    ])
+def test_get_human_age_type_error(cat_age: int, dog_age: int,
+                                  expected: list[int]) -> None:
+    with pytest.raises(Exception):
+        assert get_human_age(cat_age, dog_age) == TypeError
