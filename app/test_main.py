6f3f9db0d7c584798d1f9dc607473a8f8b7d6596
Eugen <zond342@gmail.com>
2024-02-23 13:08:35 +0200

    fixed line

--- app/test_main.py
+++ app/test_main.py
@@ -14,5 +14,6 @@ from app.main import get_human_age
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
     ])
-def test_get_human_age(cat_age: int, dog_age: int, expected: list[int]) -> None:
+def test_get_human_age(cat_age: int, dog_age: int,
+                       expected: list[int]) -> None:
     assert get_human_age(cat_age, dog_age) == expected
