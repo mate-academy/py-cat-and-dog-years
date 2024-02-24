124332edaf37e4eb562321ca2d841a0fca167f24
Alexander <sasacihun@gmail.com>
2024-02-24 19:46:19 +0200

    Rewrite and add 2 types of tests with pytest

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,49 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_dog_and_cat_years_should_convert_into_1_human_year() -> None:
-    assert get_human_age(14, 14) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        pytest.param(14, 14, [0, 0], id="1 human year"),
+        pytest.param(24, 23, [2, 1], id="2 human year"),
+        pytest.param(28, 29, [3, 3], id="3 human year"),
+
+        pytest.param(28.3, 28, [3, 2], id="cat age is double"),
+        pytest.param(28.3, 28, [3, 2], id="dog age is double"),
+
+        pytest.param(1000, 1000, [246, 197], id="large ages"),
+
+        pytest.param(-1000, 1000, [0, 197], id="negative age"),
+        pytest.param(0, 0, [0, 0], id="zero age"),
+
+    ]
+)
+def test_dog_and_cat_years_should_convert(
+        cat_age: int,
+        dog_age: int,
+        result: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result
 
 
-def test_dog_and_cat_years_should_convert_into_1_more_human_year() -> None:
-    assert get_human_age(24, 23) == [2, 1]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,error_type",
+    [
+        pytest.param("28", 29, TypeError, id="string and integer"),
+        pytest.param(28, "29", TypeError, id="integer and string"),
 
+        pytest.param(None, 28, TypeError, id="None and integer"),
+        pytest.param(29, None, TypeError, id="integer and None"),
 
-def test_dog_years_should_convert_into_1_extra_human_year() -> None:
-    assert get_human_age(28, 29) == [3, 3]
+        pytest.param((1.5,), 28, TypeError, id="tuple and integer")
+    ],
+)
+def test_cannot_add_str_and_int(
+        cat_age: int,
+        dog_age: int,
+        error_type: Exception
+) -> None:
+    with pytest.raises(error_type):
+        get_human_age(cat_age, dog_age)
