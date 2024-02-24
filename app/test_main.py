29aa66f439c6a69fdecdb2a5d13da64e25bf6097
Kolya Ostrovka <mykolaostrovka27@gmail.com>
2024-02-24 14:57:43 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 from app.main import get_human_age
+
 import pytest
 
 
@@ -15,8 +16,30 @@ import pytest
         pytest.param(28, 28, [3, 2], id="check 28 years"),
         pytest.param(100, 100, [21, 17], id="check 100 years"),
 
-
     ]
 )
-def test_cat_dog_age(cat_age: int, dog_age: int, expected: int) -> None:
+def test_cat_dog_age(
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
+) -> None:
+
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected",
+    [
+        pytest.param("0", 0, [0, 0], id="check str type"),
+        pytest.param(14, [1], [0, 0], id="check list type"),
+        pytest.param({"123": 123}, "1", [0, 0], id="check dick and str type"),
+    ]
+)
+def test_incorrect_data(
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
+) -> None:
+
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
