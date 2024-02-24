53a814c10c662ba83396960312f8952d43265d9e
Alexander <sasacihun@gmail.com>
2024-02-24 09:21:09 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_dog_and_cat_years_should_convert_into_1_human_year() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_dog_and_cat_years_should_convert_into_1_more_human_year() -> None:
+    assert get_human_age(24, 23) == [2, 1]
+
+
+def test_dog_years_should_convert_into_1_extra_human_year() -> None:
+    assert get_human_age(28, 29) == [3, 3]
