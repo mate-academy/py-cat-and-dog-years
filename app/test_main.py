2c49712d4331294a78aafa66855540dd8b92c26d
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2024-02-24 17:05:37 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,35 @@
+from typing import Type
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize("input_age, next_age, expected_result", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (16, 16, [1, 1]),
+    (24, 24, [2, 2]),
+    (28, 28, [3, 2]),
+    (30, 30, [3, 3]),
+])
+def test_get_human_age(
+        input_age: int,
+        next_age: int,
+        expected_result: list
+) -> None:
+    assert get_human_age(input_age, next_age) == expected_result
+
+
+@pytest.mark.parametrize("input_age, next_age, expected_error", [
+    (-10, 5, ValueError),
+    (30, 1000, AttributeError),
+    ("string", 25, KeyError),
+    (20, "string", KeyError),
+])
+def test_invalid_input_types(
+        input_age: int,
+        next_age: int,
+        expected_error: Type[BaseException]
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(input_age, next_age)
