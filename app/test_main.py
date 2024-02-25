26e4ab673386328058a814fcaa39286db7a8cf24
Yaroslav Sysoiev <mrsomeonepromax@gmail.com>
2024-02-25 05:10:16 +0200

    Comments removed

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,4 @@
-# from typing import Type
+from typing import Type
 import pytest
 from app.main import get_human_age
 
@@ -11,6 +11,9 @@ from app.main import get_human_age
     (24, 24, [2, 2]),
     (28, 28, [3, 2]),
     (30, 30, [3, 3]),
+    (-5, 16, [0, 1]),
+    (0, 20, [0, 1]),
+    (100, 200, [21, 37]),
 ])
 def test_get_human_age(
         input_age: int,
@@ -20,16 +23,16 @@ def test_get_human_age(
     assert get_human_age(input_age, next_age) == expected_result
 
 
-# @pytest.mark.parametrize("input_age, next_age, expected_error", [
-#     (-10, 5, ValueError),
-#     (30, 1000, AttributeError),
-#     ("string", 25, KeyError),
-#     (20, "string", KeyError),
-# ])
-# def test_invalid_input_types(
-#         input_age: int,
-#         next_age: int,
-#         expected_error: Type[BaseException]
-# ) -> None:
-#     with pytest.raises(expected_error):
-#         get_human_age(input_age, next_age)
+@pytest.mark.parametrize("input_age, next_age, expected_error", [
+    (None, 5, TypeError),
+    (30, None, TypeError),
+    ("string", 25, TypeError),
+    (20, "string", TypeError),
+])
+def test_invalid_input_types(
+        input_age: int,
+        next_age: int,
+        expected_error: Type[BaseException]
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(input_age, next_age)
