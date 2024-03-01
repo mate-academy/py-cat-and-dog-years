a9f6688689d68d6c9fd2c42a546e2949982de7ea
martyr <vitaliyburkalo81@gmail.com>
2024-03-01 19:03:16 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected",
+        [
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (23, 23, [1, 1]),
+            (28, 29, [3, 3]),
+            (27, 28, [2, 2]),
+        ],
+        ids=["cat dog age 0",
+             "cat dog age 14",
+             "cat dog age 23",
+             "cat dog age 28 29",
+             "cat dog age 27 28",
+             ]
+    )
+    def test_get_human_age_correctly(
+            self,
+            cat_age,
+            dog_age,
+            expected,
+    ):
+        assert get_human_age(cat_age, dog_age) == expected
