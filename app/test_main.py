04dc1d6f5b3684b516320458fbf787eda3e36a8b
Nazar Samar <nazarsamar14@gmail.com>
2024-03-05 01:06:54 +0200

    Solution with @pytest.mark.parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,20 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_how_old_is_14_years_old_pet() -> None:
-    assert get_human_age(14, 14) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        (1, 1, [0, 0]),
+        (16, 16, [1, 1]),
+        (27, 28, [2, 2]),
+        (32, 34, [4, 4]),
+        (28, 39, [3, 5]),
+        (23, 23, [1, 1]),
+        (14, 14, [0, 0])
+    ]
+)
 
 
-def test_how_old_is_15_years_old_pet() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_how_old_is_23_years_old_pet() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_how_old_is_24_years_old_pet() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_how_old_is_27_years_old_cat_and_28_years_old_dog() -> None:
-    assert get_human_age(27, 28) == [2, 2]
-
-
-def test_how_old_is_28_years_old_cat_and_29_years_old_dog() -> None:
-    assert get_human_age(28, 29) == [3, 3]
+def test_get_human_age(cat_age: int, dog_age: int, result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result
