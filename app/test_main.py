a2c97a66507f9325a636707927e83c6c53a29a8a
donbas777 <renary34@gmail.com>
2024-03-06 18:17:29 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_cat_and_dog_years():
+    assert get_human_age(32, 29) == [4, 3]
+
+
+def test_should_return_the_cat_is_older():
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_should_return_zero_when_less_years():
+    assert get_human_age(10, 12) == [0, 0]
+
+
+def test_should_return_one_year_in_15_to_24():
+    assert get_human_age(17, 22) == [1, 1]
