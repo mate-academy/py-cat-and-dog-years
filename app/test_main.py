fddacbaf75d4aa85a74720bd2a265dd769e09ce8
donbas777 <renary34@gmail.com>
2024-03-07 14:14:32 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,64 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_should_return_cat_and_dog_years() -> None:
-    assert get_human_age(32, 29) == [4, 3]
-
-
-def test_should_return_the_cat_is_older() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_should_return_zero_when_less_years() -> None:
-    assert get_human_age(10, 12) == [0, 0]
-
-
-def test_should_return_one_year_in_15_to_24() -> None:
-    assert get_human_age(17, 22) == [1, 1]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, human_age_list",
+    [
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="0 cat/dog years should convert into 0 human age"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="14 cat/dog years should convert into 0 human age"
+        ),
+        pytest.param(
+            15,
+            15,
+            [1, 1],
+            id="15 cat/dog years should convert into 1 human age"
+        ),
+        pytest.param(
+            23,
+            23,
+            [1, 1],
+            id="23 cat/dog years should convert into 1 human age."
+        ),
+        pytest.param(
+            24,
+            24,
+            [2, 2],
+            id="24 cat/dog years should convert into 2 human age"
+        ),
+        pytest.param(
+            27,
+            27,
+            [2, 2],
+            id="27 cat/dog years should convert into 2 human age"
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 3],
+            id="28 cat/dog years should convert cat is older than dog"
+        ),
+        pytest.param(
+            100,
+            100,
+            [21, 17],
+            id="big numbers"
+        )
+    ]
+)
+def test_age_correctly(
+        cat_age: int,
+        dog_age: int,
+        human_age_list: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == human_age_list
