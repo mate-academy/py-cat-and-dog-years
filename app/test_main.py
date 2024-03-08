691fa387292501bd0c276f357ca328c08f1ec733
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-03-08 11:23:11 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,29 +1,28 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_get_human_age_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_get_human_age_below_15() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_get_human_age_15_years() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_get_human_age_23_years() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_get_human_age_24_years() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_get_human_age_27_years() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_get_human_age_cat_over_28_years() -> None:
-    assert get_human_age(28, 28) == [3, 2]
+@pytest.mark.parametrize(
+    "cat_age ,dog_age, expected_age",
+    [
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="output should not be changed"
+        ),
+        pytest.param(
+            -20,
+            0,
+            [0, 0],
+            id="should return zeros when passed negative or zero"
+        )
+    ]
+)
+def test_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected_age: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_age
