c0f834a79140d7b65baa1c0e0e86a62360a7afbf
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-03-09 20:48:53 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -26,3 +26,23 @@ def test_human_age(
         expected_age: list
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected_age
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_error",
+    [
+        pytest.param(
+            "bad_type",
+            None,
+            TypeError,
+            id="should raise error if incorrect data type passed in"
+        )
+    ]
+)
+def test_raising_errors_correctly(
+    cat_age: int,
+    dog_age: int,
+    expected_error: object
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
