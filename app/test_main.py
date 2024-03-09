36d01aa6d8e9b691aa6306fec804e1adc7eec260
Kyrylo <kbuchinskiy1@gmail.com>
2024-03-09 19:50:22 +0200

    Fix solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,34 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_get_human_age_both_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_get_human_age_both_less_than_15() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_get_human_age_both_15() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_get_human_age_both_between_15_and_24() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_get_human_age_both_24() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_get_human_age_both_between_24_and_27() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_get_human_age_cat_28_dog_27() -> None:
-    assert get_human_age(28, 27) == [3, 2]
-
-
-def test_get_human_age_cat_100_dog_100() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_result",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 27, [3, 2]),
+        (100, 100, [21, 17]),
+    ],
+    ids=[
+        "both_zero",
+        "both_less_than_15",
+        "both_15",
+        "both_between_15_and_24",
+        "both_24",
+        "both_between_24_and_27",
+        "cat_28_dog_27",
+        "cat_100_dog_100",
+    ]
+)
+def test_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected_result: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_result
