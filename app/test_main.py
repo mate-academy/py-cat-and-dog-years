aa93a9991b8f5638853591226a721dfce15c2da6
Serhii Kravchenko <serhii.kravchenko.q@gmail.com>
2024-03-10 18:31:23 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Type
 
 from app.main import get_human_age
 
@@ -35,12 +36,22 @@ def test_human_age(cat_age: int, dog_age: int, result: list) -> None:
     [
         ("15", 15, TypeError),
         (15, "15", TypeError),
+        (-1, 10, ValueError),
+        (10, -1, ValueError),
+        (-1, -1, ValueError),
     ],
     ids=[
         "cat_age should be int!",
         "dog_age should be int!",
+        "Negative cat_age not allowed!",
+        "Negative dog_age not allowed!",
+        "Negative ages for both not allowed!"
     ]
 )
-def test_get_human_age(cat_age: int, dog_age: int, error: TypeError) -> None:
-    with pytest.raises(TypeError):
+def test_get_human_age_errors(
+        cat_age: int,
+        dog_age: int,
+        error: Type[BaseException]
+) -> None:
+    with pytest.raises(error):
         get_human_age(cat_age, dog_age)
