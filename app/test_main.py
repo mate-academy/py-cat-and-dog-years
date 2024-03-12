c631ac30f9463c31c99840a96012a9abaf1b2fcd
Damodar <alaso.se@gmail.com>
2024-03-12 18:41:33 +0200

    add new tests

--- app/test_main.py
+++ app/test_main.py
@@ -3,8 +3,7 @@ import pytest
 
 
 def test_must_return_two_elements() -> None:
-    result = get_human_age(13, 13)
-    assert len(result) == 2
+    assert len(get_human_age(13, 13)) == 2
 
 
 def test_all_elements_of_return_array_must_be_int() -> None:
@@ -12,9 +11,15 @@ def test_all_elements_of_return_array_must_be_int() -> None:
     assert isinstance(result[0], int) and isinstance(result[1], int)
 
 
+def test_should_raise_error() -> None:
+    with pytest.raises(TypeError):
+        get_human_age([1, 2], [3, 4])
+
+
 @pytest.mark.parametrize(
     "cat_age,dog_age,expected_result",
     [
+        pytest.param(-10, -10, [0, 0], id="if age < 0"),
         pytest.param(0, 0, [0, 0], id="if age is 0"),
         pytest.param(14, 14, [0, 0], id="if age < 15"),
         pytest.param(15, 15, [1, 1], id="if age >= 15"),
@@ -23,6 +28,7 @@ def test_all_elements_of_return_array_must_be_int() -> None:
         pytest.param(27, 27, [2, 2], id="if age <= 27"),
         pytest.param(28, 28, [3, 2], id="if age >= 28"),
         pytest.param(100, 100, [21, 17], id="if age == 100"),
+        pytest.param(10000, 10000, [2496, 1997], id="if age == 10000"),
     ]
 )
 def test_converted_to_human_years(
