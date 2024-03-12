0bd0803083f34b7fa3f7cac2887e603eaa170ea4
meier <Meyertal2@gmail.com>
2024-03-12 14:47:38 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_if_animal_age_15() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_if_animal_age_less_15() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_animal_age_24() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_animal_age_less_24() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_animal_age_have_second_human_age() -> None:
+    assert get_human_age(27, 28) == [2, 2]
+
+
+def test_animal_age_have_third_age() -> None:
+    assert get_human_age(28, 29) == [3, 3]
