8924a9bdfbe574f92ef2ce13aae14dcc65b1526c
Ivan Havrylyshyn <jyjuk1@gmail.com>
2024-03-14 16:40:52 +0200

    raises the correct exception

--- app/test_main.py
+++ app/test_main.py
@@ -55,9 +55,59 @@ from app.main import get_human_age
             [21, 17],
             id="check for cat and dog 100 years should return "
                "21 human years for cat and 17 human years for dog"
+        ),
+        pytest.param(
+            28.3,
+            28.4,
+            [3, 2],
+            id="check whole number of years"
+        ),
+        pytest.param(
+            -1,
+            -3,
+            [0, 0],
+            id="test negative cat/dog years"
         )
     ]
 
 )
 def test_get_human_age(cat_age: int, dog_age: int, expected_age: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected_age
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, error",
+    [
+        pytest.param(
+            "2",
+            "0",
+            TypeError,
+            id="test string type of years"
+        ),
+        pytest.param(
+            [1, ],
+            [4, ],
+            TypeError,
+            id="test list type of years"
+        ),
+        pytest.param(
+            (1,),
+            (3,),
+            TypeError,
+            id="test tuple type of years"
+        ),
+        pytest.param(
+            None,
+            None,
+            TypeError,
+            id="test None instance of years"
+        )
+    ]
+)
+def test_should_raise_errors(
+        cat_age: int,
+        dog_age: int,
+        error: TypeError
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age=cat_age, dog_age=dog_age)
