c05274afbc3f5d09a658e0abed95ed812059fa4f
skeletronys <mykytcey@gmail.com>
2024-03-15 17:52:01 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,40 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestDogAndCat:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,correct_answer",
+        [
+            pytest.param(
+                14,
+                0,
+                [0, 0],
+                id="0 dog years should into 0 human age"
+            ),
+            pytest.param(
+                15,
+                15,
+                [1, 1],
+                id="First 15 years animal should into 1 human age",
+            ),
+            pytest.param(
+                27,
+                28,
+                [2, 2],
+                id="Animal years should convert into 2 human age",
+            ),
+            pytest.param(
+                28,
+                29,
+                [3, 3],
+                id="Animal years should into 3 human age",
+            )
+        ]
+    )
+    def test_correct_get_human_age(self,
+                                   cat_age: int,
+                                   dog_age: int,
+                                   correct_answer: list) -> None:
+        assert get_human_age(cat_age, dog_age) == correct_answer
