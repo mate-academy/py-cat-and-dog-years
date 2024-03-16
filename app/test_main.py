2dbc2e1f9192e9c543057924610b76ca6ea14076
Serhii Dannyk <serhii.dannyk@gmail.com>
2024-03-16 11:36:21 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -14,5 +14,7 @@ from app.main import get_human_age
     (40, 40, [6, 5]),
     (100, 100, [21, 17]),
 ])
-def test_human_age_conversion(cat_years, dog_years, expected_result) -> None:
+def test_human_age_conversion(
+        cat_years: int, dog_years: int, expected_result: list
+) -> None:
     assert get_human_age(cat_years, dog_years) == expected_result
