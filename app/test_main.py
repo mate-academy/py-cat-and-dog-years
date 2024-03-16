a1245f6246490d50fc06543e2ad84e7043860764
Serhii Dannyk <serhii.dannyk@gmail.com>
2024-03-16 11:30:44 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
+import pytest
+
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize("cat_years, dog_years, expected_result", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (23, 23, [1, 1]),
+    (24, 24, [2, 2]),
+    (27, 27, [2, 2]),
+    (40, 40, [6, 5]),
+    (100, 100, [21, 17]),
+])
+def test_human_age_conversion(cat_years, dog_years, expected_result) -> None:
+    assert get_human_age(cat_years, dog_years) == expected_result
