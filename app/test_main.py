dfb6a29ee44c86ddb465f5d855346c9ed22dc4ec
Viesich <v.s.vesich@gmail.com>
2024-03-17 23:20:29 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_both_animal_age_is_zero() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_less_the_first_animal_year() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_is_exactly_one_animal_year() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_less_the_second_animal_year() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_is_exactly_two_animal_year() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_less_third_first_year() -> None:
+    assert get_human_age(27, 28) == [2, 2]
+
+
+def test_is_exactly_tree_animal_year() -> None:
+    assert get_human_age(28, 29) == [3, 3]
+
+
+def test_large_values() -> None:
+    assert get_human_age(100, 100) == [21, 17]
