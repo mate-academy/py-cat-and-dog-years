31daefd63717d1e85dadb61bc920ffe89ef5184e
Viesich <v.s.vesich@gmail.com>
2024-03-18 16:26:40 +0200

    negative age and more TypeErrore cases

--- app/test_main.py
+++ app/test_main.py
@@ -5,6 +5,7 @@ from app.main import get_human_age
 
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected_result", [
+        (-1, -2, [0, 0]),
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
@@ -12,7 +13,8 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
+        (5000, 9999, [1246, 1997])
     ])
 def test_get_human_age(
         cat_age: int,
@@ -22,10 +24,21 @@ def test_get_human_age(
     assert get_human_age(cat_age, dog_age) == expected_result
 
 
-def test_large_values() -> None:
-    assert get_human_age(1000, 500) == [246, 97]
-
-
-def test_invalid_types() -> None:
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_result", [
+        ("cat", 2, TypeError),
+        (0, "", TypeError),
+        ("44", "55", TypeError),
+        (None, 44, TypeError),
+        ([23], 23, TypeError),
+        ({24: ["2", "3"]}, 24, TypeError),
+        (10, (2, 3), TypeError),
+    ])
+def test_str_types(
+        cat_age: int,
+        dog_age: int,
+        expected_result: list
+) -> None:
     with pytest.raises(TypeError):
-        get_human_age("cat", "dog")
+        get_human_age(cat_age, dog_age)
+
