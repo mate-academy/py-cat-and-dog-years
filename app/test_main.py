1cf588c687bb485ee1a2c27e175f0ed2afe925a6
Serhii Dannyk <serhii.dannyk@gmail.com>
2024-03-18 09:40:48 +0200

    Added more tests

--- app/test_main.py
+++ app/test_main.py
@@ -18,3 +18,20 @@ def test_human_age_conversion(
         cat_years: int, dog_years: int, expected_result: list
 ) -> None:
     assert get_human_age(cat_years, dog_years) == expected_result
+
+
+@pytest.mark.parametrize("cat_years, dog_years", [
+    (-1, 10),
+    (10, -1),
+    (500, 500),
+])
+def test_invalid_input_raises_exception(
+        cat_years: int, dog_years: int
+) -> None:
+    with pytest.raises(ValueError):
+        get_human_age(cat_years, dog_years)
+
+
+def test_incorrect_type_of_data_raises_exception() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("10", 10)
