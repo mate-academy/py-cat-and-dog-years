a70b662380420b1f555fd627c613f41435afd7b9
Viesich <v.s.vesich@gmail.com>
2024-03-18 13:33:53 +0200

    use parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,27 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_both_animal_age_is_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_less_the_first_animal_year() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_is_exactly_one_animal_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_less_the_second_animal_year() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_is_exactly_two_animal_year() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_less_third_first_year() -> None:
-    assert get_human_age(27, 28) == [2, 2]
-
-
-def test_is_exactly_tree_animal_year() -> None:
-    assert get_human_age(28, 29) == [3, 3]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_result", [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ])
+def test_get_human_age(cat_age, dog_age, expected_result) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_result
 
 
 def test_large_values() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+    assert get_human_age(1000, 500) == [246, 97]
+
+
+def test_invalid_types() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("cat", "dog")
