a7f92e97c7e2f433ff11ccfb48bea4eb84aa4541
sudakolha <sudakolha@outlook.com>
2024-03-20 22:36:20 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,69 @@
 import pytest
+
+import traceback
+
 from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    "cat_years, dog_years, result",
+    "cat_age, dog_age, expected_result",
     [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
-        (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        pytest.param(
+            14, 14, [0, 0],
+            id="human age should be equal to 0"
+        ),
+        pytest.param(
+            23, 23, [1, 1],
+            id="human age should be equal to 1"
+        ),
+        pytest.param(
+            27, 28, [2, 2],
+            id="human age should be equal to 2"
+        ),
+        pytest.param(
+            100, 100, [21, 17],
+            id="human age should be more than 2"
+        ),
+        pytest.param(
+            - 1, - 1, [0, 0],
+            id="test_if_function_receives_negative_number"
+        ),
+        pytest.param(
+            0, 0, [0, 0],
+            id="test_if_function_receives_zero"
+        ),
+        pytest.param(
+            1000, 1000, [246, 197],
+            id="test_if_function_receives_really_large_numbers"
+        )
     ]
 )
-def test_get_human_age(cat_years: int, dog_years: int, result: list) -> None:
-    assert (get_human_age(cat_years, dog_years) == result)
+def test_correct_return(cat_age: int,
+                        dog_age: int,
+                        expected_result: list
+                        ) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_result
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+    [
+        pytest.param(
+            "cat",
+            "dog",
+            TypeError,
+            id="String_type_error"
+        ),
+        pytest.param(
+            None,
+            None,
+            TypeError,
+            id="None_type_error"
+        )
+    ]
+)
+def test_correct_value(cat_age: int,
+                       dog_age: int,
+                       expected_error: traceback) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
