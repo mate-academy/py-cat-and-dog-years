71d24779234f8d1ec5d6b6d0a635de7c5d27ed37
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2024-03-22 13:38:03 +0200

    'fixed_with_decorator'

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,28 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_should_zeros() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_should_convert_into_2_human_age() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_should_return_different_numbers() -> None:
-    assert get_human_age(29, 28) == [3, 2]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        (
+            0, 0, [0, 0]
+        ),
+        (
+            15, 15, [1, 1]
+        ),
+        (
+            24, 24, [2, 2]
+        ),
+        (
+            29, 28, [3, 2]
+        ),
+    ]
+)
+def test_can_count_years(
+        cat_age: int,
+        dog_age: int,
+        result: list[int]
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result
