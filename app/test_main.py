3088733c2bb0373c413905ba3ab39779247e854f
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2024-03-22 11:21:28 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_zeros() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_convert_into_2_human_age() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_should_return_different_numbers() -> None:
+    assert get_human_age(29, 28) == [3, 2]
