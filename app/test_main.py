8ca3d364ae09039475ce323db3b157de59a78f12
AndriiDraga <148384465+AndriiDraga@users.noreply.github.com>
2024-03-22 17:29:03 +0200

    'Errors_and_negative_numbers'

--- app/test_main.py
+++ app/test_main.py
@@ -1,23 +1,16 @@
 import pytest
-
 from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
     "cat_age,dog_age,result",
     [
-        (
-            0, 0, [0, 0]
-        ),
-        (
-            15, 15, [1, 1]
-        ),
-        (
-            24, 24, [2, 2]
-        ),
-        (
-            29, 28, [3, 2]
-        ),
+        (0, 0, [0, 0]),
+        (15, 15, [1, 1]),
+        (24, 24, [2, 2]),
+        (29, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        (1000, 1000, [246, 197]),
     ]
 )
 def test_can_count_years(
@@ -26,3 +19,21 @@ def test_can_count_years(
         result: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == result
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+    [
+        (-1, 5, ValueError),
+        (5, -1, ValueError),
+        (5.5, 5, TypeError),
+        ("5", 5, TypeError),
+    ]
+)
+def test_can_count_years_error(
+        cat_age: int,
+        dog_age: int,
+        expected_error: type[Exception]
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
