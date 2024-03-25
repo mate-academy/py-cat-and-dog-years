ab9a1e40899819ca43a5bd4fa5f68c31c7a3b3bc
antonver <anton.verbovyi@gmail.com>
2024-03-25 15:24:51 +0100

    Done

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,20 @@
 from app.main import get_human_age
+import pytest
 
 
-def test_all_numbers_are_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize(
+    "cat,dog,result",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (24, 24, [2, 2]),
+        (28, 28, [3, 2])
+    ]
+)
+def test_different_data(cat: int, dog: int, result: list) -> None:
+    assert get_human_age(cat, dog) == result
 
 
-def test_first_14_years() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_first_24_year() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_first_28_year() -> None:
-    assert get_human_age(28, 28) == [3, 2]
+def test_raise_error(cat: int, dog: int, result: list) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat, dog)
