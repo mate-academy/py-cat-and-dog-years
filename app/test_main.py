33ec7251e3cdb31214c46ee85957f30f13abede5
Dede <specialized8393@gmail.com>
2024-03-26 14:52:56 +0000

    Solution 2024

--- app/test_main.py
+++ app/test_main.py
@@ -6,20 +6,20 @@ class TestGetHumanAge:
     @pytest.mark.parametrize(
         "cat_age,dog_age,human_age",
         [
+            pytest.param(0, 0, [0, 0],
+                         id="O numbers of years should return [0, 0]"),
             pytest.param(14, 14, [0, 0],
-                         id="Up to 15 cat/dog years give 0 human year"),
-            pytest.param(23, 23, [1, 1],
-                         id="23 cat/dog years should equal 1 human age"),
+                         id="Up to 15 numbers of years given 0 human year"),
+            pytest.param(27, 28, [2, 2],
+                         id="27 cat 28 dog years should equal 2 human age"),
             pytest.param(24, 24, [2, 2],
-                         id="24 cat/dog years should be equal 1 human years"),
+                         id="24 cat/dog years should be equal 2 human years"),
             pytest.param(28, 29, [3, 3],
                          id="Every 4/5 cat/dog years give 1 extra human year"),
-            pytest.param(0, 0, [0, 0],
-                         id="If 0 cat/dog years should return [0, 0]"),
             pytest.param(-20, -34, [0, 0],
-                         id="-20/-34 cat/dog years should be equal [0, 0]"),
+                         id="Negative numbers of years should be equal [0,0]"),
             pytest.param(1000, 10000, [246, 1997],
-                         id="Realy large numbers works without errors"),
+                         id="larges numbers of ages should be equal not real"),
         ],
     )
     def test_get_human_age(self,
@@ -33,16 +33,15 @@ class TestGetHumanAge:
            f"should be equal to human years: {human_age}"
 
     @pytest.mark.parametrize(
-        "cat_age,dog_age,key_error",
+        "cat_age,dog_age",
         [
-            pytest.param("14", 14, TypeError,
+            pytest.param("14", "14",
                          id="The function receives an incorrect type of data"),
         ]
     )
-    def test_should_raise_error_when_input_not_int(self,
-                                                   cat_age: int,
-                                                   dog_age: int,
-                                                   key_error: Exception)\
-            -> None:
-        with pytest.raises(key_error):
+    def test_should_error_parameters_not_int(self,
+                                             cat_age: int,
+                                             dog_age: int
+                                             ) -> None:
+        with pytest.raises(TypeError):
             get_human_age(cat_age, dog_age)
