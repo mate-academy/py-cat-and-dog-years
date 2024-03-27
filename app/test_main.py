a304a741f349ef6b605ade165e53f90d35a6a4bd
Khrystenko_Denys <deonis062002@gmail.com>
2024-03-27 14:36:58 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_should_return_zeros_when_age_less_15() -> None:
-    assert (get_human_age(14, 14) == [0, 0]
-            ), "should return zeros when age < 15"
-
-
-def test_should_add_year_next_9_years() -> None:
-    assert (get_human_age(24, 24) == [2, 2]
-            ), "should add one year next 9 years after 15"
-
-
-def test_should_add_next_years_after_24() -> None:
-    assert (get_human_age(28, 28) == [3, 2]
-            ), "should add one year for cat every 4 years, for dog 5 years"
-
-
-def test_check_old_cat_and_dog() -> None:
-    assert (get_human_age(100, 100) == [21, 17]
-            ), "should be good with big datas"
+class TestCatAndDogYears:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, human_age",
+        [
+            pytest.param(0, 0, [0, 0]),
+            pytest.param(14, 14, [0, 0]),
+            pytest.param(15, 15, [1, 1]),
+            pytest.param(23, 23, [1, 1]),
+            pytest.param(24, 24, [2, 2]),
+            pytest.param(27, 27, [2, 2]),
+            pytest.param(28, 28, [3, 2]),
+            pytest.param(100, 100, [21, 17]),
+        ]
+    )
+    def test_param(self, cat_age: int, dog_age: int, human_age: int) -> None:
+        assert get_human_age(cat_age, dog_age) == human_age
