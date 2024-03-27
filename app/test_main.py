6207b6bd7286d873b1b08e4d0df0bbb5e9e4513d
Anton <antonrozegan1@gmail.com>
2024-03-27 16:15:33 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+class TestCatandDogYear:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, human_age",
+        [
+            pytest.param(0, 0, [0, 0]),
+            pytest.param(14, 14, [0, 0]),
+            pytest.param(15, 15, [1, 1]),
+            pytest.param(23, 23, [1, 1]),
+            pytest.param(24, 24, [2, 2]),
+            pytest.param(27, 27, [2, 2]),
+            pytest.param(28, 28, [3, 2]),
+            pytest.param(100, 100, [21, 17]),
+        ]
+    )
+    def test_prime(self, cat_age: int, dog_age: int, human_age: int) -> None:
+        assert get_human_age(cat_age, dog_age) == human_age
