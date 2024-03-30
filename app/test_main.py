0bd2122414ac02b97a8b71324cfa61bd44d1c4ae
Valentin Pozniak <waliker448@gmail.com>
2024-03-30 19:47:07 +0200

    'Fix'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,7 @@
 import pytest
+
+from typing import Any
+
 from app.main import get_human_age
 
 
@@ -20,7 +23,9 @@ from app.main import get_human_age
         pytest.param(28, 28, [3, 2],
                      id="28 must be equal to [3, 2] human age"),
         pytest.param(100, 100, [21, 17],
-                     id="100 must be equal to [21, 17] human age")
+                     id="100 must be equal to [21, 17] human age"),
+        pytest.param(-1, -1, [0, 0],
+                     id="-1  must be equal to [0, 0] human age")
     ]
 )
 def test_check(
@@ -29,3 +34,27 @@ def test_check(
         human_age: list
 ) -> None:
     assert get_human_age(cat_age, dog_age) == human_age
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expect_error",
+    [
+        pytest.param([], [], TypeError,
+                     id="`list` should raise `TypeError`"),
+        pytest.param({}, {}, TypeError,
+                     id="`dict` should raise `TypeError`"),
+        pytest.param((), (), TypeError,
+                     id="`tuple` should raise `TypeError`"),
+        pytest.param("", "", TypeError,
+                     id="`str` should raise `TypeError`"),
+        pytest.param({1, 2}, {1, 2}, TypeError,
+                     id="`set` should raise `TypeError`")
+    ]
+)
+def test_check_raise_error(
+        cat_age: Any,
+        dog_age: Any,
+        expect_error: Any
+) -> None:
+    with pytest.raises(expect_error):
+        get_human_age(cat_age, dog_age)
