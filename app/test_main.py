74c772820cb49362934e1480570bcaf7a8bd3256
Vitalina Malynovska <vitalinamalinovskaya557@gmail.com>
2024-04-01 16:25:36 +0200

    Added pytest.param and testing errors

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Type, Any
 from app.main import get_human_age
 
 
@@ -6,14 +7,24 @@ class TestCatAndDogYears:
     @pytest.mark.parametrize(
         "cat_age, dog_age, expected_result",
         [
-            (0, 0, [0, 0]),
-            (14, 14, [0, 0]),
-            (15, 15, [1, 1]),
-            (23, 23, [1, 1]),
-            (24, 24, [2, 2]),
-            (27, 27, [2, 2]),
-            (28, 28, [3, 2]),
-            (100, 100, [21, 17])
+            pytest.param(0, 0, [0, 0],
+                         id="0  must be equal to [0, 0] human age"),
+            pytest.param(14, 14, [0, 0],
+                         id="14  must be equal to [0, 0] human age"),
+            pytest.param(15, 15, [1, 1],
+                         id="15 must be equal to [1, 1] human age"),
+            pytest.param(23, 23, [1, 1],
+                         id="23 must be equal to [1, 1] human age"),
+            pytest.param(24, 24, [2, 2],
+                         id="24 must be equal to [2, 2] human age"),
+            pytest.param(27, 27, [2, 2],
+                         id="27 must be equal to [2, 2] human age"),
+            pytest.param(28, 28, [3, 2],
+                         id="28 must be equal to [3, 2] human age"),
+            pytest.param(100, 100, [21, 17],
+                         id="100 must be equal to [21, 17] human age"),
+            pytest.param(-1, -1, [0, 0],
+                         id="-1  must be equal to [0, 0] human age")
         ]
     )
     def test_get_human_age(self,
@@ -21,3 +32,22 @@ class TestCatAndDogYears:
                            dog_age: int,
                            expected_result: list[int]) -> None:
         assert get_human_age(cat_age, dog_age) == expected_result
+
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected_error",
+        [
+            pytest.param(None, None, TypeError,
+                         id="`None` should raise `TypeError`"),
+            pytest.param("10", "10", TypeError,
+                         id="`str` should raise `TypeError`"),
+            pytest.param({1, 2}, {1, 2}, TypeError,
+                         id="`set` should raise `TypeError`")
+        ]
+    )
+    def test_test_get_human_age_errors(self,
+                                       cat_age: Any,
+                                       dog_age: Any,
+                                       expected_error: Type[BaseException]
+                                       ) -> None:
+        with pytest.raises(expected_error):
+            get_human_age(cat_age, dog_age)
