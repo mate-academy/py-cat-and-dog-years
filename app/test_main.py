d4562cf44aa5a9dfe6f0b6c6671f75f2094f7fdb
Olevksy Ivan <kiperdemiwan@gmail.com>
2024-04-02 21:42:24 +0300

    Success

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ],
+    ids=[
+        "0 cat/dog years should convert into 0 human age.",
+        "14 cat/dog years should convert into 0 human age.",
+        "15 cat/dog years should convert into 1 human age.",
+        "23 cat/dog years should convert into 1 human age.",
+        "24 cat/dog years should convert into 2 human age.",
+        "27/28 cat/dog years should convert into 2 human age.",
+        "28/29 cat/dog years should convert into 3 human age.",
+        "100 cat/dog years should convert into 21 human age."]
+)
+def test_human_age(cat_age: int, dog_age: int, result: int) -> list:
+    assert get_human_age(cat_age, dog_age) == result
