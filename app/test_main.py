f97262b46630f7a77f05de478eead6542acb43eb
Andrii Chubenko <stalkerock@gmail.com>
2024-04-03 20:12:07 +0300

    My tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,32 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize("cat_age, dog_age, expected_result", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (23, 23, [1, 1]),
+    (24, 24, [2, 2]),
+    (27, 27, [2, 2]),
+    (28, 28, [3, 2])])
+def test_get_human_age_with_param_deco(
+        cat_age: int,
+        dog_age: int,
+        expected_result: list) -> None:
+    if isinstance(expected_result, list):
+        result = get_human_age(cat_age, dog_age)
+        assert result == expected_result
+
+
+@pytest.mark.parametrize("cat_age, dog_age, expected_errors", [
+    ([], [], TypeError),
+    ((), (), TypeError),
+    ("", "", TypeError),
+    ({}, {}, TypeError)])
+def test_get_human_age_with_param_error(cat_age: int,
+                                        dog_age: int,
+                                        expected_errors: list) -> None:
+    with pytest.raises(expected_errors):
+        get_human_age(cat_age, dog_age)
