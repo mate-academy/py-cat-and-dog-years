e4eb6b82d387051612b1bead44ee91e283630c93
NioviIki <charmniovi@tutanota.com>
2024-04-04 21:18:58 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -29,20 +29,25 @@ def test_type_of_returned_data() -> None:
         ([-200, -200], [0, 0])
     ],
     ids=[
-        "Two cats aged 14 should return [0, 0]",
-        "Two cats aged 15 should return [1, 1]",
-        "Two cats aged 23 should return [1, 1]",
-        "Two cats aged 24 should return [2, 2]",
+        "Cats and dogs aged 14 should return [0, 0]",
+        "Cats and dogs aged 15 should return [1, 1]",
+        "Cats and dogs aged 23 should return [1, 1]",
+        "Cats and dogs aged 24 should return [2, 2]",
         "A cat aged 27 and a dog aged 28 should return [2, 2]",
         "A cat aged 28 and a dog aged 29 should return [3, 3]",
-        "Two animals aged 200 should return [46, 37]",
+        "Cats and dogs aged 200 should return [46, 37]",
         "A cat aged 340 and a dog aged 210 should return [81, 39]",
-        "Two animals aged 0 should return [0, 0]",
+        "Cats and dogs aged 0 should return [0, 0]",
         "A cat aged 100 and a dog aged 0 should return [21, 0]",
         "A cat aged 0 and a dog aged 100 should return [0, 17]",
-        "Two animals aged -200 should return [0, 0]"
+        "Cats and dogs aged -200 should return [0, 0]"
     ]
 
 )
 def test_correct_data(ages: list, expected: list) -> None:
     assert get_human_age(*ages) == expected
+
+
+def test_get_human_age_with_invalid_input() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("cat", "dog")
