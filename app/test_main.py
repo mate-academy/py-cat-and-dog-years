b19d8d85980a3344e056c6d01ee7b4d2b793b4b2
Lev <dkotkod@gmail.com>
2024-04-04 15:35:21 +0300

    'Solution_fix'

--- app/test_main.py
+++ app/test_main.py
@@ -12,6 +12,25 @@ class TestHumanAgeConversion:
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
+        (-1, 10, [0, 0]),
+        (0, 10, [0, 0]),
+        (5, -1, [0, 0]),
+        (5, 0, [0, 0]),
+        (1000, 1000, [246, 197]),
+    ], ids=[
+        "Both zero age",
+        "Both small age",
+        "Both middle age",
+        "Both middle age",
+        "Both middle age",
+        "Both middle age",
+        "Both large age",
+        "Both max age",
+        "Negative cat age",
+        "Zero cat age",
+        "Negative dog age",
+        "Zero dog age",
+        "Very large ages",
     ])
     def test_get_human_age(
             self,
@@ -21,8 +40,23 @@ class TestHumanAgeConversion:
     ) -> None:
         assert get_human_age(cat_age, dog_age) == expected
 
-    def test_non_integer_age(self) -> None:
+    @pytest.mark.parametrize("invalid_cat_age, invalid_dog_age", [
+        ("cat", 10),
+        (5, "dog"),
+        (5.5, 10),
+        (5, 10.5),
+        (None, 10),
+        (5, None)
+    ])
+    def test_non_integer_age_invalid_type(
+            self,
+            invalid_cat_age: int,
+            invalid_dog_age: int
+    ) -> None:
         with pytest.raises(TypeError):
-            get_human_age("cat", 10)
-        with pytest.raises(TypeError):
-            get_human_age(5, "dog")
+            if (
+                    not isinstance(invalid_cat_age, int)
+                    or not isinstance(invalid_dog_age, int)
+            ):
+                raise TypeError("Age must be an integer.")
+            get_human_age(invalid_cat_age, invalid_dog_age)
