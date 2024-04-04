6b6848c146121892f4888f689bd879b43ad9418b
Oleksandr Moroz <molecxander@gmail.com>
2024-04-04 13:02:38 +0300

    Add error tests and checks

--- app/test_main.py
+++ app/test_main.py
@@ -8,6 +8,8 @@ from app.main import get_human_age
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (24, 24, [2, 2]),
+        (27, 28, [2, 2]),
+        (23, 22, [1, 1]),
         (28, 28, [3, 2]),
         (1000, 1000, [246, 197]),
         (-1, -1, [0, 0]),
@@ -15,6 +17,8 @@ from app.main import get_human_age
     ids=["Both ages are zero",
          "Both ages are less than the first_year",
          "Both ages are greater than the second_year",
+         "Dog input greater, ages are equal",
+         "Cat input greater, ages are equal",
          "Different ages between dogs and cats"
          "Dog age greater than cat age",
          "Large numbers", "Negative ages"]
@@ -23,6 +27,17 @@ def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
 
-def test_get_human_age_type_error() -> None:
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("cat", "dog"),
+        (None, None),
+        ({}, []),
+    ],
+    ids=["String inputs",
+         "None inputs",
+         "Unsupported types"]
+)
+def test_get_human_age_type_error(cat_age, dog_age) -> None:
     with pytest.raises(TypeError):
-        get_human_age("cat", "dog")
+        get_human_age(cat_age, dog_age)
