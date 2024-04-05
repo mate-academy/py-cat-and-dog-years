ec1559ef7a088fd6d5dcebf33121234cbd7f6e3c
Andrii Chubenko <stalkerock@gmail.com>
2024-04-05 23:15:28 +0300

    Add signs id, change type of errors

--- app/test_main.py
+++ app/test_main.py
@@ -13,7 +13,15 @@ from app.main import get_human_age
     (28, 28, [3, 2]),
     (-1, -1, [0, 0]),
     (100, 100, [21, 17])
-])
+], ids=["Expected human age 0 for cat age 0 and dog age 0",
+        "Expected human age 0 for cat age 14 and dog age 14",
+        "Expected human age 1 for cat age 23 and dog age 23",
+        "Expected human age 1 for cat age 23 and dog age 23",
+        "Expected human age 2 for cat age 24 and dog age 24",
+        "Expected human age 2 for cat age 27 and dog age 27",
+        "Expected human age for cat:3 and dog:2 for cat age 28 and dog age 28",
+        "Expected human age 0 for cat age -1 and dog age -1",
+        "Expected human age for cat:21 and dog:17 for cat and dog ages 100"])
 def test_get_human_age_with_param_deco(cat_age: int,
                                        dog_age: int,
                                        expected_result: list) -> None:
@@ -27,9 +35,12 @@ def test_get_human_age_with_param_deco(cat_age: int,
     ([], [], TypeError),
     ((), (), TypeError),
     ("", "", TypeError),
-    ({}, {}, TypeError)])
+    ({}, {}, TypeError)], ids=["TypeError raised when input is two lists",
+                               "TypeError raised when input is two tuples",
+                               "TypeError raised when input is two strings",
+                               "TypeError raised when input is two dicts"])
 def test_get_human_age_with_param_error(cat_age: int,
                                         dog_age: int,
                                         expected_errors: list) -> None:
-    with pytest.raises(expected_errors):
+    with pytest.raises(TypeError):
         get_human_age(cat_age, dog_age)
