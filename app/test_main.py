c1a23e656720f3f6d0c8db2a62e1b0213f6366ba
Alona Sorochynska <alona.sorochynska@gmail.com>
2024-04-06 12:40:03 +0100

    added TypeError test and length_of_output test

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
-from app.main import get_human_age
-
 import pytest
+
+
+from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
@@ -26,9 +27,31 @@ import pytest
                      id="Every 4 next cat years give 1 extra human year"),
     ]
 )
-def test_correct_math(
+def test_convert_animal_age_to_human(
         cat_age: int,
         dog_age: int,
         expected_result: list
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected_result
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        pytest.param("-1", "-1",
+                     id="Ages have to be integers"),
+        pytest.param("0", 0,
+                     id="Cats age have to be an integer"),
+        pytest.param(0, "0",
+                     id="Dogs age have to be an integer"),
+    ]
+)
+def test_types_of_input_data(cat_age: int, dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
+
+
+def test_length_of_output() -> None:
+    assert len(
+        get_human_age(0, 16)
+    ) == 2, "Expected output is a list of 2 values"
