f7fcfa675d78111ea1870dafa6c02014b0871c44
Vitalii <vitalikbashkiser@gmail.com>
2024-04-06 17:53:05 +0300

    Solution#5

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,5 @@
+from typing import List, Any
 import pytest
-from typing import List
 from app.main import get_human_age
 
 
@@ -8,6 +8,8 @@ class TestGetHumanAge:
     @pytest.mark.parametrize(
         "cat_age, dog_age, expected",
         [
+            (-1, 0, [0, 0]),
+            (0, -1, [0, 0]),
             (0, 0, [0, 0]),
             (14, 14, [0, 0]),
             (15, 15, [1, 1]),
@@ -16,32 +18,57 @@ class TestGetHumanAge:
             (27, 27, [2, 2]),
             (28, 28, [3, 2]),
             (100, 100, [21, 17]),
+        ],
+        ids=[
+            "Negative cat age",
+            "Negative dog age",
+            "Zero ages",
+            "Both animals are in young age",
+            "Both animals are in young age, just above the boundary",
+            "Boundary ages for young",
+            "Age just above the boundary",
+            "Boundary ages for adult",
+            "Age just above the boundary",
+            "Boundary ages for senior cat",
         ]
     )
     def test_basic_cases(
             self,
-            cat_age: int,
-            dog_age: int,
-            expected: List[int]
+            cat_age: Any,
+            dog_age: Any,
+            expected: List[Any]
     ) -> None:
         assert get_human_age(cat_age, dog_age) == expected
 
-
-class TestEdgeCases:
     @pytest.mark.parametrize(
         "cat_age, dog_age",
         [
-            ("-1", 10),  # Negative cat age
-            (10, "1"),  # Negative dog age
-            ("0", 10),   # Zero cat age
-            (10, "0"),   # Zero dog age
-            ("1000", "1000"),  # Very large ages
-            ("10", 10),  # String cat age
-            (10, "10"),  # String dog age
-            ([10], 10),  # List cat age
-            (10, [10]),  # List dog age
+            ("-1", 10),
+            (10, "1"),
+            ("0", 10),
+            (10, "0"),
+            ("1000", "1000"),
+            ("10", 10),
+            (10, "10"),
+            ([10], 10),
+            (10, [10]),
+        ],
+        ids=[
+            "Negative cat age",
+            "Negative dog age",
+            "Zero cat age",
+            "Zero dog age",
+            "Very large ages",
+            "String cat age",
+            "String dog age",
+            "List cat age",
+            "List dog age",
         ]
     )
-    def test_edge_cases(self, cat_age: int, dog_age: int) -> None:
+    def test_error_will_be_raised(
+        self,
+        cat_age: Any,
+        dog_age: Any
+    ) -> None:
         with pytest.raises(TypeError):
             get_human_age(cat_age, dog_age)
