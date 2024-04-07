cf1d051ee4ed5f119988d3611f382db12331f73f
Sofiia <sonagm9@gmail.com>
2024-04-07 20:02:23 +0300

    'Solution3'

--- app/test_main.py
+++ app/test_main.py
@@ -14,9 +14,21 @@ from app.main import get_human_age
         (28, 28, [3, 2]),
         (29, 29, [3, 3]),
         (100, 100, [21, 17]),
-
         (-5, -5, [0, 0]),
         (-100, -100, [0, 0]),
+    ],
+    ids=[
+        "Both zero",
+        "Both 14",
+        "Both 15",
+        "Both 23",
+        "Both 24",
+        "Both 27",
+        "Cat 28, Dog 28",
+        "Both 29",
+        "Both 100",
+        "Both -5",
+        "Both -100",
     ]
 )
 def test_get_human_age(cat_age: int,
