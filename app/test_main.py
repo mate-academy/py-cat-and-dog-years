714ac2a26606ec6db688b335fbaabb088de54ff1
Sergiy Yurov <urovsergej10@gmail.com>
2024-04-08 10:09:49 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,21 @@
+import pytest
+
+
 from app.main import get_human_age
 
 
-def test_for_get_human_age_if_years_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_for_get_human_age_if_years_less_than_fifteen() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_for_get_human_age_if_years_fifteen() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_for_get_human_age_if_years_less_than_twenty_four() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_for_get_human_age_if_years_twenty_four() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_for_get_human_age_if_years_less_than_twenty_eight() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_for_get_human_age_if_years_twenty_eight() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_for_get_human_age_if_years_more_than_twenty_eight() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_ages,dog_ages,human_ages",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_converter_age_from_human_to_animal(cat_ages, dog_ages, human_ages):
+    assert get_human_age(cat_ages, dog_ages) == human_ages
