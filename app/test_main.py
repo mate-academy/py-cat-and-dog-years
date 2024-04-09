0ea1d88902d3e1ce6a36605d4a8a0f53d48d9374
semermaks <semermaks@mail.com>
2024-04-09 11:16:51 +0300

    blank lines between different import types, pytest change

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,7 @@
-import pytest
 from typing import Any
+
+import pytest
+
 from app.main import get_human_age
 
 
@@ -7,15 +9,15 @@ class TestHumanAgeConversion:
     @pytest.mark.parametrize(
         "cat_age, dog_age, expected_result",
         [
-            pytest.param(-1, -1, [0, 0]),
-            pytest.param(0, 0, [0, 0]),
-            pytest.param(14, 14, [0, 0]),
-            pytest.param(15, 15, [1, 1]),
-            pytest.param(23, 23, [1, 1]),
-            pytest.param(24, 24, [2, 2]),
-            pytest.param(27, 27, [2, 2]),
-            pytest.param(28, 28, [3, 2]),
-            pytest.param(100, 100, [21, 17]),
+            (-1, -100, [0, 0]),
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
+            (100.001, 100, [21, 17])
         ]
     )
     def test_get_human_age(self,
