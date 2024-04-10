bdbdfe18e7ac0612959c8b55b379a5789b7ef976
Sofiia <sonagm9@gmail.com>
2024-04-10 14:56:03 +0300

    'Solution4'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+
 from app.main import get_human_age
 
 
@@ -18,25 +19,35 @@ from app.main import get_human_age
         (-100, -100, [0, 0]),
     ],
     ids=[
-        "Both zero",
-        "Both 14",
-        "Both 15",
-        "Both 23",
-        "Both 24",
-        "Both 27",
-        "Cat 28, Dog 28",
-        "Both 29",
-        "Both 100",
-        "Both -5",
-        "Both -100",
+        "Dog and Cat ages 0 must equal 0 human ages",
+        "Dog and Cat ages 14 must equal 0 human ages",
+        "Dog and Cat ages 15 must equal 1 human ages",
+        "Dog and Cat ages 23 must equal 1 human ages",
+        "Dog and Cat ages 22 must equal 2 human ages",
+        "Dog and Cat ages 27 must equal 2 human ages",
+        "Dog and Cat ages 28 must equal 3 human ages "
+        "for Cat and 2 human ages for Dog",
+        "Dog and Cat ages 29 must equal 3 human ages",
+        "Dog and Cat ages 100 must equal 21 human ages "
+        "for Cat and 17 human ages for Dog",
+        "Dog and Cat ages -5 must equal 0 human ages",
+        "Dog and Cat ages -100 must equal 0 human ages",
     ]
 )
 def test_get_human_age(cat_age: int,
                        dog_age: int,
-                       expected_human_age: int) -> None:
+                       expected_human_age: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected_human_age
 
 
-def test_get_human_age_invalid_input() -> None:
+@pytest.mark.parametrize(
+    "invalid_input",
+    [
+        ("cat", "dog"),
+        (None, None),
+        ({}, []),
+    ]
+)
+def test_get_human_age_invalid_input(invalid_input: tuple) -> None:
     with pytest.raises(TypeError):
-        get_human_age("cat", "dog")
+        get_human_age(*invalid_input)
