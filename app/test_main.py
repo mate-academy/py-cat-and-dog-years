026edb8bf61995e046c191f0bcc21dae72186c2b
Mykola Medved <kradu2001@gmail.com>
2024-04-10 23:02:46 +0300

    add raises correct exception

--- app/test_main.py
+++ app/test_main.py
@@ -2,6 +2,8 @@ import pytest
 
 from app.main import get_human_age
 
+from typing import Any
+
 
 @pytest.mark.parametrize("cat_age, dog_age, expected_human_age", [
     pytest.param(-5, -4, [0, 0], id="test should gives 0"
@@ -26,3 +28,27 @@ def test_get_human_age(
         cat_age: int, dog_age: int,
         expected_human_age: list[int]) -> None:
     assert get_human_age(cat_age, dog_age) == expected_human_age
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+    [
+        ("11", 11, TypeError),
+        (5, [5, 0], TypeError),
+        (1, (1,), TypeError),
+        ({3: 0}, 3, TypeError)
+    ],
+    ids=[
+        "Animal ages should be type 'int', not 'str'",
+        "Animal ages should be type 'int', not 'list'",
+        "Animal ages should be type 'int', not 'tuple'",
+        "Animal ages should be type 'int', not 'dict'"
+    ]
+)
+def test_get_human_age_raises_the_correct_exception(
+        cat_age: Any,
+        dog_age: Any,
+        expected_error: type[Exception]
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
