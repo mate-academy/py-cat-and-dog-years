f68bbcff63dbb7ae187ae32065be8d4bf49d08d6
onyevyerov <onyevyerov@gmail.com>
2024-04-11 13:55:23 +0200

    Fixed flake8

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,6 @@
-from app.main import get_human_age
 import pytest
+from app.main import get_human_age
+from typing import Any
 
 
 @pytest.mark.parametrize(
@@ -20,13 +21,19 @@ import pytest
         pytest.param(27, 27, [2, 2],
                      id="not enough cat/dog years to 3 humans years"),
         pytest.param(28, 28, [3, 2],
-                     id="28 years of dogs live is not enough for 3 humans years"),
+                     id="28 years of dogs live is not enough for"
+                        " 3 humans years"),
         pytest.param(100, 100, [21, 17],
-                     id="after first 24 years of dog/cat every 5 for dog and every 4 for cats lives"
+                     id="after first 24 years of dog/cat age"
+                        " every 5 for dog and every 4 for cats lives"
                      " give 1 additional year for humans live"),
     ]
 )
-def test_function_returns_good_result(cat_age: int, dog_age: int, human_age_to_animal: list):
+def test_function_returns_good_result(
+        cat_age: int,
+        dog_age: int,
+        human_age_to_animal: list
+) -> None:
     assert get_human_age(cat_age, dog_age) == human_age_to_animal
 
 
@@ -39,6 +46,6 @@ def test_function_returns_good_result(cat_age: int, dog_age: int, human_age_to_a
         ({"cat": 2}, {"dog": 2}),
     ]
 )
-def test_age_pets_if_not_integer(cat_age, dog_age) -> None:
+def test_age_pets_if_not_integer(cat_age: Any, dog_age: Any) -> None:
     with pytest.raises(TypeError):
         get_human_age(cat_age, dog_age)
