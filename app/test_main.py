233c3e8e94976f5b6eadeb1a84d5ec999b8b2a10
Marko Panchak <marko.panchak1@gmail.com>
2024-04-19 13:02:08 +0300

    Solution_Using_Mock

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,35 @@
 import pytest
+from unittest.mock import patch
 from app.main import get_human_age
 
 
 class TestForGetHumanAge:
     @pytest.mark.parametrize(
-        "cat_age, dog_age, human_age",
+        "cat_age, dog_age, expected_cat_human_age, expected_dog_human_age",
         [
-            pytest.param(-1, -2, [0, 0], id="-1, -2 should [0, 0]"),
-            pytest.param(0, 0, [0, 0], id="0, 0 should [0, 0]"),
-            pytest.param(14, 14, [0, 0], id="14, 14 should [0, 0]"),
-            pytest.param(15, 15, [1, 1], id="15, 15 should [1, 1]"),
-            pytest.param(23, 23, [1, 1], id="23, 23 should [1, 1]"),
-            pytest.param(24, 24, [2, 2], id="24, 24 should [2, 2]"),
-            pytest.param(27, 27, [2, 2], id="27, 27 should [2, 2]"),
-            pytest.param(28, 28, [3, 2], id="28, 28 should [3, 2]"),
-            pytest.param(100, 100, [21, 17], id="100, 100 should [21, 17]"),
+            (0, 0, 0, 0),
+            (14, 14, 0, 0),
+            (15, 15, 1, 1),
+            (23, 23, 1, 1),
+            (24, 24, 2, 2),
+            (27, 27, 2, 2),
+            (28, 28, 3, 2),
+            (100, 100, 21, 17),
         ]
     )
-    def test_args(self, cat_age: any, dog_age: any, human_age: any) -> None:
-        assert get_human_age(cat_age, dog_age) == human_age
+    @patch("app.main.convert_to_human")
+    def test_get_human_age_with_mock(self,
+                                     mock_convert_to_human: any,
+                                     cat_age: any,
+                                     dog_age: any,
+                                     expected_cat_human_age: any,
+                                     expected_dog_human_age: any) -> None:
+        mock_convert_to_human.side_effect = [
+            expected_cat_human_age,
+            expected_dog_human_age
+        ]
+        result = get_human_age(cat_age, dog_age)
+        assert result == [expected_cat_human_age, expected_dog_human_age]
+        assert mock_convert_to_human.call_count == 2
+        mock_convert_to_human.assert_any_call(cat_age, 15, 9, 4)
+        mock_convert_to_human.assert_any_call(dog_age, 15, 9, 5)
