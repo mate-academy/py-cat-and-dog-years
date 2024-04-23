342d5e09813260c8a570437c58aab825502a80fc
Liudmyla <4888814@gmail.com>
2024-04-23 12:28:06 +0300

    'Solution'

--- app/main.py
+++ app/main.py
@@ -1,12 +1,17 @@
-def get_human_age(cat_age: int, dog_age: int) -> list:
-    cat_to_human = convert_to_human(cat_age, 15, 9, 4)
-    dog_to_human = convert_to_human(dog_age, 15, 9, 5)
+def get_human_age(cat_age: int | float, dog_age: int | float) -> list:
+    if not (isinstance(cat_age, (int, float))
+            and isinstance(dog_age, (int, float))):
+        raise TypeError("Age should be a number")
+    cat_to_human = convert_to_human(int(cat_age), 15, 9, 4)
+    dog_to_human = convert_to_human(int(dog_age), 15, 9, 5)
     return [cat_to_human, dog_to_human]
 
 
 def convert_to_human(
         animal_age: int, first_year: int, second_year: int, each_year: int
 ) -> int:
+    if not 0 < animal_age < 50:
+        raise ValueError("The data out of normal range")
     if animal_age < first_year:
         return 0
     if animal_age < first_year + second_year:
--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,32 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat,dog,expected",
+    [
+        (23, 23, [1, 1]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+    ]
+)
+def test_of_correct_counting(cat: int, dog: int, expected: list) -> None:
+    assert get_human_age(cat, dog) == expected
+
+
+def test_should_return_integer_even_if_float_input() -> None:
+    assert get_human_age(15.5, 15.1) == [1, 1]
+
+
+def test_should_raise_error_when_data_out_of_normal_range() -> None:
+    with pytest.raises(ValueError):
+        get_human_age(1000, 150)
+
+
+def test_should_raise_error_when_input_data_not_a_number() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("one", 15)
