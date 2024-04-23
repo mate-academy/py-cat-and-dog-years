b1282ce368aa922f24fbb269a21ada644dd62de2
Liudmyla <4888814@gmail.com>
2024-04-23 13:55:58 +0300

    'solution'

--- app/test_main.py
+++ app/test_main.py
@@ -7,26 +7,35 @@ from app.main import get_human_age
     [
         (23, 23, [1, 1]),
         (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
-        (28, 28, [3, 2]),
+        (15.5, 15.1, [1, 1])
     ]
 )
 def test_of_correct_counting(cat: int, dog: int, expected: list) -> None:
     assert get_human_age(cat, dog) == expected
 
 
-def test_should_return_integer_even_if_float_input() -> None:
-    assert get_human_age(15.5, 15.1) == [1, 1]
+@pytest.mark.parametrize(
+    "cat,dog,expected",
+    [
+        pytest.param(1000, 150, ValueError, id='extremely large value'),
+        pytest.param(-5, 12, ValueError, id='zero value'),
+        pytest.param(0, -100, ValueError, id='negative value')
+    ]
+)
+def test_should_raise_error_when_data_out_of_normal_range(cat, dog, expected) -> None:
+    with pytest.raises(expected):
+        get_human_age(cat, dog)
 
 
-def test_should_raise_error_when_data_out_of_normal_range() -> None:
-    with pytest.raises(ValueError):
-        get_human_age(1000, 150)
-
-
-def test_should_raise_error_when_input_data_not_a_number() -> None:
-    with pytest.raises(TypeError):
-        get_human_age("one", 15)
+@pytest.mark.parametrize(
+    "cat,dog,expected",
+    [
+        pytest.param({}, 12, TypeError, id='dict input'),
+        pytest.param([], 0, TypeError, id='list input'),
+        pytest.param(set(), 21, TypeError, id='set input'),
+        pytest.param("one", 15, TypeError, id='string input'),
+    ]
+)
+def test_should_raise_error_when_input_data_not_a_number(cat, dog, expected) -> None:
+    with pytest.raises(expected):
+        get_human_age(cat, dog)
