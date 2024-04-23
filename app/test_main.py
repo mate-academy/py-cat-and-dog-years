ad97121f8589be6589a0332672be5bd7a2acbf73
Liudmyla <4888814@gmail.com>
2024-04-23 14:02:55 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+
 from app.main import get_human_age
 
 
@@ -17,12 +18,16 @@ def test_of_correct_counting(cat: int, dog: int, expected: list) -> None:
 @pytest.mark.parametrize(
     "cat,dog,expected",
     [
-        pytest.param(1000, 150, ValueError, id='extremely large value'),
-        pytest.param(-5, 12, ValueError, id='zero value'),
-        pytest.param(0, -100, ValueError, id='negative value')
+        pytest.param(1000, 150, ValueError, id="extremely large value"),
+        pytest.param(-5, 12, ValueError, id="zero value"),
+        pytest.param(0, -100, ValueError, id="negative value")
     ]
 )
-def test_should_raise_error_when_data_out_of_normal_range(cat, dog, expected) -> None:
+def test_should_raise_error_when_data_out_of_normal_range(
+        cat: int,
+        dog: int,
+        expected: str
+) -> None:
     with pytest.raises(expected):
         get_human_age(cat, dog)
 
@@ -30,12 +35,16 @@ def test_should_raise_error_when_data_out_of_normal_range(cat, dog, expected) ->
 @pytest.mark.parametrize(
     "cat,dog,expected",
     [
-        pytest.param({}, 12, TypeError, id='dict input'),
-        pytest.param([], 0, TypeError, id='list input'),
-        pytest.param(set(), 21, TypeError, id='set input'),
-        pytest.param("one", 15, TypeError, id='string input'),
+        pytest.param({}, 12, TypeError, id="dict input"),
+        pytest.param([], 0, TypeError, id="list input"),
+        pytest.param(set(), 21, TypeError, id="set input"),
+        pytest.param("one", 15, TypeError, id="string input"),
     ]
 )
-def test_should_raise_error_when_input_data_not_a_number(cat, dog, expected) -> None:
+def test_should_raise_error_when_input_data_not_a_number(
+        cat: int,
+        dog: int,
+        expected: str
+) -> None:
     with pytest.raises(expected):
         get_human_age(cat, dog)
