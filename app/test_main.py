21275d60b2c670aa5120e712638dc2944fdc5d06
Viktor Zhdanovych <cryingtm27@gmail.com>
2024-04-28 15:02:10 +0300

    cat and dog years solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (28, 28, [3, 2]),
+        (-1, -11, [0, 0])
+
+    ],
+    ids=[
+        "If cat and dog age = 0 should return [0, 0]",
+        "Should return [0, 0]",
+        "Should return [1, 1]",
+        "Should return [3, 2]",
+        "If age is negative, should return [0, 0]"
+    ]
+)
+def test_get_human_age(cat_age, dog_age, result):
+    assert get_human_age(cat_age, dog_age) == result
