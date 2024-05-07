1c0778857e547e12fadc909c87603a751afe2d80
Nazar <legoman1858@gmail.com>
2024-05-07 23:48:43 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_cat_dog_age_must_be_0_when_under_15() -> None:
+    assert get_human_age(13, 13) == [0, 0]
+
+
+def test_cat_dog_age_must_be_1_when_under_24() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_cat_dog_age_must_be_1_next_years() -> None:
+    assert get_human_age(28, 28) == [3, 2]
