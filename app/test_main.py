0744f29876204d145598e41343d5c1dc27420e33
Triukhan <tryukhan.d@gmail.com>
2024-05-08 22:42:09 +0300

    triukhan solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,converted_age_list", [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_get_human_age_boundary_values(
+        cat_age: int,
+        dog_age: int,
+        converted_age_list: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == converted_age_list
