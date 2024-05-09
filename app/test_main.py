d44db25af115935b9490aa123272fd1941321ae1
= <=>
2024-05-09 18:37:09 +0300

    wrote tests for get_human_age()

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_list_with_2_integers():
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_convert_animals_14_years_to_one_human():
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_convert_animals_23_years_to_1_human():
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_convert_animals_24_years_to_2_human():
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_should_convert_cats_27_and_dogs_28_years_to_3_human():
+    assert get_human_age(27, 28) == [2, 2]
+
+
+def test_should_convert_cats_28_and_dogs_29_years_to_3_human():
+    assert get_human_age(28, 29) == [3, 3]
