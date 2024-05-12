d73104a5d1f58cd2eb11e23993dd0ff43b327add
Oleksandr Mykhailyk <stekloduv45@gmail.com>
2024-05-12 14:46:21 +0200

    Done

--- app/test_main.py
+++ app/test_main.py
@@ -1,29 +1,18 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_should_return_0_year_cat_and_dog() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_should_return_1_year_cat_and_0_year_dog() -> None:
-    assert get_human_age(16, 9) == [1, 0]
-
-
-def test_should_return_0_year_cat_and_1_year_dog() -> None:
-    assert get_human_age(8, 17) == [0, 1]
-
-
-def test_should_return_1_year_cat_and_dog() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_should_return_2_year_cat_and_dog() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_should_return_3_year_cat_and_dog() -> None:
-    assert get_human_age(30, 31) == [3, 3]
-
-
-def test_should_return_4_year_cat_and_3_year_dog() -> None:
-    assert get_human_age(28, 28) == [3, 2]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (14, 14, [0, 0]),
+        (16, 9, [1, 0]),
+        (8, 17, [0, 1]),
+        (15, 15, [1, 1]),
+        (24, 24, [2, 2]),
+        (30, 31, [3, 3]),
+        (28, 28, [3, 2]),
+    ],
+)
+def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
