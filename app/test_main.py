bdf8df23936a0a9fe759a8a514512e57c940a81c
Viktor Shcherbak <dev.psychologist@gmail.com>
2024-05-13 23:33:15 +0300

    Animal testing

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_animal_not_first_year() -> None:
+    assert get_human_age(14, 12) == [0, 0]
+
+
+def test_animal_one_years() -> None:
+    assert get_human_age(15, 23) == [1, 1]
+
+
+def test_animal_two_years() -> None:
+    assert get_human_age(28, 28) == [3, 2]
