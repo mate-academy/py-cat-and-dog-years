90aa821da84df8236935b3c084c50338ea0d7586
Vladimir Dolhyi <volodymyr_dolgyi@ukr.net>
2024-05-14 18:14:16 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_with_zeros():
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_with_cat_less_15_and_dog_less_15():
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_with_cat_15_and_dog_15():
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_with_cat_more_15_and_dog_less_15():
+    assert get_human_age(23, 7) == [1, 0]
+
+
+def test_with_cat_less_15_and_dog_more_15():
+    assert get_human_age(12, 20) == [0, 1]
+
+
+def test_with_cat_more_24_and_dog_more_24():
+    assert get_human_age(25, 26) == [2, 2]
+
+
+def test_with_cat_28_and_dog_28():
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_with_cat_50_and_dog_50():
+    assert get_human_age(50, 50) == [8, 7]
