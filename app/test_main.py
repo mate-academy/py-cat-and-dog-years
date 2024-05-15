97a3c71dfa1872eeace0aa2891fb2e6bf12cd80f
Viktor Shcherbak <dev.psychologist@gmail.com>
2024-05-15 22:23:42 +0300

    Animal testing (test Error)

--- app/test_main.py
+++ app/test_main.py
@@ -6,20 +6,15 @@ import pytest
     (14, 12, [0, 0]),  # Testing when both animals are less than the first year
     (15, 23, [1, 1]),  # Testing when both animals are one year old
     (28, 28, [3, 2]),  # Testing when both animals are two years old
-    (-1, 5, None),  # Testing negative cat_age
-    (5, -1, None),  # Testing negative dog_age
-    (0, 5, None),  # Testing zero cat_age
-    (5, 0, None),  # Testing zero dog_age
-    (1000000, 5, None),  # Testing very large cat_age
-    (5, 1000000, None),  # Testing very large dog_age
-    (5, 5.5, None),  # Testing dog_age as float
-    ("5", 5, None),  # Testing cat_age as string
-    (5, "5", None),  # Testing dog_age as string
-    (5, 5, None),  # Testing each_year as 0
 ])
 def test_get_human_age(cat_age: int, dog_age: int, expected: int) -> None:
-    if expected is None:
-        with pytest.raises((TypeError, ValueError)):
-            get_human_age(cat_age, dog_age)
-    else:
-        assert get_human_age(cat_age, dog_age) == expected
+    assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize("cat_age, dog_age, expected_error", [
+    ("5", 5, TypeError),  # Testing cat_age as string
+    (5, "5", TypeError),  # Testing dog_age as string
+])
+def test_error_key_value(cat_age: int, dog_age: int, expected_error) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
