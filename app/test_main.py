4cd209881910f363029bc923dbe5422509ce75a9
RuslanLevchuk <rusik.levchuk.m@gmail.com>
2024-05-22 18:20:58 +0300

    hell yeah fixes

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,5 @@
+import pytest
+
 from app.main import get_human_age
 
 
@@ -5,30 +7,40 @@ def test_is_list_returned() -> None:
     assert isinstance(get_human_age(0, 0), list)
 
 
-def test_is_length_of_list_equal_two() -> None:
+def test_is_list_length_equal_two() -> None:
     assert len(get_human_age(0, 0)) == 2
 
 
-def test_are_values_of_list_integers() -> None:
+def test_are_list_values_integers() -> None:
     result = get_human_age(18, 18)
     assert isinstance(result[0], int) and isinstance(result[1], int)
 
 
-def test_should_return_zeros_if_pets_ages_are_less_than_15() -> None:
-    assert get_human_age(14, 12) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected",
+    [
+        ("cat", 17, TypeError),
+        (44, "dog", TypeError)
+    ]
+)
+def test_exception_rising(
+        cat_age: int,
+        dog_age: int,
+        expected: type[TypeError]
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
 
 
-def test_one_human_year_check() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_two_human_years_check() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_three_human_years_check() -> None:
-    assert get_human_age(28, 29) == [3, 3]
-
-
-def test_more_than_three_human_year_check() -> None:
-    assert get_human_age(45, 57) == [7, 8]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected",
+    [
+        (14, 12, [0, 0]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (28, 29, [3, 3]),
+        (45, 57, [7, 8])
+    ]
+)
+def test_output_results(cat_age: int, dog_age: int, expected: list) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
