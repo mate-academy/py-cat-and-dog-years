42df75b475a53c3f6a1a58a560d680af477c277a
Dima Drozdenko <anonimchik920@gmail.com>
2024-05-24 15:33:11 +0300

    develop

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_to_human, dog_to_human, expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_get_human_age(cat_to_human: int,
+                       dog_to_human: int,
+                       expected: list) -> None:
+    assert get_human_age(cat_to_human, dog_to_human) == expected
