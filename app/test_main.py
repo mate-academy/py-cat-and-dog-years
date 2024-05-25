a5350bac21cceb8b99756e906c5b2af65b7dd8bd
Kateryna <kat.rin47u@gmail.com>
2024-05-25 12:03:55 +0100

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -2,26 +2,36 @@ import pytest
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize("cat_age, dog_age, expected", [
-    (0, 0, [0, 0]),
-    (14, 14, [0, 0]),
-    (15, 15, [1, 1]),
-    (23, 23, [1, 1]),
-    (24, 24, [2, 2]),
-    (27, 27, [2, 2]),
-    (28, 28, [3, 2]),
-    (100, 100, [21, 17])
-])
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (-1, 10, [0, 0]),
+        (10, -1, [0, 0]),
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        (10000, 10000, [2496, 1997]),
+        (9999, 9999, [2495, 1997])
+    ]
+)
 def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
 
-@pytest.mark.parametrize("cat_age, dog_age", [
-    ("15", 15),
-    (15, "15"),
-    (None, 15),
-    (15, None)
-])
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("15", 15),
+        (15, "15"),
+        (None, 15),
+        (15, None)
+    ]
+)
 def test_get_human_age_with_invalid_types(
         cat_age: int,
         dog_age: int
