4919acab6e4bb7f6e9fe64b715d0c46757fef244
Tsvetan Kichuk <toys4babyodua@gmail.com>
2024-05-29 20:32:45 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_zero_age() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_return_animals_have_not_a_year() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_animals_have_one_year() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_return_animals_have_two_years() -> None:
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_should_return_animal_have_different_age() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_should_return_animals_have_their_100_years() -> None:
+    assert get_human_age(100, 100) == [21, 17]
