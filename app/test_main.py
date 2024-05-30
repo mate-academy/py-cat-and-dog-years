a6b5363c4795ab7efa445fbcfecfd0b44f4104a0
Andrew Chugreev <andrey.4.1985@gmail.com>
2024-05-30 14:28:25 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,27 +7,32 @@ from app.main import get_human_age
     [
         pytest.param(
             5, 6, [0, 0],
-            id="return zero if age < 15 years"),
+            id="return zero if age < 15 years"
+        ),
         pytest.param(
             23, 17, [1, 1],
-            id="add 1 for 15 years"),
+            id="add 1 for 15 years"
+        ),
         pytest.param(
             26, 27, [2, 2],
-            id="add 1 for 24 years"),
+            id="add 1 for 24 years"
+        ),
         pytest.param(
             28, 0, [3, 0],
-            id="add 1 for next 4 cat years"),
+            id="add 1 for next 4 cat years"
+        ),
         pytest.param(
             0, 29, [0, 3],
-            id="add 1 for next 5 dog years"),
+            id="add 1 for next 5 dog years"
+        ),
         pytest.param(
             -1, -2, [0, 0],
-            id="return 0 for negative value of age"),
+            id="return 0 for negative value of age"
+        ),
         pytest.param(
-            100,
-            100,
-            [21, 17],
-            id="return different values if cat_age = dog_age"),
+            100, 100, [21, 17],
+            id="return different values if cat_age = dog_age"
+        ),
     ]
 )
 def test_convert_human_age(
