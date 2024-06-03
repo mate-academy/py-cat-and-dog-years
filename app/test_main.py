14255aa02ffcaef858366ada44b33d654bf3c733
Bohdan Bastiuk <bbastyuk@gmail.com>
2024-06-03 22:56:03 +0200

    New solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,49 @@
-import pytest
-from typing import List
 from app.main import get_human_age
+import pytest
 
 
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected",
-    [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
-        (28, 28, [3, 2]),
-        (100, 100, [21, 17])
-    ]
-)
-def test_get_human_age(cat_age: int,
-                       dog_age: int,
-                       expected: List[int]) -> None:
-    assert get_human_age(cat_age, dog_age) == expected
+class TestConvertHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected",
+        [
+            pytest.param(14, 14, [0, 0],
+                         id="14 cat/dog years should "
+                         "convert into 0 human age."),
+            pytest.param(15, 15, [1, 1],
+                         id="15 cat/dog years should "
+                         "convert into 0 human age."),
+            pytest.param(23, 23, [1, 1],
+                         id="23 cat/dog years should "
+                         "convert into 1 human age."),
+            pytest.param(24, 24, [2, 2],
+                         id="24 cat/dog years should "
+                         "convert into 2 human age."),
+            pytest.param(27, 28, [3, 2],
+                         id="27/28 cat/dog years should "
+                         "convert into 2 human age."),
+            pytest.param(29, 28, [3, 3],
+                         id="28/29 cat/dog years should "
+                         "convert into 3 human age."),
+            pytest.param(-1, -1, [0, 0],
+                         id="-1 cat/dog years should "
+                            "convert into 0 human age."),
+        ]
+    )
+    def test_convert_human_age(self,
+                               dog_age: int,
+                               cat_age: int,
+                               expected: list
+                               ) -> None:
+
+        assert get_human_age(dog_age, cat_age) == expected
+
+
+class TestInvalidInputType:
+    @pytest.mark.parametrize("cat_age, dog_age", [
+        ("string", 10),
+        (10, "string"),
+        ("string", "string")
+    ])
+    def test_invalid_input_type(self, cat_age: int, dog_age: int) -> None:
+        with pytest.raises(TypeError):
+            get_human_age(cat_age, dog_age)
