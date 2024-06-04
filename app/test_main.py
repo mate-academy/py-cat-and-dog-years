59ca0e30642f85265858c884f1883f1cb4e9fba2
Sergii Musiienko <vpnmus@ukr.net>
2024-06-04 21:52:00 +0300

    fix Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,15 +5,21 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, result",
     [
-        (0, 0, [0, 0]),
+        (1, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
+        (-1, -1, [0, 0])
     ]
 )
 def test_get_human_age(cat_age: int, dog_age: int, result: list[int]) -> None:
     assert get_human_age(cat_age, dog_age) == result
+
+
+def test_incorrect_type_of_value() -> None:
+    with pytest.raises(TypeError):
+        get_human_age(2, "3")
