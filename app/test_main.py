a1e5f2d4b0964794475acfa6f127b6b7f8fb3431
Bohdan Kuzik <bogdankuzik2509@gmail.com>
2024-06-04 17:55:13 +0200

    added mark parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,38 +1,28 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_get_human_age() -> None:
+@pytest.mark.parametrize(
+    "input1, input2, expected_result",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_get_human_age(
+        input1: int, input2: int, expected_result: list
+) -> None:
     assert (
-        get_human_age(0, 0) == [0, 0]
-    ), "get_human_age(0, 0) should return [0, 0]"
-    assert (
-        get_human_age(14, 14) == [0, 0]
-    ), "get_human_age(14, 14) should return [0, 0]"
-    assert (
-        get_human_age(15, 15) == [1, 1]
-    ), "get_human_age(15, 15) should return [1, 1]"
-    assert (
-        get_human_age(23, 23) == [1, 1]
-    ), "get_human_age(23, 23) should return [1, 1]"
-    assert (
-        get_human_age(24, 24) == [2, 2]
-    ), "get_human_age(24, 24) should return [2, 2]"
-    assert (
-        get_human_age(27, 27) == [2, 2]
-    ), "get_human_age(27, 27) should return [2, 2]"
-    assert (
-        get_human_age(28, 28) == [3, 2]
-    ), "get_human_age(28, 28) should return [3, 2]"
-    assert (
-        get_human_age(100, 100) == [21, 17]
-    ), "get_human_age(100, 100) should return [21, 17]"
+        get_human_age(input1, input2) == expected_result
+    ), f"get_human_age({input1}, {input2}) should return {expected_result}"
 
-    try:
-        get_human_age(-1, -2)
-    except ValueError:
-        print("ValueError: get_human_age(-1, -2) should be with correct value")
 
-    try:
-        get_human_age("1", "3")
-    except TypeError:
-        print("TypeError: get_human_age('1', '3') should be int")
+def test_get_human_age_exceptions() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("1", "2")
