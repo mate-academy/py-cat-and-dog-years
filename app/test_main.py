8952774cb6c061dcc245b9d99c9163def62de38b
Ivan <ivan.seeker6@gmail.com>
2024-06-06 17:57:05 +0300

    sol1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+def test_when_years_is_zero() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+    assert get_human_age(9, 0) == [0, 0]
+    assert get_human_age(0, 9) == [0, 0]
+
+
+def test_when_age_is_too_low_for_cat() -> None:
+    assert get_human_age(14, 16) == [0, 1]
+    assert get_human_age(9, 24) == [0, 2]
+    assert get_human_age(4, 29) == [0, 3]
+
+
+def test_when_age_is_too_low_for_dog() -> None:
+    assert get_human_age(16, 14) == [1, 0]
+    assert get_human_age(24, 9) == [2, 0]
+    assert get_human_age(28, 4) == [3, 0]
+
+
+def test_for_big_numbers() -> None:
+    assert get_human_age(100, 100) == [21, 17]
+    assert get_human_age(150, 150) == [33, 27]
+
+
+if __name__ == "__main__":
+    pytest.main()
