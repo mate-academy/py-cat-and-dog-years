b1dc07e28b51db892ee8da5602c7e4710ee0b5f6
unlie9 <unlie.devops9@gmail.com>
2024-06-06 14:31:51 +0300

    sol1

--- app/test_main.py
+++ app/test_main.py
@@ -16,5 +16,5 @@ from app.main import get_human_age
         (100, 100, [21, 17])
     ]
 )
-def test_ages(cat_age, dog_age, expected):
+def test_ages(cat_age: int, dog_age: int, expected: list[int, int]) -> None:
     assert get_human_age(cat_age, dog_age) == expected
