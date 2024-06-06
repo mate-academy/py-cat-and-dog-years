266ed52abc0e7a646f9acd77222346f202eed87b
necobra <necobrane105@gmail.com>
2024-06-06 08:53:04 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,41 +1,35 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_should_return_zeros_when_inputs_are_zeros() -> None:
+@pytest.mark.parametrize(
+    "cat_age,dog_age,cat_and_dog_age_in_human",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        [24, 24, [2, 2]],
+        [27, 27, [2, 2]],
+        [28, 28, [3, 2]],
+        [100, 100, [21, 17]]
+    ],
+    ids=[
+        "zeros",
+        "almost 1 human year",
+        "1 human year after 15 animal",
+        "almost 2 human year",
+        "2 human year after 24 animal",
+        "almost 3 human year",
+        "human age for cat is one more than for dog when animals ages are 28",
+        "human age equal two plus animal age divided by 4 for cat and by 5 for dog"
+    ]
+)
+def test_non_negative_inputs_get_human_age(cat_age, dog_age, cat_and_dog_age_in_human) -> None:
+    assert get_human_age(cat_age, dog_age) == cat_and_dog_age_in_human
+
+
+def test_negative_inputs_get_human_age() -> None:
     expected_value = [0, 0]
-    assert get_human_age(0, 0) == expected_value
-
-
-def test_should_return_zeros_when_values_almost_15() -> None:
-    expected_value = [0, 0]
-    assert get_human_age(14, 14) == expected_value
-
-
-def test_should_return_ones_when_values_are_exactly_15() -> None:
-    expected_value = [1, 1]
-    assert get_human_age(15, 15) == expected_value
-
-
-def test_should_return_ones_when_values_are_almost_24() -> None:
-    expected_value = [1, 1]
-    assert get_human_age(23, 23) == expected_value
-
-
-def test_should_return_twos_when_values_are_exactly_24() -> None:
-    expected_value = [2, 2]
-    assert get_human_age(24, 24) == expected_value
-
-
-def test_should_return_twos_when_values_are_almost_27() -> None:
-    expected_value = [2, 2]
-    assert get_human_age(27, 27) == expected_value
-
-
-def test_should_return_three_for_cat_and_two_for_dog_when_values_are_28() -> None:
-    expected_value = [3, 2]
-    assert get_human_age(28, 28) == expected_value
-
-
-def test_should_return_two_plus_amount_of_animals_years_devided_by_5_for_cat_and_by_4_for_dog() -> None:
-    expected_value = [21, 17]
-    assert get_human_age(100, 100) == expected_value
+    assert get_human_age(-3, -9) == expected_value
