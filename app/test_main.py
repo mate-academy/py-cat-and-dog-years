3d12f19aa32f8f039cb7eaada5fb858280072cdd
ilgan <2ilgan1601@gmail.com>
2024-06-07 19:37:14 +0300

    3

--- app/test_main.py
+++ app/test_main.py
@@ -1,8 +1,11 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_get_human_age() -> None:
-    test_data = [
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
@@ -10,6 +13,22 @@ def test_get_human_age() -> None:
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
     ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, expected: int) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
 
-    for cat_age, dog_age, result in test_data:
-        assert get_human_age(cat_age, dog_age) == result
+
+@pytest.mark.parametrize(
+    "cat_age_err, dog_age_err",
+    [
+        (14, "14"),
+        ("15", 15),
+        ([23], 23),
+        (24, [24]),
+    ]
+)
+def test_incorrect_data(
+        cat_age_err: int | str | list,
+        dog_age_err: int | str | list) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age_err, dog_age_err)
