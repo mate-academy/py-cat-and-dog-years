6ec9885e4948ee75a54e3bb5f7eb98061a98af69
Iryna Honcharenko <irynahoncharenko9@gmail.com>
2024-06-07 22:59:48 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,12 +1,18 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_get_human_age() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(23, 23) == [1, 1]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(27, 27) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [pytest.param(0, 0, [0, 0], id="zero animal years"),
+     pytest.param(14, 14, [0, 0], id="14 animal years"),
+     pytest.param(15, 15, [1, 1], id="15 animal years"),
+     pytest.param(27, 27, [2, 2], id="27 animal years"),
+     pytest.param(28, 28, [3, 2], id="28 animal years"),
+     pytest.param(-5, -3, [0, 0], id="negative value"),
+     pytest.param(100, 100, [21, 17], id="100 animal years")]
+)
+def test_get_human_age(cat_age: int, dog_age: int, result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result, \
+        (f"When cat is {cat_age} cat years, and dog is {dog_age} "
+         f"you should get this {result} list")
