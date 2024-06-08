ed4a67c5d39a853814da628916b88dedf324e0de
NikitaNikolaitsev <159700503+NikitaNikolaitsev@users.noreply.github.com>
2024-06-08 10:31:56 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -15,7 +15,9 @@ import pytest
         (-2, -5, [0, 0])
     ]
 )
-def test_human_age_in_cat_and_dog_age(cat_age: int, dog_age: int, result: int) -> None:
+def test_human_age_in_cat_and_dog_age(
+        cat_age: int, dog_age: int, result: int
+) -> None:
     assert get_human_age(cat_age, dog_age) == result
 
 
@@ -26,6 +28,8 @@ def test_human_age_in_cat_and_dog_age(cat_age: int, dog_age: int, result: int) -
         (3, None, TypeError),
     ]
 )
-def test_func_wait_int(cat_age: int, dog_age: int, error: type(TypeError)) -> None:
+def test_func_wait_int(
+        cat_age: int, dog_age: int, error: type(TypeError)
+) -> None:
     with pytest.raises(error):
         get_human_age(cat_age, dog_age)
