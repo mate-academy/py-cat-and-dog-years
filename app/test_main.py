60a6364329de332476f985d74c68609928e60f7b
Alex Bul <tarasov57228@gmail.com>
2024-06-11 19:32:37 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -8,11 +8,13 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age,dog_age,result",
     [
+        (-1, -3, [0, 0]),
         (1, 1, [0, 0]),
         (15, 15, [1, 1]),
         (24, 24, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17])
+
     ]
 )
 def test_cat_or_dog_yars(cat_age: int, dog_age: int, result: list) -> None:
