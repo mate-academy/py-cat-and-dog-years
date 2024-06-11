67063501572ed1faf619a2441fde7927cf8d5c84
Alex Bul <tarasov57228@gmail.com>
2024-06-11 19:17:59 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,36 @@
+import pytest
+
 from app.main import get_human_age
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        (1, 1, [0, 0]),
+        (15, 15, [1, 1]),
+        (24, 24, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ]
+)
+def test_cat_or_dog_yars(cat_age: int, dog_age: int, result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,typeerror",
+    [
+        ("25", 0, TypeError),
+        (15, "0", TypeError),
+        ({1: 2}, [], TypeError),
+    ]
+)
+def test_cat_integer(cat_age, dog_age, typeerror) -> None:
+    with pytest.raises(typeerror):
+        get_human_age(cat_age, dog_age)
 
 
 def test_length_return_get_human_age() -> None:
     assert len(get_human_age(0, 0)) == 2
 
 
-def test_cat_integer() -> None:
-    assert isinstance(get_human_age(100, 100)[0], int)
-
-
-def test_dog_integer() -> None:
-    assert isinstance(get_human_age(100, 100)[1], int)
-
-
-def test_if_cat_or_dog_14_yars() -> None:
-    assert get_human_age(1, 1) == [0, 0]
-
-
-def test_if_cat_or_dog_15_yars() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_if_cat_or_dog_24_yars() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_if_cat_or_dog_28_yars() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_if_cat_or_dog_100_yars() -> None:
-    assert get_human_age(100, 100) == [21, 17]
