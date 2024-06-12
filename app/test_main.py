ee14fa9cac2d29e82c775f0e5523373836cdbe85
mykyta <nikita.fedotov222@gmail.com>
2024-06-12 20:08:05 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from app.main import get_human_age
+
 import pytest
 
 
@@ -14,7 +15,8 @@ import pytest
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
+        (-1, 3, [0, 0]),
     ],
     ids=[
         "0 of cat age and 0 of dog age should equal 0, 0 of human ages",
@@ -25,6 +27,7 @@ import pytest
         "27 of cat age and 27 of dog age should equal 2, 2 of human ages",
         "28 of cat age and 28 of dog age should equal 3, 2 of human ages",
         "100 of cat age and 100 of dog age should equal 21, 17 of human ages",
+        "-1 of cat age and 3 of dog age should equal 0, 0 of human ages",
     ]
 )
 def test_of_human_age_in_cat_age_and_dog_age(
@@ -39,6 +42,14 @@ def test_of_human_age_in_cat_age_and_dog_age(
     "cat_age, dog_age, expected_error",
     [
         pytest.param("a", 3, TypeError, id="Age should be integer"),
+        pytest.param(
+            [1, 2],
+            {"key": 2},
+            TypeError,
+            id="Both param should be integer"
+        ),
+
+
     ]
 )
 def test_of_human_age_in_cat_age_and_dog_age_for_errors(
