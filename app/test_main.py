aae40b4e84df9184c940b498d5d75edbcb4f42ff
Maksym Protsak <maksym.protsak@gmail.com>
2024-06-13 22:06:18 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,7 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    "cat_age,dog_age,result",
+    "cat_age, dog_age, result",
     [
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
@@ -18,3 +18,8 @@ from app.main import get_human_age
 )
 def test_can_sum(cat_age: int, dog_age: int, result: list[int]) -> None:
     assert (get_human_age(cat_age, dog_age) == result)
+
+
+def test_incorrect_type_of_data() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("0", 0)
