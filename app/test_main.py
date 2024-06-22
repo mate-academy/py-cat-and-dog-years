6f85edd078b540807e09f78a7bf225451aaeac8d
Alex93Troy <troianovskiy93alex@gmail.com>
2024-06-22 21:37:38 +0200

    done

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
 from app.main import get_human_age
 
-# write your code here
+def test_if_age_both_zero() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_convert_years_first() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_convert_years_second() -> None:
+    assert  get_human_age(24, 24) == [2, 2]
+
+
+def test_conver_more_then_two_years() -> None:
+    assert  get_human_age(28,29) == [3, 3]
