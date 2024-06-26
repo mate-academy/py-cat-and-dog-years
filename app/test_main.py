31244ffa66f33ff7db5aa8cdc9acde4c4c1493a2
Yevheniy Dolinskiy <zkorvoz02@gmail.com>
2024-06-26 13:38:46 +0200

    fix-01

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,13 @@
 import pytest
+
 from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected_human_age",
     [
-        (0 , 0, [0, 0]),
+        (-1, -1, [0, 0]),
+        (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
@@ -21,3 +23,18 @@ def test_get_human_age(
         expected_human_age: list
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected_human_age
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("cat", "dog"),
+        ([10], [10])
+    ]
+)
+def test_cannot_convert_any_type_but_integers(
+        cat_age: str | list,
+        dog_age: str | list
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
