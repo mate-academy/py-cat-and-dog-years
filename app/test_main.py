b1f1f640141940cb7ef7ae0b4611c0c170dedc87
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-06-28 17:00:16 +0300

    "sol"

--- app/test_main.py
+++ app/test_main.py
@@ -16,5 +16,5 @@ from app.main import get_human_age
         (100, 100, [21, 17])
     ]
 )
-def test_modify_classes(cat_age: int, dog_age: int, expected: list) -> None:
+def test_modify_classes(cat_age: int, dog_age: int, expected: list[int]) -> None:
     assert get_human_age(cat_age, dog_age) == expected
