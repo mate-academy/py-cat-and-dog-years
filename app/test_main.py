99c50b6ba841cdc643f8643058b71fa877108405
Arsen Markotskyi <arsen.markotskui@gmail.com>
2024-06-28 17:29:34 +0300

    "sol"

--- app/test_main.py
+++ app/test_main.py
@@ -20,3 +20,18 @@ def test_modify_classes(
         cat_age: int, dog_age: int, expected: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        (10, [1, 2]),
+        ([1, 2], 10),
+        (10, "0"),
+        ({"age": 10}, 10),
+        (10, None)
+    ]
+)
+def test_get_human_age_invalid_input(cat_age: int, dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
