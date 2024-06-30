db688f835fbc608b561819fe275ad9bbca059373
Ppllupold <kotnazar2005@gmail.com>
2024-06-30 17:28:20 +0300

    checked exception

--- app/test_main.py
+++ app/test_main.py
@@ -17,3 +17,20 @@ from app.main import get_human_age
 )
 def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        (1, []),
+        (1, {}),
+        ([], 1),
+        ({}, 1),
+        ([], set()),
+        (None, 1),
+        (1, None),
+    ]
+)
+def test_get_human_age_raise_exception(cat_age: int, dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
