1a123a9884ab1c0ec1f477c5fccc1834cfbd6104
Anna Lyubinska <anna.lyubinskagmail.com>
2024-07-01 21:07:46 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_zero_when_arguments_are_zero():
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_return_zero_when_arguments_are_less_than_15():
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_1_when_arguments_are_less_than_24():
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_return_2_when_cat_year_less_than_28_end_dog_than_29():
+    assert get_human_age(27, 28) == [2, 2]
+
+
+def test_should_return_expected_result_when_cat_year_ore_than_28_end_dog_than_29():
+    assert get_human_age(100, 100) == [21, 17]
+
+
+
+
+
