914287e4605fdb80f306ba00348464e4e9e6e873
Vova <v.kampanchenko@gmail.com>
2024-07-04 17:20:11 +0300

    Add new tests

--- app/test_main.py
+++ app/test_main.py
@@ -22,3 +22,24 @@ def test_should_convert_animal_years_to_human(
         "'get_human_age' should return right "
         "list with converted age"
     )
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        (-4, -1, [0, 0]),
+        (-20, 3, [0, 0]),
+        (7, -18, [0, 0]),
+    ]
+)
+def test_argument_less_than_zero(
+    cat_age: int, dog_age: int, result: list
+) -> None:
+    assert (get_human_age(cat_age, dog_age) == result), (
+        "'get_human_age' should return zero list"
+    )
+
+
+def test_rise_error_while_incorrect_arguments() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("23", 38.05)
