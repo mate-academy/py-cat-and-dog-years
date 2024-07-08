b685383f9f27bd9dc101f7ea10bde3c964b7ad9e
Daniil <daniilrizhenko@gmail.com>
2024-07-08 11:07:57 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,33 @@
 from app.main import get_human_age
 
 
-def test_should_return_zeros_when_zero_ages():
+def test_should_return_zeros_when_zero_ages() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_should_return_zeros_when_age_less_than_first_threshold():
-    assert get_human_age(14, 14) == [0,0]
+def test_should_return_zeros_when_age_less_than_first_threshold() -> None:
+    assert get_human_age(14, 14) == [0, 0]
 
 
-def test_should_return_ones_when_age_equal_to_first_threshold():
+def test_should_return_ones_when_age_equal_to_first_threshold() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_should_return_ones_when_age_between_first_and_second_threshold():
+def test_should_return_ones_when_between_first_and_second_threshold() -> None:
     assert get_human_age(23, 23) == [1, 1]
 
 
-def test_should_return_twos_when_age_equal_to_sum_of_first_and_second_thresholds():
+def test_should_return_twos_when_age_equal_to_sum_of_thresholds() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
 
-def test_should_return_twos_when_age_just_above_second_threshold():
+def test_should_return_twos_when_age_just_above_second_threshold() -> None:
     assert get_human_age(27, 27) == [2, 2]
 
 
-def test_should_return_different_animal_age_when_same_years():
+def test_should_return_different_animal_age_when_same_years() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
 
-def test_high_ages():
+def test_high_ages() -> None:
     assert get_human_age(100, 100) == [21, 17]
