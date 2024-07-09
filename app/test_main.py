7ec162561f27f6ac1169771cdf708fa669f5c232
Maksimens <Muskam.dan@gmail.com>
2024-07-09 11:18:09 +0300

    add new test cases

--- app/test_main.py
+++ app/test_main.py
@@ -4,15 +4,17 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    "cat_age, dog_age, result",
+    "cat_age,dog_age,result",
     [
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
+        (20, 20, [1, 1]),
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
+        (29, 29, [3, 3]),
+        (32, 32, [4, 3]),
         (100, 100, [21, 17])
     ]
 )
@@ -20,6 +22,18 @@ def test_get_human_age(cat_age: int, dog_age: int, result: list) -> None:
     assert get_human_age(cat_age, dog_age) == result
 
 
-def test_get_human_age_can_work_with_int() -> None:
-    with pytest.raises(TypeError):
-        get_human_age("cat", "dog")
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+    [
+        ("32", 32.543, TypeError),
+        (10, [10], TypeError),
+        (None, -6, TypeError)
+    ]
+)
+def test_get_human_age_for_error(
+        cat_age: int,
+        dog_age: int,
+        expected_error: Exception
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
