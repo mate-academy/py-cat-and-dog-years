27fb8ed7c448a2b3d795300f58dddef1a12a44b2
limbxbx <ilya.msd2003@gmail.com>
2024-07-16 14:36:05 +0300

    Fix flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -1,29 +1,29 @@
 from app.main import get_human_age
 
 
-def test_result_should_be_zeroes_if_given_age_is_zero() -> None:
+def test_result_should_zeroes_when_given_age_is_zero() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_result_should_be_zeroes_if_given_age_is_less_than_one_animal_year() -> None:
+def test_should_return_zeroes_if_less_then_one_year() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
 
-def test_result_should_be_one_year_when_given_humans_years_are_equal_to_one() -> None:
+def test_should_return_one_year() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_result_should_be_one_year_when_given_humans_year_between_one_and_two_animal_years() -> None:
+def test_should_return_one_if_between_one_and_two_humans_years() -> None:
     assert get_human_age(23, 23) == [1, 1]
 
 
-def test_result_sohuld_be_one_year_when_given_humans_year_equals_two_animal_years() -> None:
+def test_should_return_two_when_equals_two_human_years() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
 
-def test_result_should_be_different_for_different_animals_when_age_is_not_equal() -> None:
+def test_should_return_different_humans_age() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
 
-def test_result_should_return_correct_animals_years_when_arguments_are_different() -> None:
+def test_should_return_correct_result_for_age_more_than_three() -> None:
     assert get_human_age(62, 74) == [11, 12]
