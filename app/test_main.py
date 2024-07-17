a3706218c2bf67549bb81234a70ae3cb6ee58db4
maksym_korniev <maksymkorniev88@gmail.com>
2024-07-17 19:16:52 +0300

    added missing return type annotations

--- app/test_main.py
+++ app/test_main.py
@@ -1,37 +1,36 @@
 from app.main import get_human_age
 
 
-def test_zeroes_years():
+def test_zeroes_years() -> None:
     cat_age = 0
     dog_age = 0
 
     assert get_human_age(cat_age, dog_age) == [0, 0]
 
 
-def test_negative_years():
+def test_negative_years() -> None:
     cat_age = -1
     dog_age = -2
 
     assert get_human_age(cat_age, dog_age) == [0, 0]
 
 
-def test_normal_years():
+def test_normal_years() -> None:
     cat_age = 14
     dog_age = 45
 
     assert get_human_age(cat_age, dog_age) == [0, 6]
 
 
-def test_normal_years2():
+def test_normal_years2() -> None:
     cat_age = 56
     dog_age = 23
 
     assert get_human_age(cat_age, dog_age) == [10, 1]
 
 
-def test_big_and_not_rounded_years():
+def test_big_and_not_rounded_years() -> None:
     cat_age = 11893
     dog_age = 8919
 
     assert get_human_age(cat_age, dog_age) == [2969, 1781]
-
