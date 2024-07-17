9c7a055257dd79f08b777033cfe8b3150d4c2fe5
maksym_korniev <maksymkorniev88@gmail.com>
2024-07-17 19:12:03 +0300

    implemented tests for cat and dog years

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,37 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_zeroes_years():
+    cat_age = 0
+    dog_age = 0
+
+    assert get_human_age(cat_age, dog_age) == [0, 0]
+
+
+def test_negative_years():
+    cat_age = -1
+    dog_age = -2
+
+    assert get_human_age(cat_age, dog_age) == [0, 0]
+
+
+def test_normal_years():
+    cat_age = 14
+    dog_age = 45
+
+    assert get_human_age(cat_age, dog_age) == [0, 6]
+
+
+def test_normal_years2():
+    cat_age = 56
+    dog_age = 23
+
+    assert get_human_age(cat_age, dog_age) == [10, 1]
+
+
+def test_big_and_not_rounded_years():
+    cat_age = 11893
+    dog_age = 8919
+
+    assert get_human_age(cat_age, dog_age) == [2969, 1781]
+
