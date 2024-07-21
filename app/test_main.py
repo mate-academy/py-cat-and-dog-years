2c83ebae76eeb1f4b3c22b9554a71b50324503fb
Mykola <jacktyler2391@gmail.com>
2024-07-21 09:25:14 +0300

    fix to according to mentor comment

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,5 @@
 from app.main import get_human_age
 import pytest
-# write your code here
 
 
 @pytest.mark.parametrize(
@@ -13,9 +12,19 @@ import pytest
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
-
+        (100, 100, [21, 17]),
+        (-28, -28, [0, 0])
     ]
 )
 def test_get_correct_human_age(cat: int, dog: int, result: list[int]) -> None:
     assert get_human_age(cat, dog) == result
+
+
+@pytest.mark.parametrize(
+    "cat, dog",
+    [("28", "28"),
+     ("eight", "seven")]
+)
+def test_get_human_age_invalid_input(cat, dog):
+    with pytest.raises(TypeError):
+        get_human_age(cat, dog)
