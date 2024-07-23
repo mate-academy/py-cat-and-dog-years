1d918d96e28e7c86dce63d454a8b6f1cb09576b2
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-07-23 15:29:11 +0300

    new separate test added

--- app/test_main.py
+++ app/test_main.py
@@ -3,30 +3,41 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    "cat_age, dog_age, result_list, expected_error",
+    "cat_age, dog_age, result_list",
     [
-
-        (-10, -5, [0, 0], None),
-        (0, 0, [0, 0], None),
-        (14, 14, [0, 0], None),
-        (15, 15, [1, 1], None),
-        (23, 23, [1, 1], None),
-        (24, 24, [2, 2], None),
-        (28, 28, [3, 2], None),
-        (50, 60, [8, 9], None),
-        (100, 100, [21, 17], None),
-        (1000000, 1000000, [249996, 199997], None),
-        ("10", 10, None, pytest.raises(TypeError))
+        (-10, -5, [0, 0]),
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        (1000000, 1000000, [249996, 199997])
     ]
 )
 def test_can_convert_to_human_age(
         cat_age: int,
         dog_age: int,
         result_list: list,
-        expected_error: None | Exception
 ) -> None:
-    if expected_error:
-        with expected_error:
-            get_human_age(cat_age, dog_age)
-    else:
-        assert get_human_age(cat_age, dog_age) == result_list
+    assert get_human_age(cat_age, dog_age) == result_list
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result_list, expected_error",
+    [
+        ("10", 10, None, pytest.raises(TypeError)),
+        ("Fifteen", 15, None, pytest.raises(TypeError)),
+        (16, "puppy", None, pytest.raises(TypeError)),
+        ("kitty", 25, None, pytest.raises(TypeError))
+    ]
+)
+def test_human_age_convertation_raise_error(
+        cat_age: int,
+        dog_age: int,
+        result_list: list,
+        expected_error: Exception
+) -> None:
+    with expected_error:
+        get_human_age(cat_age, dog_age)
