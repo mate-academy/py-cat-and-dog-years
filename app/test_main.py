21c2785fb526b822ed71d209a0726b9aae96ff5f
Maksym Bondarevskyi <bondarevskiymaks@gmail.com>
2024-07-23 14:28:58 +0300

    new tests added

--- app/test_main.py
+++ app/test_main.py
@@ -3,21 +3,28 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    "cat_age, dog_age, result_list",
+    "cat_age, dog_age, result_list, expected_error",
     [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (28, 28, [3, 2]),
-        (50, 60, [8, 9]),
-        (100, 100, [21, 17])
+        (-10, -5, [0, 0], None),
+        (0, 0, [0, 0], None),
+        (14, 14, [0, 0], None),
+        (15, 15, [1, 1], None),
+        (23, 23, [1, 1], None),
+        (24, 24, [2, 2], None),
+        (28, 28, [3, 2], None),
+        (50, 60, [8, 9], None),
+        (100, 100, [21, 17], None),
+        pytest.param("10", 10, None, pytest.raises(TypeError))
     ]
 )
 def test_can_convert_to_human_age(
         cat_age: int,
         dog_age: int,
-        result_list: list
+        result_list: list,
+        expected_error
 ) -> None:
-    assert get_human_age(cat_age, dog_age) == result_list
+    if expected_error:
+        with expected_error:
+            get_human_age(cat_age, dog_age)
+    else:
+        assert get_human_age(cat_age, dog_age) == result_list
