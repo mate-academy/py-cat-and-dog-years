6ebc61c8028385c7d00330db9fd728a189559f15
Nazarii Khalimonov <nazar131188@gmail.com>
2024-07-26 23:18:23 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,37 +1,100 @@
 from app.main import get_human_age
+import pytest
 
 
-def test_should_return_zero_when_input_is_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="test_should_return_zero_when_input_is_zero"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="test_should_return_zero_when_age_is_less_than_15"
+        ),
+        pytest.param(
+            15,
+            15,
+            [1, 1],
+            id="test_should_return_correct_result_after_15_years"
+        ),
+        pytest.param(
+            23,
+            23,
+            [1, 1],
+            id="test_should_return_correct_"
+               "result_between_15_and_next_9_years"
+        ),
+        pytest.param(
+            24,
+            24,
+            [2, 2],
+            id="test_should_return_correct_result_between_next_9_years"
+        ),
+        pytest.param(
+            27,
+            27,
+            [2, 2],
+            id="test_should_return_"
+               "correct_result_between_9_and_next_4_cat_years"
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="test_should_return_correct_cat_next_4_years"
+        ),
+        pytest.param(
+            100,
+            100,
+            [21, 17],
+            id="test_should_return_correct_"
+               "result_over_a_long_period_of_time"
+        )
+    ]
+)
+def test_correct_human_year_result(cat_age: int,
+                                   dog_age: int,
+                                   result: list[int]) -> None:
+    assert get_human_age(cat_age, dog_age) == result
 
 
-def test_should_return_zero_when_age_is_less_than_15() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_should_return_correct_result_after_15_years() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_should_return_correct_result_between_15_and_next_9_years() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_should_return_correct_result_after_9_more_years() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_should_return_correct_result_between_9_and_next_4_cat_years() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_should_return_correct_cat_next_4_years() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_should_return_correct_dog_next_5_years() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_should_return_correct_result_over_a_long_period_of_time() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+    [
+        pytest.param(
+            -1,
+            -5,
+            KeyError,
+            id="test_should_raise_error_when_incorrect_value_given"
+        ),
+        pytest.param(
+            1001,
+            1001,
+            ValueError,
+            id="test_should_raise_error_for_large_numbers"
+        ),
+        pytest.param(
+            "two",
+            3,
+            TypeError,
+            id="test_should_raise_error_for_incorrect_type_cat_age"
+        ),
+        pytest.param(
+            2,
+            "three",
+            TypeError,
+            id="test_should_raise_error_for_incorrect_type_dog_age"
+        ),
+    ]
+)
+def test_correct_error_raises(cat_age: int,
+                              dog_age: int,
+                              expected_error: Exception) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
