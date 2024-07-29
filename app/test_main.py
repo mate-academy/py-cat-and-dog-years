0edaad913a0d26253b388eed428e558be9dc9707
Viktor Panasiuk <Viktor66462@gmail.com>
2024-07-29 23:16:03 +0300

    'Solution_corrected_annotations'

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,13 @@
 from app.main import get_human_age
 
 
-def test_the_animal_is_one_year_old():
+def test_the_animal_is_one_year_old() -> None:
     assert get_human_age(23, 15) == [1, 1]
 
 
-def test_the_animal_is_two_year_old():
+def test_the_animal_is_two_year_old() -> None:
     assert get_human_age(24, 27) == [2, 2]
 
 
-def test_each_subsequent_year():
+def test_each_subsequent_year() -> None:
     assert get_human_age(28, 29) == [3, 3]
