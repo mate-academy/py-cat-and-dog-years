07b0660843c7fc1c0504b493ce04252dbe067c66
andreykolomiec <akolomiec982@gmail.com>
2024-07-31 23:57:06 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,56 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize("cat_age, dog_age, expected", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (23, 23, [1, 1]),
+    (24, 24, [2, 2]),
+    (27, 27, [2, 2]),
+    (28, 28, [3, 2]),
+    (100, 100, [21, 17])
+])
+def test_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize("cat_age, dog_age", [
+    ("ten", 15),
+    (15, "fifteen"),
+    (10.5, 15),
+    (10, 15.5),
+    (None, 15),
+    (10, None)
+])
+def test_get_human_age_invalid_types(cat_age: int, dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
+
+
+@pytest.mark.parametrize("cat_age, dog_age", [
+    (-10, 15),
+    (10, -15),
+    (-10, -15)
+])
+def test_get_human_age_negative_values(cat_age, dog_age) -> None:
+    with pytest.raises(ValueError):
+        get_human_age(cat_age, dog_age)
+
+
+@pytest.mark.parametrize("cat_age, dog_age", [
+    (10 ** 6, 10 ** 6),
+    (10 ** 12, 10 ** 12)
+])
+def test_get_human_age_large_values(cat_age, dog_age) -> None:
+    result = get_human_age(cat_age, dog_age)
+    assert isinstance(result, list)
+    assert len(result) == 2
+    assert all(isinstance(x, int) for x in result)
+
+
