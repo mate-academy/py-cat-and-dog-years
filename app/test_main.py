b032718ce7edc2c31bb717a8ef7f56f77ceb5b29
Maksim <golevmaksim52@gmail.com>
2024-08-05 21:49:04 +0300

    create a test file based on the examples in the readme file

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_age_zero() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_age_fourteen() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_age_fifteen() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_age_twenty_three() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_age_twenty_four() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_age_twenty_seven() -> None:
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_age_twenty_eight() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_age_hundred() -> None:
+    assert get_human_age(100, 100) == [21, 17]
