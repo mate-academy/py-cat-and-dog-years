3360b1f313be971209a887f52841ca3539fa7da0
Vladyslav Tsybuliak <vladislav.tsybuliak@gmail.com>
2024-08-06 13:55:02 +0300

    write tests for get_human_age function

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
+    validate(cat_age, dog_age)
     cat_to_human = convert_to_human(cat_age, 15, 9, 4)
     dog_to_human = convert_to_human(dog_age, 15, 9, 5)
     return [cat_to_human, dog_to_human]
@@ -12,3 +13,15 @@ def convert_to_human(
     if animal_age < first_year + second_year:
         return 1
     return 2 + (animal_age - first_year - second_year) // each_year
+
+
+def validate(cat_age: int, dog_age: int) -> None:
+    if cat_age < 0 or dog_age < 0:
+        raise ValueError("Value must be >= 0")
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+        raise TypeError("Invalid type")
+    if isinstance(cat_age, bool) or isinstance(dog_age, bool):
+        raise TypeError("Invalid type")
+
+
+print(get_human_age(40, 50))
--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,63 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,human_age",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        (40, 50, [6, 7])
+    ],
+    ids=[
+        "should convert 0 cat/dog years to 0 human years",
+        "should convert 14 cat/dog years to 0 human years",
+        "should convert 15 cat/dog years to 1 human years",
+        "should convert 23 cat/dog years to 1 human years",
+        "should convert 24 cat/dog years to 2 human years",
+        "should convert 27 cat/dog years to 2 human years",
+        "should convert 28 cat/dog years to 3 and 2 human years respectively",
+        "should convert 100 cat/dog years to 21 and 17 human years respectively",
+        "should convert 40 cat and 50 dog years to 5 and 7 human years respectively"
+    ]
+)
+def test_ages(cat_age: int, dog_age: int, human_age: list[int]) -> None:
+    assert get_human_age(cat_age, dog_age) == human_age
+
+
+def test_negative_ages() -> None:
+    with pytest.raises(ValueError):
+        get_human_age(-1, -1)
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age",
+    [
+        (10.5, 20.4),
+        (True, False),
+        ("10", "10"),
+        ([10], [20]),
+        ({"cat_age": 20}, {"dog_age": 25}),
+        ((25,), (25,)),
+        ({25}, {25})
+    ],
+    ids=[
+        "float is invalid type",
+        "bool is invalid type",
+        "string is invalid type",
+        "list is invalid type",
+        "dict is invalid type",
+        "tuple is invalid type",
+        "set is invalid type"
+    ]
+)
+def test_invalid_type(cat_age, dog_age) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
