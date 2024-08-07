2f9026bdfb443c9deb460905e13d1699b27283b8
Valeriia Kyrylenko <valerya.kyrylenko@gmail.com>
2024-08-07 14:02:39 +0200

    implement py-cat-and-dog-years with changes

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,41 @@
 from app.main import get_human_age
+import pytest
 
 
-def test_should_return_valid_list() -> None:
-    result = get_human_age(100, 100)
-
-    assert result == [21, 17]
-
-
-def test_should_return_zero_if_the_animal_age_under_fifteen() -> None:
-    result = get_human_age(14, 14)
-
-    assert result == [0, 0]
-
-
-def test_age_should_be_one_when_animal_older_fourteen() -> None:
-    result = get_human_age(23, 23)
-
-    assert result == [1, 1]
-
-
-def test_age_should_be_two_when_animal_older_twenty_three() -> None:
-    result = get_human_age(27, 27)
-
-    assert result == [2, 2]
-
-
-def test_cat_age_should_be_three_when_animal_older_twenty_four() -> None:
-    result = get_human_age(28, 28)
-
-    assert result == [3, 2]
+@pytest.mark.parametrize(
+    "cat, dog, result",
+    [
+        pytest.param(
+            100,
+            100,
+            [21, 17],
+            id="should return valid list"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="should return zero if the animal age under fifteen"
+        ),
+        pytest.param(
+            23,
+            23,
+            [1, 1],
+            id="should return zero if the animal age under fifteen"
+        ),
+        pytest.param(
+            27,
+            27,
+            [2, 2],
+            id="age should be two when animal older twenty three"
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="cat age should be three when animal older twenty four"
+        )
+    ]
+)
+def test_for_get_human_age(cat: int, dog: int, result: list[int]) -> None:
+    assert get_human_age(cat, dog) == result
