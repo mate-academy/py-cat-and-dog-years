c6623ff17f9795f415c63521a8fd813138d0f72e
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-08-08 21:25:22 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -14,10 +14,20 @@ from app.main import get_human_age
         (27, 27, [2, 2]),
         (100, 100, [21, 17]),
         (101, 101, [21, 17]),
-        (-1, 0, ValueError),
-        (-2, -5, ValueError),
-        ("two", -2, TypeError),
     ]
 )
 def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("two", -1),
+        ("ten", "five"),
+        (None, None),
+    ]
+)
+def test_get_human_age_invalid_input(cat_age: int, dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
