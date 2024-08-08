a5f6aed6503416a76171e77b666d3f8ecfa64e66
Olena Cherneha <166736208+olena4reunited@users.noreply.github.com>
2024-08-08 21:26:25 +0000

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,58 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="should return zeros when inputs are zeros"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="should return zeros when inputs less then fifteen"
+        ),
+        pytest.param(
+            15,
+            15,
+            [1, 1],
+            id="should return ones when inputs are fifteen"
+        ),
+        pytest.param(
+            23,
+            23,
+            [1, 1],
+            id="should return ones when inputs are less then twenty four"
+        ),
+        pytest.param(
+            24,
+            24,
+            [2, 2],
+            id="should return twos when inputs are equal to twenty four"
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="should return three for cat and four for dog when inputs are twenty-eight"
+        ),
+        pytest.param(
+            100,
+            100,
+            [21, 17],
+            id="should return 21 for cat and 17 for dog when inputs are one hundred"
+        ),
+    ]
+)
+def test_correct_human_year_result(
+    cat_age: int,
+    dog_age: int,
+    result: list[int]
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result, "Incorrect years convert"
