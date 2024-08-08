21b301bbb3ef9135af0810e2fda286ad70dbe599
Maxim Turenko <turenkomaksim099@gmail.com>
2024-08-08 22:12:20 +0300

    implement py-cat-and-dog-year(fixed flake8 problems)

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,32 @@
 from app.main import get_human_age
 
 
-def test_get_human_age_zero():
+def test_get_human_age_zero() -> None:
     assert (
-            get_human_age(0, 0) == [0, 0]
+        get_human_age(0, 0) == [0, 0]
     ), "Test should return [0, 0] if both ages equals zero"
 
 
-def test_get_human_age_less_than_first_year():
+def test_get_human_age_less_than_first_year() -> None:
     assert (
-            get_human_age(14, 14) == [0, 0]
+        get_human_age(14, 14) == [0, 0]
     ), "Test should return [0, 0] if both of ages less than 15"
 
 
-def test_get_human_age_exactly_first_year():
+def test_get_human_age_exactly_first_year() -> None:
     assert (
-            get_human_age(15, 15) == [1, 1]
+        get_human_age(15, 15) == [1, 1]
     ), "Test should return [1, 1] if 15 <= both of ages <= 23"
 
 
-def test_get_human_age_exactly_second_year():
+def test_get_human_age_exactly_second_year() -> None:
     assert (
-            get_human_age(24, 24) == [2, 2]
-    ), "Test should return [2, 2] if 24 <= cat_age <= 27 and 24 <= dog_age <= 28"
+        get_human_age(24, 24) == [2, 2]
+    ), ("Test should return [2, 2] if 24 <= cat_age <= 27 "
+        "and 24 <= dog_age <= 28")
 
 
-def test_get_human_age_extra_years_for_cat():
+def test_get_human_age_extra_years_for_cat() -> None:
     assert (
-            get_human_age(28, 28) == [3, 2]
+        get_human_age(28, 28) == [3, 2]
     ), "Test should return extra year for cat"
