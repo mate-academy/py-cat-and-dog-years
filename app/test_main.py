aec09b72d9697f04c750e767f0d0510368a99feb
Denys Piuro <denys.pyuro@gmail.com>
2024-08-08 13:17:44 +0300

    Decided to add ids

--- app/test_main.py
+++ app/test_main.py
@@ -14,6 +14,17 @@ from app.main import get_human_age
         (16, 10, [1, 0]),
         (15, 0, [1, 0]),
         (0, 15, [0, 1]),
+    ],
+    ids=[
+        "both_0",
+        "both_14",
+        "both_15",
+        "both_28",
+        "both_100",
+        "both_negative",
+        "cat_16_dog_10",
+        "cat_15_dog_0",
+        "cat_0_dog_15",
     ]
 )
 def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
