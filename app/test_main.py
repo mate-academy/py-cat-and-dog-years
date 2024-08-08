ea5b2cd07734034ac50bb64b6376a9b893f073b0
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-08 17:28:36 +0300

    first commit

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestGetAge:
+    @pytest.mark.parametrize(
+        "dog_age", "cat_age", "excepted_age",
+        [
+            (0, 0, [0, 0]),
+            (14, 14,  [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
+            (100, 100, [21, 17]),
+        ]
+    )
+    def test_correctly_age(
+            self,
+            dog_age: int,
+            cat_age: int,
+            excepted_age: list
+    ) -> None:
+        assert get_human_age(dog_age, cat_age) == excepted_age
