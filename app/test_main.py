2f0052cc29d9f3b87d68b684c8eb9c750776b8e1
Misha Havryliuk <misharob31415@gmail.com>
2024-08-09 12:19:29 +0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Any
 
 
 from app.main import get_human_age
@@ -11,7 +12,9 @@ class TestGetHumanAge:
             pytest.param(-3, 0, [0, 0], id="0 when less then first year"),
             pytest.param(23, 24, [1, 2], id="age > first and second year"),
             pytest.param(33, 47, [4, 6], id="big age"),
-            pytest.param(28, 29, [3, 3], id="third step")
+            pytest.param(28, 29, [3, 3], id="third step"),
+            pytest.param(-63, -29, [0, 0], id="two negative"),
+            pytest.param(20, -4, [1, 0], id="one negative")
 
         ]
     )
@@ -22,3 +25,15 @@ class TestGetHumanAge:
             expected_result: list
     ) -> None:
         assert get_human_age(cat_age, dog_age) == expected_result
+
+    @pytest.mark.parametrize(
+        "cat_age, dog_age",
+        [
+            ("a", "b"),
+            (3.5, 4.2),
+            ([1], [2])
+        ]
+    )
+    def test_incorrect_types(self, cat_age: Any, dog_age: Any) -> None:
+        with pytest.raises(TypeError):
+            get_human_age(cat_age, dog_age)
