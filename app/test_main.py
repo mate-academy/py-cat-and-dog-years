71c4535964a5d04c62372242b6d0a3fbf1a3e9b0
serhii <ockar2018@gmail.com>
2024-08-09 17:37:49 +0300

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -3,19 +3,30 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    "cat_age, dog_age, expected_result",
+    "cat_age, dog_age, expected_result, description",
     [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (100, 100, [21, 17])
+        (14, 14, [0, 0], "Age less than 15 should return [0, 0]"),
+        (24, 24, [2, 2], "When age 24 should return [2, 2]"),
+        (-1, -1, [0, 0], "Negative ages should return [0, 0]"),
+        (10.2, 10.2, [0, 0], "Not integer should return [0, 0]"),
+        (
+            17,
+            17,
+            [1, 1],
+            "When age equals to 15 and less than 24 should return [1, 1]"
+        ),
+        (
+            100,
+            100,
+            [21, 17],
+            "When age 100 should return 21 for cat and 17 for dog"
+        ),
     ]
 )
 def test_get_human_age(
         cat_age: int,
         dog_age: int,
-        expected_result: int
+        expected_result: int,
+        description: str
 ) -> None:
-    assert get_human_age(cat_age, dog_age) == expected_result
+    assert get_human_age(cat_age, dog_age) == expected_result, description
