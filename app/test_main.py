74e150f96e31857518c00e75cf4ca704ef1449f6
Maxim Turenko <turenkomaksim099@gmail.com>
2024-08-09 17:52:53 +0300

    implement py-cat-and-dog-year(used parametrize)

--- app/test_main.py
+++ app/test_main.py
@@ -1,32 +1,16 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_get_human_age_zero() -> None:
+@pytest.mark.parametrize("cat_age, dog_age, expected", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (24, 24, [2, 2]),
+    (28, 28, [3, 2])
+])
+def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert (
-        get_human_age(0, 0) == [0, 0]
-    ), "Test should return [0, 0] if both ages equals zero"
-
-
-def test_get_human_age_less_than_first_year() -> None:
-    assert (
-        get_human_age(14, 14) == [0, 0]
-    ), "Test should return [0, 0] if both of ages less than 15"
-
-
-def test_get_human_age_exactly_first_year() -> None:
-    assert (
-        get_human_age(15, 15) == [1, 1]
-    ), "Test should return [1, 1] if 15 <= both of ages <= 23"
-
-
-def test_get_human_age_exactly_second_year() -> None:
-    assert (
-        get_human_age(24, 24) == [2, 2]
-    ), ("Test should return [2, 2] if 24 <= cat_age <= 27 "
-        "and 24 <= dog_age <= 28")
-
-
-def test_get_human_age_extra_years_for_cat() -> None:
-    assert (
-        get_human_age(28, 28) == [3, 2]
-    ), "Test should return extra year for cat"
+        get_human_age(cat_age, dog_age) == expected
+    ), f"Test failed for cat_age={cat_age}, dog_age={dog_age}"
