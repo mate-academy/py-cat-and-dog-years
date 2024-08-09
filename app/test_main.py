ea571231f46571b803ba49c8b61cbb85d2c1da5b
Egor Derepovskiy <derepovskiy98@gmail.com>
2024-08-09 16:37:51 +0300

    first commit

--- app/test_main.py
+++ app/test_main.py
@@ -7,14 +7,23 @@ class TestGetAge:
     @pytest.mark.parametrize(
         "dog_age, cat_age, excepted_age",
         [
-            (0, 0, [0, 0]),
-            (14, 14, [0, 0]),
-            (15, 15, [1, 1]),
-            (23, 23, [1, 1]),
-            (24, 24, [2, 2]),
-            (27, 27, [2, 2]),
-            (28, 28, [3, 2]),
-            (100, 100, [21, 17]),
+            (0, 0, [0, 0], "Both ages are zero"),
+            (
+                    14, 14, [0, 0],
+                    "Both ages below the threshold for human years conversion"
+            ),
+            (15, 15, [1, 1], "Minimum age for first human age"),
+            (
+                    23, 23, [1, 1],
+                    "Age just below the next human year conversion."
+            ),
+            (24, 24, [2, 2], "Second human year"),
+            (
+                    27, 27, [2, 2],
+                    "Age just below the third human year for dogs"
+            ),
+            (28, 28, [3, 2], "Third year for dog, but not cat"),
+            (100, 100, [21, 17], "High age for both dog and cat"),
         ]
     )
     def test_correctly_age(
@@ -24,3 +33,22 @@ class TestGetAge:
             excepted_age: list
     ) -> None:
         assert get_human_age(dog_age, cat_age) == excepted_age
+
+    @pytest.mark.parametrize(
+        "dog_age, cat_age, exception_type, description",
+        [
+            (-3, 15, ValueError, "If dog age negative - raise ValueError"),
+            (10, -1, ValueError, "If cat age negative - raise ValueError"),
+            ("ten", 10, TypeError, "Non-integer raise TypeError"),
+            (10, "eight", TypeError, "Non-integer raise TypeError."),
+        ]
+    )
+    def test_invalid_inputs(
+            self,
+            dog_age: int,
+            cat_age: int,
+            exception_type: type,
+            description: str
+    ) -> None:
+        with pytest.raises(exception_type, match=description):
+            get_human_age(dog_age, cat_age)
