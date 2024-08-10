7cdd637963be287a7c49e1f2a3b26d6a9f99a0b1
Bogdan <bogdan.rylov422@gmail.com>
2024-08-10 16:36:21 +0300

    Reformat code according to mentor's requirements

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,3 @@
-from typing import Type
-
 import pytest
 
 from app.main import get_human_age
@@ -30,27 +28,3 @@ def test_converting_animal_age_to_human_age(
         expected_human_age: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected_human_age
-
-
-@pytest.mark.parametrize(
-    "cat_age,dog_age,expected_error",
-    [
-        (0, 0, ValueError),
-        (-1, -1, ValueError),
-        (1000, 1000, ValueError),
-        ("10", [1, 2], TypeError)
-    ],
-    ids=[
-        "should rise error if cat/dog ages equal to 0.",
-        "should rise error if cat/dog ages equal to negative number.",
-        "should rise error if cat/dog ages are large numbers.",
-        "should rise error if cat/dog ages are of incorrect type."
-    ]
-)
-def test_rising_errors_correctly(
-        cat_age: int,
-        dog_age: int,
-        expected_error: Type[Exception]
-) -> None:
-    with pytest.raises(expected_error):
-        get_human_age(cat_age, dog_age)
