9489737f48a40a01930dec387b66f28b9bfcd060
Bogdan <bogdan.rylov422@gmail.com>
2024-08-10 16:26:07 +0300

    Reformat code according to mentor's requirements

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,5 @@
+from typing import Type
+
 import pytest
 
 from app.main import get_human_age
@@ -29,28 +31,26 @@ def test_converting_animal_age_to_human_age(
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected_human_age
 
-# In the checklist.md it is required to test "data out of normal range,
-# such as negative numbers, zero, or really large numbers"...
 
-# @pytest.mark.parametrize(
-#     "cat_age,dog_age,expected_error",
-#     [
-#         (0, 0, ValueError),
-#         (-1, -1, ValueError),
-#         (1000, 1000, ValueError),
-#         ("10", [1, 2], TypeError)
-#     ],
-#     ids=[
-#         "should rise error if cat/dog ages equal to 0.",
-#         "should rise error if cat/dog ages equal to negative number.",
-#         "should rise error if cat/dog ages are large numbers.",
-#         "should rise error if cat/dog ages are of incorrect type."
-#     ]
-# )
-# def test_rising_errors_correctly(
-#         cat_age: int,
-#         dog_age: int,
-#         expected_error: Type[Exception]
-# ) -> None:
-#     with pytest.raises(expected_error):
-#         get_human_age(cat_age, dog_age)
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+    [
+        (0, 0, ValueError),
+        (-1, -1, ValueError),
+        (1000, 1000, ValueError),
+        ("10", [1, 2], TypeError)
+    ],
+    ids=[
+        "should rise error if cat/dog ages equal to 0.",
+        "should rise error if cat/dog ages equal to negative number.",
+        "should rise error if cat/dog ages are large numbers.",
+        "should rise error if cat/dog ages are of incorrect type."
+    ]
+)
+def test_rising_errors_correctly(
+        cat_age: int,
+        dog_age: int,
+        expected_error: Type[Exception]
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
