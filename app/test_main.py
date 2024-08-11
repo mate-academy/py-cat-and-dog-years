e24750f3ce4dd647e9e0dae672c59daa6b6a2b06
Alexander Baidin <a.g.e.n.t.4282@gmail.com>
2024-08-11 19:02:44 +0300

    'added_additional_tests_and_messages'

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Any
 
 from app.main import get_human_age
 
@@ -6,14 +7,15 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age,dog_age,human_years",
     [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
-        (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        pytest.param(0, 0, [0, 0], id="Mistake during convertation"),
+        pytest.param(14, 14, [0, 0], id="Mistake during convertation"),
+        pytest.param(15, 15, [1, 1], id="Mistake during convertation"),
+        pytest.param(23, 23, [1, 1], id="Mistake during convertation"),
+        pytest.param(24, 24, [2, 2], id="Mistake during convertation"),
+        pytest.param(27, 28, [2, 2], id="Mistake during convertation"),
+        pytest.param(28, 29, [3, 3], id="Mistake during convertation"),
+        pytest.param(100, 100, [21, 17], id="Mistake during convertation"),
+        pytest.param(-3, -10, [0, 0], id="If negative number passed: return 0")
     ]
 )
 def test_cat_age_and_dog_age_converted_to_human_age_correctly(
@@ -22,3 +24,33 @@ def test_cat_age_and_dog_age_converted_to_human_age_correctly(
         human_years: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == human_years
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age",
+    [
+        pytest.param(
+            "two",
+            "eight",
+            id="Should raise TypeError if passed age is not a number"
+        ),
+        pytest.param(
+            [2],
+            [8],
+            id="Should raise TypeError if passed age is not a number"
+        ),
+        pytest.param(
+            {100: 55},
+            {},
+            id="Should raise TypeError if passed age is not a number"
+        )
+    ]
+)
+def test_should_raise_error_if_argument_not_a_number(
+        cat_age: Any,
+        dog_age: Any
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
+        get_human_age(cat_age, dog_age)
+        get_human_age(cat_age, dog_age)
