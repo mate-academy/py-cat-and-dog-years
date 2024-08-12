cebb7ab8fd2714e8e923e4752b5f9cd9216166bd
olena4reunited <olenaduringintermission@gmail.com>
2024-08-12 11:40:25 +0300

    fix: function names replaced by more suitable names, remove backslashes as line breakers, parametrize added to raising-exeption-test

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Any
 
 from app.main import get_human_age
 
@@ -10,45 +11,43 @@ from app.main import get_human_age
             0,
             0,
             [0, 0],
-            id="should return zeros when inputs are zeros"
+            id="should return zeros"
         ),
         pytest.param(
             14,
             14,
             [0, 0],
-            id="should return zeros when inputs less then fifteen"
+            id="should return zeros"
         ),
         pytest.param(
             15,
             15,
             [1, 1],
-            id="should return ones when inputs are fifteen"
+            id="should return ones"
         ),
         pytest.param(
             23,
             23,
             [1, 1],
-            id="should return ones when inputs are less then twenty four"
+            id="should return ones"
         ),
         pytest.param(
             24,
             24,
             [2, 2],
-            id="should return twos when inputs are equal to twenty four"
+            id="should return twos"
         ),
         pytest.param(
             28,
             28,
             [3, 2],
-            id="should return three for cat and \
-                four for dog when inputs are twenty-eight"
+            id="should return 3 for cat and 2 for dog"
         ),
         pytest.param(
             100,
             100,
             [21, 17],
-            id="should return 21 for cat and \
-                17 for dog when inputs are one hundred"
+            id="should return 21 for cat and 17 for dog"
         ),
         pytest.param(
             -10,
@@ -58,14 +57,17 @@ from app.main import get_human_age
         ),
     ]
 )
-def test_correct_human_year_result(
+def test_get_human_age_works_correctly(
     cat_age: int,
     dog_age: int,
     result: list[int]
 ) -> None:
-    assert get_human_age(cat_age, dog_age) == result, "Incorrect years convert"
+    assert get_human_age(cat_age, dog_age) == result
 
 
-def test_raise_incorrect_type() -> None:
+@pytest.mark.parametrize("input1, input2", [
+    ("10", "10"),
+])
+def test_get_human_age_raises_exception(input1: Any, input2: Any) -> None:
     with pytest.raises(TypeError):
-        get_human_age("10", "10")
+        get_human_age(input1, input2)
