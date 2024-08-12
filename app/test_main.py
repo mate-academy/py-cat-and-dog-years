7c6e690f3f81300a8228b78fc6696af85ce24b17
Vlada <vlada.belayy@mail.com>
2024-08-12 14:58:21 +0300

    catdog commit

--- app/test_main.py
+++ app/test_main.py
@@ -1,52 +1,41 @@
 from app.main import get_human_age
-
 import pytest
 
 
-@pytest.mark.parametrize(
-    "cat_years, dog_years, expected",
-    [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-    ]
-)
-def test_get_human_age_zero_years(
-    cat_years: int, dog_years: int, expected: list
-) -> None:
-    assert get_human_age(cat_years, dog_years) == expected
-
-
-class TestCatYearsToHumanYears:
+class TestGetHumanAge:
     @pytest.mark.parametrize(
-        "cat_years, expected_human_years",
+        "cat_years, dog_years, expected",
         [
-            (0, 0),
-            (14, 0),
-            (15, 1),
-            (23, 1),
-            (24, 2),
-            (27, 2),
-            (28, 3),
-            (100, 21),
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (15, 0, [1, 0]),
+            (0, 15, [0, 1]),
+            (100, 100, [21, 17]),
         ]
     )
-    def test_cat_age(self, cat_years: int, expected_human_years: int) -> None:
-        assert get_human_age(cat_years, 0)[0] == expected_human_years
+    def test_correct_behavior(
+        self,
+        cat_years: int,
+        dog_years: int,
+        expected: list
+    ) -> None:
+        assert get_human_age(cat_years, dog_years) == expected
 
-
-class TestDogYearsToHumanYears:
     @pytest.mark.parametrize(
-        "dog_years, expected_human_years",
+        "cat_years, dog_years, expected_exception",
         [
-            (0, 0),
-            (14, 0),
-            (15, 1),
-            (23, 1),
-            (24, 2),
-            (27, 2),
-            (28, 2),
-            (100, 17),
+            (-1, 0, ValueError),
+            (0, -1, ValueError),
+            (-10, -10, ValueError),
+            ("cat", 0, TypeError),
+            (0, "dog", TypeError),
         ]
     )
-    def test_dog_age(self, dog_years: int, expected_human_years: int) -> None:
-        assert get_human_age(0, dog_years)[1] == expected_human_years
+    def test_invalid_input(
+        self,
+        cat_years: int,
+        dog_years: int,
+        expected_exception: type[Exception]
+    ) -> None:
+        with pytest.raises(expected_exception):
+            get_human_age(cat_years, dog_years)
