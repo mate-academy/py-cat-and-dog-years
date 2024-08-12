af832e2f83b0ea3fe848efe921fe0694f1637863
Ivan Shakhman <shakhmanivan@gmail.com>
2024-08-12 19:13:48 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -3,22 +3,11 @@ import pytest
 from app.main import get_human_age
 
 
-def test_should_return_list_with_two_values() -> None:
-    assert (
-        isinstance(get_human_age(1, 1), list)
-        and len(get_human_age(1, 1)) == 2
-    ), f"must return list but was {type(get_human_age(1, 1))}"
-
-
-def test_should_return_integers_numbers_in_list() -> None:
-    assert (
-        isinstance(num, int) for num in get_human_age(30, 30)
-    ), "human age must be integer"
-
-
 @pytest.mark.parametrize(
     "cat_years,dog_years,expected_result",
     [
+        (-20, -50, [0, 0]),
+        (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (23, 23, [1, 1]),
         (25, 27, [2, 2]),
@@ -26,6 +15,8 @@ def test_should_return_integers_numbers_in_list() -> None:
         (521, 1077, [126, 212])
     ],
     ids=[
+        "-20 cats and -50 dogs years must convert to 0, 0 humans years",
+        "0 cats and 0 dogs years must convert to 0, 0 humans years",
         "14 cats and 14 dogs years must convert to 0, 0 humans years",
         "23 cats and 23 dogs years must convert to 1, 1 humans years",
         "25 cats and 27 dogs years must convert to 2, 2 humans years",
@@ -33,11 +24,11 @@ def test_should_return_integers_numbers_in_list() -> None:
         "521 cats and 1077 dogs years must convert to 126, 212 humans years"
     ]
 )
-def test_should_return_expected_result_for_each_param(
+def test_should_correct_convert_animals_ages_to_humans(
     cat_years: int,
     dog_years: int,
     expected_result: list[int]
 ) -> None:
     assert (
         get_human_age(cat_years, dog_years) == expected_result
-    ), f"must return {expected_result} when years is {cat_years, dog_years}"
+    )
