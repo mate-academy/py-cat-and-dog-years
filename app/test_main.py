8f77bd0d027a9a3fb9ac1795a0c20987d36d79a2
Myroslava Nezbrytska <miroslavanezbricka@gmail.com>
2024-08-13 22:42:35 +0300

    fixed

--- app/test_main.py
+++ app/test_main.py
@@ -16,17 +16,21 @@ from app.main import get_human_age
         (100, 100, [21, 17])
     ],
     ids=[
-        "both values are zero",
-        "both values are negative",
-        "one value is negative",
-        "one animals year less than human year",
-        "first human year",
-        "23 not enough for two years",
-        "two human years",
-        "with big numbers"
+        "0 cat and dog age must equal 0 in human age",
+        "negative dog and cat age must equal 0 in human age",
+        "negative cat age and 14 dog years must equal 0 in human age",
+        "14 cat and dog years must equal 0 in human age",
+        "15 cat and dog years must equal 1 in human age",
+        "23 cat and dog years must equal 1 in human age",
+        "24 cat and dog years must equal 2 in human age",
+        "100 cat and dog years must equal 21 and 17 in human age"
     ]
 )
-def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
+def test_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
+) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
 
@@ -38,11 +42,14 @@ def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
         (None, None),
     ],
     ids=[
-        "string and negative number",
-        "two strings",
-        "two None type"
+        "TypeError raised when input is a string",
+        "TypeError raised when input is a string",
+        "TypeError raised when input is None"
     ],
 )
-def test_get_human_age_invalid_input(cat_age: int, dog_age: int) -> None:
+def test_get_human_age_invalid_input(
+        cat_age: int | str | None,
+        dog_age: int | str | None
+) -> None:
     with pytest.raises(TypeError):
         get_human_age(cat_age, dog_age)
