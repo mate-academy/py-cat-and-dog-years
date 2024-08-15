fb88d857d74a1f37e1321969d1189d40598ea114
Tkachov Mykhailo <mtkachov435@gmail.com>
2024-08-15 22:06:30 +0300

    correct according to flake8

--- app/test_main.py
+++ app/test_main.py
@@ -66,35 +66,40 @@ import pytest
 def test_universal_age(cat_age: int, dog_age: int, result: list) -> None:
     assert get_human_age(cat_age, dog_age) == result
 
+
 @pytest.mark.parametrize(
     "cat_age,dog_age,expected_error",
-        [
-            pytest.param(
+    [
+        pytest.param(
             "a",
             "b",
             TypeError,
             id="expected error check"
         ),
-            pytest.param(
+        pytest.param(
             "100",
             "15",
             TypeError,
             id="expected error check"
         ),
-            pytest.param(
+        pytest.param(
             [],
             [],
             TypeError,
             id="expected error check"
         ),
-            pytest.param(
+        pytest.param(
             [1, 2, 3],
             [1],
             TypeError,
             id="expected error check"
         ),
-            ]
+    ]
 )
-def test_universal_error(cat_age: Any, dog_age: Any, expected_error: None) -> None:
+def test_universal_error(
+        cat_age: Any,
+        dog_age: Any,
+        expected_error: None
+) -> None:
     with pytest.raises(expected_error):
         get_human_age(cat_age, dog_age)
