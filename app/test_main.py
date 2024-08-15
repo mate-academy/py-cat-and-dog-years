acd344e85103f05aabff5fc3b4cb4c0fe0333e86
Tkachov Mykhailo <mtkachov435@gmail.com>
2024-08-15 21:59:18 +0300

    added checking for expected errors

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 from app.main import get_human_age
+from typing import Any
 
 import pytest
 
@@ -62,5 +63,38 @@ import pytest
         ),
     ]
 )
-def test_zero_age_check(cat_age: int, dog_age: int, result: list) -> None:
+def test_universal_age(cat_age: int, dog_age: int, result: list) -> None:
     assert get_human_age(cat_age, dog_age) == result
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+        [
+            pytest.param(
+            "a",
+            "b",
+            TypeError,
+            id="expected error check"
+        ),
+            pytest.param(
+            "100",
+            "15",
+            TypeError,
+            id="expected error check"
+        ),
+            pytest.param(
+            [],
+            [],
+            TypeError,
+            id="expected error check"
+        ),
+            pytest.param(
+            [1, 2, 3],
+            [1],
+            TypeError,
+            id="expected error check"
+        ),
+            ]
+)
+def test_universal_error(cat_age: Any, dog_age: Any, expected_error: None) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
