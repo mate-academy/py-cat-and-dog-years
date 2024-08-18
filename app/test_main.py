0a87a8d2e586e4f8b59504e36270e6a1383e823b
Yehor <sizngg9@gmail.com>
2024-08-18 14:37:00 +0300

    TypeError solution?

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,19 @@
 import pytest
-
 from app.main import get_human_age
 
 
 class TestCatDogAge:
 
-    @pytest.mark.parametrize("cat_age, dog_age, expected", [
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (25, 25, [2, 2]),
-        (28, 29, [3, 3]),
-        (32, 34, [4, 4]),
-        (100, 100, [21, 17]),
-        (-1, -5, [0, 0]),
-    ],
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected",
+        [
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (25, 25, [2, 2]),
+            (28, 29, [3, 3]),
+            (32, 34, [4, 4]),
+        ],
         ids=[
             "14 cat/dog years should convert into 0 human age.",
             "15 cat/dog years should convert into 1 human age.",
@@ -22,9 +21,7 @@ class TestCatDogAge:
             "25 cat/dog years should convert into 2 human age.",
             "28/29 cat/dog years should convert into 3 human age.",
             "32/34 cat/dog years should convert into 4 human age.",
-            "cat/dog don't live for so long",
-            "Negative cat/dog years should convert into 0 human age."
-    ]
+        ]
     )
     def test_get_human_age(
             self,
@@ -33,3 +30,18 @@ class TestCatDogAge:
             expected: list
     ) -> None:
         assert get_human_age(cat_age, dog_age) == expected
+
+    @pytest.mark.parametrize(
+        "cat_age, dog_age",
+        [
+            (-1, -5),
+            (150, 200),
+        ],
+        ids=[
+            "Negative cat/dog years should raise TypeError.",
+            "Too large cat/dog years should raise TypeError.",
+        ]
+    )
+    def test_get_human_age_typeerror(self, cat_age: int, dog_age: int) -> None:
+        with pytest.raises(TypeError):
+            get_human_age(cat_age, dog_age)
