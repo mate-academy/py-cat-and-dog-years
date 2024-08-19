3d635af7f7e7b71240a0b3e3ffd2334ed6045ef8
Yehor <sizngg9@gmail.com>
2024-08-19 12:39:29 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,25 +2,29 @@ import pytest
 from app.main import get_human_age
 
 
-class TestCatDogAge:
+class TestGet:
 
     @pytest.mark.parametrize(
         "cat_age, dog_age, expected",
         [
+            (-1, -1, [0, 0]),
             (14, 14, [0, 0]),
             (15, 15, [1, 1]),
             (23, 23, [1, 1]),
             (25, 25, [2, 2]),
             (28, 29, [3, 3]),
             (32, 34, [4, 4]),
+            (90, 90, [18, 15]),
         ],
         ids=[
+            "cat/dog age can not be negative",
             "14 cat/dog years should convert into 0 human age.",
             "15 cat/dog years should convert into 1 human age.",
             "23 cat/dog years should convert into 1 human age.",
             "25 cat/dog years should convert into 2 human age.",
             "28/29 cat/dog years should convert into 3 human age.",
             "32/34 cat/dog years should convert into 4 human age.",
+            "Max cat/dog years value",
         ]
     )
     def test_get_human_age(
@@ -31,17 +35,6 @@ class TestCatDogAge:
     ) -> None:
         assert get_human_age(cat_age, dog_age) == expected
 
-    @pytest.mark.parametrize(
-        "cat_age, dog_age",
-        [
-            (-1, -5),
-            (150, 200),
-        ],
-        ids=[
-            "Negative cat/dog years should raise TypeError.",
-            "Too large cat/dog years should raise TypeError.",
-        ]
-    )
-    def test_get_human_age_typeerror(self, cat_age: int, dog_age: int) -> None:
+    def test_get_human_age_typeerror(self) -> None:
         with pytest.raises(TypeError):
-            get_human_age(cat_age, dog_age)
+            assert get_human_age("cat_age", "dog_age")
