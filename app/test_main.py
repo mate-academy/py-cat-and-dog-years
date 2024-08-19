99442675b496bc15cf86fc3fe7dd514aa0fd1440
user <graban2253@gmail.com>
2024-08-19 14:59:20 +0300

    Finished

--- app/test_main.py
+++ app/test_main.py
@@ -8,26 +8,29 @@ class TestAge:
         "cat_age, dog_age, expected",
         [
             (0, 0, [0, 0]),
-            (-2, -3, [0, 0]),
-            (-2, 3, [0, 0]),
             (14, 14, [0, 0]),
             (15, 15, [1, 1]),
             (23, 23, [1, 1]),
             (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
             (100, 100, [21, 17])
         ],
         ids=[
-            "Both cat and dog age 0 should result in [0, 0] human years",
-            "Negative cat and dog ages should result in [0, 0] human years",
-            "Negative cat age and 3 dog years should result in [0, 0] human years",
-            "14 cat and dog years should result in [0, 0] human years",
-            "15 cat and dog years should result in [1, 1] human years",
-            "23 cat and dog years should result in [1, 1] human years",
-            "24 cat and dog years should result in [2, 2] human years",
-            "100 cat and dog years should result in [21, 17] human years"
+            "0 cat and dog age must equal 0 in human age",
+            "14 cat and dog years must equal 0 in human age",
+            "15 cat and dog years must equal 1 in human age",
+            "23 cat and dog years must equal 1 in human age",
+            "24 cat and dog years must equal 2 in human age",
+            "27 cat and dog years must equal 2 in human age",
+            "28 cat and dog years must equal 3 and 2 in human age",
+            "100 cat and dog years must equal 21 and 17 in human age"
         ]
     )
-    def test_get_human_age(self, cat_age: int, dog_age: int, expected: int) -> None:
+    def test_get_human_age(self,
+                           cat_age: int,
+                           dog_age: int,
+                           expected: int) -> None:
         assert get_human_age(cat_age, dog_age) == expected
 
     def test_type_errors(self) -> None:
