9f62c8dafbf79befa32664104836688341276379
dkn3r <n.diachuk333@gmail.com>
2024-08-22 20:19:40 +0300

    implement py-cat-and-dog-years

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,6 @@
 from app.main import get_human_age
 
+
 def test_should_return_zero_for_zero_cat_and_dog_age() -> None:
     assert get_human_age(0, 0) == [0, 0], (
         "Expected [0, 0] for input (0, 0)."
@@ -36,7 +37,8 @@ def test_should_return_three_and_two_for_cat_and_dog_age_28() -> None:
     )
 
 
-def test_should_return_twenty_one_and_seventeen_for_cat_and_dog_age_100() -> None:
+def test_should_return_twenty_one_and_seventeen_for_cat_and_dog_age_100()\
+        -> None:
     assert get_human_age(100, 100) == [21, 17], (
         "Expected [21, 17] for input (100, 100)."
     )
