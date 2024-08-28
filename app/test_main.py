f1bb9637bc946d4d5da9b5bebc11b8089b4b17f3
AntonBessarab <antonbessarab55@gmail.com>
2024-08-28 21:12:37 +0300

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -14,5 +14,8 @@ class TestHumanAgeFunction:
             pytest.param(27, 28, [2, 2], id="function should return 2")
         ]
     )
-    def test_is_human_age_right(self, cat_age: int, dog_age: int, result: list) -> None:
+    def test_is_human_age_right(self,
+                                cat_age: int,
+                                dog_age: int,
+                                result: list) -> None:
         assert get_human_age(cat_age, dog_age) == result
