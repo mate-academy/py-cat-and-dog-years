3d742b9b4489383e253559126b663a5cc4a8d319
Roman Zaliznyi <romanzaliznyi.python@gmail.com>
2024-08-30 21:31:54 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+
 from app.main import get_human_age
 
 
@@ -23,3 +24,31 @@ def test_cat_and_dogs_human_years(
     assert (
         get_human_age(cats_int, dogs_int) == result
     )
+
+
+@pytest.mark.parametrize(
+    "cats_input, dogs_input, exception",
+    [
+        (-1, -1, ValueError),
+        (14, -10, ValueError),
+        (-15, 15, ValueError),
+        ("sdad", 23, TypeError),
+        (22, "sad", TypeError),
+        ("ten", "nine", TypeError),
+        (-1, 10, ValueError),
+        (10, -1, ValueError),
+        (1500, 10, ValueError),
+        (10, 1500, ValueError),
+        ("ten", 10, TypeError),
+        (10, "ten", TypeError),
+        (None, 10, TypeError),
+        (10, None, TypeError),
+    ]
+)
+def test_invalid_inputs(
+        cats_input: int | str,
+        dogs_input: int | str,
+        exception: TypeError | ValueError
+) -> None:
+    with pytest.raises(exception):
+        get_human_age(cats_input, dogs_input)
