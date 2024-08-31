50295220a586c94dc21fd5d9f4933492bb2e7063
Rudge0 <maksim.pariychuk07@gmail.com>
2024-08-31 11:17:42 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_the_output_has_to_be_equal_to_expected() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+    assert get_human_age(14, 14) == [0, 0]
+    assert get_human_age(15, 15) == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
+    assert get_human_age(24, 24) == [2, 2]
+    assert get_human_age(27, 27) == [2, 2]
+    assert get_human_age(28, 28) == [3, 2]
+    assert get_human_age(100, 100) == [21, 17]
+
+
+def test_the_output_has_to_be_integer() -> None:
+    assert isinstance(get_human_age(24, 24)[0], int)
+    assert isinstance(get_human_age(24, 24)[1], int)
