52ce06fdc920ca06d05d48363ad4a8ebfd0d2bfc
Artem Mahda <artm.magda@gmail.com>
2024-09-06 19:24:25 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_get_human_age_cat_dog_below_threshold() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_get_human_age_cat_dog_at_first_threshold() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_get_human_age_cat_dog_between_thresholds() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_get_human_age_cat_dog_at_second_threshold() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_get_human_age_cat_dog_between_second_and_third_thresholds() -> None:
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_get_human_age_cat_dog_at_third_threshold() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_get_human_age_cat_dog_large_values() -> None:
+    assert get_human_age(100, 100) == [21, 17]
