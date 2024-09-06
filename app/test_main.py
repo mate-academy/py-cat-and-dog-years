6863ede7c779509c374ec32e4773172f4deaf4db
Roman Zaliznyi <romanzaliznyi.python@gmail.com>
2024-09-06 23:42:07 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -23,8 +23,8 @@ def test_raising_error_correctly(
 @pytest.mark.parametrize(
     "cats_int, dogs_int, result",
     [
-        (20, -20, [0, 0]),
-        (15, -15, [0, 0]),
+        (13, -13, [0, 0]),
+        (12, -12, [0, 0]),
         (-10, 0, [0, 0]),
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
