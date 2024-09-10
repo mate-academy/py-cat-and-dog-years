40c3ce9e84d8e8409ff1a60d943eb9f2eb4a200f
Ivan <vanya.bidnenko@gmail.com>
2024-09-10 13:59:42 +0300

    Minimise the number of tests to 2

--- app/test_main.py
+++ app/test_main.py
@@ -1,98 +1,21 @@
-from app.main import get_human_age
 import pytest
 
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected",
-    [
-        (0, 0, [0, 0])
-    ]
-)
-def test_zero_age(
-        cat_age: int,
-        dog_age: int,
-        expected: list[int]
-) -> None:
-    assert get_human_age(cat_age, dog_age) == expected
+from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
-        (14, 14, [0, 0])
-    ]
-)
-def test_slightly_below_the_first_threshold(
-        cat_age: int,
-        dog_age: int,
-        expected: list[int]
-) -> None:
-    assert get_human_age(cat_age, dog_age) == expected
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected",
-    [
-        (15, 15, [1, 1])
-    ]
-)
-def test_first_human_year(
-        cat_age: int,
-        dog_age: int,
-        expected: list[int]
-) -> None:
-    assert get_human_age(cat_age, dog_age) == expected
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected",
-    [
-        (16, 16, [1, 1])
-    ]
-)
-def test_slightly_above_the_first_threshold(
-        cat_age: int,
-        dog_age: int,
-        expected: list[int]
-) -> None:
-    assert get_human_age(cat_age, dog_age) == expected
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected",
-    [
-        (23, 23, [1, 1])
-    ]
-)
-def test_slightly_below_the_second_threshold(
-        cat_age: int,
-        dog_age: int,
-        expected: list[int]
-) -> None:
-    assert get_human_age(cat_age, dog_age) == expected
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected",
-    [
-        (24, 24, [2, 2])
-    ]
-)
-def test_second_human_year(
-        cat_age: int,
-        dog_age: int,
-        expected: list[int]
-) -> None:
-    assert get_human_age(cat_age, dog_age) == expected
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected",
-    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (16, 16, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
         (28, 28, [3, 2])
     ]
 )
-def test_slightly_above_the_second_threshold(
+def test_get_human_age(
         cat_age: int,
         dog_age: int,
         expected: list[int]
@@ -101,34 +24,22 @@ def test_slightly_above_the_second_threshold(
 
 
 @pytest.mark.parametrize(
-    "cat_age, dog_age",
+    "cat_age, dog_age, expected_exception",
     [
-        (-1, 0),
-        (0, -1),
-        (-1, -1)
+        (-1, 0, ValueError),
+        (0, -1, ValueError),
+        (-1, -1, ValueError),
+        (2.4, 5, TypeError),
+        (5, 2.4, TypeError),
+        ("string", 5, TypeError),
+        ({2}, "string", TypeError),
+        ({3: 1}, (2), TypeError)
     ]
 )
-def test_negative_values(
+def test_invalid_values(
         cat_age: int,
-        dog_age: int
+        dog_age: int,
+        expected_exception: type[Exception]
 ) -> None:
-    with pytest.raises(ValueError):
-        get_human_age(cat_age, dog_age)
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age",
-    [
-        (2.4, 5),
-        (5, 2.4),
-        ("string", 5),
-        ({2}, "string"),
-        ({3: 1}, (2))
-    ]
-)
-def test_non_integer_values(
-        cat_age: int,
-        dog_age: int
-) -> None:
-    with pytest.raises(TypeError):
+    with pytest.raises(expected_exception):
         get_human_age(cat_age, dog_age)
