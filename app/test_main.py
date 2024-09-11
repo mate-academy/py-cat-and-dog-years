956ef71e56ed15a2aeabc788dd23d8c9a2698816
Ihor Bachuk <igor.bachuk04@gmail.com>
2024-09-11 19:28:59 +0200

    fix error test

--- app/test_main.py
+++ app/test_main.py
@@ -6,6 +6,7 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age,dog_age,result",
     [
+        (-1, 0, [0, 0]),
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
@@ -14,12 +15,13 @@ from app.main import get_human_age
         (29, 29, [3, 3]),
     ],
     ids=[
+        "Converts negative numbers to zero",
         "Converts zeros to zeros",
         "Converts both ages to zero when: age < 15",
         "Converts both ages to 1 when age == 15",
         "Next 9 years should add 1 to both ages",
         "Next 4 years should add 1 to cat years",
-        "Next 5 years should add 1 to dog years",
+        "Next 5 years should add 1 to dog years"
     ]
 )
 def test_get_human_age(
@@ -30,23 +32,6 @@ def test_get_human_age(
     assert get_human_age(cat_age, dog_age) == result
 
 
-@pytest.mark.parametrize(
-    "cat_age,dog_age,error",
-    [
-        (-1, 15, ValueError),
-        ("1", 15, TypeError),
-        (False, False, TypeError),
-    ],
-    ids=[
-        "Should not accept negative values",
-        "Should not accept type string",
-        "Should not accept type bool",
-    ]
-)
-def test_get_human_age_errors(
-        cat_age: int,
-        dog_age: int,
-        error: object
-) -> None:
-    with pytest.raises(error):
-        get_human_age(cat_age, dog_age)
+def test_get_human_age_string() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("1", 0)
