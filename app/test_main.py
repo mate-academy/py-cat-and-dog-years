c42ec73dab5b16713e5343b3571e694f22f1b936
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-11 23:11:29 +0300

    add test for 2 human year from 27/28 cat/dog years

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,6 @@
 from app.main import get_human_age
 
+
 def test_should_return_0_human_years_for_14_cat_dog_years():
     assert get_human_age(14, 14) == [0, 0]
 
@@ -11,3 +12,6 @@ def test_should_return_1_human_years_for_23_cat_dog_years():
 
 def test_should_return_2_human_years_for_24_cat_dog_years():
     assert get_human_age(24, 24) == [2, 2]
+
+def test_should_return_2_human_years_for_27_cats_and_28_dogs_years():
+    assert get_human_age(27, 28) == [2, 2]
