1114804b262ed91ce6d4f5cb9d18f412f607266d
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-12 00:21:38 +0300

    refactor for pytest parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,41 @@
 from app.main import get_human_age
+import pytest
 
 
-def test_should_return_0_human_years_for_14_cat_dog_years() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_should_return_1_human_years_for_15_cat_dog_years() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_should_return_1_human_years_for_23_cat_dog_years() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_should_return_2_human_years_for_24_cat_dog_years() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_should_return_2_human_years_for_27_cats_and_28_dogs_years() -> None:
-    assert get_human_age(27, 28) == [2, 2]
-
-
-def test_should_return_3_human_years_for_28_cats_and_29_dogs_year() -> None:
-    assert get_human_age(28, 29) == [3, 3]
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_human_age",
+        [
+            pytest.param(
+                14, 14, [0, 0],
+                id="return 0 human years for 14 cat/dog years",
+            ),
+            pytest.param(
+                0, 0, [0, 0],
+                id="return 0 human years for 0 cat/dog years",
+            ),
+            pytest.param(
+                15, 23, [1, 1],
+                id="return 1 human years for 15 cats and 23 dogs years",
+            ),
+            pytest.param(
+                27, 28, [2, 2],
+                id="return 2 human years for 27 cats and 28 dogs years",
+            ),
+            pytest.param(
+                28, 28, [3, 2],
+                id="return 3 and 2 human years for 28 cats/dogs year",
+            ),
+            pytest.param(
+                100, 100, [21, 17],
+                id="return 21 human years for cat and 17 for dogs 100 years",
+            ),
+        ]
+    )
+    def test_get_human_age_correctly(
+        self,
+        cat_age: int,
+        dog_age: int,
+        expected_human_age: list[int],
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_human_age
