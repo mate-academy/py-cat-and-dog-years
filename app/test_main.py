07b885fc4fa0cdc411bf906e9d95f0805a5d2c17
Ivan <vanya.bidnenko@gmail.com>
2024-09-11 17:40:49 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Any
 
 from app.main import get_human_age
 
@@ -14,7 +15,10 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (28, 28, [3, 2]),
         (-1, -1, [0, 0]),
-        (-35, -45, [0, 0])
+        (-35, -45, [0, 0]),
+        (-1, 18, [0, 1]),
+        (14, 15, [0, 1]),
+        (26, 12, [2, 0])
     ]
 )
 def test_get_human_age(
@@ -29,12 +33,16 @@ def test_get_human_age(
     "cat_age, dog_age",
     [
         ("cat", "dog"),
-        ("11", "12")
+        ("11", "12"),
+        ([3], [5]),
+        ({3}, {0}),
+        ((1, 2), (3)),
+        ({"cat": 1}, {"dog": 2})
     ]
 )
 def test_invalid_values(
-        cat_age: int,
-        dog_age: int
+        cat_age: Any,
+        dog_age: Any
 ) -> None:
     with pytest.raises(TypeError):
         get_human_age(cat_age, dog_age)
