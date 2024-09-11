03dc9ef1740ecccb1f0f21d050936487f32f1174
Denys Podobrii <denys.podobrii@gmail.com>
2024-09-11 23:13:40 +0300

    refactor code for flake8 rules

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,25 @@
 from app.main import get_human_age
 
 
-def test_should_return_0_human_years_for_14_cat_dog_years():
+def test_should_return_0_human_years_for_14_cat_dog_years() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
-def test_should_return_1_human_years_for_15_cat_dog_years():
+
+def test_should_return_1_human_years_for_15_cat_dog_years() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
-def test_should_return_1_human_years_for_23_cat_dog_years():
+
+def test_should_return_1_human_years_for_23_cat_dog_years() -> None:
     assert get_human_age(23, 23) == [1, 1]
 
-def test_should_return_2_human_years_for_24_cat_dog_years():
+
+def test_should_return_2_human_years_for_24_cat_dog_years() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
-def test_should_return_2_human_years_for_27_cats_and_28_dogs_years():
+
+def test_should_return_2_human_years_for_27_cats_and_28_dogs_years() -> None:
     assert get_human_age(27, 28) == [2, 2]
 
-def test_should_return_3_human_years_for_28_cats_and_29_dogs_year():
+
+def test_should_return_3_human_years_for_28_cats_and_29_dogs_year() -> None:
     assert get_human_age(28, 29) == [3, 3]
