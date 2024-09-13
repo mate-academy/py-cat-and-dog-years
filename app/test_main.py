c36816c76f45e68dcaebe9e01089c66d5f9b7163
Mykyta Paziuka <nikvitka.it@gmail.com>
2024-09-13 12:02:13 +0300

    change test for new values (bigint and string)

--- app/test_main.py
+++ app/test_main.py
@@ -7,12 +7,14 @@ from app.main import get_human_age
     "cat_age, dog_age, result",
     [
         (0, 0, [0, 0]),
+        (-1, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
         (14, 15, [0, 1]),
         (24, 24, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
+        (20452371, 11, [5113088, 0]),
     ]
 )
 def test_convert_dog_or_cat_age(
@@ -24,3 +26,8 @@ def test_convert_dog_or_cat_age(
         get_human_age(cat_age, dog_age) == result
     ), (f"Cat age on human should equal: {result[0]}, "
         f"dog age on human should equal: {result[1]}")
+
+
+def test_should_raise_error_if_argument_is_not_integer() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("131", 12)
