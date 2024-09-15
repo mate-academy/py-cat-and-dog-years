0cd4abec5719780bf8acc899a6673df57c5138cd
Fibonacci <volokitin.vitaliy@icloud.com>
2024-09-15 16:09:32 +0300

    Complated

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,7 @@
 from app.main import get_human_age
 
 
-def test_cat_years():
+def test_cat_years() -> None:
     assert get_human_age(0, 0) == [0, 0]
     assert get_human_age(14, 14) == [0, 0]
     assert get_human_age(15, 15) == [1, 1]
@@ -10,5 +10,3 @@ def test_cat_years():
     assert get_human_age(27, 27) == [2, 2]
     assert get_human_age(28, 28) == [3, 2]
     assert get_human_age(100, 100) == [21, 17]
-
-
