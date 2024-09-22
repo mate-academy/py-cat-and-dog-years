968129f9e0bab75d6e3fc865600d0ece4a875fb8
Alex <54539kalashnikov@gmail.com>
2024-09-22 20:46:50 +0300

    Compact code

--- app/test_main.py
+++ app/test_main.py
@@ -8,60 +8,15 @@ class TestGradationOfYears:
     @pytest.mark.parametrize(
         "cat_age,dog_age,result",
         [
-            pytest.param(
-                0,
-                0,
-                [0, 0],
-                id="zero year"
-            ),
-            pytest.param(
-                14,
-                14,
-                [0, 0],
-                id="before first year"
-            ),
-            pytest.param(
-                15,
-                15,
-                [1, 1],
-                id="first year"
-            ),
-            pytest.param(
-                23,
-                23,
-                [1, 1],
-                id="before second year"
-            ),
-            pytest.param(
-                24,
-                24,
-                [2, 2],
-                id="second year"
-            ),
-            pytest.param(
-                27,
-                28,
-                [2, 2],
-                id="before each_year"
-            ),
-            pytest.param(
-                28,
-                29,
-                [3, 3],
-                id="each_year"
-            ),
-            pytest.param(
-                -20,
-                -30,
-                [0, 0],
-                id="negative age of animals"
-            ),
-            pytest.param(
-                40,
-                44,
-                [6, 6],
-                id="unattainable age for animals"
-            )
+            pytest.param(0, 0, [0, 0], id="zero year"),
+            pytest.param(14, 14, [0, 0], id="before first year"),
+            pytest.param(15, 15, [1, 1], id="first year"),
+            pytest.param(23, 23, [1, 1], id="before second year"),
+            pytest.param(24, 24, [2, 2], id="second year"),
+            pytest.param(27, 28, [2, 2], id="before each_year"),
+            pytest.param(28, 29, [3, 3], id="each_year"),
+            pytest.param(-20, -30, [0, 0], id="negative age of animals"),
+            pytest.param(40, 44, [6, 6], id="unattainable age for animals")
         ]
     )
     def test_gradation_of_years(
@@ -75,21 +30,9 @@ class TestGradationOfYears:
     @pytest.mark.parametrize(
         "cat_age,dog_age",
         [
-            pytest.param(
-                "15",
-                "15",
-                id="type streng"
-            ),
-            pytest.param(
-                [],
-                [],
-                id="type list"
-            ),
-            pytest.param(
-                {},
-                {},
-                id="type dict"
-            )
+            pytest.param("15", "15", id="type streng"),
+            pytest.param([], [], id="type list"),
+            pytest.param({}, {}, id="type dict")
         ]
     )
     def test_of_error(
