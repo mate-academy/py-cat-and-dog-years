e376ad149dde86ff2bfc0dca53d4dc83e1e3467a
Nazar Makivchuk <nazar.makivchuk@gmail.com>
2024-09-24 18:34:13 +0300

    add few tests for cats&dogs

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_get_human_age_zero_ages():
+    """Tests conversion of zero cat and dog ages."""
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_get_human_age_under_fifteen_cat_and_dog():
+    """Tests conversion for cat and dog ages under 15."""
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_get_human_age_fifteen_cat_and_dog():
+    """Tests conversion for cat and dog ages of 15."""
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_get_human_age_over_fifteen_but_under_twentyfour_cat():
+    """Tests conversion for cat age between 15 and 24 (inclusive)."""
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_get_human_age_over_twentyfour_cat():
+    """Tests conversion for cat age exceeding 24."""
+    assert get_human_age(24, 24) == [2, 2]
+    assert get_human_age(27, 27) == [2, 2]
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_get_human_age_large_cat_age():
+    """Tests conversion for a larger cat age."""
+    assert get_human_age(100, 100) == [21, 17]
