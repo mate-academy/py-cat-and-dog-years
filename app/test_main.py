a5213ddd5d1d31911ca50fcce493c865825b8bad
Ivan Bilobrov <ivan.b@team.voodoo-digital.com>
2024-09-28 04:18:09 +0300

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -2,19 +2,30 @@ import pytest
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize("cat_age, dog_age, expected", [
-    pytest.param(0, 0, [0, 0], id="Test with zero ages"),
-    pytest.param(15, 15, [1, 1], id="Test the boundary of the first segment"),
-    pytest.param(23, 23, [1, 1], id="Test just before the second segment"),
-    pytest.param(24, 24, [2, 2], id="Test the boundary of the second segment"),
-    pytest.param(27, 28, [2, 2], id="Test just before the third segment"),
-    pytest.param(28, 28, [3, 2],
-                 id="Test at the third segment "
-                 "start for cat and just before for dog"),
-    pytest.param(30, 29, [3, 3], id="Test the third segment"),
-    pytest.param(35, 34, [4, 4],
+@pytest.mark.parametrize("cat_age, dog_age, expected, exception", [
+    pytest.param(0, 0, [0, 0], None, id="Test with zero ages"),
+    pytest.param(15, 15, [1, 1], None, id=("Test the boundary \
+                  of the first segment")),
+    pytest.param(23, 23, [1, 1], None, id="Test before the second segment"),
+    pytest.param(24, 24, [2, 2], None, id=("Test the boundary of \
+                                           the second segment")),
+    pytest.param(27, 28, [2, 2], None, id="Test before the third segment"),
+    pytest.param(28, 28, [3, 2], None,
+                 id=("Test at the third segment start for \
+                 cat and just before for dog")),
+    pytest.param(30, 29, [3, 3], None, id="Test the third segment"),
+    pytest.param(35, 34, [4, 4], None,
                  id="Test another step within the third segment"),
-    pytest.param(100, 100, [21, 17], id="Test with high ages")
+    pytest.param(100, 100, [21, 17], None, id="Test with high ages"),
+    pytest.param(-1, -1, [0, 0], None, id="Test with negative ages"),
+    pytest.param("twenty", "thirty", None, TypeError, id=("Test with \
+                                                           incorrect type")),
+    pytest.param(5000, 5000, [1246, 997], None, id="Test with large numbers")
 ])
-def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
-    assert get_human_age(cat_age, dog_age) == expected
+def test_get_human_age(cat_age: int, dog_age: int,
+                       expected: list, exception: Exception) -> None:
+    if exception:
+        with pytest.raises(exception):
+            get_human_age(cat_age, dog_age)
+    else:
+        assert get_human_age(cat_age, dog_age) == expected
