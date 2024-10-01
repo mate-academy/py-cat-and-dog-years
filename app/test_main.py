23ac958e2a4f27b3b447916f7e9e57c1b8954cd7
Nastya Kalinina <kalininan.kalinina@gmail.com>
2024-10-01 20:28:26 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -16,9 +16,28 @@ class TestAnimalAge:
             (100, 100, [21, 17])
         ]
     )
-    def test_check_age_for_cat_and_dog_in_human_age(self,
-                                                    age_cat: int,
-                                                    age_dog: int,
-                                                    expected_result: list
-                                                    ) -> None:
+    def test_check_age_for_cat_and_dog_in_human_age(
+            self,
+            age_cat: int,
+            age_dog: int,
+            expected_result: list
+    ) -> None:
+
         assert get_human_age(age_cat, age_dog) == expected_result
+
+    @pytest.mark.parametrize(
+        "age_cat, age_dog, expected_exception",
+        [
+            (-100, -200.05, ValueError),
+            (311232321, 323123215, ValueError),
+            ("311232321", 22, TypeError)
+        ]
+    )
+    def test_invalid_inputs(
+            self,
+            age_cat: int,
+            age_dog: int,
+            expected_exception: Exception
+    ) -> None:
+        with pytest.raises(expected_exception):
+            get_human_age(age_cat, age_dog)
