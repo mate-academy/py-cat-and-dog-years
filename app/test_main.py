43d15a30e066e6a2b80a9de933ca94ad5be5d1f0
yura <pollo7964132@gmail.com>
2024-10-12 00:54:07 +0300

    added some tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,5 @@
+from typing import Any, Type
+
 import pytest
 
 from app.main import get_human_age
@@ -50,6 +52,12 @@ class TestCatDogYears:
                    "test should return "
                    "3 cat age and 2 dog age "
                    "when cat and dog age == 28"
+            ),
+            pytest.param(
+                -1,
+                15,
+                [0, 1],
+                id="test should return zero when some age < 0"
             )
         ]
     )
@@ -60,3 +68,25 @@ class TestCatDogYears:
             human_age: list
     ) -> None:
         assert get_human_age(cat_age, dog_age) == human_age
+
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,exception",
+        [
+            pytest.param(
+                "1",
+                2,
+                TypeError,
+                id=""
+                   "test should raise TypeError "
+                   "when some age is not a int type"
+            )
+        ]
+    )
+    def test_correct_errors(
+            self,
+            cat_age: Any,
+            dog_age: Any,
+            exception: Type[Exception]
+    ) -> None:
+        with pytest.raises(exception):
+            get_human_age(cat_age, dog_age)
