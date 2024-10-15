3742d0a4c6d7847b542b13c1bd38896bf51125c2
Dima Havrylenko <94nj111@gmail.com>
2024-10-15 14:15:21 +0300

    cat_and_dog_years fix*

--- app/test_main.py
+++ app/test_main.py
@@ -8,16 +8,54 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age,dog_age,result",
     [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 23, [1, 1]),
-        (27, 27, [2, 2]),
-        (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="return zeros for zeros"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="return zeros for if values < 15"
+        ),
+        pytest.param(
+            15,
+            23,
+            [1, 1],
+            id="return one if values in range 15 23"
+        ),
+        pytest.param(
+            27,
+            27,
+            [2, 2],
+            id="return two for both if age 27"
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="cat should be older than dog"
+
+        ),
+        pytest.param(
+            100,
+            100,
+            [21, 17],
+            id="should Work correct with big age"
+        ),
+        pytest.param(
+            -15,
+            -40,
+            [0, 0],
+            id="return zeros for negative values"
+        )
     ]
 )
 def test_get_human_age(
         cat_age: int,
+
         dog_age: int,
         result: list[int, int]
 ) -> None:
@@ -27,9 +65,24 @@ def test_get_human_age(
 @pytest.mark.parametrize(
     "cat_age_incorrect,dog_age_incorrect,error",
     [
-        ([], [], TypeError),
-        (" ", " ", TypeError),
-        ({}, {}, TypeError),
+        pytest.param(
+            [],
+            [],
+            TypeError,
+            id="should raise TypeError for lists"
+        ),
+        pytest.param(
+            " ",
+            " ",
+            TypeError,
+            id="should raise TypeError for strings"
+        ),
+        pytest.param(
+            {},
+            {},
+            TypeError,
+            id="should raise TypeError for sets"
+        ),
     ]
 )
 def test_raises_error_when_incorrect_data_type(
