cd4ac75f42db66350035c6fd40706ec0919cdb46
Dima Havrylenko <94nj111@gmail.com>
2024-10-15 14:02:54 +0300

    cat_and_dog_years fix

--- app/test_main.py
+++ app/test_main.py
@@ -20,3 +20,20 @@ def test_get_human_age(
         result: list[int, int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == result
+
+
+@pytest.mark.parametrize(
+    "cat_age_incorrect,dog_age_incorrect,error",
+    [
+        ([], [], TypeError),
+        (" ", " ", TypeError),
+        ({}, {}, TypeError),
+    ]
+)
+def test_raises_error_when_incorrect_data_type(
+        cat_age_incorrect,
+        dog_age_incorrect,
+        error
+) -> None:
+    with pytest.raises(error):
+        get_human_age(cat_age_incorrect, dog_age_incorrect)
