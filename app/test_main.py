5b42c4ce76536c28faca3a3bbd7606d80283151d
roman <skyfoxwork@gmail.com>
2024-10-15 18:24:54 +0300

    Solution-py-cat-and-dog-years v2.0

--- app/test_main.py
+++ app/test_main.py
@@ -15,6 +15,7 @@ from app.main import get_human_age
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
+        (-10, -100, [0, 0])
     ],
     ids=[
         "for 0 years cat and dog 0",
@@ -24,8 +25,29 @@ from app.main import get_human_age
         "for 24 years cat and dog similar all +1 year",
         "for 27 years cat and dog similar all +1 year",
         "for 28 years cat +1 and dog different all +1 year",
-        "for 100 years cat +1 and dog different all +1 year"
+        "for 100 years cat +1 and dog different all +1 year",
+        "years is negative numbers"
     ]
 )
 def test_first(cat_age: int, dog_age: int, result: list) -> None:
     assert get_human_age(cat_age, dog_age) == result
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+    [
+        (0, [], TypeError),
+        ([], 0, TypeError),
+        (0, "", TypeError),
+        ("", 0, TypeError),
+    ],
+    ids=[
+        "TypeError for cat list",
+        "TypeError for dog list",
+        "TypeError for dog string",
+        "TypeError for dog string",
+    ]
+)
+def test_raising_errors_correctly(cat_age: int, dog_age: int, expected_error: Exception) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
