747d754c17c00c6ff10e7325e733970ea8b6c8cf
Olha Stadnik <olgastadnik2507@gmail.comcd>
2024-10-17 20:53:36 +0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_human_age_zero() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_human_age_in_1_year() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_ages_between_15_and_24() -> None:
+    assert get_human_age(22, 22) == [1, 1]
+
+
+def test_human_age_in_2_year() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_difference_between_cat_dog_year() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_human_age_in_3_year() -> None:
+    assert get_human_age(28, 29) == [3, 3]
+
+
+def test_large_ages() -> None:
+    assert get_human_age(100, 100) == [21, 17]
