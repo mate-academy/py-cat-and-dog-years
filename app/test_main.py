cffe6f92f20e604d83b578dc8a289ce6cb6a98e5
Ivan Fursyk <ivan.fursyk@gmail.com>
2024-10-21 01:02:00 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,9 +1,25 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_get_human_age_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_get_human_age_1_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (100, 100, [21, 17]),
+        (-1, -1, [0, 0]),
+    ],
+    ids=[
+        "0 cat/dog years should convert into 0 human age.",
+        "15 cat/dog years should convert into 1 human age.",
+        "23 cat/dog years should convert into 1 human age.",
+        "24 cat/dog years should convert into 2 human age.",
+        "100 cat/dog years should convert into 21/17 human age.",
+        "Negative values should return 0 human age.",
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, expected: int) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
