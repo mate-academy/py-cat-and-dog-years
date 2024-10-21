599dd0e43fabf43ac4c5e6ad577ac29b7859a18f
Nazar <nazargorodyko@gmail.com>
2024-10-21 12:28:20 +0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,17 +1,59 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_15_years_animals_is_1_human() -> None:
-    assert get_human_age(15, 14) == [1, 0]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        pytest.param(
+            15,
+            14,
+            [1, 0],
+            id="15 years animals is 1 human"
+        ),
+        pytest.param(
+            27,
+            27,
+            [2, 2],
+            id="27 years animals is 2 human"
+        ),
+        pytest.param(
+            28,
+            29,
+            [3, 3],
+            id="28/29 cat/dogs years is 3 human"
+        ),
+        pytest.param(
+            100.1,
+            100.5,
+            [21, 17],
+            id="remainder should be discarded"
+        ),
+        pytest.param(
+            -5,
+            -3,
+            [0, 0],
+            id="value is negative should return 0"
+        ),
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="value is 0 should return 0"
+        ),
+        pytest.param(
+            1000,
+            1000,
+            [246, 197],
+            id="result should be correct when value is large"
+        ),
+    ]
+)
 
-
-def test_15_plus_9_years_is_1_human() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_28_29_cat_dog_is_3_human() -> None:
-    assert get_human_age(28, 29) == [3, 3]
-
-
-def test_remainder_should_discarded() -> None:
-    assert get_human_age(100.1, 100.5) == [21, 17]
+def test_modify_convert_to_human(
+        cat_age,
+        dog_age,
+        result
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result
