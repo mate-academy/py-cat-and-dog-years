3d8dc32b2233224108d13c97e34c24ec8df8081a
Sonya Savkova <silva.vvss12@gmail.com>
2024-10-23 15:21:05 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,19 +2,28 @@ import pytest
 from app.main import get_human_age
 
 
-def test_get_human_age() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(23, 23) == [2, 2]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(27, 27) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(100, 100) == [21, 17]
-
-    assert get_human_age(16, 15) == [1, 1]
-    assert get_human_age(25, 25) == [2, 2]
-    assert get_human_age(30, 30) == [3, 3]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_human_ages",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        (16, 15, [1, 1]),
+        (25, 25, [2, 2]),
+        (30, 30, [3, 3]),
+    ]
+)
+def test_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected_human_ages: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_human_ages
 
 
 if __name__ == "__main__":
