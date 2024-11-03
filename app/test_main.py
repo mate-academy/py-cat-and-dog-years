78db26beb9932de27ef3aca8a7877272ca6f8636
Nikita Ponomarov <wertyciti29@gmail.com>
2024-11-03 14:09:13 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
 from app.main import get_human_age
 
-# write your code here
+import pytest
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (24, 24, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_works_correctly(
+        cat_age: int,
+        dog_age: int,
+        result: list[int]
+) -> None:
+    assert (get_human_age(cat_age, dog_age) == result), \
+        f"The conversion of {cat_age} and {dog_age} must be equal to {result}"
