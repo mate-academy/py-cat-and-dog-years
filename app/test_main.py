6488d8f85cec6875145a9a20b62e5ef19d8c00d1
Greena1c <shchedrykserhii@gmail.com>
2024-11-07 10:54:38 +0200

    Test about dog&cat

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_age_zero() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_age_less_than_first_threshold() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_age_for_one_year_human() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_age_less_for_second_threshold() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_age_for_two_years_or_second_threshold() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_age_less_for_third_threshold() -> None:
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_after_second_threshold_for_cat() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_big_age() -> None:
+    assert get_human_age(100, 100) == [21, 17]
