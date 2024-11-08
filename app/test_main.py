36c46d57a72858bf6d16cfff7b6f97a92a1df275
Evgeniya <didovichevgeniya@gmail.com>
2024-11-08 14:18:50 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,24 +5,12 @@ def test_get_cat_age_and_dog_age_equal_zero() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_get_cat_age_and_dog_age_equal_zero2() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
 def test_get_cat_age_and_dog_age_one() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_get_cat_age_and_dog_age_23() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
 def test_get_cat_age_and_dog_age_two() -> None:
     assert get_human_age(24, 24) == [2, 2]
-
-
-def test_get_cat_age_and_dog_age_third() -> None:
-    assert get_human_age(27, 27) == [2, 2]
 
 
 def test_get_cat_age_and_dog_age_equal_different() -> None:
