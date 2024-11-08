c1b2028a15eeb48c3e8858c058f259c04af0fc0e
Evgeniya <didovichevgeniya@gmail.com>
2024-11-08 14:15:13 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_get_cat_age_and_dog_age_equal_zero() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_get_cat_age_and_dog_age_equal_zero2() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_get_cat_age_and_dog_age_one() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_get_cat_age_and_dog_age_23() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_get_cat_age_and_dog_age_two() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_get_cat_age_and_dog_age_third() -> None:
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_get_cat_age_and_dog_age_equal_different() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_get_cat_age_and_dog_age() -> None:
+    assert get_human_age(100, 100) == [21, 17]
