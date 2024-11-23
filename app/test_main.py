c0d0bd36a7b1279f4798f0202991931f4af31237
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2024-11-23 13:20:14 +0100

    solution3

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
 from app.main import get_human_age
 import pytest
 
+
 @pytest.mark.parametrize(
     "init_cat_age,init_dog_age,expected_result",
     [
@@ -12,8 +13,9 @@ import pytest
         pytest.param(28, 29, [3, 3], id="28/29 cat/dog age"),
     ]
 )
-
-def test_get_human_age(init_cat_age, init_dog_age, expected_result) -> None:
+def test_get_human_age(init_cat_age: int,
+                       init_dog_age: int,
+                       expected_result: list) -> None:
     cat_age = init_cat_age
     dog_age = init_dog_age
     result = get_human_age(cat_age=cat_age, dog_age=dog_age)
