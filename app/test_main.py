7b268b3dae178281c7743974e4b74a56c5fa9a36
Mateusz Pawlak <pawlakmateusz96@gmail.com>
2024-11-23 13:17:10 +0100

    solution2

--- app/test_main.py
+++ app/test_main.py
@@ -1,39 +1,20 @@
 from app.main import get_human_age
+import pytest
 
+@pytest.mark.parametrize(
+    "init_cat_age,init_dog_age,expected_result",
+    [
+        pytest.param(24, 24, [2, 2], id="24/24 cat/dog age"),
+        pytest.param(14, 14, [0, 0], id="14/14 cat/dog age"),
+        pytest.param(15, 15, [1, 1], id="15/15 cat/dog age"),
+        pytest.param(23, 23, [1, 1], id="23/23 cat/dog age"),
+        pytest.param(27, 28, [2, 2], id="27/28 cat/dog age"),
+        pytest.param(28, 29, [3, 3], id="28/29 cat/dog age"),
+    ]
+)
 
-class TestGetHumanAge:
-    def test_get_human_age_24_24(self) -> None:
-        cat_age = 24
-        dog_age = 24
-        result = get_human_age(cat_age=cat_age, dog_age=dog_age)
-        assert result == [2, 2], "Result should be [2, 2]"
-
-    def test_get_human_age_14_14(self) -> None:
-        cat_age = 14
-        dog_age = 14
-        result = get_human_age(cat_age=cat_age, dog_age=dog_age)
-        assert result == [0, 0], "Result should be [0, 0]"
-
-    def test_get_human_age_15_15(self) -> None:
-        cat_age = 15
-        dog_age = 15
-        result = get_human_age(cat_age=cat_age, dog_age=dog_age)
-        assert result == [1, 1], "Result should be [1, 1]"
-
-    def test_get_human_age_23_23(self) -> None:
-        cat_age = 23
-        dog_age = 23
-        result = get_human_age(cat_age=cat_age, dog_age=dog_age)
-        assert result == [1, 1], "Result should be [1, 1]"
-
-    def test_get_human_age_27_28(self) -> None:
-        cat_age = 27
-        dog_age = 28
-        result = get_human_age(cat_age=cat_age, dog_age=dog_age)
-        assert result == [2, 2], "Result should be [2, 2]"
-
-    def test_get_human_age_28_29(self) -> None:
-        cat_age = 28
-        dog_age = 29
-        result = get_human_age(cat_age=cat_age, dog_age=dog_age)
-        assert result == [3, 3], "Result should be [3, 3]"
+def test_get_human_age(init_cat_age, init_dog_age, expected_result) -> None:
+    cat_age = init_cat_age
+    dog_age = init_dog_age
+    result = get_human_age(cat_age=cat_age, dog_age=dog_age)
+    assert result == expected_result
