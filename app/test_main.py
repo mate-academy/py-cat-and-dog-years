729e7b0bdace6695b0b52d5b4cca761c91a7c1d5
Serhiy <sergeynechaenko@ukr.net>
2024-11-28 00:03:18 +0200

    add test checking for large numbers, correct test cheking for two numbers

--- app/test_main.py
+++ app/test_main.py
@@ -21,14 +21,14 @@ def test_should_return_integer_age() -> None:
     assert all(isinstance(age, int) for age in check_func_get_human_age)
 
 
-def test_should_check_len_list() -> None:
+def test_should_checking_for_len_list() -> None:
     cat_age = 20
     dog_age = 20
     check_func_get_human_age = get_human_age(cat_age, dog_age)
     assert len(check_func_get_human_age) == 2
 
 
-def test_should_check_zero_human_year() -> None:
+def test_should_checking_for_zero_human_year() -> None:
     cat_age = 14
     dog_age = 14
     check_func_get_human_age = get_human_age(cat_age, dog_age)
@@ -39,7 +39,7 @@ def test_should_check_zero_human_year() -> None:
         assert check_func_get_human_age[1] == 0
 
 
-def test_should_check_one_human_year() -> None:
+def test_should_checking_for_one_human_year() -> None:
     cat_age = 23
     dog_age = 23
     check_func_get_human_age = get_human_age(cat_age, dog_age)
@@ -50,12 +50,26 @@ def test_should_check_one_human_year() -> None:
         assert check_func_get_human_age[1] == 1
 
 
-def test_should_check_two_human_year() -> None:
+def test_should_checking_for_two_human_year() -> None:
     cat_age = 24
     dog_age = 24
     check_func_get_human_age = get_human_age(cat_age, dog_age)
 
-    if second_year <= cat_age < first_year + second_year + each_year_cat:
+    if (first_year + second_year <= cat_age
+            < first_year + second_year + each_year_cat):
         assert check_func_get_human_age[0] == 2
-    if second_year <= dog_age < first_year + second_year + each_year_dog:
+    if (first_year + second_year <= dog_age
+            < first_year + second_year + each_year_dog):
         assert check_func_get_human_age[1] == 2
+
+def test_should_checking_for_large_numbers() -> None:
+    cat_age = 100
+    dog_age = 100
+    check_func_get_human_age = get_human_age(cat_age, dog_age)
+
+    if cat_age > first_year + second_year + each_year_cat:
+        assert (check_func_get_human_age[0] == 2 +
+                (cat_age - first_year - second_year) // each_year_cat)
+    if dog_age > first_year + second_year + each_year_dog:
+        assert (check_func_get_human_age[1] == 2 +
+                (dog_age - first_year - second_year) // each_year_dog)
