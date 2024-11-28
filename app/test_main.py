e4e1fa5867288fd475049712824a07b4c79c2809
Viacheslav Kaliak <trinidaa@gmial.com>
2024-11-28 15:31:32 +0100

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -13,5 +13,6 @@ from app.main import get_human_age
     (40, 40, [6, 5]),
     (100, 100, [21, 17]),
 ])
+
 def test_get_human_age(cat_age: int, dog_age: int, human_age: any) -> None:
     assert get_human_age(cat_age, dog_age) == human_age
