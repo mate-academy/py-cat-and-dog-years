80756ca77d0ab62bfb0cb77abad3eee5959ae9e5
Beliar24 <blackdid7@gmail.com>
2024-11-29 00:41:08 +0200

    check ages for cat and dog

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,21 @@
 from app.main import get_human_age
 
 
-def test_should_be_correct_visible_convert_to_human_age():
+def test_should_be_correct_visible_convert_to_human_age() -> None:
     result = get_human_age(15, 15)
     assert result == [1, 1]
 
 
-def test_should_be_correct_convert_to_human_age_with_different_ages():
+def test_should_be_correct_convert_to_human_age_with_different_ages() -> None:
     result = get_human_age(15, 40)
     assert result == [1, 5]
 
 
-def test_should_be_correct_convert_to_human_age_with_next_step_ages():
+def test_should_be_correct_convert_to_human_age_with_next_step_ages() -> None:
     result = get_human_age(25, 25)
     assert result == [2, 2]
 
 
-def test_should_be_correct_convert_to_human_age_with_wrong_ages():
+def test_should_be_correct_convert_to_human_age_with_wrong_ages() -> None:
     result = get_human_age(-5, 40)
     assert result == [0, 5]
