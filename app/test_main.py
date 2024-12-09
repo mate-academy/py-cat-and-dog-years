89ce13ba24e3814b7c4193395b6134e9185d1a9b
Kirill <138298380+Psychox1k@users.noreply.github.com>
2024-12-09 23:10:10 +0100

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from app.main import get_human_age
 
+
 @pytest.mark.parametrize(
     "cat_age,dog_age,result",
     [
@@ -28,5 +29,6 @@ from app.main import get_human_age
         "Fractional boundary -> correct rounding",
     ],
 )
-def test_check_age_converting(cat_age: int, dog_age: int, result: list) -> None:
+def test_check_age_converting(cat_age: int, dog_age: int, result: list
+                             ) -> None:
     assert get_human_age(cat_age, dog_age) == result
