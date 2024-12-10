854e00bed31c5eb7e3bedf31355cb9029eb3a72b
Valdyslav <magnetto54@gmail.com>
2024-12-10 19:45:56 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_check_correctly_converted_less_fifteen_cats_and_dogs_years_in_human_age():
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_check_correctly_converted_in_first_human_year():
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_check_correctly_converted_in_second_human_year():
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_check_correctly_converted_cats_and_dogs_years_in_third_and_more_human_years():
+    assert get_human_age(28, 28) == [3, 2]
