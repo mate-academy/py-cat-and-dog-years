3eadedfccce62a12f89dbb1b031d4bc16dfc5492
git_practic <dogoberrr@gmail.com>
2024-12-19 00:44:46 +0200

    Fix-flake-errors

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,9 @@
+from typing import Type
+
 import pytest
 
 from app.main import get_human_age
+
 
 class TestConvertAnimalAgeToHumanAge:
     @pytest.mark.parametrize(
@@ -20,9 +23,13 @@ class TestConvertAnimalAgeToHumanAge:
             "100 cat/dog years should convert into 21/17 human age."
         ]
     )
-    def test_convert_animal_age_to_human(self, cat_age, dog_age, human_age) -> None:
+    def test_convert_animal_age_to_human(
+            self,
+            cat_age: int,
+            dog_age: int,
+            human_age: list[int]
+    ) -> None:
         assert get_human_age(cat_age, dog_age) == human_age
-
 
     @pytest.mark.parametrize(
         "cat_age,dog_age,error",
@@ -39,6 +46,11 @@ class TestConvertAnimalAgeToHumanAge:
             "You need to use only int as cat/dog years.",
         ]
     )
-    def test_raising_errors(self, cat_age, dog_age, error) -> None:
+    def test_raising_errors(
+            self,
+            cat_age: int,
+            dog_age: int,
+            error: Type[ValueError | TypeError]
+    ) -> None:
         with pytest.raises(error):
             get_human_age(cat_age, dog_age)
