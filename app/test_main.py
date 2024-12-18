074494b97c8893f4823de9021914aaa6c2c28bac
Maksym <chukhno.maksym@gmail.com>
2024-12-18 21:43:47 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,26 @@
 import pytest
 from app.main import get_human_age
 
-
 @pytest.mark.parametrize(
-    "cat_age, dog_age, result",
+    "cat_age, dog_age, expected_result",
     [
+        (15, 15, [1, 1]),
+        (24, 24, [2, 2]),
+        (23, 23, [1, 1]),
         (14, 14, [0, 0]),
-        (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (28, 29, [3, 3]),
+        (27, 28, [2, 2])
+
     ],
     ids=[
-        "cat and dog both 14 years",
-        "cat and dog both 28 years",
-        "cat and dog both 100 years"
+        "15 cat/dog years should convert into 1 human age.",
+        "24 cat/dog years should convert into 2 human age.",
+        "23 cat/dog years should convert into 1 human age.",
+        "14 cat/dog years should convert into 0 human age.",
+        "28/29 cat/dog years should convert into 3 human age.",
+        "27/28 cat/dog years should convert into 2 human age."
     ]
 )
-def correct_func(cat_age: int, dog_age: int, result: list) -> None:
-    assert get_human_age(cat_age, dog_age) == result
+def test_get_human_age(cat_age, dog_age, expected_result):
+    assert get_human_age(cat_age, dog_age) == expected_result
+
