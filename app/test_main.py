e453c6881164751b3ea609f9c15878ec32a91fd3
Oleksandr Barabanov <imtence@icloud.com>
2024-12-19 13:41:44 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,16 +2,21 @@ from app.main import get_human_age, convert_to_human
 
 
 def test_convert_to_human_age() -> None:
-    assert convert_to_human(10, 15, 9, 4) == 0
-    assert convert_to_human(20, 15, 9, 4) == 1
-    assert convert_to_human(30, 15, 9, 4) == 3
-    assert convert_to_human(40, 15, 9, 4) == 6
-    assert convert_to_human(50, 15, 9, 4) == 8
-
+    assert convert_to_human(0, 15, 9 ,4) == 0
+    assert convert_to_human(14, 15, 9, 4) == 0
+    assert convert_to_human(15, 15, 9, 4) == 1
+    assert convert_to_human(23, 15, 9, 4) == 1
+    assert convert_to_human(24, 15, 9 ,4) == 2
+    assert convert_to_human(27, 15, 9, 4) == 2
+    assert convert_to_human(28, 15, 9, 4) == 3
+    assert convert_to_human(100, 15, 9, 4) == 21
 
 def test_get_human_age() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+    assert get_human_age(14, 14) == [0, 0]
     assert get_human_age(15, 15) == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
     assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(50, 50) == [8, 7]
-    assert get_human_age(10, 20) == [0, 1]
-    assert get_human_age(40, 25) == [6, 2]
+    assert get_human_age(27, 28) == [2, 2]
+    assert get_human_age(28, 29) == [3, 3]
+    assert get_human_age(100, 100) == [21, 17]
