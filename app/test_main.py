b1f6f8a7eb0c64e41b792be85067634323aed60b
Kateryna <katerina.a.biruk@gmail.com>
2024-12-20 00:04:52 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,37 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_get_human_age_zeroes() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_get_human_age_negative() -> None:
+    assert get_human_age(-1, -1) == [0, 0]
+
+
+def test_get_human_age_sub_fifteen() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_get_human_age_fifteen() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_get_human_age_nineteen() -> None:
+    assert get_human_age(19, 19) == [1, 1]
+
+
+def test_get_human_age_twenty_four() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_get_human_age_twenty_eight() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_get_human_age_hundred() -> None:
+    assert get_human_age(100, 100) == [21, 17]
+
+
+def test_get_human_age_two_hundred() -> None:
+    assert get_human_age(200, 200) == [46, 37]
