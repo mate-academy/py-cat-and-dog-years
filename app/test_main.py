3ab9c41265dbef8ffea3c67a30ff3ecba5ec44c6
Oleksandr Barabanov <imtence@icloud.com>
2024-12-19 13:44:08 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,14 +2,15 @@ from app.main import get_human_age, convert_to_human
 
 
 def test_convert_to_human_age() -> None:
-    assert convert_to_human(0, 15, 9 ,4) == 0
+    assert convert_to_human(0, 15, 9,4) == 0
     assert convert_to_human(14, 15, 9, 4) == 0
     assert convert_to_human(15, 15, 9, 4) == 1
     assert convert_to_human(23, 15, 9, 4) == 1
-    assert convert_to_human(24, 15, 9 ,4) == 2
+    assert convert_to_human(24, 15, 9,4) == 2
     assert convert_to_human(27, 15, 9, 4) == 2
     assert convert_to_human(28, 15, 9, 4) == 3
     assert convert_to_human(100, 15, 9, 4) == 21
+
 
 def test_get_human_age() -> None:
     assert get_human_age(0, 0) == [0, 0]
