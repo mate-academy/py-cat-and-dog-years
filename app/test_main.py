5df555798a0b65d9d665235cf677eeb178c2ed35
Ihor <andreiunderwhat@mail.com>
2024-12-19 23:06:55 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -2,3 +2,15 @@ from app.main import get_human_age
 
 def test_get_human_age_should_return_animal_age_in_human_years() -> None:
     assert get_human_age(28, 28) == [3, 2]
+
+
+def test_get_human_age_should_return_zero() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_get_human_age_with_negative_numbers() -> None:
+    assert get_human_age(-19, -74) == [0, 0]
+
+
+def test_get_human_age_with_big_numbers() -> None:
+    assert get_human_age(1000, 789) == [246, 155]
