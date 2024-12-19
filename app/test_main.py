c2bc60de45911eef60ebcf98557f67a30b9dcd27
Arsen Myroniuk <arisenmyr@gmail.com>
2024-12-19 23:31:15 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
 from app.main import get_human_age
 
-# write your code here
+def test_get_human_age_zero_years():
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_get_human_age_fourteen_years_is_zero_in_humans():
+    assert get_human_age(14, 14) == [0, 0]
+
+def test_get_human_age_fifteen_years_is_one_in_humans():
+    assert get_human_age(15, 15) == [1, 1]
+
+def test_get_human_age_between_fifteen_and_twenty_four():
+    assert get_human_age(17, 20) == [1, 1]
+
+def test_get_human_age_cat_age_after_twenty_four():
+    assert get_human_age(28, 28) == [3, 2]
+
+def test_get_human_age_dog_age_after_twenty_four():
+    assert get_human_age(28, 29) == [3, 3]
