0675855aacfe153a50ce4c2d35f8585f03ffbdf5
shafran <shafran.andriy@gmail.com>
2024-12-22 22:35:31 +0200

    Created all tests using parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -3,40 +3,63 @@ import pytest
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize(
-    "cat_years, dog_years, expected_years",
-    [
-        pytest.param(0,
-                     0,
-                     [0, 0],
-                     id="cat and/or dog years is 0"),
-        pytest.param(-1,
-                     -1,
-                     [0, 0],
-                     id="cat and/or dog years is less than 0"),
-        pytest.param(14,
-                     14,
-                     [0, 0],
-                     id="first 15 cat or dog years give 1 human year"),
-        pytest.param(15,
-                     15,
-                     [1, 1],
-                     id="the next 9 cat or dog years give 1 more human year"),
-        pytest.param(28,
-                     28,
-                     [3, 2],
-                     id="every 4 next cat years give 1 extra human year"),
-        pytest.param(100,
-                     100,
-                     [21, 17],
-                     id="every 5 next dog years give 1 extra human year"),
-        pytest.param(101,
-                     101,
-                     [21, 17],
-                     id="cat and/or dog years is more then 100 years")
-    ]
-)
-def test_get_human_age(cat_years: int,
-                       dog_years: int,
-                       expected_years: list) -> None:
-    assert get_human_age(cat_years, dog_years) == expected_years
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_years, dog_years, expected_years",
+        [
+            pytest.param(0,
+                         0,
+                         [0, 0],
+                         id="cat and/or dog years is 0"),
+            pytest.param(-1,
+                         -1,
+                         [0, 0],
+                         id="cat and/or dog years is less than 0"),
+            pytest.param(14,
+                         14,
+                         [0, 0],
+                         id="first 15 cat or dog"
+                            " years give 1 human year"),
+            pytest.param(15,
+                         15,
+                         [1, 1],
+                         id="the next 9 cat or dog"
+                            " years give 1 more human year"),
+            pytest.param(28,
+                         28,
+                         [3, 2],
+                         id="every 4 next cat"
+                            " years give 1 extra human year"),
+            pytest.param(100,
+                         100,
+                         [21, 17],
+                         id="every 5 next dog"
+                            " years give 1 extra human year"),
+            pytest.param(101,
+                         101,
+                         [21, 17],
+                         id="cat and/or dog"
+                            " years is more then 100 years")
+        ]
+    )
+    def test_get_human_age(self,
+                           cat_years: int,
+                           dog_years: int,
+                           expected_years: list) -> None:
+        assert get_human_age(cat_years, dog_years) == expected_years
+
+    @pytest.mark.parametrize(
+        "cat_years, dog_years, expected_years",
+        [
+            pytest.param("101",
+                         "101",
+                         TypeError,
+                         id="cat and/or dog years is more then 100 years"),
+        ]
+    )
+    def test_get_human_age_correct_types(self,
+                                         cat_years: int,
+                                         dog_years: int,
+                                         expected_years: list) -> None:
+        with pytest.raises(expected_years):
+            get_human_age(cat_years, dog_years)
