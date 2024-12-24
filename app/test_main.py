0ad61464a19c9ffbe3d58746bce497039eb1dba6
Maks Vakulenko <maksimvakulenkooo@gmail.com>
2024-12-24 18:08:51 +0100

    solution (styling)

--- app/test_main.py
+++ app/test_main.py
@@ -2,10 +2,13 @@ import pytest
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize("cat_age, dog_age, expected", [
-    (0, 0, [0, 0]),
-    (14, 14, [0, 0]),
-])
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+    ]
+)
 def test_returns_zero_for_ages_below_first_year(
         cat_age: int,
         dog_age: int,
@@ -14,10 +17,13 @@ def test_returns_zero_for_ages_below_first_year(
     assert get_human_age(cat_age, dog_age) == expected
 
 
-@pytest.mark.parametrize("cat_age, dog_age, expected", [
-    (15, 15, [1, 1]),
-    (23, 23, [1, 1]),
-])
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+    ]
+)
 def test_returns_one_for_ages_in_second_year_range(
         cat_age: int,
         dog_age: int,
@@ -26,10 +32,13 @@ def test_returns_one_for_ages_in_second_year_range(
     assert get_human_age(cat_age, dog_age) == expected
 
 
-@pytest.mark.parametrize("cat_age, dog_age, expected", [
-    (24, 24, [2, 2]),
-    (27, 27, [2, 2]),
-])
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+    ]
+)
 def test_returns_two_for_ages_in_third_year_range(
         cat_age: int,
         dog_age: int,
@@ -38,10 +47,13 @@ def test_returns_two_for_ages_in_third_year_range(
     assert get_human_age(cat_age, dog_age) == expected
 
 
-@pytest.mark.parametrize("cat_age, dog_age, expected", [
-    (28, 28, [3, 2]),
-    (100, 100, [21, 17]),
-])
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ]
+)
 def test_returns_correct_age_for_years_after_second_year(
         cat_age: int,
         dog_age: int,
