0527e6f70653883f787f3ec81275fc356cd82e3e
Liza Shulika <liza.shulika2009@gmail.com>
2025-01-03 18:27:30 +0200

    animal

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,50 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+class Animal:
+    @pytest.mark.parametrize(
+        "cat_age",
+        "dog_age",
+        "result",
+        [
+            pytest.param(
+                0,
+                0,
+                [0, 0],
+                id="should_zero_cat_and_dog"
+            ),
+            pytest.param(
+                15,
+                15,
+                [1, 1],
+                id="should_one_year"
+            ),
+            pytest.param(
+                23,
+                23,
+                [1, 1],
+                id="should_one_years"
+            ),
+            pytest.param(
+                24,
+                24,
+                [2, 2],
+                id="should_two_year"
+            ),
+            pytest.param(
+                28,
+                28,
+                [3, 2],
+                id="should_two_and_three_year"
+            ),
+        ]
+    )
+
+    def test_modify_class(self, cat_age: int,
+                          dog_age: int,
+                          result: list) -> None:
+        cat = cat_age
+        dog = dog_age
+        get_human_age(cat, dog)
+        assert get_human_age == result
