d58540ad08e4830eb0e35b3665407c1a72e5f825
Taras <t.kudinov90@gmail.com>
2025-01-03 15:18:17 +0200

    Another solution with passed autotests after first review.

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,18 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_get_human_age() -> None:
-    assert get_human_age(28, 28) == [3, 2]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        pytest.param(0, 0, [0, 0]),
+        pytest.param(14, 14, [0, 0]),
+        pytest.param(15, 15, [1, 1]),
+        pytest.param(23, 23, [1, 1]),
+        pytest.param(28, 28, [3, 2]),
+        pytest.param(100, 100, [21, 17])
+    ]
+
+)
+def test_get_human_age(cat_age: int, dog_age: int, result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result
