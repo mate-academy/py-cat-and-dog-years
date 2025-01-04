df51934416066143cb1e0b1b805b581869033414
Misha Bigun <bigun.mykhaylo@gmail.com>
2025-01-04 19:57:48 +0200

    add check many years

--- app/test_main.py
+++ app/test_main.py
@@ -7,9 +7,10 @@ from app.main import get_human_age
     "cat_age,dog_age,human_age",
     [
         pytest.param(14, 14, [0, 0], id="zero human year"),
+        pytest.param(160, 135, [36, 24], id="many human year"),
         pytest.param(15, 15, [1, 1], id="one human year"),
         pytest.param(24, 24, [2, 2], id="two human years"),
-        pytest.param(28, 29, [3, 3], id="tree human years"),
+        pytest.param(28, 29, [3, 3], id="three human years"),
         pytest.param(28, 28, [3, 2],
                      id="three human years for cat and two for dog"),
     ]
