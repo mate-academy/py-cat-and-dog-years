29c8ed3fd302cc8ca22ca1a19232c45d2e6f9b30
Oksana Dzeryn <oks.dzeryn@gmail.com>
2025-01-06 17:22:52 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,31 @@
+import pytest
 from app.main import get_human_age
 
 
-def check_get_human() -> None:
-    assert (get_human_age(0, 0) == [0, 0])
-
-
-def check_middle_age() -> None:
-    assert (get_human_age(14, 14) == [0, 0])
-
-
-def check_next_age_gap() -> None:
-    assert (get_human_age(15, 15) == [1, 1])
-
-
-def check_next_age_gap1() -> None:
-    assert (get_human_age(23, 23) == [1, 1])
-
-
-def check_next_age_gap2() -> None:
-    assert (get_human_age(24, 24) == [2, 2])
-
-
-def check_next_age_gap3() -> None:
-    assert (get_human_age(27, 27) == [2, 2])
-
-
-def check_next_age_gap4() -> None:
-    assert (get_human_age(28, 28) == [3, 2])
-
-
-def check_last_age_gap() -> None:
-    assert (get_human_age(100, 100) == [21, 17])
+@pytest.mark.parametrize(
+    "cat, dog, result",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ],
+    ids=[
+        "1",
+        "2",
+        "3",
+        "4",
+        "5",
+        "6",
+        "7",
+        "8",
+    ],
+)
+def test_get_human_age(cat: int, dog: int, result: list) -> None:
+    assert get_human_age(cat, dog) == result, (
+        f"Human years {cat} and {dog} equal to {result}"
+    )
