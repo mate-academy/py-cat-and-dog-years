af698b1a839ee04559093af804030d00a36e5c7d
Vitalii Ivanchenko <vanno1234567890@gmail.com>
2025-01-08 00:03:38 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,7 +1,7 @@
 from app.main import get_human_age, convert_to_human
 
 
-def test_get_human_age():
+def test_get_human_age() -> None:
     assert get_human_age(0, 0) == [0, 0]
     assert get_human_age(14, 14) == [0, 0]
     assert get_human_age(15, 15) == [1, 1]
@@ -16,7 +16,7 @@ def test_get_human_age():
     assert get_human_age(15, 24) == [1, 2]
 
 
-def test_convert_to_human():
+def test_convert_to_human() -> None:
     assert convert_to_human(0, 15, 9, 4) == 0
     assert convert_to_human(15, 15, 9, 4) == 1
     assert convert_to_human(24, 15, 9, 4) == 2
