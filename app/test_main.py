7531ad350f046e6ad41e85f4bc028a5e60106080
Vlad <vladk4996@gmail.com>
2025-01-10 14:55:31 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -9,16 +9,8 @@ def test_first_animals_year() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_correct_age() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
 def test_second_animal_year() -> None:
     assert get_human_age(24, 24) == [2, 2]
-
-
-def test_correct_age_after_two_years() -> None:
-    assert get_human_age(27, 27) == [2, 2]
 
 
 def test_cat_three_years() -> None:
