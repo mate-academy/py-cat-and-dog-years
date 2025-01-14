4a6a63dee8552f672c444b9b1b23b7cac4841cee
Olha Yukhnenko <pyolh36@gmail.com>
2025-01-14 15:59:12 +0200

    fix_missing_return_type_annotation

--- app/test_main.py
+++ app/test_main.py
@@ -2,31 +2,31 @@ import pytest
 from app.main import get_human_age
 
 
-def test_when_current_year_less_than_first_year():
+def test_when_current_year_less_than_first_year() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
 
-def test_when_current_year_equal_first_year():
+def test_when_current_year_equal_first_year() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_when_current_year_less_than_than_sum_of_first_and_second_year():
+def test_when_current_year_less_than_than_sum_of_first_and_second_year() -> None:
     assert get_human_age(23, 23) == [1, 1]
 
 
-def test_when_current_year_greater_than_sum_of_first_and_second_year():
+def test_when_current_year_greater_than_sum_of_first_and_second_year() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
 
-def test_when_current_years_greater_than_subtract_and_divide():
+def test_when_current_years_greater_than_subtract_and_divide() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
 
-def test_if_current_year_unusually_large():
+def test_if_current_year_unusually_large() -> None:
     assert get_human_age(100, 100) == [21, 17]
 
 
-def test_raises_type_error():
+def test_raises_type_error() -> None:
     with pytest.raises(TypeError):
         get_human_age("cat", "dog")
 
@@ -44,5 +44,5 @@ def test_raises_type_error():
 def test_various_human_age(
         each_year_cat: int,
         each_year_dog: int,
-        expected) -> None:
+        expected: int) -> None:
     assert get_human_age(each_year_cat, each_year_dog) == expected
