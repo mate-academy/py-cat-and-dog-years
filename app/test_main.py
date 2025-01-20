7ba0ec3da8914d80335fde605ad16549de971cf8
Atom-gnomov <gorbach0864213579@gmail.com>
2025-01-20 11:06:19 +0200

    complete

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,31 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_give_0_cat_dog_year() -> None:
+    cat = 0
+    dog = 0
+    assert get_human_age(cat, dog) == [0, 0]
+
+
+def test_give_20_cat_20_dog_year() -> None:
+    cat = 20
+    dog = 20
+    assert get_human_age(cat, dog) == [1, 1]
+
+
+def test_give_200_cat_200_dog_year() -> None:
+    cat = 200
+    dog = 200
+    assert get_human_age(cat, dog) == [46, 37]
+
+
+def test_give_negative_year() -> None:
+    cat = -200
+    dog = -200
+    assert get_human_age(cat, dog) == [0, 0]
+
+
+def test_cat_14_dog_23() -> None:
+    cat = 14
+    dog = 23
+    assert get_human_age(cat, dog) == [0, 1]
