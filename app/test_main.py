68bd9a22748715739819202989caba6bc30f6234
Atom-gnomov <gorbach0864213579@gmail.com>
2025-01-20 11:10:55 +0200

    complete

--- app/test_main.py
+++ app/test_main.py
@@ -1,31 +1,15 @@
+import pytest
 from app.main import get_human_age
 
-
-def test_give_0_cat_dog_year() -> None:
-    cat = 0
-    dog = 0
-    assert get_human_age(cat, dog) == [0, 0]
-
-
-def test_give_20_cat_20_dog_year() -> None:
-    cat = 20
-    dog = 20
-    assert get_human_age(cat, dog) == [1, 1]
-
-
-def test_give_200_cat_200_dog_year() -> None:
-    cat = 200
-    dog = 200
-    assert get_human_age(cat, dog) == [46, 37]
-
-
-def test_give_negative_year() -> None:
-    cat = -200
-    dog = -200
-    assert get_human_age(cat, dog) == [0, 0]
-
-
-def test_cat_14_dog_23() -> None:
-    cat = 14
-    dog = 23
-    assert get_human_age(cat, dog) == [0, 1]
+@pytest.mark.parametrize(
+    "cat_years, dog_years, expected",
+    [
+        (0, 0, [0, 0]),
+        (20, 20, [2, 2]),
+        (200, 200, [46, 37]),
+        (-200, -200, [0, 0]),
+        (14, 23, [0, 1])
+    ]
+)
+def test_get_human_age(cat_years, dog_years, expected):
+    assert get_human_age(cat_years, dog_years) == expected
