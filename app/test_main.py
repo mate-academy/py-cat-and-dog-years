86280d658a73cdb2c813a987c9cc7ba49daf1693
Atom-gnomov <gorbach0864213579@gmail.com>
2025-01-20 11:15:04 +0200

    complete

--- app/test_main.py
+++ app/test_main.py
@@ -6,7 +6,6 @@ from app.main import get_human_age
     "cat_years, dog_years, expected",
     [
         (0, 0, [0, 0]),
-        (20, 20, [2, 2]),
         (200, 200, [46, 37]),
         (-200, -200, [0, 0]),
         (14, 23, [0, 1])
