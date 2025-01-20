d4dca538a7bfd20ac51e02ab2fb3a37ecdb452d8
Atom-gnomov <gorbach0864213579@gmail.com>
2025-01-20 11:12:24 +0200

    complete

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from app.main import get_human_age
 
+
 @pytest.mark.parametrize(
     "cat_years, dog_years, expected",
     [
@@ -11,5 +12,5 @@ from app.main import get_human_age
         (14, 23, [0, 1])
     ]
 )
-def test_get_human_age(cat_years, dog_years, expected):
+def test_get_human_age(cat_years: int, dog_years: int, expected: int) -> None:
     assert get_human_age(cat_years, dog_years) == expected
