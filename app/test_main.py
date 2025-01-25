b5a537d807255f88ad0ecfd2c654cce6f5c3ee08
Andrii Sokolan <your@mail.com>
2025-01-25 22:39:20 +0200

    'Solution_2

--- app/test_main.py
+++ app/test_main.py
@@ -25,23 +25,27 @@ def test_valid_values(cat_age: int, dog_age: int, expected: list[int]) -> None:
 
 
 @pytest.mark.parametrize(
-    "cat_age, dog_age",
+    "cat_age, dog_age, expected",
     [
-        (-1, 5),
-        (5, -1),
-        (-1, -1)
+        (-1, 5, [0, 0]),
+        (5, -1, [0, 0]),
+        (-1, -1, [0, 0])
     ]
 )
-def test_negative_values(cat_age: int, dog_age: int) -> None:
-    with pytest.raises(ValueError):
-        calculate_result(cat_age, dog_age)
+def test_negative_values(
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
+) -> None:
+    result = calculate_result(cat_age, dog_age)
+    assert result == expected, f"Expected {expected}, but got {result}"
 
 
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
-        (0, 10, [0, 0]),     # Кіт = 0, собака починається з 1
-        (10, 0, [0, 0]),     # Собака = 0, кіт повертає 0
+        (0, 10, [0, 0]),
+        (10, 0, [0, 0]),
     ]
 )
 def test_edge_cases(cat_age: int, dog_age: int, expected: list[int]) -> None:
