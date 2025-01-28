7b98bde860ae6a8a9979920a718ace725f6f36a6
Ksenia Malashchuk <malashchuk.ksenia@gmail.com>
2025-01-28 22:27:45 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,31 @@
 from app.main import get_human_age
 
-# write your code here
+def test_cat_and_dog_age_zero():
+    assert get_human_age(0, 0) == [0, 0]
+
+def test_cat_age_less_than_15():
+    assert get_human_age(10, 0) == [1, 0]
+
+def test_dog_age_less_than_15():
+    assert get_human_age(0, 10) == [0, 1]
+
+def test_cat_age_between_15_and_24():
+    assert get_human_age(20, 0) == [2, 0]
+
+def test_dog_age_between_15_and_24():
+    assert get_human_age(0, 20) == [0, 2]
+
+def test_cat_age_above_24():
+    assert get_human_age(28, 0) == [3, 0]  # 15 -> 1, 9 -> 2, +4 -> 3
+    assert get_human_age(32, 0) == [4, 0]  # 15 -> 1, 9 -> 2, +8 -> 4
+
+def test_dog_age_above_24():
+    assert get_human_age(0, 29) == [0, 3]  # 15 -> 1, 9 -> 2, +5 -> 3
+    assert get_human_age(0, 34) == [0, 4]  # 15 -> 1, 9 -> 2, +10 -> 4
+
+def test_cat_and_dog_mixed_ages():
+    assert get_human_age(28, 34) == [3, 4]
+    assert get_human_age(15, 15) == [1, 1]
+
+if __name__ == "__main__":
+    pytest.main()
