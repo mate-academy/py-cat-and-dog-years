1f772d2ac341182139c87a88193a781b0a3ac1d8
Pavlo Danylkiv <danylkiv.pavlo@gmail.com>
2025-01-29 13:47:13 +0000

    improved test

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,23 @@
+import pytest
+
 from app.main import get_human_age
 
-def test_check_animal_to_human_is_zero():
-    assert get_human_age(0, 0) == [0,0]
 
-
-def test_check_animal_to_human_is_one():
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_check_animal_to_human_is_two():
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_check_cat_to_human_and_dog_to_human_are_different():
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_extra_human_years():
-    assert get_human_age(100, 100) == [21, 17]
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,expected_result",
+        [
+            (0, 0, [0, 0]),
+            (15, 15, [1, 1]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
+            (100, 100, [21, 17]),
+        ]
+    )
+    def test_animals_to_human(
+            self,
+            cat_age: int,
+            dog_age: int,
+            expected_result: list,
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected_result
