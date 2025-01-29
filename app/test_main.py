6eb28e5d389b245d464951599e762f5ed3a685cc
Tymofii Khlistik <tima0323199@gmail.com>
2025-01-29 17:24:16 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_check_when_values_are_incomplete() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_check_when_values_zero() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_check_when_values_one_year() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_should_check_when_values_not_two_years() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_check_when_values_two_years() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_should_check_when_values_not_three_years() -> None:
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_should_check_when_values_three_years() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_should_check_when_values_one_hundred_years() -> None:
+    assert get_human_age(100, 100) == [21, 17]
