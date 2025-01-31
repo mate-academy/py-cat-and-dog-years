f5272ab0434cd36051b57192a439dacbc58c1f35
Mykyta <mitina1900@gmail.com>
2025-01-31 12:18:26 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -16,7 +16,6 @@ from app.main import get_human_age
         (100, 100, [21, 17])
     ]
 )
-
 def test_get_human_age(cat_age: int,
                        dog_age: int,
                        expected_res: list
