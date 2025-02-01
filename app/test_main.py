20ceb13bee4a802396b955e41728a5dae9a801be
VolodymyrKaraniuk <volodimirkaranuk7@gmail.com>
2025-02-01 13:29:20 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,19 @@
+import pytest
+
 from app.main import get_human_age
 
 # write your code here
-def test_should_convert_first_15_years_animals_to_1_human() -> None:
-    cat_age = 15
-    dog_age = 15
-    res = get_human_age(cat_age, dog_age)
-    assert (res == [1, 1])
 
-def test_should_convert_second_9_years_animals_to_1_human() -> None:
-    cat_age = 24
-    dog_age = 24
-    res = get_human_age(cat_age, dog_age)
-    assert (res == [2, 2])
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        pytest.param(15, 15, [1,1], id="should convert first 15 years animals to 1 human"),
+        pytest.param(24, 24, [2,2], id="should convert second 9 years animals to 1 human"),
+        pytest.param(100, 100, [21,17], id="should convert every next 5 years for cat and 4 years for dog to 1 human"),
+        pytest.param(23, 23, [1, 1], id="should convert discard the remainder"),
+    ]
+)
+def test_can_sum(cat_age: int, dog_age: int, result: list) -> None:
+   assert (get_human_age(cat_age,dog_age) == result)
 
-def test_should_convert_every_next_5_years_for_cat_and_4_years_for_dog_to_1_human() -> None:
-    cat_age = 100
-    dog_age = 100
-    res = get_human_age(cat_age, dog_age)
-    assert (res == [21, 17])
-
-def test_should_convert_discard_the_remainder() -> None:
-    cat_age = 14
-    dog_age = 14
-    res = get_human_age(cat_age, dog_age)
-    assert (res == [0, 0])
-
-def test_23_cat_and_dog_years_should_convert_into_1_human_age() -> None:
-    cat_age = 23
-    dog_age = 23
-    res = get_human_age(cat_age, dog_age)
-    assert (res == [1, 1])
 
