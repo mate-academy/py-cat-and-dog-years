b2a5d0e3d047c20e453c4829077bcb99d75abc28
Volodimir <vova.lox1337@gmail.com>
2025-02-05 20:18:41 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_get_human_age() -> None:
+
+    assert get_human_age(0, 0) == [0, 0]
+    assert get_human_age(14, 14) == [0, 0]
+    assert get_human_age(15, 15) == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
+    assert get_human_age(24, 24) == [2, 2]
+    assert get_human_age(27, 27) == [3, 3]  # Очікується 3, 3
+    assert get_human_age(28, 28) == [3, 3]  # Очікується 3, 3
+    assert get_human_age(100, 100) == [21, 17]
+
+    # Тести для екстремальних значень
+    assert get_human_age(30, 30) == [4, 3]
+    assert get_human_age(50, 50) == [9, 7]
+    assert get_human_age(1000, 1000) == [246, 201]
+
+    # Тести для граничних випадків
+    assert get_human_age(15, 15) == [1, 1]
+    assert get_human_age(16, 16) == [2, 2]
+    assert get_human_age(24, 24) == [2, 2]
+    assert get_human_age(25, 25) == [3, 3]  # Очікується 3, 3
+
+    # Тести для випадків, коли вік менше 15 років
+    assert get_human_age(0, 1) == [0, 0]
+    assert get_human_age(5, 10) == [0, 0]
