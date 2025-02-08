4e617b6f88a3c8fbd87251f5739d4cb5f483d1f9
Marek Smolana <msmolana@gmail.com>
2025-02-08 18:44:49 +0100

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -26,31 +26,20 @@ def test_human_age_boundaries(
 
 
 @pytest.mark.parametrize(
-    "cat_age,dog_age,exp_exception",
+    "cat_age,dog_age",
     [
-        (14.0, 16, True),
-        (14, 16.0, True),
-        (15, "15", True),
-        ("15", 15, True),
-        (True, 15, True),
-        (15, True, True),
-        ([15], 15, True),
-        (15, [15], True),
-        ({"age": 15}, 29, True),
-        (15, {"age": 15}, True),
-        ((15), 15, True),
-        (15, (15), True),
-        (15, 15, False)
+        (15, "15"),
+        ("15", 15),
+        ([15], 15),
+        (15, [15]),
+        (15, (15, 13)),
+        ((15, 13), 15),
     ]
 )
 def test_incorrect_type_raises_exception(
     cat_age: any,
     dog_age: any,
-    exp_exception: bool
 ) -> None:
-    try:
+    with pytest.raises(TypeError) as ex_info:
         get_human_age(cat_age, dog_age)
-    except TypeError:
-        assert exp_exception
-    except Exception:
-        assert not exp_exception
+    assert ex_info != 0
