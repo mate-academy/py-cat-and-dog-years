a1c4cf7cc2684b63d9182c90ce2773e05bd8c16e
Marek Smolana <msmolana@gmail.com>
2025-02-08 18:17:21 +0100

    update based on ai mentor suggestion

--- app/test_main.py
+++ app/test_main.py
@@ -28,18 +28,18 @@ def test_human_age_boundaries(
 @pytest.mark.parametrize(
     "cat_age,dog_age,exp_exception",
     [
-        # (14.0, 16, True),
-        # (14, 16.0, True),
+        (14.0, 16, True),
+        (14, 16.0, True),
         (15, "15", True),
         ("15", 15, True),
-        # (True, 15, True),
-        # (15, True, True),
-        # ([15], 15, True),
-        # 15, [15], True),
+        (True, 15, True),
+        (15, True, True),
+        ([15], 15, True),
+        (15, [15], True),
         ({"age": 15}, 29, True),
         (15, {"age": 15}, True),
-        # ((15), 15, True),
-        # (15, (15), True),
+        ((15), 15, True),
+        (15, (15), True),
         (15, 15, False)
     ]
 )
