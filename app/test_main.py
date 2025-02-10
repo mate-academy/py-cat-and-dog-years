bb7cc52ea715d46f13ff9139b077e4ab081b4cd2
Bartolomeu <bartoszokroj@wp.pl>
2025-02-10 03:59:43 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+def test_should_return_zero_if_age_is_under_fifteen() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+def test_should_return_one_if_age_is_over_fifteen_and_under_twenty_four() -> None:
+    assert get_human_age(15, 23) == [1, 1]
+
+def test_should_return_correct_age_of_four() -> None:
+    assert get_human_age(32, 34) == [4, 4]
+
+def test_should_return_different_age() -> None:
+    assert get_human_age(28, 28) == [3, 2]
