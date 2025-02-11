93d0fb4e1565c788f6fc5663b071172010e40cae
Kateryna <kateryna.kotyh@gmail.com>
2025-02-11 13:27:50 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -17,18 +17,3 @@ from app.main import get_human_age
     ])
 def test_convert_age(cat_age, dog_age, expected) -> None:
     assert (get_human_age(cat_age, dog_age) == expected)
-
-# def test_of_zero_years() -> None:
-#     assert get_human_age(0, 0) == [0, 0]
-#
-#
-# def test_years_more_that_15() -> None:
-#     assert get_human_age(16, 16) == [1, 1]
-#
-#
-# def test_years_more_that_24() -> None:
-#     assert get_human_age(25, 25) == [2, 2]
-#
-#
-# def test_years_more_that_30() -> None:
-#     assert get_human_age(30, 31) == [3, 3]
