1f948e2d0621897a768a26c85a6b90d533a869b3
Yaroslav <yaroslav.liubarets@icloud.com>
2025-02-12 20:39:35 +0200

    Solution 1.0

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,50 @@
+import pytest
 from app.main import get_human_age
+from typing import Any
 
-# write your code here
+
+class TestConvertHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected",
+        [
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 28, [2, 2]),
+            (28, 29, [3, 3]),
+        ],
+        ids=[
+            "14 cat/dog years should convert into 0 human age.",
+            "15 cat/dog years should convert into 1 human age.",
+            "23 cat/dog years should convert into 1 human age.",
+            "24 cat/dog years should convert into 2 human age.",
+            "27/28 cat/dog years should convert into 2 human age.",
+            "28/29 cat/dog years should convert into 3 human age."
+        ]
+    )
+    def test_convert_human_age(
+        self,
+        cat_age: int,
+        dog_age: int,
+        expected: list
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected
+
+
+class TestInvalidInputType:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age",
+        [
+            (10, "string"),
+            ("string", 10),
+            ("string", "string")
+        ]
+    )
+    def test_invalid_input_type(
+        self,
+        cat_age: Any,
+        dog_age: Any
+    ) -> None:
+        with pytest.raises(TypeError):
+            get_human_age(cat_age, dog_age)
