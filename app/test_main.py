568fd08b208058049f74f2ea127ffd7ee9fbd06a
PythonToGo <pythontogoplease@gmail.com>
2025-02-13 12:30:29 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,31 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_first_fifteen_cat_years_should_give_one_human_year() -> None:
+    result = get_human_age(15, 15)
+    assert result == [1, 1]
+
+
+def test_next_nine_cat_years_should_give_one_more_human_year() -> None:
+    result = get_human_age(24, 24)
+    assert result == [2, 2]
+
+
+def test_every_four_next_cat_years_should_give_one_extra_human_year() -> None:
+    result = get_human_age(28, 28)
+    assert result == [3, 2]
+
+
+def test_first_fifteen_dog_years_should_give_one_human_year() -> None:
+    result = get_human_age(15, 15)
+    assert result == [1, 1]
+
+
+def test_next_nine_dog_years_should_give_one_more_human_year() -> None:
+    result = get_human_age(24, 24)
+    assert result == [2, 2]
+
+
+def test_every_five_next_dog_years_should_give_one_extra_human_year() -> None:
+    result = get_human_age(29, 29)
+    assert result == [3, 3]
