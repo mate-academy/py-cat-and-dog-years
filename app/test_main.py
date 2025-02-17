14a5db02e9a93d8182743d845293ee78a575dd36
Mykyta Kovalenko <mykyta.kovalenko0804@gmail.com>
2025-02-17 18:40:52 +0200

    fix the issues

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,18 @@
 from app.main import get_human_age
+import pytest
 
 
-def test_when_pets_ages_less_than_1_human_year() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_when_pet_ages_are_exactly_1_huma_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_when_pets_ages_greater_than_1_human_year() -> None:
-    assert get_human_age(22, 22) == [1, 1]
-
-
-def test_when_pets_ages_are_exactly_2_human_year() -> None:
-    assert get_human_age(25, 25) == [2, 2]
-
-
-def test_when_dog_human_age_is_lower_than_cat() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_when_pet_ages_are_100() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        pytest.param(4, 4, [0, 0], id="When pets ages = 0"),
+        pytest.param(16, 16, [1, 1], id="When pets ages = 1"),
+        pytest.param(24, 24, [2, 2], id="When pets ages = 2"),
+        pytest.param(28, 28, [3, 2], id="When cat's getting older than dogs"),
+        pytest.param(-15, 15, [0, 1], id="When one of ages is negative"),
+        pytest.param(0, 15, [0, 1], id="When one of ages = 0"),
+        pytest.param(1000, 1000, [246, 197], id="When ages are large numbers"),
+    ]
+)
+def test_pets_ages(cat_age: int, dog_age: int, result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result
