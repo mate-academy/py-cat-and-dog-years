d9df01303331c35e480145b0721db7eacbd24e19
yet92 <zyzy92187@gmail.com>
2025-02-17 17:11:03 +0200

    implement TestGetHumanAge

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,36 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,cat_human_years,dog_human_years",
+        [
+            (0, 0, 0, 0),
+            (15, 15, 1, 1),
+            (23, 23, 1, 1),
+            (24, 24, 2, 2),
+            (28, 29, 3, 3),
+            (100, 100, 21, 17),
+        ],
+        ids=[
+            "less than first limit should convert to 0",
+            "first limit should convert to 1",
+            "less than second limit should convert to 1",
+            "second limit should convert to 2",
+            "third limit should convert to 3",
+            "much bigger than second limit should convert to different"
+        ]
+    )
+    def test_convert_ages_correctly(
+            self,
+            cat_age: int,
+            dog_age: int,
+            cat_human_years: int,
+            dog_human_years: int
+    ) -> None:
+        assert get_human_age(cat_age, dog_age) == [
+            cat_human_years,
+            dog_human_years
+        ]
