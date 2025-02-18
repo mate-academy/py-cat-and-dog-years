b25f2d540b1f490cca143bb660a10c22209c0378
Oleksandr Shepel <oleksandr.sh@mate.academy>
2025-02-18 19:23:33 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_main_get_0_human_age_for_0_animal_age():
+    cat_to_human_age, dog_to_human_age = get_human_age(0, 0)
+    assert cat_to_human_age == dog_to_human_age == 0
+
+
+def test_main_get_0_human_age_for_less_than_15_animal_age():
+    cat_to_human_age, dog_to_human_age = get_human_age(14, 14)
+    assert cat_to_human_age == dog_to_human_age == 0
+
+
+def test_main_get_1_human_age_for_less_than_24_animal_age():
+    cat_to_human_age, dog_to_human_age = get_human_age(23, 23)
+    assert cat_to_human_age == dog_to_human_age == 1
+
+
+def test_main_get_2_human_age_for_less_than_28_animal_age():
+    cat_to_human_age, dog_to_human_age = get_human_age(27, 27)
+    assert cat_to_human_age == dog_to_human_age == 2
+
+
+def test_main_get_3_human_age_for_28_cat_age_and_2_human_age_for_28_dog_age():
+    cat_to_human_age, dog_to_human_age = get_human_age(28, 28)
+    assert cat_to_human_age == 3 and dog_to_human_age == 2
