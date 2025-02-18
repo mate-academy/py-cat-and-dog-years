91144aa2219134b7bc0a9ff4165968c50ed452a1
Oleksandr Shepel <oleksandr.sh@mate.academy>
2025-02-18 19:32:09 +0200

    parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,26 +1,18 @@
+import pytest
+
 from app.main import get_human_age
 
-
-def test_main_get_0_human_age_for_0_animal_age():
-    cat_to_human_age, dog_to_human_age = get_human_age(0, 0)
-    assert cat_to_human_age == dog_to_human_age == 0
-
-
-def test_main_get_0_human_age_for_less_than_15_animal_age():
-    cat_to_human_age, dog_to_human_age = get_human_age(14, 14)
-    assert cat_to_human_age == dog_to_human_age == 0
-
-
-def test_main_get_1_human_age_for_less_than_24_animal_age():
-    cat_to_human_age, dog_to_human_age = get_human_age(23, 23)
-    assert cat_to_human_age == dog_to_human_age == 1
-
-
-def test_main_get_2_human_age_for_less_than_28_animal_age():
-    cat_to_human_age, dog_to_human_age = get_human_age(27, 27)
-    assert cat_to_human_age == dog_to_human_age == 2
-
-
-def test_main_get_3_human_age_for_28_cat_age_and_2_human_age_for_28_dog_age():
-    cat_to_human_age, dog_to_human_age = get_human_age(28, 28)
-    assert cat_to_human_age == 3 and dog_to_human_age == 2
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_cat_human_age,expected_dog_human_age",
+    [
+        (0, 0, 0, 0),
+        (14, 14, 0, 0),
+        (23, 23, 1, 1),
+        (27, 27, 2, 2),
+        (28, 28, 3, 2),
+        (29, 29, 3, 3),
+    ]
+)
+def test_main_get_human_age(cat_age: int, dog_age: int, expected_cat_human_age: int, expected_dog_human_age: int) -> None:
+    actual_cat_human_age, actual_dog_human_age = get_human_age(cat_age, dog_age)
+    assert actual_cat_human_age == expected_cat_human_age and actual_dog_human_age == expected_dog_human_age
