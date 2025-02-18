024867dd5650db15a8bd4298b1ab21b756789cf6
yet92 <zyzy92187@gmail.com>
2025-02-18 14:28:27 +0200

    add tests for negative numbers and raising errors

--- app/test_main.py
+++ app/test_main.py
@@ -13,6 +13,7 @@ class TestGetHumanAge:
             (24, 24, 2, 2),
             (28, 29, 3, 3),
             (100, 100, 21, 17),
+            (-1, -1, 0, 0),
         ],
         ids=[
             "less than first limit should convert to 0",
@@ -20,7 +21,8 @@ class TestGetHumanAge:
             "less than second limit should convert to 1",
             "second limit should convert to 2",
             "third limit should convert to 3",
-            "much bigger than second limit should convert to different"
+            "much bigger than second limit should convert to different",
+            "negative values should convert to 0s",
         ]
     )
     def test_convert_ages_correctly(
@@ -34,3 +36,23 @@ class TestGetHumanAge:
             cat_human_years,
             dog_human_years
         ]
+
+    @pytest.mark.parametrize(
+        "cat_ages,dog_ages,expected_error",
+        [
+            ("", 1, TypeError),
+            (1, "", TypeError),
+        ],
+        ids=(
+            "should raise type error if 'cat_age' is not a number",
+            "should raise type error if 'dog_age' is not a number",
+        )
+    )
+    def test_raising_errors_correctly(
+            self,
+            cat_ages: str | int,
+            dog_ages: str | int,
+            expected_error: type[BaseException]
+    ) -> None:
+        with pytest.raises(expected_error):
+            get_human_age(cat_ages, dog_ages)
