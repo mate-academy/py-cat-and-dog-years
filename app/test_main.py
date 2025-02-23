f6ccbe1517e7d6627ab582a749a44fc02ca499a8
nikita-plotnytskyi <nikita.plotnitckiy@gmail.com>
2025-02-23 20:02:25 +0200

    Removed test for (24, 24, 2, 2) and assigned unique values for test names

--- app/test_main.py
+++ app/test_main.py
@@ -6,30 +6,23 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "first_year, second_year, each_year_cat, each_year_dog",
     [
-        pytest.param(0, 0, 0, 0, id="return 0 for age less than 15"),
-        pytest.param(14, 14, 0, 0, id="return 0 for age less than 15"),
-        pytest.param(15, 15, 1, 1, id="return 1 for age between 15 and 23"),
-        pytest.param(23, 23, 1, 1, id="return 1 for age between 15 and 23"),
+        pytest.param(0, 0, 0, 0, id="lower value for 0"),
+        pytest.param(14, 14, 0, 0, id="upper value for 0"),
+        pytest.param(15, 15, 1, 1, id="lower value for 1 year"),
+        pytest.param(23, 23, 1, 1, id="upper value for 1 year"),
         pytest.param(
             24,
             24,
             2,
             2,
-            id="increment human age after  24 cat years and 24 dog years"
-        ),
-        pytest.param(
-            27,
-            27,
-            2,
-            2,
-            id="increment human age after  24 cat years and 24 dog years"
+            id="lower value for 2 years and older"
         ),
         pytest.param(
             28,
             28,
             3,
             2,
-            id="increment human age after  24 cat years and 24 dog years"
+            id="different result in Cat and Dog"
         ),
         pytest.param(100, 100, 21, 17, id="correctly convert large ages"),
     ],
