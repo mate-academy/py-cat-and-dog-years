2f3cf474f9c761b61db4b651cd9e7def61a39aed
Sotnikov Oleksandr <ssotnikovvv55@gmail.com>
2025-02-26 14:54:50 +0200

    Fix: Flake8

--- app/test_main.py
+++ app/test_main.py
@@ -10,9 +10,9 @@ from app.main import get_human_age
     ]
 )
 def test_should_return_zero_for_age_below_15(
-        cat_age,
-        dog_age,
-        expected
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
@@ -25,9 +25,9 @@ def test_should_return_zero_for_age_below_15(
     ]
 )
 def test_should_return_one_for_age_between_15_and_23(
-        cat_age,
-        dog_age,
-        expected
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
@@ -40,9 +40,9 @@ def test_should_return_one_for_age_between_15_and_23(
     ]
 )
 def test_should_return_two_for_age_between_24_and_27(
-        cat_age,
-        dog_age,
-        expected
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
@@ -54,9 +54,9 @@ def test_should_return_two_for_age_between_24_and_27(
     ]
 )
 def test_should_return_different_ages_for_28_years(
-        cat_age,
-        dog_age,
-        expected
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
@@ -68,8 +68,8 @@ def test_should_return_different_ages_for_28_years(
     ]
 )
 def test_should_return_correct_age_for_large_values(
-        cat_age,
-        dog_age,
-        expected
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
