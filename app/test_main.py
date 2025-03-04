0aa5b75d69a1f3a266956bb671f9e18cd355f8bd
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-03-04 13:37:33 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,27 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, result",
+        [
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
+            (15, 15, [1, 1]),
+            (100, 100, [21, 17])
+        ]
+    )
+    def test_correct_answear(self,
+                             cat_age: int,
+                             dog_age: int,
+                             result: int) -> None:
+        assert get_human_age(cat_age, dog_age) == result
+
+    def test_length_of_result(self) -> None:
+        assert len(get_human_age(16, 16)) == 2
