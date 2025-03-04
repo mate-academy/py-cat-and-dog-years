3893929038519c743000fca1adcffb49560dd602
Marcin Djaczuk <marcindjaczuk@gmail.com>
2025-03-04 13:45:44 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -13,7 +13,6 @@ class TestGetHumanAge:
             (24, 24, [2, 2]),
             (27, 27, [2, 2]),
             (28, 28, [3, 2]),
-            (15, 15, [1, 1]),
             (100, 100, [21, 17])
         ]
     )
@@ -25,3 +24,13 @@ class TestGetHumanAge:
 
     def test_length_of_result(self) -> None:
         assert len(get_human_age(16, 16)) == 2
+
+    def test_negative_numbers(self) -> None:
+        assert get_human_age(-20,-10) == [0,0]
+
+    def test_very_big_numbers(self) -> None:
+        assert get_human_age(10000, 20000) == [2496, 3997]
+
+    def test_raise_TypeError(self):
+        with pytest.raises(TypeError):
+            get_human_age("123", 43)
