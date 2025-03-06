59d562e84c736cba6bb0e9555f8ba7bad5ac93a1
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-03-06 14:12:40 +0200

    fixed flake8

--- app/test_main.py
+++ app/test_main.py
@@ -13,5 +13,5 @@ from app.main import get_human_age
                           (100, 100, [21, 17])])
 def test_parametrize(cat_age: int,
                      dog_age: int,
-                     result: list):
+                     result: list) -> None:
     assert get_human_age(cat_age, dog_age) == result
