f796fe0134b911610c1c320cfce8166bac6b851f
Maksym Omelkov <omelkov85@gmail.com>
2025-03-06 20:49:28 +0200

    Cat and Dog years (1.0)

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,37 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_result",
+    [
+        (0, 0, [0, 0]),
+        (-1, 0, []),
+        (0, -1, []),
+        (1_000_028, 1, [250_000, 0]),
+        (1, 1_000_029, [0, 200_000]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (29, 29, [3, 3]),
+        (100, 100, [21, 17])
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int,
+                       expected_result: list[int]) -> None:
+    assert get_human_age(cat_age, dog_age) == expected_result
+
+# @pytest.mark.parametrize(
+#     "cat_age, dog_age, exception",
+#     [
+#         # ("0", 3, TypeError),
+#         # (1, "3", TypeError)
+#     ]
+# )
+# def test_get_human_age_raises_exceptions(
+#             cat_age, dog_age, exception) -> None:
+#     with pytest.raises(exception):
+#         get_human_age(cat_age, dog_age)
