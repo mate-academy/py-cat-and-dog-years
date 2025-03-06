64080d8c25f01d8ffb20c83a9f80961c39e612b2
Khrystyna Sen <anollina@ukr.net>
2025-03-06 09:20:28 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,20 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_animals_does_not_have_a_year() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_animals_younger_fifteen_years() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_animals_younger_twenty_fours_years() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_animals_before_twenty_eight_years() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_animals_after_twenty_eight_years() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_very_large_ages() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (23, 23, [1, 1]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ]
+)
+def test_animals_age_convert_to_human_age(cat_age: int,
+                                          dog_age: int,
+                                          expected: list) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
