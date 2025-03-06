87d77105f8c77a2eb4197179679db21f5f268a69
Denys Dikhtiar <dinis.dihtjar08@gmail.com>
2025-03-06 14:10:31 +0200

    fixed solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,17 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_0_human_age() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_0_human_age_but_14_cat_and_dog() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_1_human_age() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_1_human_age_but_23_cat_dog() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_2_human_age() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_2_human_age_but_27_cat_dog() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_3_human_age() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_such_a_big_age() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize("cat_age, dog_age, result",
+                         [(0, 0, [0, 0]),
+                          (14, 14, [0, 0]),
+                          (15, 15, [1, 1]),
+                          (23, 23, [1, 1]),
+                          (24, 24, [2, 2]),
+                          (27, 27, [2, 2]),
+                          (28, 28, [3, 2]),
+                          (100, 100, [21, 17])])
+def test_parametrize(cat_age: int,
+                     dog_age: int,
+                     result: list):
+    assert get_human_age(cat_age, dog_age) == result
