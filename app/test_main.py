19f4c0d28f5a005bba5d855ccde8156c2011db43
Polina Haltseva <polinayut@gmail.com>
2025-03-07 12:43:46 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_0_when_age_less_than_15() -> None:
+    assert get_human_age(13, 13) == [0, 0]
+
+
+def test_should_return_1_when_age_less_than_24() -> None:
+    assert get_human_age(22, 23) == [1, 1]
+
+
+def test_should_return_array_when_age_more_than_29() -> None:
+    assert get_human_age(30, 46) == [3, 6]
