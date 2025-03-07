42d31872f32b28d421a81ba328a8e6e5932b0513
Nikita Tkachenko <7heswatgg@gmail.com>
2025-03-07 04:06:12 +0100

    fixed flake

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,29 @@
 from app.main import get_human_age
 
-def test_should_return_zeros_when_ages_are_equals_zero():
+
+def test_should_return_zeros_when_ages_are_equals_zero() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
-def test_should_return_zeros_when_ages_less_than_fifteen():
+
+def test_should_return_zeros_when_ages_less_than_fifteen() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
-def test_should_return_zero_when_ages_are_negative():
+
+def test_should_return_zero_when_ages_are_negative() -> None:
     assert get_human_age(-2, -2) == [0, 0]
 
-def test_cat_and_dog_age_15_years():
+
+def test_cat_and_dog_age_15_years() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
-def test_cat_and_dog_age_24_years():
+
+def test_cat_and_dog_age_24_years() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
-def test_cat_and_dog_age_28_years():
+
+def test_cat_and_dog_age_28_years() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
-def test_should_work_correctly_with_unreal_number_of_years():
+
+def test_should_work_correctly_with_unreal_number_of_years() -> None:
     assert get_human_age(1000, 1500) == [246, 297]
