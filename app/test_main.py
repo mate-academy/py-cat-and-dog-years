46cfbbb18b9d9911cdaee38db80bf3dfc2e85177
Taras Pavlovskyi <taras.pavlovskyi@gmail.com>
2025-03-08 17:21:07 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,32 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_zero_age_for_pets() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 17, [0, 1]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, expected: int) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
 
 
-def test_first_element_zero_if_cat_is_young() -> None:
-    assert get_human_age(14, 17) == [0, 1]
-
-
-def test_second_element_zero_if_dog_is_young() -> None:
-    assert get_human_age(17, 14) == [1, 0]
-
-
-def test_first_element_one_if_cat_is_middle_age() -> None:
-    assert get_human_age(23, 32) == [1, 3]
-
-
-def test_second_element_one_if_dog_is_middle_age() -> None:
-    assert get_human_age(35, 22) == [4, 1]
-
-
-def test_first_element_correct_if_cat_is_old() -> None:
-    assert get_human_age(54, 19) == [9, 1]
-
-
-def test_second_element_correct_if_dog_is_old() -> None:
-    assert get_human_age(32, 100) == [4, 17]
-
-
-def test_diff_values_if_age_is_equal_and_above_27() -> None:
-    assert get_human_age(28, 28) == [3, 2]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (-1, -15, [0, 0]),
+        (1000, 1000, [246, 197]),
+        (99999, 99999, [24995, 19997])
+    ]
+)
+def test_get_human_age_edge_cases(cat_age: int, dog_age: int,
+                                  expected: int) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
