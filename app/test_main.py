67c2e810729207671819d99daa937645e091260c
Roman Snitsarenko <roman.snitsarenko@gmail.com>
2025-03-09 12:19:54 +0100

    Add parameterized tests for get_human_age function

--- app/test_main.py
+++ app/test_main.py
@@ -11,7 +11,6 @@ from app.main import get_human_age
         pytest.param(20, 20, [1, 1], id="ages 20 should return 1 1"),
         pytest.param(23, 23, [1, 1], id="ages 23 should return 1 1"),
         pytest.param(27, 28, [2, 2], id="ages 27 28 should return 2 2"),
-        pytest.param(28, 29, [3, 3], id="ages 28 29 should return 3 3"),
         pytest.param(30, 31, [3, 3], id="ages 30 31 should return 3 3"),
         pytest.param(32, 34, [4, 4], id="ages 32 34 should return 4 4"),
         pytest.param(100, 100, [21, 17], id="ages 100 should return 21 17"),
