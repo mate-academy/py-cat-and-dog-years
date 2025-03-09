ac691fab909652ce3b061088bc6da009871e3b37
Oleksandr Kurin <profysss8712@gmail.com>
2025-03-09 18:31:50 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,4 +1,14 @@
-def get_human_age(cat_age: int, dog_age: int) -> list:
+def get_human_age(cat_age: int, dog_age: int) -> list[int]:
+    """Преобразует возраст кота и собаки в человеческие годы.
+
+    Аргументы:
+    cat_age -- возраст кота в кошачьих годах
+    dog_age -- возраст собаки в собачьих годах
+
+    Возвращает:
+    Список, где первый элемент - возраст кота в человеческих годах,
+    а второй элемент - возраст собаки в человеческих годах.
+    """
     cat_to_human = convert_to_human(cat_age, 15, 9, 4)
     dog_to_human = convert_to_human(dog_age, 15, 9, 5)
     return [cat_to_human, dog_to_human]
@@ -7,6 +17,17 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
 def convert_to_human(
         animal_age: int, first_year: int, second_year: int, each_year: int
 ) -> int:
+    """Преобразует возраст животного в человеческие годы.
+
+    Аргументы:
+    animal_age -- возраст животного в годах животного
+    first_year -- количество лет за первый год
+    second_year -- количество лет за второй год
+    each_year -- количество лет за каждый следующий год
+
+    Возвращает:
+    Возраст животного в человеческих годах.
+    """
     if animal_age < first_year:
         return 0
     if animal_age < first_year + second_year:
--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,18 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_get_human_age() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+    assert get_human_age(14, 14) == [0, 0]
+    assert get_human_age(15, 15) == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
+    assert get_human_age(24, 24) == [2, 2]
+    assert get_human_age(27, 27) == [2, 2]
+    assert get_human_age(28, 28) == [3, 2]
+    assert get_human_age(100, 100) == [21, 17]
+
+
+# Запуск тестов
+if __name__ == "__main__":
+    test_get_human_age()
+    print("Все тесты пройдены успешно!")
