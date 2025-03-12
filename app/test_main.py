4e3621534f0f9d36c79b986c4bc9b29a0fb324f3
dima <giomozda74@gmail.com>
2025-03-12 13:42:56 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_zero_if_age_equal_to_zero() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_return_zero_if_age_less_15() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_one_year_if_age_less_24() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_return_two_if_age_equal_24() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_should_return_three_for_cat_and_two_for_dog_for_age_28() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_should_return_correct_value_for_age_100() -> None:
+    assert get_human_age(100, 100) == [21, 17]
