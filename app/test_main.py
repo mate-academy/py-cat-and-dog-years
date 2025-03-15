dca4796fc010c49d5c47b7eabd96235effdf2f5f
Евгений Наволоков <goustg62@gmail.com>
2025-03-15 11:00:09 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,17 @@
+import pytest
 from app.main import get_human_age
 
-def test_human_age_for_zero_years():
-    assert get_human_age(0, 0) == [0, 0]
-
-def test_human_age_before_first_human_year():
-    assert get_human_age(14, 14) == [0, 0]
-
-def test_human_age_at_first_human_year():
-    assert get_human_age(15, 15) == [1, 1]
-
-def test_human_age_before_second_human_year():
-    assert get_human_age(23, 23) == [1, 1]
-
-def test_human_age_at_second_human_year():
-    assert get_human_age(24, 24) == [2, 2]
-
-def test_human_age_before_third_human_year():
-    assert get_human_age(27, 27) == [2, 2]
-
-def test_human_age_at_third_human_year():
-    assert get_human_age(28, 28) == [3, 2]
-
-def test_human_age_large_values():
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize("cat_age, dog_age, expected", [
+    (0, 0, [0, 0]),         
+    (14, 14, [0, 0]),       
+    (15, 15, [1, 1]),       
+    (23, 23, [1, 1]),       
+    (24, 24, [2, 2]),       
+    (27, 27, [2, 2]),       
+    (28, 28, [3, 2]),       
+    (100, 100, [21, 17]),   
+    (-1, -1, [0, 0]),       
+    (1000, 1000, [196, 157])
+])
+def test_human_age(cat_age, dog_age, expected):
+    assert get_human_age(cat_age, dog_age) == expected
