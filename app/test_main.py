98ba0e42e444300611dd04c94159eb82a01f7dce
Ruslan Samsonovych <arankutakepakuta@gmail.com>
2025-03-15 20:14:07 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,23 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_years,dog_years,in_people_years_list",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ]
+)
+def test_conversion_check(
+    cat_years: int,
+    dog_years: int,
+    in_people_years_list: list[int]
+):
+    assert get_human_age(cat_years, dog_years) == in_people_years_list
