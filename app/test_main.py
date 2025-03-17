4209c7ad29cfe8e8bef0b92ada490b827ade1e68
Oleh Kozdryn <kozdryn.oleg@gmail.com>
2025-03-17 14:11:38 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+def test_get_human_age():
+    assert get_human_age(0, 0) == [0, 0]
+    assert get_human_age(15, 0) == [1, 0]
+    assert get_human_age(24, 0) == [2, 0]
+    assert get_human_age(28, 0) == [3, 0]
+    assert get_human_age(32, 0) == [4, 0]
+    assert get_human_age(36, 0) == [5, 0]
+    assert get_human_age(0, 15) == [0, 1]
+    assert get_human_age(0, 24) == [0, 2]
+    assert get_human_age(0, 29) == [0, 3]
+    assert get_human_age(0, 34) == [0, 4]
+    assert get_human_age(0, 39) == [0, 5]
+    assert get_human_age(28, 29) == [3, 3]
+    assert get_human_age(36, 39) == [5, 5]
