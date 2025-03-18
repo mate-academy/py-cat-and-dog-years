350b965e6f22a498ac533b5bb59e920774cb4e8f
Anton Bliznuk <antonbliznuk71@gmail.com>
2025-03-18 17:19:08 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,24 +1,67 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_from_0_to_14_years_are_0_in_humans_years() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_from_15_to_23_years_are_1_in_humans_years() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_from_24_to_27_years_are_2_in_humans_years() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_cats_28_years_are_3_humans_years_28_dogs_years_are_2_humans() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_cats_100_years_are_21_humans_years_100_dogs_years_are_17_humans() -> None:  # noqa: E501
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="test from 0 to 14 years are 0 in humans years"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="test from 0 to 14 years are 0 in humans years"
+        ),
+        pytest.param(
+            15,
+            15,
+            [1, 1],
+            id="test from 15 to 23 years are 1 in humans years"
+        ),
+        pytest.param(
+            23,
+            23,
+            [1, 1],
+            id="test from 15 to 23 years are 1 in humans years"
+        ),
+        pytest.param(
+            24,
+            24,
+            [2, 2],
+            id="test from 24 to 27 years are 2 in humans years"
+        ),
+        pytest.param(
+            27,
+            27,
+            [2, 2],
+            id="test from 24 to 27 years are 2 in humans years"
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="test cats 28 years are 3 humans years 28 dogs years are 2 humans"
+        ),
+        pytest.param(
+            100,
+            100,
+            [21, 17],
+            id="test cats 100 years are 21 humans years 100 dogs years are 17 humans"
+        ),
+        pytest.param(
+            -1,
+            -1,
+            [0, 0],
+            id="test age cannot be negative"
+        ),
+    ]
+)
+def test_basic_options(cat_age: int, dog_age: int, result: list) -> None:
+    assert (
+        get_human_age(cat_age, dog_age) == result
+    ), f"If cat's age is {cat_age} and dog's age is {dog_age}, result should be equal to {result}"  # noqa: E501
