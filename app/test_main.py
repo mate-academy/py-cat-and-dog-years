9157765cd947e6b00f18293463eb20c92197d570
Andrii Shtaher <andriishtaher@gmail.com>
2025-03-18 20:46:50 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        (5, 5, 0),
+        (15, 15, 1),
+        (23, 23, 1),
+        (24, 24, 2),
+        (27, 28, 2),
+        (28, 29, 3),
+    ],
+    ids=[
+        "5 cat/dog years should convert into 0 human age.",
+        "15 cat/dog years should convert into 1 human age.",
+        "23 cat/dog years should convert into 1 human age.",
+        "24 cat/dog years should convert into 2 human age.",
+        "27/28 cat/dog years should convert into 2 human age.",
+        "28/29 cat/dog years should convert into 3 human age.",
+    ]
+)
+def test_check_convert_correctly(cat_age: int, dog_age: int, result: int):
+    assert get_human_age(cat_age, dog_age) == result
