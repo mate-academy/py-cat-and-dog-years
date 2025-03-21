01f763310c26ebd0a7330e517456ca9a9378ffee
Anton Vynychenko <kinid@ukr.net>
2025-03-21 12:43:14 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,38 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_year,dog_year,result",
+    [
+        (
+            0, 0, [0, 0]
+        ),
+        (
+            14, 14, [0, 0]
+        ),
+        (
+            27, 27, [2, 2]
+        ),
+        (
+            100, 100, [21, 17]
+        ),
+    ]
+)
+def test_get_human_age(cat_year: int, dog_year: int, result: list) -> None:
+    assert get_human_age(cat_year, dog_year) == result
+
+
+@pytest.mark.parametrize(
+    "cat_year,dog_year,exception",
+    [
+        (
+            1, "1", TypeError
+        )
+    ]
+)
+def test_should_raise_correct_exception(
+        cat_year: int, dog_year: int, exception: Exception
+) -> None:
+    with pytest.raises(exception):
+        get_human_age(cat_year, dog_year)
