70e2820a681bbc500cb8cf3ecf0259f287e8f969
Dmytro Lisovyi <kenedy11@ukr.net>
2025-03-22 12:23:40 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,30 +1,16 @@
 from app.main import get_human_age
+import pytest
 
-def test_zero_age_cat_dog() -> None:
-    cat_age = 0
-    dog_age = 0
-    result = get_human_age(cat_age, dog_age)
-    assert result == [0, 0]
 
-def test_middle_age_cat_dog()-> None:
-    cat_age = 24
-    dog_age = 24
+@pytest.mark.parametrize("cat_age, dog_age, expected", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (24, 24, [2, 2]),
+    (35, 29, [4, 3]),
+])
+def test_ages(cat_age: int, dog_age: int, expected: int) -> None:
     result = get_human_age(cat_age, dog_age)
-    assert result == [2, 2]
-
-def test_near_age_cat_dog()-> None:
-    cat_age = 14
-    dog_age = 14
-    result = get_human_age(cat_age, dog_age)
-    if cat_age < 15 and dog_age < 15:
-        assert result == [0, 0]
-
-def test_different_age_cat_dog()-> None:
-    cat_age = 35
-    dog_age = 29
-    result = get_human_age(cat_age, dog_age)
-    if 32 <= cat_age <= 35 and 28 <= dog_age <= 32:
-        assert result == [4, 3]
+    assert result == expected
 
 
 # write your code here
