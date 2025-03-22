9f4dcf44d31fadb9a076227f439b7f7922ab7d68
Fallen Hero <failent.hero@gmail.com>
2025-03-22 21:51:50 +0100

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -13,28 +13,16 @@ class TestGetHumanAge:
                 id="test convert 0 animal years"
             ),
             pytest.param(
-                14.9,
-                14.9,
-                [0, 0],
-                id="test convert 14.9 animal years"
-            ),
-            pytest.param(
                 15,
                 15,
                 [1, 1],
                 id="test convert 15 animal years"
             ),
             pytest.param(
-                15.1,
-                15.1,
-                [1, 1],
-                id="test convert 15.1 animal years"
-            ),
-            pytest.param(
-                23.9,
-                23.9,
-                [1, 1],
-                id="test convert 23.9 animal years"
+                15,
+                26,
+                [1, 2],
+                id="test convert 15, 26 animal years"
             ),
             pytest.param(
                 24,
@@ -43,16 +31,10 @@ class TestGetHumanAge:
                 id="test convert 24 animal years"
             ),
             pytest.param(
-                24.1,
-                24.1,
-                [2, 2],
-                id="test convert 24.1 animal years"
-            ),
-            pytest.param(
-                27.9,
-                27.9,
-                [2, 2],
-                id="test convert 27.9 animal years"
+                36,
+                25,
+                [5, 2],
+                id="test convert 36, 25 animal years"
             ),
             pytest.param(
                 28,
@@ -65,6 +47,12 @@ class TestGetHumanAge:
                 100,
                 [21, 17],
                 id="test convert 100 animal years"
+            ),
+            pytest.param(
+                -1,
+                -15,
+                [0, 0],
+                id="test convert negative value of years"
             ),
 
         ]
