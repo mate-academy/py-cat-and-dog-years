153de9e869f38b02fac5cb0953564ac248782f37
Maxim Dovganiuk <maxim20029@gmail.com>
2025-03-27 01:55:48 +0200

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from app.main import get_human_age
 
+
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
@@ -15,17 +16,17 @@ from app.main import get_human_age
         (100, 100, [21, 17]),
     ],
 )
-def test_get_human_age(cat_age, dog_age, expected):
+def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
 
-def test_negative_ages():
+def test_negative_ages() -> None:
     with pytest.raises(ValueError):
         get_human_age(-1, 5)
     with pytest.raises(ValueError):
         get_human_age(10, -3)
 
 
-def test_float_ages():
+def test_float_ages() -> None:
     with pytest.raises(TypeError):
         get_human_age(5.5, 10.2)
