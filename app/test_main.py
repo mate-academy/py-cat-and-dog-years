613f109f6a68bc5eebc5603bc620468e585a143a
Maxim <maxim20029@gmail.com>
2025-03-27 01:51:32 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,31 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (29, 29, [3, 2]),
+        (100, 100, [21, 17]),
+    ],
+)
+def test_get_human_age(cat_age, dog_age, expected):
+    assert get_human_age(cat_age, dog_age) == expected
+
+
+def test_negative_ages():
+    with pytest.raises(ValueError):
+        get_human_age(-1, 5)
+    with pytest.raises(ValueError):
+        get_human_age(10, -3)
+
+
+def test_float_ages():
+    with pytest.raises(TypeError):
+        get_human_age(5.5, 10.2)
