9b341e51616dfdea66e65f4ba00f002ccff6d1eb
Maxim Dovganiuk <maxim20029@gmail.com>
2025-03-27 02:28:02 +0200

    Update 123test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,14 +1,21 @@
 import pytest
 from app.main import get_human_age
 
-def test_get_human_age():
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(16, 16) == [2, 2]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(25, 25) == [3, 3]
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(9, 9) == [0, 0]
-    assert get_human_age(30, 35) == [4, 5]
-    assert get_human_age(100, 100) == [21, 22]
-    assert get_human_age(10, 50) == [0, 7]
-    assert get_human_age(100, 10) == [21, 0]
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (15, 15, [1, 1]),
+        (16, 16, [2, 2]),
+        (24, 24, [2, 2]),
+        (25, 25, [3, 3]),
+        (14, 14, [0, 0]),
+        (9, 9, [0, 0]),
+        (30, 35, [4, 5]),
+        (100, 100, [21, 22]),
+        (10, 50, [0, 7]),
+        (100, 10, [21, 0])
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
