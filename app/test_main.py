a10d1d36ad0d5707161d8c07de8d0b28eae84bf7
Anastasiia <anastasiia.chikrizova@dashdevs.com>
2025-03-28 20:50:04 +0200

    finish

--- app/test_main.py
+++ app/test_main.py
@@ -8,14 +8,16 @@ from app.main import get_human_age
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
+        (23.99, 23.99, [1, 1]),
         (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
+        (27.99, 27.99, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17]),
         (29, 18, [3, 3]),
+        (100, 100, [21, 17]),
         (46, 24, [6, 2]),
     ],
 )
-def test_get_human_age(cat_age, dog_age, expected):
+
+def test_get_human_age_edge_cases(cat_age, dog_age, expected):
     assert get_human_age(cat_age, dog_age) == expected
+
