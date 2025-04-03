2e9b66f9dd5c7c2ad8c3f98539fe0ff1124f0e78
andrey <serdyukov963@gmail.com>
2025-04-03 19:40:41 +0300

    used pytest parametrize for solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -7,35 +7,45 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_years,dog_years,human_age",
     [
-        # test_14_cat_and_dog_years
         pytest.param(
             14, 14, [0, 0],
             id="For 14 cat and dog years result should be [0, 0]"
         ),
-        # test_14_cat_and_dog_years
         pytest.param(
             24, 24, [2, 2],
             id="For 24 cat and dog years result should be [2, 2]"
         ),
-        # test_14_cat_and_dog_years
         pytest.param(
             23, 23, [1, 1],
             id="For 23 cat and dog years result should be [1, 1]"
         ),
-        # test_14_cat_and_dog_years
         pytest.param(
             15, 15, [1, 1],
             id="For 15 cat and dog years result should be [1, 1]"
         ),
-        # test_14_cat_and_dog_years
         pytest.param(
             27, 28, [2, 2],
             id="For 27 cat and 28 dog years result should be [2, 2]"
         ),
-        # test_14_cat_and_dog_years
         pytest.param(
             28, 29, [3, 3],
             id="For 28 cat and 29 dog years result should be [3, 3]"
+        ),
+        pytest.param(
+            0, 0, [0, 0],
+            id="For 0 cat and dog years result should be [0, 0]"
+        ),
+        pytest.param(
+            28, 28, [3, 2],
+            id="For 28 cat and dog years result should be [3, 2]"
+        ),
+        pytest.param(
+            100, 100, [21, 17],
+            id="For 100 cat and dog years result should be [21, 17]"
+        ),
+        pytest.param(
+            -1, -1, [0, 0],
+            id="For negative cat and dog years result should be [0, 0]"
         ),
     ]
 )
