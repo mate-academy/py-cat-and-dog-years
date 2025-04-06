8b7cbeacb722e9daf938a3a417ede7a53e65d29d
Timur Khvat <timurkhvat@gmail.com>
2025-04-06 10:18:52 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,23 +1,27 @@
 from app.main import get_human_age
+import pytest
 
 
-
-def test_returns_zeroes_for_zero_ages():
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_returns_zeroes_for_ages_below_15():
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_returns_ones_for_ages_between_15_and_24():
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_calculates_next_years_correctly():
-    assert get_human_age(27, 27) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_returns_correct_human_age_for_100():
-    assert get_human_age(100, 100) == [21, 17]
+class TestGetHumanAge:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, result",
+        [
+            pytest.param(0, 0, [0, 0],
+                         id="returns zeroes for zero ages"),
+            pytest.param(14, 14, [0, 0],
+                         id="returns zeroes for ages below 15"),
+            pytest.param(23, 23, [1, 1],
+                         id="returns ones for ages between 15 and 24"),
+            pytest.param(27, 27, [2, 2],
+                         id="calculates next years correctly"),
+            pytest.param(28, 28, [3, 2],
+                         id="calculates next years correctly"),
+            pytest.param(100, 100, [21, 17],
+                         id="returns correct human age for 100"),
+        ]
+    )
+    def test_modify_class_correctly(self,
+                                    cat_age: int,
+                                    dog_age: int,
+                                    result: list) -> None:
+        assert get_human_age(cat_age, dog_age) == result
