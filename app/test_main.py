24e8bc68db6db30dd827ed70e97015edd29d23a4
Denys Halytskyi <denys.halytskyi17@gmail.com>
2025-04-07 22:40:34 +0300

    'Solution2'

--- app/test_main.py
+++ app/test_main.py
@@ -1,11 +1,13 @@
 from app.main import get_human_age
 
 
-def test_first_15_years_give_one_human_year() -> None:
+def test_zero_years() -> None:
     assert get_human_age(0, 0) == [0, 0]
+
+
+def test_first_15_years_give_one_human_year() -> None:
     assert get_human_age(14, 14) == [0, 0]
     assert get_human_age(15, 14) == [1, 0]
-    assert get_human_age(14, 15) == [0, 1]
     assert get_human_age(14, 15) == [0, 1]
     assert get_human_age(15, 15) == [1, 1]
 
@@ -20,5 +22,13 @@ def test_next_nine_years_give_one_human_year() -> None:
 def test_next_four_years_give_one_cat_and_five_to_dog() -> None:
     assert get_human_age(28, 28) == [3, 2]
     assert get_human_age(28, 29) == [3, 3]
-    assert get_human_age(28, 29) == [3, 3]
+
+
+def test_big_numbers_of_years() -> None:
     assert get_human_age(100, 100) == [21, 17]
+    assert get_human_age(1000, 1000) == [246, 197]
+
+
+def test_diferents_years() -> None:
+    assert get_human_age(0, 30) == [0, 3]
+    assert get_human_age(30, 0) == [3, 0]
