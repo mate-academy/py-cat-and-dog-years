69e3df5c031ceb0c426a77bfd1e6ff9ea556ff00
Denys Halytskyi <denys.halytskyi17@gmail.com>
2025-04-07 22:46:27 +0300

    'S'

--- app/test_main.py
+++ app/test_main.py
@@ -5,30 +5,30 @@ def test_zero_years() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_first_15_years_give_one_human_year() -> None:
-    assert get_human_age(14, 14) == [0, 0]
+def test_zero_age() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_first_human_year() -> None:
     assert get_human_age(15, 14) == [1, 0]
     assert get_human_age(14, 15) == [0, 1]
-    assert get_human_age(15, 15) == [1, 1]
 
 
-def test_next_nine_years_give_one_human_year() -> None:
-    assert get_human_age(23, 23) == [1, 1]
+def test_second_human_year() -> None:
     assert get_human_age(24, 23) == [2, 1]
     assert get_human_age(23, 24) == [1, 2]
-    assert get_human_age(24, 24) == [2, 2]
 
 
-def test_next_four_years_give_one_cat_and_five_to_dog() -> None:
+def test_additional_years() -> None:
     assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(28, 29) == [3, 3]
+    assert get_human_age(30, 35) == [3, 4]
 
 
-def test_big_numbers_of_years() -> None:
+def test_large_values() -> None:
     assert get_human_age(100, 100) == [21, 17]
     assert get_human_age(1000, 1000) == [246, 197]
 
 
-def test_diferents_years() -> None:
-    assert get_human_age(0, 30) == [0, 3]
-    assert get_human_age(30, 0) == [3, 0]
+def test_asymmetric_inputs() -> None:
+    assert get_human_age(40, 0) == [6, 0]
+    assert get_human_age(0, 40) == [0, 5]
