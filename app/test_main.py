1013146e1a8444baf70ff8bd4b83310b53719b65
Alexandr <oleksandrglushen2005@gmail.com>
2025-04-12 23:30:16 +0300

    fixed code

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,37 @@
 import pytest
-
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize(
-    "cat_age, dog_age, expected",
-    [
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
-        (28, 28, [3, 2]),
-        (100, 100, [21, 17]),
-    ]
-)
-def test_get_human_age(cat_age: int, dog_age: int, expected: int) -> None:
-    result = get_human_age(cat_age, dog_age)
-    assert result == expected
+class TestGetHumanAge:
+
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, expected",
+        [
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (15, 14, [1, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
+            (100, 100, [21, 17]),
+            (1000, 1000, [246, 197]),
+        ]
+    )
+    def test_get_human_age(self, cat_age: int, dog_age: int, expected: list) -> None:
+        result = get_human_age(cat_age, dog_age)
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        "cat_age, dog_age",
+        [
+            (-5, -5),
+            (0,0),
+            (None, None),
+            ("cat","dog")
+        ]
+    )
+    def test_invalid_input(self, cat_age: int, dog_age: int) -> None:
+        with pytest.raises((ValueError, TypeError)):
+            get_human_age(cat_age, dog_age)
