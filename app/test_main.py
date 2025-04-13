a884358c879d9a1f974efe2963f034290779d23b
Oleksandr Lytvyn <litvinalex632@gmail.com>
2025-04-14 00:39:20 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,19 +1,16 @@
+import pytest
 from app.main import get_human_age
 
-def test_if_cat_and_dog_age_are_zeros() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-def test_for_zero_human_year() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-def test_for_one_human_year() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-def test_for_two_human_years() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-def test_for_three_human_years() -> None:
-    assert get_human_age(28, 29) == [3, 3]
-
-def test_with_big_numbers() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat,dog,result",
+    [
+        pytest.param(0, 0, [0, 0], id="cat and dog age are zero"),
+        pytest.param(14, 14, [0,0], id="zero human year"),
+        pytest.param(23, 23, [1, 1], id="one human year"),
+        pytest.param(24, 24, [2, 2], id="two human years"),
+        pytest.param(28, 29, [3, 3], id="three human years"),
+        pytest.param(100, 100, [21, 17], id="big numbers")
+    ]
+)
+def test_get_human_age(cat: int, dog: int, result: list) -> None:
+    assert get_human_age(cat, dog) == result
