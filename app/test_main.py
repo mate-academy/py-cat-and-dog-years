b0853cc5627a0561f699ca2dc8256705eb31ff90
Michal-Naron <michanaron@gmail.com>
2025-04-14 12:13:36 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,13 +1,10 @@
 from app.main import get_human_age
+import pytest
 
-
-def test_age_below_14_should_be_equle_zero() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_second_condtion_how_fast_getting_older() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_third_condition_how_fast_getting_older() -> None:
-    assert get_human_age(28, 28) == [3, 2]
+@pytest.mark.parametrize("age_cat, age_dog, expected", [
+    (14, 14, [0, 0]),
+    (24, 24, [2, 2]),
+    (28, 28, [3, 2])
+])
+def test_get_human_age(age_cat, age_dog, expected) -> None:
+    assert get_human_age(age_cat, age_dog) == expected
