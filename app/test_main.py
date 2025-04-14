1791b49ab8dde06bacd0790f49521f3f0d3bac44
Michal-Naron <michanaron@gmail.com>
2025-04-14 11:47:33 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_age_below_14_should_be_equle_zero() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_second_condtion_how_fast_getting_older() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_third_condition_how_fast_getting_older() -> None:
+    assert get_human_age(28, 28) == [3, 2]
