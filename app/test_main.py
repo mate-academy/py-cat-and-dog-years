675cf3bca47446b591bc9720c4ee4458a2618edb
Ярослав Шевченко <yaroslav_0205@MacBook-Pro-Aroslav.local>
2025-04-17 11:09:06 +0300

    Cat_and_Dog_years

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,12 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_get_human_age() -> None:
+    assert get_human_age(16, 16) == [1, 1]
+    assert get_human_age(30, 30) == [3, 3]
+    assert get_human_age(31, 31) == [3, 3]
+    assert get_human_age(32, 32) == [4, 3]
+    assert get_human_age(39, 39) == [5, 5]
+    assert get_human_age(40, 40) == [6, 5]
+    assert get_human_age(45, 45) == [7, 6]
+    assert get_human_age(46, 46) == [7, 6]
