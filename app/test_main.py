3b265addac2c67c1ae0da9f0643356c2c9122f5d
Krystian Wojtiuk <krystianwwork@gmail.com>
2025-04-19 21:13:37 +0100

    v1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_return_zeros_if_zeros_given() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_return_zeros_if_age_less_than_15() -> None:
+    assert get_human_age(14, 9) == [0, 0]
+
+
+def test_return_one_if_age_less_than_24() -> None:
+    assert get_human_age(23, 15) == [1, 1]
+
+
+def test_return_two_if_age_more_than_24() -> None:
+    assert get_human_age(25, 27) == [2, 2]
+
+
+def test_return_correct_value_for_age_28() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_return_correct_value_for_age_100() -> None:
+    assert get_human_age(100, 100) == [21, 17]
