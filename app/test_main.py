5c4c5e13aca1bc27350c71445930dbb249a0ab75
Oleksandr Tarnavsky <sasha.tarnavsky@gmail.com>
2025-04-20 00:29:44 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,33 @@
 from app.main import get_human_age
 
 
-def test_zero_ages() -> None:
+def test_00() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_under_first_threshold() -> None:
+def test_14() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
 
-def test_exact_first_threshold() -> None:
+def test_15() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_under_second_threshold() -> None:
+def test_23() -> None:
     assert get_human_age(23, 23) == [1, 1]
 
 
-def test_exact_second_threshold() -> None:
+def test_24() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
 
-def test_almost_next_human_year() -> None:
+def test_27() -> None:
     assert get_human_age(27, 27) == [2, 2]
 
 
-def test_next_human_year_for_cat() -> None:
+def test_28() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
 
-def test_large_ages() -> None:
+def test_100() -> None:
     assert get_human_age(100, 100) == [21, 17]
