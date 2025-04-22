2441c8d7a65b5a3d636ae8509d3b6b49f05d9c26
Mateusz Mroz <mrozu@student.agh.edu.pl>
2025-04-22 17:55:43 +0200

    'Solution'

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,37 @@
+import pytest
+
+
 from app.main import get_human_age
 
-# write your code here
+
+class TestAgesClasses:
+
+    @pytest.mark.parametrize(
+        "dog_age, cat_age, result",
+        [pytest.param(0, 0, [0, 0]),
+         pytest.param(14, 14, [0, 0])]
+    )
+    def test_should_good_result(
+            self,
+            dog_age: int,
+            cat_age: int,
+            result: int
+    ) -> None:
+        wyn = get_human_age(dog_age, cat_age)
+        assert wyn == result
+
+    @pytest.mark.parametrize(
+        "dog_age, cat_age",
+        [
+            (14, 16),  # za mały wiek psa
+            (16, 14),  # za mały wiek kota
+            (14, 14),  # oba za małe
+        ]
+    )
+    def test_cat_and_dog_minimal_age(
+            self,
+            dog_age: int,
+            cat_age: int
+    ) -> None:
+        assert dog_age > 15, "Dog age must be greater than 15"
+        assert cat_age > 15, "Cat age must be greater than 15"
