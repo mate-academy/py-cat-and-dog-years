6a938640409bf4d1e19b487e5dffefa202ea2ecc
Kyrylo Fursov <kirfurs31@gmail.com>
2025-04-25 21:43:14 +0300

    Add unit tests for get_human_age function

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_get_human_age_cat_and_dog_young() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_get_human_age_cat_and_dog_just_one_year() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_get_human_age_cat_and_dog_two_years() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_get_human_age_cat_and_dog_three_years() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_get_human_age_cat_extra_years() -> None:
+    assert get_human_age(27, 28) == [2, 2]
+
+
+def test_get_human_age_dog_extra_years() -> None:
+    assert get_human_age(28, 29) == [3, 3]
