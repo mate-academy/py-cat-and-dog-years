257f087d1caf7b66bc2a3095c3a8f6d9f2f18698
Bogdan0101 <kononovb777@gmail.com>
2025-04-29 11:55:59 +0300

    Solution#1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,59 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+class TestMainModule:
+    @pytest.mark.parametrize(
+        "cat_age, dog_age, result",
+        [
+            pytest.param(
+                0,
+                0,
+                [0, 0],
+                id=f"Test"
+            ),
+            pytest.param(
+                14,
+                14,
+                [0, 0],
+                id=f"Test"
+            ),
+            pytest.param(
+                15,
+                15,
+                [1, 1],
+                id=f"Test"
+            ),
+            pytest.param(
+                23,
+                23,
+                [1, 1],
+                id=f"Test"
+            ),
+            pytest.param(
+                24,
+                24,
+                [2, 2],
+                id=f"Test"
+            ),
+            pytest.param(
+                27,
+                27,
+                [2, 2],
+                id=f"Test"
+            ),
+            pytest.param(
+                28,
+                28,
+                [3, 2],
+                id=f"Test"
+            ),
+            pytest.param(
+                100,
+                100,
+                [21, 17],
+                id=f"Test"
+            ),
+        ]
+    )
+    def test_module(self, cat_age, dog_age, result) -> None:
+        assert get_human_age(cat_age, dog_age) == result
