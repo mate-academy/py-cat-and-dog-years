a7bca99ba1902ab7b69b1d650d8f980e92b8f4f1
Vlad <7nomer7@gmail.com>
2025-05-06 16:00:49 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_at_least_0_years_ago():
+    assert get_human_age(0, 0) == [0, 0]
+
+def test_under_15_years_old():
+    assert get_human_age(14, 14) == [0, 0]
+
+def test_minimum_value_for_animals():
+    assert get_human_age(15, 15) == [1, 1]
+
+def test_average_value_of_years():
+    assert get_human_age(28, 28) == [3, 2]
+
+def test_maximum_value_for_animals():
+    assert get_human_age(100, 100) == [21, 17]
