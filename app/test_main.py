9516022efe6fdc13111a1fa5f6fee4428403fed2
Alex S. <aleks.shaparenko@gmail.com>
2025-05-19 13:00:56 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_for_return_type() -> None:
+    result = get_human_age(15, 15)
+    assert isinstance(result, list)
+
+
+def test_for_zero_values() -> None:
+    result = get_human_age(0, 0)
+    assert result == [0, 0]
+
+
+def test_for_results() -> None:
+    result = get_human_age(28, 28)
+    assert result == [3, 2]
