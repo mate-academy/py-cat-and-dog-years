e6ca0c7680f0917cb27d83b177df509c6782ebcb
Tetiana <yatan.shan94@gmail.com>
2025-05-21 21:13:15 -0700

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,18 +1,19 @@
 from app.main import get_human_age
 
+
 def test_if_pet_age_lower_15() -> None:
     human_age = get_human_age(14, 14)
-    assert  human_age == [0, 0]
+    assert human_age == [0, 0]
 
 
 def test_if_pet_age_between_15_and_23() -> None:
     human_age = get_human_age(23, 23)
-    assert  human_age == [1, 1]
+    assert human_age == [1, 1]
 
 
 def test_if_pet_age_27_28() -> None:
     human_age = get_human_age(27, 28)
-    assert  human_age == [2, 2]
+    assert human_age == [2, 2]
 
 
 def test_if_animal_age_is_100() -> None:
