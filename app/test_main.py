4e03de9edd14bc38b9898dde44719f028533c3e2
Kyrylo Mikushyn <mikushin98@gmail.com>
2025-05-29 13:19:27 +0200

    test for the function which calculate age,dog years

--- app/test_main.py
+++ app/test_main.py
@@ -13,6 +13,5 @@ from app.main import get_human_age
     (100, 100, [21, 17]),
 ])
 
-
-def test_get_human_age_examples(cat_age, dog_age, expected):
+def test_get_human_age_examples(cat_age: int, dog_age: int, expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
