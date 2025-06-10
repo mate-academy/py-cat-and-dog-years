5809c878d8ccacce26c7f420569b0a625458860f
Sasha <kluzodota@gmail.com>
2025-06-10 19:41:34 +0300

    fix second problem

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,16 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_dog_zero_and_cat_zero() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_first_condition() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_second_condition() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_third_condition() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_fourth_condition() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_fifth_condition() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_sixth_condition() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_seventh_condition() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize("cat_age, dog_age, expected_ages", [
+    (0, 0, [0, 0]),
+    (14, 14, [0, 0]),
+    (15, 15, [1, 1]),
+    (23, 23, [1, 1]),
+    (24, 24, [2, 2]),
+    (27, 27, [2, 2]),
+    (28, 28, [3, 2]),
+    (100, 100, [21, 17])
+])
+def test_get_human_age_valid_cases(cat_age, dog_age, expected_ages):
+    assert get_human_age(cat_age, dog_age) == expected_ages
