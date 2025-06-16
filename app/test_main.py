362a0b297055a4bdc09e9283f618c13d594b541d
Leonid Lachmann <leocyanid@gmail.com>
2025-06-16 19:40:24 +0300

    New-Perfect

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,7 @@
 import pytest
 from app.main import get_human_age
 
+
 @pytest.mark.parametrize("cat_age, dog_age, expected", [
     (0, 0, [0, 0]),
     (14, 14, [0, 0]),
@@ -33,5 +34,9 @@ from app.main import get_human_age
     (96, 96, [20, 16]),
     (100, 100, [21, 17])
 ])
-def test_get_human_age(cat_age, dog_age, expected):
+def test_get_human_age(
+    cat_age: int,
+    dog_age: int,
+    expected: list[int]
+) -> None:
     assert get_human_age(cat_age, dog_age) == expected
