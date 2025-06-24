7f62b3fdea3e02ab3b1c3149c4bd276af61d79ff
turulko-oleksandr <SashaTurulko@i.ua>
2025-06-24 12:51:13 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -7,10 +7,16 @@ from app.main import get_human_age
     "cats_age,dogs_age,expected_human_age",
     [
 
-        pytest.param(0, 0, [0, 0], id="Zeros case"),
-        pytest.param(14, 14, [0, 0], id="0 humans year case"),
-        pytest.param(15, 15, [1, 1], id="1 year case"),
-        pytest.param(24, 24, [2, 2], id="2 years case")
+        pytest.param(0, 0, [0, 0],
+                     id="0 cat/dog years should convert into 0 human age."),
+        pytest.param(14, 14, [0, 0],
+                     id="14 cat/dog years should convert into 0 human age."),
+        pytest.param(15, 15, [1, 1],
+                     id="15 cat/dog years should convert into 1 human age."),
+        pytest.param(23, 23, [1, 1],
+                     id="23 cat/dog years should convert into 1 human age."),
+        pytest.param(24, 24, [2, 2],
+                     id="24 cat/dog years should convert into 2 human age.")
 
     ]
 )
