5d0ba82e3623155ee0cef8252a2f2b6971bd2332
Anna Marchenko <marchenkoanna0464@gmail.com>
2025-07-09 20:26:36 +0200

    added-tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,17 @@
 from app.main import get_human_age
 
-# write your code here
+def test_less_than_15() -> None:
+    assert (get_human_age(0, 0) == [0, 0])
+
+def test_equal_15() -> None:
+    assert (get_human_age(15, 15) == [1, 1])
+
+def test_next_9() -> None:
+    assert (get_human_age(24, 24) == [2, 2])
+
+def test_next_5() -> None:
+    assert (get_human_age(28, 28) == [3, 2])
+
+def test_big_age() -> None:
+    assert (get_human_age(100, 100) == [21, 17])
+
