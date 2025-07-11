4273dbad9c30f0ba612b82e1fa9eef4401cf32a7
helix09825 <maksilczuk@gmail.com>
2025-07-11 09:51:48 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_get_human_age_if_zeros_was_given() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_get_human_age_first_year_end() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_get_human_age_second_year_start() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_get_human_age_second_year_end() -> None:
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_get_human_age_different_each_year() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_get_human_age_if_hundreds() -> None:
+    assert get_human_age(100, 100) == [21, 17]
