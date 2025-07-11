0bef8bc4a077cee240940071d84a9afef5b9cc8c
Oleksii Kostenikov <akotenikov@gmail.com>
2025-07-11 19:01:59 +0300

    Fix flake8 errors

--- app/test_main.py
+++ app/test_main.py
@@ -2,28 +2,29 @@ import pytest
 
 from app.main import get_human_age
 
+
 @pytest.mark.parametrize(
-  "cat_age,dog_age,expected_result",
-  [
-      (-20, -30, [0, 0]),
-      (0, 0, [0, 0]),
-      (14, 14, [0, 0]),
-      (15, 15, [1, 1]),
-      (23, 23, [1, 1]),
-      (24, 24, [2, 2]),
-      (28, 29, [3, 3]),
-      (36, 39, [5, 5]),
-  ],
-  ids=[
-      "negative number of years",
-      "cat and dog zero years old",
-      "cat and dog almost one human year",
-      "cat and dog like one human year",
-      "cat and dog almost 2 human years",
-      "cat and dog like exactly 2 human years",
-      "cat and dog like exactly 3 human years",
-      "cat and dog like exactly 5 human years",
-  ]
+    "cat_age,dog_age,expected_result",
+    [
+        (-20, -30, [0, 0]),
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (28, 29, [3, 3]),
+        (36, 39, [5, 5]),
+    ],
+    ids=[
+        "negative number of years",
+        "cat and dog zero years old",
+        "cat and dog almost one human year",
+        "cat and dog like one human year",
+        "cat and dog almost 2 human years",
+        "cat and dog like exactly 2 human years",
+        "cat and dog like exactly 3 human years",
+        "cat and dog like exactly 5 human years",
+    ]
 )
 def test_can_sum(cat_age: int, dog_age: int, expected_result: list) -> None:
-  assert get_human_age(cat_age, dog_age) == expected_result
+    assert get_human_age(cat_age, dog_age) == expected_result
