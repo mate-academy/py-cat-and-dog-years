7460bf2525911d18f6288d37e833b8382e6c266d
reinvvay <reinvvay@gmail.com>
2025-07-14 20:28:56 +0300

    Fix

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,25 @@
 from app.main import get_human_age
 
 
-def test_correct_age_calculation():
+def test_correct_age_calculation() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_age_is_zero():
+def test_age_is_zero() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_big_number():
+def test_big_number() -> None:
     assert get_human_age(416, 514) == [100, 100]
 
 
-def test_cat_is_older():
+def test_cat_is_older() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
 
-def test_dog_is_older():
+def test_dog_is_older() -> None:
     assert get_human_age(15, 28) == [1, 2]
 
 
-def test_not_enough():
+def test_not_enough() -> None:
     assert get_human_age(5, 5) == [0, 0]
