c5d71c3c6ed12b75b756c117485a45b8472e2ebf
Lev <lev01111989@gmail.com>
2025-07-16 13:39:22 +0200

    Solution

---
+++ app/test test.py
@@ -0,0 +1,22 @@
+def test_should_return_zero_for_zero_ages():
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_convert_cat_and_dog_age_under_15():
+    assert get_human_age(10, 10) == [0, 0]
+
+
+def test_should_convert_cat_and_dog_age_under_24():
+    assert get_human_age(20, 20) == [1, 1]
+
+
+def test_should_convert_cat_age_above_24():
+    assert get_human_age(39, 0) == [5, 0]
+
+
+def test_should_convert_dog_age_above_24():
+    assert get_human_age(0, 34) == [0, 4]
+
+
+def test_should_convert_both_cat_and_dog_ages():
+    assert get_human_age(28, 29) == [3, 3]
--- app/test_main.py
+++ app/test_main.py
@@ -4,7 +4,7 @@ from app.main import get_human_age
 
 
 @pytest.mark.parametrize(
-    "cat_age, dog_age, extended",
+    "cat_age, dog_age, expected",
     [
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
@@ -13,40 +13,12 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (100, 100, [21, 17]),
     ]
 )
-def test_get_human_age_parametrize(
+def test_get_human_age(
         cat_age: int,
         dog_age: int,
-        extended: list[int]
+        expected: list[int]
 ) -> None:
-    assert get_human_age(cat_age, dog_age) == extended
-
-
-def test_get_human_age_negative_value() -> None:
-    assert get_human_age(-1, -1) == [0, 0]
-    assert get_human_age(-5, 10) == [0, 0]
-    assert (get_human_age(10, -5) == [0, 0]
-            or get_human_age(10, -5) == [0, 0])
-
-
-def test_get_human_age_zero() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_get_human_age_large_numbers() -> None:
-    result = get_human_age(1000, 1000)
-    assert result == [246, 197]  # змінено очікування згідно з логікою
-    assert isinstance(result, list)
-    assert len(result) == 2
-    assert all(isinstance(x, int) for x in result)
-
-
-def test_get_human_age_type_errors() -> None:
-    with pytest.raises(TypeError):
-        get_human_age(15, None)
-
-    with pytest.raises(TypeError):
-        get_human_age(15.5, 20)
+    assert get_human_age(cat_age, dog_age) == expected
