dd0a0461c14dbb7aed4d3c2f0595fef09debb4a4
Mateusz <szanieckimateusz@gmail.com>
2025-08-05 11:43:30 +0200

    Implication of first two tests checking the values in normal cases and if not enough age

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,11 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_correct_values() -> None:
+    result = get_human_age(cat_age=21, dog_age=17)
+    assert result == [1, 1], f"Expected [1, 1], got {result}"
+
+
+def test_should_return_zeros_if_not_enough_age_to_calculate() -> None:
+    result = get_human_age(cat_age=10, dog_age=10)
+    assert result == [0, 0], f"Expected [0, 0], got {result}"
