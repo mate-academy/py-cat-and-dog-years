802e6b6298278265d52d01bf4dc1f0aff28b4a74
Dmytro <demon9709@gmail.com>
2025-08-07 22:59:11 +0300

    last

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,23 @@
-def test_cats_and_dog_age_equals_zero() -> None:
-    dog_age = 14
-    assert get_human_age(cat_age, dog_age) == [0, 0]
+from app.main import get_human_age
+import pytest
 
 
-def test_cats_and_dog_age_equals_two() -> None:
-    cat_age = 24
-    dog_age = 24
-    assert get_human_age(cat_age, dog_age) == [2, 2]
-
-
-def test_cats_and_dog_age_equals_one() -> None:
-    cat_age = 15
-    dog_age = 15
-    assert get_human_age(cat_age, dog_age) == [1, 1]
-
-
-def test_cats_and_dog_age_equals_one_2() -> None:
-    cat_age = 23
-    dog_age = 23
-    assert get_human_age(cat_age, dog_age) == [1, 1]
-
-
-def test_cats_and_dog_age_equals_three() -> None:
-    cat_age = 28
-    dog_age = 29
-    assert get_human_age(cat_age, dog_age) == [3, 3]
-
-
-def test_cats_and_dog_age_more_three() -> None:
-    cat_age = 100
-    dog_age = 100
-    assert get_human_age(cat_age, dog_age) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,human_age",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ]
+)
+def test_should_convert_right_values(
+        cat_age: int,
+        dog_age: int,
+        human_age: list[int]
+) -> None:
+    assert get_human_age(cat_age, dog_age) == human_age
