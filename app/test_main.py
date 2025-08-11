97478ed761193606c7b97a2bbc3ab24ba46ad4d9
filip <filip1folga@gmail.com>
2025-08-11 15:26:43 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -12,27 +12,44 @@ class TestBoundaryCatDogAge:
                                      14, 14, [0, 0],
                                      id="both 0 when human age 14"),
                                  pytest.param(
-                                     15, 15, [1, 1],
-                                     id="both 1 when human age 15"),
+                                     15, 0, [1, 0],
+                                     id="cat age 1 when human age 15"),
                                  pytest.param(
-                                     23, 23, [1, 1],
-                                     id="both 1 when human age 23"),
+                                     0, 15, [0, 1],
+                                     id="dog age 1 when human age 15"),
                                  pytest.param(
-                                     24, 24, [2, 2],
-                                     id="both 2 when human age 24"),
+                                     23, 0, [1, 0],
+                                     id="cat age 1 when human age 23"),
                                  pytest.param(
-                                     27, 27, [2, 2],
-                                     id="both 2 when human age 27"),
+                                     0, 23, [0, 1],
+                                     id="dog age 1 when human age 23"),
                                  pytest.param(
-                                     28, 28, [3, 2],
-                                     id="cat 3, dog 2  when human age 28"),
+                                     24, 0, [2, 0],
+                                     id="cat age 2 when human age 24"),
                                  pytest.param(
-                                     29, 29, [3, 3],
-                                     id="both 3 when human age 29"),
+                                     0, 24, [0, 2],
+                                     id="dog age 2 when human age 24"),
+                                 pytest.param(
+                                     27, 0, [2, 0],
+                                     id="cat age 2 when human age 27"),
+                                 pytest.param(
+                                     0, 27, [0, 2],
+                                     id="dog age 2 when human age 27"),
+                                 pytest.param(
+                                     28, 0, [3, 0],
+                                     id="cat age 3 when human age 28"),
+                                 pytest.param(
+                                     0, 28, [0, 2],
+                                     id="dog age 2 when human age 28"),
+                                 pytest.param(
+                                     29, 0, [3, 0],
+                                     id="cat age 3 when human age 29"),
+                                 pytest.param(
+                                     0, 29, [0, 3],
+                                     id="dog age 3 when human age 29"),
                                  pytest.param(
                                      100, 100, [21, 17],
                                      id="cat 21, dog 17  when human age 28"),
-
                              ]
                              )
     def test_get_human_age(
@@ -47,3 +64,18 @@ class TestBoundaryCatDogAge:
 def test_invalid_input_type_raises_error() -> None:
     with pytest.raises(TypeError):
         get_human_age("a", "b")
+
+
+def test_input_value_less_than_zero_raises_error() -> None:
+    with pytest.raises(ValueError):
+        get_human_age(-3, -3)
+
+
+def test_input_value_not_a_integer_raises_error() -> None:
+    with pytest.raises(TypeError):
+        get_human_age(2.5, 2.5)
+
+
+def test_input_value_unrealistic_big_raises_error() -> None:
+    with pytest.raises(ValueError):
+        get_human_age(1000, 1000)
