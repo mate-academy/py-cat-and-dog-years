f1bd5faf9b4a3d0ddc1d4b83b9dbe32bef09644f
Yaroslav Matviienko <volodimirovicaroslav7@gmail.com>
2025-08-17 18:53:48 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,22 +1,73 @@
+import pytest
+
+
 from app.main import get_human_age
 
 
-def test_should_return_zeros_if_value_is_0() -> None:
-    result = get_human_age(0, 0)
-    assert result == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,result",
+    [
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="should return zeros if value is 0"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="should return zeros if value is less than 15"
+        ),
+        pytest.param(
+            15,
+            15,
+            [1, 1],
+            id="should return 1 if age is equal to 15"
+        ),
+        pytest.param(
+            16,
+            16,
+            [1, 1],
+            id="should return whole number of age"
+        ),
+        pytest.param(
+            1000,
+            1000,
+            [246, 197],
+            id="should return correct age for big numbers"
+        )
+    ]
+)
+def test_should_correctly_konvert_age(
+        cat_age: int,
+        dog_age: int,
+        result: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == result
 
 
-def test_should_return_zeros_if_value_is_less_than_15() -> None:
-    result = get_human_age(14, 14)
-    assert result == [0, 0]
-
-
-def test_should_correctly_konvert_different_ages() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_age_should_be_a_whole_number_of_years() -> None:
-    result = get_human_age(100, 100)
-    assert result == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected_error",
+    [
+        pytest.param(
+            -1,
+            -2,
+            ValueError,
+            id="should raise 'ValueError' if age is less then 0"
+        ),
+        pytest.param(
+            "cat",
+            1.3,
+            AttributeError,
+            id="should raise 'AttributeError' if age's type is wrong"
+        )
+    ]
+)
+def test_should_correctly_raising_errors(
+        cat_age: int,
+        dog_age: int,
+        expected_error: Exception
+) -> None:
+    with pytest.raises(expected_error):
+        get_human_age(cat_age, dog_age)
