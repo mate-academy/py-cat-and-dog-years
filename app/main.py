cf4cfa5e12a453b502f015736c699210f0e75f5a
Igor Shulga <igorsulga6@gmail.com>
2025-08-23 22:48:50 +0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,14 +1,28 @@
-def get_human_age(cat_age: int, dog_age: int) -> list:
-    cat_to_human = convert_to_human(cat_age, 15, 9, 4)
-    dog_to_human = convert_to_human(dog_age, 15, 9, 5)
-    return [cat_to_human, dog_to_human]
-
-
-def convert_to_human(
-        animal_age: int, first_year: int, second_year: int, each_year: int
-) -> int:
-    if animal_age < first_year:
+def cat_years_to_human(cat_years: int) -> int:
+    if not isinstance(cat_years, int) or cat_years < 0:
+        raise ValueError("Cat years must be a non-negative integer")
+    if cat_years == 0:
         return 0
-    if animal_age < first_year + second_year:
-        return 1
-    return 2 + (animal_age - first_year - second_year) // each_year
+    elif cat_years == 1:
+        return 15
+    elif cat_years == 2:
+        return 24
+    else:
+        return 24 + (cat_years - 2) * 4
+
+
+def dog_years_to_human(dog_years: int) -> int:
+    if not isinstance(dog_years, int) or dog_years < 0:
+        raise ValueError("Dog years must be a non-negative integer")
+    if dog_years == 0:
+        return 0
+    elif dog_years == 1:
+        return 15
+    elif dog_years == 2:
+        return 24
+    else:
+        return 24 + (dog_years - 2) * 5
+
+
+def get_human_age(cat_years: int, dog_years: int) -> list[int]:
+    return [cat_years_to_human(cat_years), dog_years_to_human(dog_years)]
--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,23 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+def test_get_human_age() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+    assert get_human_age(1, 1) == [15, 15]
+    assert get_human_age(2, 2) == [24, 24]
+    assert get_human_age(3, 3) == [28, 29]
+    assert get_human_age(4, 4) == [32, 34]
+    assert get_human_age(5, 5) == [36, 39]
+    assert get_human_age(14, 14) == [72, 84]
+
+
+def test_invalid_input() -> None:
+    with pytest.raises(ValueError):
+        get_human_age(-1, 10)
+    with pytest.raises(ValueError):
+        get_human_age(10, -1)
+    with pytest.raises(ValueError):
+        get_human_age("15", 10)
+    with pytest.raises(ValueError):
+        get_human_age(10, "15")
