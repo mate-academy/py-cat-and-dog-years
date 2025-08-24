25982ec56b3e2e321b9ea7a619d1904edc67d639
Zabodashka <igorsulga6@gmail.com>
2025-08-24 14:24:45 +0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,27 +1,29 @@
 def cat_years_to_human(cat_years: int) -> int:
     if not isinstance(cat_years, int) or cat_years < 0:
         raise ValueError("Cat years must be a non-negative integer")
-    if cat_years == 0:
+
+    if cat_years <= 14:
         return 0
-    elif cat_years == 1:
-        return 15
-    elif cat_years == 2:
-        return 24
+    elif cat_years <= 23:
+        return 1
+    elif cat_years <= 27:
+        return 2
     else:
-        return 24 + (cat_years - 2) * 4
+        return 3 + (cat_years - 28) // 4
 
 
 def dog_years_to_human(dog_years: int) -> int:
     if not isinstance(dog_years, int) or dog_years < 0:
         raise ValueError("Dog years must be a non-negative integer")
-    if dog_years == 0:
+
+    if dog_years <= 14:
         return 0
-    elif dog_years == 1:
-        return 15
-    elif dog_years == 2:
-        return 24
+    elif dog_years <= 23:
+        return 1
+    elif dog_years <= 28:
+        return 2
     else:
-        return 24 + (dog_years - 2) * 5
+        return 3 + (dog_years - 29) // 5
 
 
 def get_human_age(cat_years: int, dog_years: int) -> list[int]:
