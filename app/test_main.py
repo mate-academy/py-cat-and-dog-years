337c0954632d4100d021a2aea9b02f0d73dbfd49
Oleksandr Shkliar <shklyara29@gmail.com>
2025-08-25 17:54:52 +0300

    add @pytest.mark.parametrize

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,32 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_should_be_zero_animal_age() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize(
+    "cat,dog,expected",
+    [
+        (0, 0, [0, 0]),
+        (-1, 5, [0, 0]),
+        (5, -1, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (27, 28, [2, 2]),
+        (28, 29, [3, 3]),
+        (100, 100, [21, 17]),
+        (10**6, 10**6, [249996, 199997]),
+        ("3", 4, int),
+        (3.5, 4, int),
+        (None, 2, int)
+    ]
+)
+def test_should_be_output_human_age(cat: int, dog: int, expected: int) -> None:
+    assert get_human_age(cat, dog) == expected
 
 
-def test_should_be_output_age_zero_human_age() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_should_be_output_age_one_human_age() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_should_be_output_age_one_human_age_too() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_should_be_output_age_two_human_age() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_should_be_output_age_three_human_age() -> None:
-    assert get_human_age(28, 29) == [3, 3]
+def test_should_not_be_invalid_inputs(cat: int,
+                                      dog: int,
+                                      expected: int) -> None:
+    assert (type(cat) and type(dog)) == expected
