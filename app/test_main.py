59f3a205272997703d3613a8db3a27e5c90c99e9
Oleksandr Shkliar <shklyara29@gmail.com>
2025-08-25 16:16:05 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_be_zero_animal_age() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_be_output_age_zero_human_age() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_be_output_age_one_human_age() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_should_be_output_age_one_human_age_too() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_be_output_age_two_human_age() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_should_be_output_age_three_human_age() -> None:
+    assert get_human_age(28, 29) == [3, 3]
