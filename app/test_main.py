35cdbacff5cb3a801a16c5a6b822b3acffc6244c
Arseniy Kushnir <arseniykushnir2007@gmail.com>
2025-08-25 13:38:58 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -5,29 +5,25 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, result",
     [
-        # Task Tests
-        (14, 14, [0, 0]),
+        # Values for tests
+        (20, 7, [1, 0]),
         (15.1, 15.1, [1, 1]),
-        (23, 23, [1, 1]),
         (24, 24, [2, 2]),
         (27, 28, [2, 2]),
         (28, 29, [3, 3]),
-        # My tests
-        (20, 7, [1, 0]),
-        (24, 15, [2, 1]),
-        (0, -1, [0, 0]),
-
+        (31, 33, [3, 3]),
+        (32, 34, [4, 4]),
+        (0, -1, [0, 0])
     ],
     ids=[
-        "Age cat/dog is 14",
-        "Age cat/dog is 15.1",
-        "Age cat/dog is 23",
+        "Age cat/dog is 20/7",
+        "Age cat/dog is 15.1(float type)",
         "Age cat/dog is 24",
-        "Age cat is 27, dog is 28",
-        "Age cat is 28, dog is 29",
-        "Default age",
-        "Ages is transitional value",
-        "Age cat is zero and age dog is transitional value",
+        "Age cat/dog is 27/28",
+        "Age cat/dog is 28/29",
+        "Age cat/dog is 31/33",
+        "Age cat/dog is 32/34",
+        "Age cat is Zero, age dog is negative value"
     ]
 )
 def test_get_human_age_valid_cases(cat_age: int,
