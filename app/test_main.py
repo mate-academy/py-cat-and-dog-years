9b924d15c0cfac951df50f490609efe9433047fd
slop <slopcloset@gmail.com>
2025-08-26 21:51:38 -0400

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -4,35 +4,39 @@ from typing import Any
 
 
 @pytest.mark.parametrize(
-        "cat_age, dog_age, result",
-        [
-            (0, 0, [0, 0]),
-            (14, 14, [0, 0]),
-            (15, 15, [1, 1]),
-            (23, 23, [1, 1]),
-            (24, 24, [2, 2]),
-            (27, 27, [2, 2]),
-            (28, 28, [3, 2]),
-            (29, 29, [3, 3]),
-            (31, 31, [3, 3]),
-            (32, 32, [4, 3]),
-            (33, 33, [4, 3]),
-            (34, 34, [4, 4]),
-            (35, 38, [4, 4]),
-            (36, 39, [5, 5]),
-            (39, 43, [5, 5]),
-            (40, 44, [6, 6]),
-            (16, 49, [1, 7]),
-            (44, 24, [7, 2]),
-            (416, 514, [100, 100])
-        ]
+    "cat_age, dog_age, result",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (29, 29, [3, 3]),
+        (31, 31, [3, 3]),
+        (32, 32, [4, 3]),
+        (33, 33, [4, 3]),
+        (34, 34, [4, 4]),
+        (35, 38, [4, 4]),
+        (36, 39, [5, 5]),
+        (39, 43, [5, 5]),
+        (40, 44, [6, 6]),
+        (16, 49, [1, 7]),
+        (44, 24, [7, 2]),
+        (416, 514, [100, 100])
+    ]
 )
-def test_get_human_age_valid_int(cat_age: int, dog_age: int, result: list) -> None:
+def test_get_human_age_valid_int(
+    cat_age: int,
+    dog_age: int,
+    result: list
+) -> None:
     assert get_human_age(cat_age, dog_age) == result
 
 
 @pytest.mark.parametrize(
-    "cat_age, dog_age", 
+    "cat_age, dog_age",
     [
         (-1, 5),
         (5, -1),
@@ -41,7 +45,7 @@ def test_get_human_age_valid_int(cat_age: int, dog_age: int, result: list) -> No
         (-7, 14)
     ]
 )
-def test_get_human_age_negative_ValueError(cat_age: int, dog_age: int) -> None:
+def test_get_human_age_negative_value_err(cat_age: int, dog_age: int) -> None:
     with pytest.raises(ValueError):
         get_human_age(cat_age, dog_age)
 
@@ -61,6 +65,6 @@ def test_get_human_age_negative_ValueError(cat_age: int, dog_age: int) -> None:
         (6, {"dog": 8})
     ]
 )
-def test_get_human_age_TypeError(cat_age: Any, dog_age: Any) -> None:
+def test_get_human_age_type_err(cat_age: Any, dog_age: Any) -> None:
     with pytest.raises(TypeError):
         get_human_age(cat_age, dog_age)
