334de6ac552c5af79c3312f51a079be243ff5962
vasylhladyshev404 <vasyl@nextgear.nl>
2025-09-01 12:13:04 +0200

    1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,37 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_under_fifteen() -> None:
+    assert (
+        get_human_age(0, 0) == [0, 0]
+    ), "Ages under 15 should return zeros."
+
+
+def test_1() -> None:
+    assert (
+        get_human_age(14, 14) == [0, 0]
+    ), "Ages under 15 should return zeros."
+
+
+def test_2() -> None:
+    assert (
+        get_human_age(15, 15) == [1, 1]
+    ), "Ages under 15 should return zeros."
+
+
+def test_3() -> None:
+    assert (
+        get_human_age(23, 23) == [1, 1]
+    ), "Ages under 15 should return zeros."
+
+
+def test_4() -> None:
+    assert (
+        get_human_age(24, 24) == [2, 2]
+    ), "Ages under 15 should return zeros."
+
+
+def test_5() -> None:
+    assert (
+        get_human_age(100, 100) == [21, 17]
+    ), "Ages under 15 should return zeros."
