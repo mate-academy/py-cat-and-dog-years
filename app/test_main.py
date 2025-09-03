3be42a26233cc7cbf8e702c058615c114148364b
Guilherme Felipe <guilhermeempresa158@gmail.com>
2025-09-03 19:25:52 -0300

    resolution

--- app/test_main.py
+++ app/test_main.py
@@ -12,11 +12,10 @@ import pytest
         (27, 28, [2, 2]),
         (28, 29, [3, 3]),
         (24, 29, [2, 3]),
-        (-24, 29, [0, 3]),
         (14, 29, [0, 3]),
-        (-2, 100, [0, 17]),
         (100, 100, [21, 17]),
-        (10.0, 10.0, [0, 0]),
+        pytest.param(-24, 29, [0, 3], id="negative value return zero."),
+        pytest.param(10.0, 10.0, [0, 0], id="float value return 0")
     ]
 )
 def test_logic_return_human_years(
@@ -26,3 +25,11 @@ def test_logic_return_human_years(
 ) -> None:
 
     assert get_human_age(cat_years, dog_years) == result_human_years
+
+
+def test_input_type() -> None:
+    with pytest.raises(TypeError):
+        # just input type int are accepted
+        get_human_age([10], 10)
+        get_human_age("10", 10)
+        get_human_age({10}, 10)
