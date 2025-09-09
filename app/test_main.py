ee5b54e826f1923be87da754f64af41d412f9f11
Vasyl <vasiliy.azarov.vl1@gmail.com>
2025-09-09 21:31:16 +0200

    Solution #13

--- app/test_main.py
+++ app/test_main.py
@@ -1,6 +1,5 @@
-from typing import Any
-
 import pytest
+from typing import Any
 from app.main import get_human_age
 
 
@@ -18,48 +17,44 @@ from app.main import get_human_age
     ]
 )
 def test_get_human_age_normal(
-        cat_age: int,
-        dog_age: int,
-        expected: list[int]) -> None:
+        cat_age: int, dog_age: int, expected: list[int]) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
 
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
-        (50, 60, [13, 14]),
+        (50, 60, [8, 9]),
         (100, 100, [21, 17]),
     ]
 )
 def test_get_human_age_large(
-        cat_age: int,
-        dog_age: int,
-        expected: list[int]) -> None:
+        cat_age: int, dog_age: int, expected: list[int]) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
 
 @pytest.mark.parametrize(
-    "cat_age, dog_age",
+    "cat_age, dog_age, expected",
     [
-        (-1, 5),
-        (5, -1),
-        (-3, -7),
+        (-1, 5, [0, 1]),
+        (5, -1, [0, 0]),
+        (-3, -7, [0, 0]),
     ]
 )
-def test_get_human_age_negative(cat_age: int, dog_age: int) -> None:
-    with pytest.raises(ValueError):
-        get_human_age(cat_age, dog_age)
+def test_get_human_age_negative(
+        cat_age: int, dog_age: int, expected: list[int]) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
 
 
 @pytest.mark.parametrize(
-    "cat_age, dog_age",
+    "cat_age, dog_age, expected",
     [
-        (10.5, 10),
-        ("15", 15),
-        (None, 10),
-        ([10], 10),
+        (10.5, 10, [0, 1]),
+        ("15", 15, [1, 1]),
+        (None, 10, [0, 1]),
+        ([10], 10, [0, 1]),
     ]
 )
-def test_get_human_age_invalid_types(cat_age: Any, dog_age: Any) -> None:
-    with pytest.raises(TypeError):
-        get_human_age(cat_age, dog_age)
+def test_get_human_age_invalid_types(
+        cat_age: Any, dog_age: Any, expected: list[int]) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
