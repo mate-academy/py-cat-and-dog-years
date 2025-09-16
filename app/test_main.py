0542517a50ee5faeaa4d2648d2c6fff761f5f64a
Eduardo Doerner <eduardodoerner@gmail.com>
2025-09-16 12:06:07 -0300

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -15,9 +15,18 @@ from app.main import get_human_age
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
         (100, 100, [21, 17]),
+        (14.9, 15.1, [0, 1]),
+        (-5, -15, [0, 0]),
+        (15000, 12500, [3746, 2497]),
     ]
 )
 def test_logic_of_the_function(
-    cat_age: int, dog_age: int, human_age: int
+    cat_age: int, dog_age: int, human_age: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == human_age
+
+def test_ages_are_numbers():
+    with pytest.raises(TypeError):
+        get_human_age("5", 10)
+    with pytest.raises(TypeError):
+        get_human_age(5, "10")
