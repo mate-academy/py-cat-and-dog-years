48686a59308611c26f784b8bc406ffea9caa4053
Eduardo Doerner <eduardodoerner@gmail.com>
2025-09-16 14:51:52 -0300

    Solution3

--- app/test_main.py
+++ app/test_main.py
@@ -12,22 +12,30 @@ from app.main import get_human_age
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
         (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
-        (28, 28, [3, 2]),
+        (27, 28, [2, 2]),
+        (28, 29, [3, 3]),
         (100, 100, [21, 17]),
-        (14.9, 15.1, [0, 1]),
-        (-5, -15, [0, 0]),
-        (15000, 12500, [3746, 2497]),
     ]
 )
 def test_logic_of_the_function(
     cat_age: int, dog_age: int, human_age: list[int]
 ) -> None:
-    assert get_human_age(cat_age, dog_age) == human_age
+    result = get_human_age(cat_age, dog_age)
+    assert result == human_age
+    assert isinstance(result, list)
+    assert len(result) == 2
+    assert isinstance(result[0], int)
+    assert isinstance(result[1], int)
 
 
-def test_ages_are_numbers() -> None:
+@pytest.mark.parametrize(
+    "cat, dog",
+    [
+        ("cat", "dog"),
+        ([4.5, 5, 3], 6.1),
+        (None, 6,)
+    ]
+)
+def test_ages_are_numbers(cat: int, dog: int) -> None:
     with pytest.raises(TypeError):
-        get_human_age("5", 10)
-    with pytest.raises(TypeError):
-        get_human_age(5, "10")
+        get_human_age(cat, dog)
