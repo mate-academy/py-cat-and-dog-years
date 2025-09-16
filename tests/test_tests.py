fea1d7c724453df57d3f457a3fb63a41eae17fff
Gena <gena.anzin@ukr.net>
2025-09-16 14:35:38 +0200

    'Solution'

--- tests/test_tests.py
+++ tests/test_tests.py
@@ -1,3 +1,4 @@
+import pytest
 from app import main
 from app.main import get_human_age
 
@@ -46,28 +47,3 @@ def test_ages(monkeypatch, first_year, second_year, each_year_cat, each_year_dog
     test_result = pytest.main(["app/test_main.py"])
     print(f"test_result{test_result}")
     assert test_result.value == 1
-
-
-def test_zero_years() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-def test_years_post_first_animal_first_year() -> None:
-    assert  get_human_age(14, 14) == [0, 0]
-
-def test_years_first_animal_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-def test_years_post_first_animal_second_year() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-def test_years_second_year() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-def test_years_last_time_together_two_years() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-def test_years_firs_only_cat_third_year() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-def test_years_100_years_old() -> None:
-    assert get_human_age(100, 100) == [21, 17]
