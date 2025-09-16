d5ac58874f91ed2f7350d7c5a7fa12db197dbc5f
Eduardo Doerner <eduardodoerner@gmail.com>
2025-09-16 15:22:37 -0300

    Solution3

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Any
 
 
 from app.main import get_human_age
@@ -7,14 +8,17 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, human_age",
     [
+        (-5, -20, [0, 0]),
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
         (23, 23, [1, 1]),
         (24, 24, [2, 2]),
         (27, 28, [2, 2]),
+        (28, 28, [3, 2]),
         (28, 29, [3, 3]),
         (100, 100, [21, 17]),
+        (10000, 20000, [2496, 3997]),
     ]
 )
 def test_logic_of_the_function(
@@ -32,10 +36,38 @@ def test_logic_of_the_function(
     "cat, dog",
     [
         ("cat", "dog"),
-        ([4.5, 5, 3], 6.1),
+        ([4, 5, 3], 6),
         (None, 6,)
     ]
 )
-def test_ages_are_numbers(cat: int, dog: int) -> None:
+def test_ages_are_numbers(cat: Any, dog: Any) -> None:
     with pytest.raises(TypeError):
         get_human_age(cat, dog)
+
+
+@pytest.mark.parametrize(
+    "age_before, age_limit",
+    [
+        (14, 15),
+        (23, 24),
+        (27, 28),
+    ]
+)
+def test_cat_age_limits(age_before: int, age_limit: int) -> None:
+    human_before = get_human_age(age_before, 0)[0]
+    human_limit = get_human_age(age_limit, 0)[0]
+    assert human_before != human_limit
+
+
+@pytest.mark.parametrize(
+    "age_before, age_limit",
+    [
+        (14, 15),
+        (23, 24),
+        (28, 29),
+    ]
+)
+def test_dog_age_limits(age_before: int, age_limit: int) -> None:
+    human_before = get_human_age(0, age_before)[1]
+    human_limit = get_human_age(0, age_limit)[1]
+    assert human_before != human_limit
