a7e185776005967d8a795faa7e91851e162a1f4f
Tanya <tanakutova74@gmail.com>
2025-09-17 20:47:55 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -12,8 +12,25 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (29, 29, [3, 3]),
+        (100, 100, [21, 17]),
+        (-2, -2, [0, 0])
     ]
 )
-def test_get_human_age(cat_age: int, dog_age: int, result: int) -> None:
+def test_get_human_age(cat_age: int, dog_age: int, result: list[int]) -> None:
     assert get_human_age(cat_age, dog_age) == result
+    assert isinstance(get_human_age(cat_age, dog_age), list)
+    assert all(isinstance(element, int)
+               for element in get_human_age(cat_age, dog_age))
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        (4.5, "4"),
+        (None, 6.7)
+    ]
+)
+def test_get_human_age_type_error(cat_age: int, dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
