d558529444c76223085a954a1000a8ef5ed09f18
Hrechukha-Oleksii <Oleksii_Hrechukha@outlook.com>
2025-09-21 22:17:48 +0300

    First_Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,26 @@
 from app.main import get_human_age
+import pytest
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, human_age",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ]
+)
+def test_check_correct_calculate_cat_and_dog_age_in_human_age(
+        cat_age: int,
+        dog_age: int,
+        human_age: list) -> None:
+    assert get_human_age(cat_age, dog_age) == human_age
+
+
+def test_age_in_human_age_is_integer() -> None:
+    assert all(isinstance(age, int) for age in get_human_age(100, 100))
