e1faaecb2bc4beb30314d116a2dd6109aeaa61c3
AKupriichuk <alexa05kup@gmail.com>
2025-09-22 10:54:13 +0300

    Commit 2

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 from app.main import get_human_age
 import pytest
 
@@ -15,5 +17,22 @@ import pytest
         (100, 100, [21, 17])
     ]
 )
-def test_get_human_age(cat_age : int, dog_age : int, expected : list) -> None:
+def test_get_human_age(cat_age: int, dog_age: int, expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected_exception",
+    [
+        ("cat", 10, TypeError),
+        (10.5, 10, TypeError),
+        (-1, 10, ValueError),
+    ]
+)
+def test_get_human_age_with_invalid_input(
+        cat_age: Any,
+        dog_age: Any,
+        expected_exception: TypeError | ValueError
+) -> None:
+    with pytest.raises(expected_exception):
+        get_human_age(cat_age, dog_age)
