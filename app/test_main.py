8b93587bff1c96244527a34fd4bdb69a3c504353
dmm1024 <dmn.mk.ua@gmail.com>
2025-09-22 15:48:13 +0300

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -1,34 +1,19 @@
-
-import pytest
 from app.main import get_human_age
+import pytest
+
 
 @pytest.mark.parametrize(
-    "cat_years, dog_years, expected",
-    [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-        (24, 24, [2, 2]),
-        (27, 27, [2, 2]),
-        (28, 28, [3, 2]),
-        (100, 100, [21, 17]),
-    ]
+	"cat_age, dog_age, expected",
+	[
+		(0, 0, [0, 0]),
+		(14, 14, [0, 0]),
+		(15, 15, [1, 1]),
+		(23, 23, [1, 1]),
+		(24, 24, [2, 2]),
+		(27, 27, [2, 2]),
+		(28, 28, [3, 2]),
+		(100, 100, [21, 17]),
+	]
 )
-def test_get_human_age(cat_years, dog_years, expected):
-    assert get_human_age(cat_years, dog_years) == expected
-
-@pytest.mark.parametrize(
-    "cat_years, dog_years",
-    [
-        ("a", 10),
-        (10, "b"),
-        (None, 10),
-        (10, None),
-        (1.5, 10),
-        (10, 2.7),
-    ]
-)
-def test_invalid_types(cat_years, dog_years):
-    with pytest.raises((TypeError, ValueError)):
-        get_human_age(cat_years, dog_years)
+def test_get_human_age(cat_age, dog_age, expected):
+	assert get_human_age(cat_age, dog_age) == expected
