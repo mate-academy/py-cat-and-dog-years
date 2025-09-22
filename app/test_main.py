681a146c88765c7fd6180c378e1e0b4b7ac5f4d3
dmm1024 <dmn.mk.ua@gmail.com>
2025-09-22 15:40:57 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,12 +1,34 @@
+
+import pytest
 from app.main import get_human_age
 
+@pytest.mark.parametrize(
+    "cat_years, dog_years, expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_get_human_age(cat_years, dog_years, expected):
+    assert get_human_age(cat_years, dog_years) == expected
 
-def test_should_be_equal() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(23, 23) == [1, 1]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(27, 27) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_years, dog_years",
+    [
+        ("a", 10),
+        (10, "b"),
+        (None, 10),
+        (10, None),
+        (1.5, 10),
+        (10, 2.7),
+    ]
+)
+def test_invalid_types(cat_years, dog_years):
+    with pytest.raises((TypeError, ValueError)):
+        get_human_age(cat_years, dog_years)
