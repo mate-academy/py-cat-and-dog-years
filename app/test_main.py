a901d1fcafc543dce2a7479dfac3778115d12a4f
Vasyl Cherkes <cherkesva@gmail.com>
2025-09-27 21:42:13 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -13,16 +13,22 @@ import pytest
     (28, 28, [3, 2]),
     (100, 100, [21, 17]),
 ])
-def test_get_human_age_examples(cat_age, dog_age, expected):
+def test_get_human_age_examples(
+        cat_age: int,
+        dog_age: int,
+        expected: list
+) -> None:
     assert get_human_age(cat_age, dog_age) == expected
 
-def test_cat_edge_cases():
+
+def test_cat_edge_cases() -> None:
     assert get_human_age(15, 0)[0] == 1
     assert get_human_age(24, 0)[0] == 2
     assert get_human_age(28, 0)[0] == 3
     assert get_human_age(32, 0)[0] == 4
 
-def test_dog_edge_cases():
+
+def test_dog_edge_cases() -> None:
     assert get_human_age(0, 15)[1] == 1
     assert get_human_age(0, 24)[1] == 2
     assert get_human_age(0, 29)[1] == 3
