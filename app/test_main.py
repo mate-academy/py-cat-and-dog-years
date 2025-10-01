c5cc418f18a01ae23eb4c13108b93f8b099e0fc9
Nazar <your@mail.com>
2025-10-01 20:32:31 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,13 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_when_age_lower_than_first_year() -> None:
+    assert get_human_age(0, 14) == [0, 0]
+
+
+def test_different_each_year() -> None:
+    assert get_human_age(100, 1000) == get_human_age(21, 17)
+
+
+def check_integer() -> None:
+    assert all(isinstance(x, int) for x in get_human_age(28, 28))
