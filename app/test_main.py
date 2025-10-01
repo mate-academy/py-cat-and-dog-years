4a93fc63e0d7d391446b9ff1e0fd8a8f6d675b3a
Nazar <your@mail.com>
2025-10-01 20:40:50 +0300

    Solution_fix

--- app/test_main.py
+++ app/test_main.py
@@ -5,8 +5,12 @@ def test_when_age_lower_than_first_year() -> None:
     assert get_human_age(0, 14) == [0, 0]
 
 
+def test_cat_and_dog_convert() -> None:
+    assert get_human_age(23, 0) == [0, 0]
+
+
 def test_different_each_year() -> None:
-    assert get_human_age(100, 1000) == get_human_age(21, 17)
+    assert get_human_age(100, 100) == [21, 17]
 
 
 def check_integer() -> None:
