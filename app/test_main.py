dad1b3955b2c028ef1123181636f842ad6d49b1e
Oleksandra Tymoshenko <aleksacat13@gmail.com>
2025-10-02 16:46:43 +0200

    first solution pretty please

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,30 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+class TestForWorkingCases:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,result",
+        [
+            (0, 0, [0, 0]),
+            (14, 14, [0, 0]),
+            (15, 15, [1, 1]),
+            (23, 23, [1, 1]),
+            (24, 24, [2, 2]),
+            (27, 27, [2, 2]),
+            (28, 28, [3, 2]),
+            (100, 100, [21, 17])
+        ]
+    )
+    def test_to_get_correct_result(
+            self,
+            cat_age,
+            dog_age, result
+    ):
+        assert get_human_age(cat_age, dog_age) == result
+
+
+class TestForErrors:
+    def test_incorrect_type(self):
+        with pytest.raises(TypeError):
+            get_human_age("28", "28")
