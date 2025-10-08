a5559ffeb9c0e25a17b42c94439283bc87a3b205
Oleksandr <gubar3113@gmail.com>
2025-10-08 21:06:56 +0300

    'solution_next_upd'

--- app/test_main.py
+++ app/test_main.py
@@ -21,3 +21,66 @@ def test_years_func(
         expected: list
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        [-5, 3],
+        [1, -1],
+        [-3, -3],
+    ]
+)
+def test_negative_numbers_in_input(
+        cat_age: int,
+        dog_age: int
+) -> None:
+    with pytest.raises(ValueError):
+        get_human_age(cat_age, dog_age)
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ["age_of_cat", 3],
+        [1.5, 5.5],
+        [None, 1],
+    ]
+)
+def test_wrong_types_of_input(
+        cat_age: int,
+        dog_age: int
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (15, 24, [1, 2]),
+        (0, 15, [0, 1]),
+        (28, 29, [3, 3]),
+    ]
+)
+def test_mixed_when_ages_are_different(
+        cat_age: int,
+        dog_age: int,
+        expected: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (28, 29, [3, 3]),
+        (29, 29, [3, 3]),
+    ]
+)
+def test_dog_boundaries_check_after_twenty_four(
+        cat_age: int,
+        dog_age: int,
+        expected: list
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
