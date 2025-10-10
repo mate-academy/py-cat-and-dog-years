0b01ff289932a03d6b7e87539f8effbfa56363fd
Kacper Kogut <kogutkacper7@gmail.com>
2025-10-10 21:56:58 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,25 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_animal_age_is_lower_than_first_year() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_animal_age_between_first_and_second_year() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_animal_age_is_equal_to_first_year_and_second_year() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_animal_age_is_between_second_year_and_each_year() -> None:
+    assert get_human_age(27, 28) == [2, 2]
+
+
+def test_animal_age_is_greater_than_first_year_and_second_each_year() -> None:
+    assert get_human_age(28, 29) == [3, 3]
+    assert get_human_age(100, 100) == [21, 17]
