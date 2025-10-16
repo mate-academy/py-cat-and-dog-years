9b48d87998d0afae94216261e5ff456b4b7de301
Maksym Dazidov <maksymdazidov@gmail.com>
2025-10-16 17:29:10 +0200

    tests 1

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
+import pytest
 from app.main import get_human_age
 
 # write your code here
+def test_zero_ages() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_large_ages() -> None:
+    assert get_human_age(200, 200) == [46, 37]
+
+def test_low_ages() -> None:
+    assert get_human_age(15, 17) == [1, 1]
+
+
+def test_without_the_remained() -> None:
+    assert get_human_age(5, 30) == [0, 3]
+
+
+def test_non_integer_input() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("15", "15")
