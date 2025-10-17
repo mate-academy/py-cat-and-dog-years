875e8f9a4ac53fa660580166d33a284a488a3393
Arkadiusz Bałakier <arkadiusz.balakier@gmail.com>
2025-10-17 11:32:32 +0200

    adding more tests

--- app/test_main.py
+++ app/test_main.py
@@ -1,5 +1,6 @@
 import pytest
 
+
 from app.main import get_human_age
 
 
@@ -13,7 +14,15 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
-        (100, 100, [21, 17])
+        (29, 29, [3, 3]),
+        (30, 30, [3, 3]),
+        (31, 33, [3, 3]),
+        (100, 100, [21, 17]),
+        (15, 24, [2, 1]),
+        (24, 15, [1, 2]),
+        (1000, 1000, [246, 197]),
+        (-1, -1, [0, 0]),
+        (-100, -100, [0, 0]),
     ]
 )
 def test_get_human_age(
@@ -22,3 +31,13 @@ def test_get_human_age(
         expected: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+def test_get_human_age_get_value_error_for_string() -> None:
+    with pytest.raises(ValueError):
+        get_human_age("100", 100)
+
+
+def test_get_human_age_get_value_error_for_float() -> None:
+    with pytest.raises(ValueError):
+        get_human_age(1.1, 1.1)
