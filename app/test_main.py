3e1da9fd8ba85e7cdef33b42fc727e575f79c217
AlmostVictor <zhovtanetskyj.victor@gmail.com>
2025-10-19 20:19:21 +0300

    fix2

--- app/test_main.py
+++ app/test_main.py
@@ -1,4 +1,5 @@
 import pytest
+from typing import Any
 from app.main import get_human_age
 
 
@@ -10,6 +11,7 @@ from app.main import get_human_age
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
+        (15, 24, [1, 2]),
         (23, 23, [1, 1]),
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
@@ -18,17 +20,21 @@ from app.main import get_human_age
     ]
 )
 def test_can_get_human_age(cat_age: int, dog_age: int, result: list) -> None:
-    assert get_human_age(cat_age, dog_age) == result
+    expected = get_human_age(cat_age, dog_age)
+    assert (isinstance(expected, list),
+            len(expected) == 2,
+            expected == result)
 
 
 @pytest.mark.parametrize(
     "cat_age, dog_age",
     [
-        ("7", None),
+        (7, "8"),
+        ("7", 8),
         ([2], (5, 3)),
         ({1}, {2: 6})
     ]
 )
-def test_invalid_types(cat_age, dog_age) -> None:
+def test_invalid_types(cat_age: Any, dog_age: Any) -> None:
     with pytest.raises(TypeError):
         get_human_age(cat_age, dog_age)
