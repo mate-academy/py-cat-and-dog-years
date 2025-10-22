6d76d491f159d6b1c46bf925da9d42f7ba74a10c
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2025-10-22 16:01:42 +0200

    solution2

--- app/test_main.py
+++ app/test_main.py
@@ -19,22 +19,22 @@ from app.main import get_human_age
             id="check zeros when no zero ages"
         ),
         pytest.param(
+            15,
+            15,
+            [1, 1],
+            id="check human year - 1, when years are 15"
+        ),
+        pytest.param(
             23,
             23,
             [1, 1],
-            id="check human year - 1, when years are 15"
+            id="check human year - 1, when years are 23"
         ),
         pytest.param(
             24,
             24,
             [2, 2],
             id="check human year - 2, when years are 24"
-        ),
-        pytest.param(
-            28,
-            24,
-            [3, 2],
-            id="check correct cat - human years"
         ),
         pytest.param(
             28,
@@ -56,3 +56,29 @@ def test_get_human_age(
         expected: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected",
+    [
+        pytest.param(
+            "0",
+            0,
+            [0, 0],
+            id="check error when cat years is str"
+        ),
+        pytest.param(
+            14,
+            "14",
+            [0, 0],
+            id="check error when dog years is str"
+        ),
+    ]
+)
+def test_error_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected: list[[int]]
+) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
