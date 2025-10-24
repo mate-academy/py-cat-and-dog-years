ae8510cde2924ad9379f9075a564903e78a8a539
Anastasiia Pohonets <29karasik@gmail.com>
2025-10-24 11:27:05 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,22 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_zero_ages() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_first_group_boundary() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_ages_bigger_than_first_year() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+    assert get_human_age(24, 24) == [2, 2]
+    assert get_human_age(27, 27) == [2, 2]
+    assert get_human_age(28, 28) == [3, 2]
+    assert get_human_age(29, 29) == [3, 3]
+
+
+def test_large_values() -> None:
+    assert get_human_age(100, 100) == [21, 17]
