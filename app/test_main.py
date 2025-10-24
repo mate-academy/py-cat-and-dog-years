27467da16b9b5f8f93fff022425bd99abe42d468
Vladislav <grechkosij.vlados@gmail.com>
2025-10-24 18:07:28 +0300

    Fix6

--- app/test_main.py
+++ app/test_main.py
@@ -1,18 +1,58 @@
-import pytest
 from app.main import get_human_age
+import pytest
 
 
-@pytest.mark.parametrize("cat_age, dog_age, result", [
-    (0, 0, [0, 0]),
-    (14, 14, [0, 0]),
-    (15, 15, [1, 1]),
-    (23, 23, [1, 1]),
-    (24, 24, [2, 2]),
-    (27, 27, [2, 2]),
-    (28, 28, [3, 2]),
-    (100, 100, [21, 17]),
-    (-1, -5, [0, 0])
-])
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        pytest.param(
+            0, 0, [0, 0], id="cat and dog zero ages should return zeroes"
+        ),
+        pytest.param(
+            -2, -5, [0, 0], id="cat and dog negative ages should return zeroes"
+        ),
+        pytest.param(
+            15,
+            -5,
+            [1, 0],
+            id="cat only positive age should return zero for dog",
+        ),
+        pytest.param(
+            0,
+            29,
+            [0, 3],
+            id="dog only positive age should return zero for cat",
+        ),
+        pytest.param(
+            27,
+            27,
+            [2, 2],
+            id="cat and dog positive ages should return both positive",
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="cat and dog less than one human year should return zeroes",
+        ),
+        pytest.param(
+            15, 15, [1, 1], id="cat and dog one human year should return ones"
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="cat and dog different calculation \
+            should return different human ages",
+        ),
+        pytest.param(
+            4532,
+            5700,
+            [1129, 1137],
+            id="cat and dog big ages should return correct values",
+        ),
+    ],
+)
 def test_should_return_elements(
         cat_age: int,
         dog_age: int,
@@ -21,6 +61,19 @@ def test_should_return_elements(
     assert get_human_age(cat_age, dog_age) == result
 
 
-def test_called_error() -> None:
-    with pytest.raises(TypeError):
-        get_human_age("a", "b")
+@pytest.mark.parametrize(
+    "cat_age, dog_age, exception",
+    [
+        pytest.param(
+            "4", 5, TypeError, id="should raise error if data is string"
+        ),
+        pytest.param(
+            None, None, TypeError, id="should raise error if no data"
+        ),
+    ],
+)
+def test_called_error(
+        cat_age: str | None, dog_age: int | None, exception: type(Exception)
+) -> None:
+    with pytest.raises(exception):
+        get_human_age(cat_age, dog_age)
