59f71b2d2cecf52b240177c7d5c8dee5786cb0d7
venlivan <venlivandovskyi@gmail.com>
2025-10-26 19:00:52 +0100

    used 'parametrize'

--- app/test_main.py
+++ app/test_main.py
@@ -1,24 +1,21 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_returns_zero_for_ages_less_than_15() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_returns_one_for_ages_between_15_and_23()-> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_returns_two_for_ages_between_24_and_27() -> None:
-    assert get_human_age(24, 27) == [2, 2]
-
-
-def test_returns_two_for_ages_between_28_and_31()-> None:
-    assert get_human_age(28, 31) == [3, 3]
-
-def test_returns_different_values_for_cat_and_dog()-> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_returns_three_for_large_ages()-> None:
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (15, 15, [1, 1]),
+        (24, 27, [2, 2]),
+        (28, 31, [3, 3]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ],
+)
+def test_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
+) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
