d1188b77fdadb72b9e436a89773b83e94dfd988d
VadymVolynko <world.ow1@ukr.net>
2025-10-28 11:56:55 +0200

    my work version1

--- app/main.py
+++ app/main.py
@@ -1,14 +1,24 @@
-def get_human_age(cat_age: int, dog_age: int) -> list:
-    cat_to_human = convert_to_human(cat_age, 15, 9, 4)
-    dog_to_human = convert_to_human(dog_age, 15, 9, 5)
-    return [cat_to_human, dog_to_human]
+from typing import List
 
 
-def convert_to_human(
-        animal_age: int, first_year: int, second_year: int, each_year: int
-) -> int:
+def convert_to_human(animal_age: int, first_year: int,
+                     second_year: int, each_year: int) -> int:
     if animal_age < first_year:
         return 0
     if animal_age < first_year + second_year:
         return 1
     return 2 + (animal_age - first_year - second_year) // each_year
+
+
+def get_human_age(cat_age: int, dog_age: int) -> List[int]:
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+        raise TypeError("Вік повинен бути цілим числом")
+    if cat_age < 0 or dog_age < 0:
+        raise ValueError("Вік повинен бути додатнім числом")
+
+    cat_human = convert_to_human(cat_age, first_year=15,
+                                 second_year=9, each_year=4)
+    dog_human = convert_to_human(dog_age, first_year=15,
+                                 second_year=9, each_year=5)
+
+    return [cat_human, dog_human]
