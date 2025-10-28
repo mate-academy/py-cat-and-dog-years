1cea9371ca44a0190892f574d0f3175308c2d2c6
Kostya <HeineRx2@gmail.com>
2025-10-28 22:14:29 +0300

    Done2

--- app/test_main.py
+++ app/test_main.py
@@ -1,20 +1,25 @@
 from app.main import get_human_age
 
-def test_func_return_basic_value():
+
+def test_func_return_basic_value() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
-def test_first_up_age():
+
+def test_first_up_age() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
-def test_age_low_15():
+
+def test_age_low_15() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
-def test_age_high_15():
+
+def test_age_high_15() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
-def test_age_low_dog_age():
+
+def test_age_low_dog_age() -> None:
     assert get_human_age(28, 24) == [3, 2]
 
-def test_human_age_100():
-    assert get_human_age(100, 100) == [21, 17]
 
+def test_human_age_100() -> None:
+    assert get_human_age(100, 100) == [21, 17]
