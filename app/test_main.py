f843fbb3867b9d9301c8cad570ccf907f8e8f59f
Artur <arturvovk780@gmail.com>
2025-10-31 15:41:57 +0200

    fix refactor

--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,19 @@
 from app.main import get_human_age
+import pytest
 
 
-def test_should_return_zero_for_zero_age() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_should_return_zero_for_14() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_should_return_one_for_15() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_should_return_one_for_23() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_should_return_two_for_24() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_should_return_two_for_27() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_should_return_three_and_two_for_28() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_should_return_twenty_one_and_seventeen_for_100() -> None:
-    assert get_human_age(28, 28) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, expected: list[int]) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
