9d0698793de5e33f19654eaa5d007bbfac977016
Sasha <alex.acmate@gmail.com>
2025-10-31 13:55:19 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,32 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+class TestPyCatAndDogYears:
+    @pytest.mark.parametrize(
+        "cat_age,dog_age,result_list",
+        [
+            (
+                14,
+                14,
+                [0, 0]
+            ),
+            (
+                15,
+                15,
+                [1, 1]
+            ),
+            (
+                28,
+                28,
+                [3, 2]
+            ),
+            (
+                0,
+                0,
+                [0, 0]
+            ),
+        ]
+    )
+    def test_py_cat_and_dog_years(self, cat_age: int, dog_age: int, result_list: list) -> None:
+        assert get_human_age(cat_age, dog_age) == result_list
