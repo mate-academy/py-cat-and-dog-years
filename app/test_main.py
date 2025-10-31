b627dcde3b0cdcfc39b060057b17d4622f08b578
Artur <arturvovk780@gmail.com>
2025-10-31 15:44:34 +0200

    fix line to long

--- app/test_main.py
+++ app/test_main.py
@@ -15,5 +15,9 @@ import pytest
         (100, 100, [21, 17]),
     ]
 )
-def test_get_human_age(cat_age: int, dog_age: int, expected: list[int]) -> None:
+def test_get_human_age(
+        cat_age: int,
+        dog_age: int,
+        expected: list[int]
+) -> None:
     assert get_human_age(cat_age, dog_age) == expected
