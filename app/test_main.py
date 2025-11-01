00230eef9504468a1b5c233920e969018ce6ebd5
Stas Sytnykov <stassytnykov@gmail.com>
2025-11-01 19:21:10 +0200

    fix: cover more edge cases by tests

--- app/test_main.py
+++ app/test_main.py
@@ -11,6 +11,7 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, expected",
     [
+        (-10, -21, [0, 0]),
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
@@ -25,3 +26,7 @@ def test_get_human_age(
         cat_age: int, dog_age: int, expected: List[int]) -> None:
     """Test get_human_age for cats and dogs with multiple scenarios."""
     assert get_human_age(cat_age, dog_age) == expected
+
+def test_should_raise_exception_for_invalid_values() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("ten", "nine")
