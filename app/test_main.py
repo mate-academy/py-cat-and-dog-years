98697f33f9ebf0d73de5d6caeea7775ea6eebf48
viloker <yarmolykvadym@gmail.com>
2025-11-03 20:06:17 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -6,10 +6,12 @@ import pytest
     "cat_age,dog_age,expected",
     [
         pytest.param(0, 0, [0, 0]),
-        pytest.param(14, 14, [0, 0]),
+        pytest.param(15, 15, [1, 1]),
+        pytest.param(16, 16, [1, 1]),
         pytest.param(23, 23, [1, 1]),
         pytest.param(24, 24, [2, 2]),
         pytest.param(55, 55, [9, 8]),
+        pytest.param(66.6, 77.7, [12.0, 12.0]),
         pytest.param(100, 100, [21, 17])
     ]
 )
@@ -18,3 +20,4 @@ def test_get_human_age(cat_age: int,
                        expected: list[int]) -> None:
     result = get_human_age(cat_age, dog_age)
     assert result == expected
+
