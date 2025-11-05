6371b90f4d3cfb74594d7961681454ea5950fc8d
Anton Tsikhanovich <alluspeh@gmail.com>
2025-11-05 23:52:04 +0100

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -9,10 +9,14 @@ class TestGetHumanAge:
             pytest.param(0, 0, [0, 0],
                          id="should return zero when age is equal zero"),
             pytest.param(14, 14, [0, 0],
-                         id="should return 0 when age is less than 15"),
+                         id="should return 0 "
+                            "when age is less than first year"),
             pytest.param(15, 15, [1, 1],
                          id="should return 1 if animal age "
-                            "is less than sum of two years"),
+                            "is equal to first year"),
+            pytest.param(23, 23, [1, 1],
+                         id="should return 1 "
+                            "if animal age less than sum of two years"),
             pytest.param(24, 24, [2, 2],
                          id="should return 2 if animal age "
                             "is equal to sum of two years"),
@@ -20,6 +24,8 @@ class TestGetHumanAge:
                          id="should add 2 to when animal age - years is 0"),
             pytest.param(28, 28, [3, 2],
                          id="should return different resuld for dog and cat"),
+            pytest.param(100, 100, [21, 17],
+                         id="should return correct result with huge numbers"),
         ]
     )
     def test_get_human_age(self,
@@ -27,3 +33,8 @@ class TestGetHumanAge:
                            dog_age: int,
                            expected: list) -> None:
         assert get_human_age(cat_age, dog_age) == expected
+
+
+def test_get_error_if_incorrect_type() -> None:
+    with pytest.raises(TypeError):
+        get_human_age(1, "1")
