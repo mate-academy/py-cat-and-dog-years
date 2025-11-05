859bd68f427bd3f139a149cfd9514721f5d3e63e
Oleh Bozhak <olbozhak@gmail.com>
2025-11-05 18:12:25 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -19,23 +19,30 @@ class TestCatAndDogs:
                 23, 23, [1, 1],
             ),
             (
-                24, 24, [2, 2]
+                24, 24, [2, 2],
             ),
             (
-                27, 27, [2, 2]
+                27, 27, [2, 2],
             ),
             (
-                28, 28, [3, 2]
+                28, 28, [3, 2],
             ),
             (
-                100, 100, [21, 17]
-            )
+                100, 100, [21, 17],
+            ),
+            (
+                -10, -5, [0, 0],
+            ),
         ]
     )
     def test_check_cat_and_dogs_year(
             self,
             cat_age: int,
             dog_age: int,
-            human_age: int
+            human_age: int,
     ) -> None:
         assert get_human_age(cat_age, dog_age) == human_age
+
+    def test_convert_age_invalid_type(self) -> None:
+        with pytest.raises(TypeError):
+            get_human_age("asdasd", -5)
