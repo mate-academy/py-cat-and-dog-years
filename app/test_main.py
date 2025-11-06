f0457d5e1aa75b13c83e1d3666a0702704bcaf78
Siarhei Nestsiaronak <nesterka375@gmail.com>
2025-11-06 21:13:25 +0100

    solved py-cat-and-dog-years

--- app/test_main.py
+++ app/test_main.py
@@ -1,12 +1,36 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_get_human_age() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-    assert get_human_age(14, 14) == [0, 0]
-    assert get_human_age(15, 15) == [1, 1]
-    assert get_human_age(23, 23) == [1, 1]
-    assert get_human_age(24, 24) == [2, 2]
-    assert get_human_age(27, 27) == [2, 2]
-    assert get_human_age(28, 28) == [3, 2]
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        (-1, -3, [0, 0]),
+    ],
+    ids=[
+        "get_human_age_0",
+        "get_human_age_14",
+        "get_human_age_15",
+        "get_human_age_23",
+        "get_human_age_24",
+        "get_human_age_27",
+        "get_human_age_28",
+        "get_human_age_100",
+        "get_human_age_negative",
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result
+
+
+def test_get_human_age_for_str() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("2", "3")
