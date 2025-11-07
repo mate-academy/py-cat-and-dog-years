8dfddcdf9573a4b38c80c9facd950a60b041c767
artem <artemasarkisan598@gmail.com>
2025-11-07 16:29:17 +0200

    py-Cat and dog years

--- app/test_main.py
+++ app/test_main.py
@@ -1,26 +1,18 @@
+import pytest
 from app.main import get_human_age
 
-
-def test_first_dog_age():
-    assert get_human_age(0, 0) == [0, 0]
-
-def test_two_cat_age():
-    assert get_human_age(0, 0) == [0, 0]
-
-def test_tree_name_age():
-    assert get_human_age(15, 15) == [1, 1]
-
-def test_four_name_age():
-    assert get_human_age(23, 23) == [1, 1]
-
-def test_five_name_age():
-    assert get_human_age(24, 24) == [2, 2]
-
-def test_six_name_age():
-    assert get_human_age(27, 27) == [2, 2]
-
-def test_eight_name_age():
-    assert get_human_age(28, 28) == [3, 2]
-
-def test_nine_name_age():
-    assert get_human_age(100, 100) == [21, 17]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_cat_dog_age(cat_age, dog_age, expected):
+    assert get_human_age(cat_age, dog_age) == expected
