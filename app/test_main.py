13740990c7cb5588ce0ab0bb0c9b515b4dc20672
Vadym Vynokur <king.krollie@gmail.com>
2025-11-07 17:28:27 +0200

    improve my tests

--- app/test_main.py
+++ app/test_main.py
@@ -2,28 +2,41 @@ from app.main import get_human_age
 import pytest
 
 
-def test_correct_results() -> None:
-    assert get_human_age(cat_age=15, dog_age=15) == [1, 1]
-    assert get_human_age(cat_age=14, dog_age=14) == [0, 0]
-    assert get_human_age(cat_age=23, dog_age=23) == [1, 1]
-    assert get_human_age(cat_age=24, dog_age=24) == [2, 2]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        (-1, -1, [0, 0]),
+        (0, 0, [0, 0]),
+    ]
+)
+def test_different_values(cat_age, dog_age, expected) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
 
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (100, 100, [21, 17]),
+        (-1, -1, [0, 0]),
+        (0, 0, [0, 0]),
+    ]
+)
+def test_incorrect_values(cat_age, dog_age, expected) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
 
-def test_incorrect_values() -> None:
-    with pytest.raises(ValueError):
-        get_human_age(cat_age=-1, dog_age=-1)
-
-    with pytest.raises(ValueError):
-        get_human_age(cat_age=0, dog_age=0)
-
-    with pytest.raises(ValueError):
-        get_human_age(cat_age=1000, dog_age=2000)
-
-
-def test_incorrect_data_types() -> None:
-
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("100", "five"),
+        ([1, 2, 3], None),
+    ]
+)
+def test_incorrect_data_types(cat_age, dog_age) -> None:
     with pytest.raises(TypeError):
-        get_human_age(cat_age="five", dog_age=None)
-
-    with pytest.raises(TypeError):
-        get_human_age(cat_age=[5], dog_age={"age": 10})
+        get_human_age(cat_age, dog_age)
