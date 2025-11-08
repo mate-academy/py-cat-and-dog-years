69a82082648dda78c8f4a23faf0729e1c8c27fb4
vadym-zahrebelnyi <zahrebelnyivadym@gmail.com>
2025-11-08 20:02:00 +0200

    Added suggested changes

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import pytest
 
 from app.main import get_human_age
@@ -37,6 +39,22 @@ from app.main import get_human_age
             )
         ),
         pytest.param(
+            27, 27, [2, 2],
+            id=(
+                "should convert to 2 "
+                "after the first 15 years "
+                "and more than the following 9 have been already lived"
+            )
+        ),
+        pytest.param(
+            28, 28, [3, 2],
+            id=(
+                    "should convert to [3, 2] "
+                    "after the first 15 years "
+                    "and more than the following 9 have been already lived"
+            )
+        ),
+        pytest.param(
             100, 100, [21, 17],
             id=(
                 "should correctly add "
@@ -51,3 +69,33 @@ def test_check_first_and_next_years(
         converted_ages_list: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == converted_ages_list
+
+@pytest.mark.parametrize(
+    "cat_invalid_age,dog_invalid_age,error",
+    [
+        pytest.param(
+            -2, -2, ValueError,
+            id="should raise error when negative"
+        ),
+        pytest.param(
+            [], 24, TypeError,
+            id="should raise error when not int"
+        ),
+        pytest.param(
+            14, "22", TypeError,
+            id="should raise error when not int"
+        ),
+        pytest.param(
+            "two", (12, 1), TypeError,
+            id="should raise error when not int"
+        )
+    ]
+)
+def test_invalid_datatypes(
+        cat_invalid_age: Any,
+        dog_invalid_age: Any,
+        error: type[Exception]
+) -> None:
+   with pytest.raises(error):
+       get_human_age(cat_invalid_age, dog_invalid_age)
+
