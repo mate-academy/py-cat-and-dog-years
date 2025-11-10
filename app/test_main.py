fc71dcf5ed19edb55478a82a91bee6d3e8c24ed5
Andrii Yermolenko <vakt159@gmail.com>
2025-11-10 17:22:42 +0200

    Added some boundaries check tests

--- app/test_main.py
+++ app/test_main.py
@@ -12,6 +12,14 @@ class TestGetHumanAge:
             pytest.param(0, 0, [0, 0], id="2 zeros"),
             pytest.param(15, 15, [1, 1], id="two 15 years"),
             pytest.param(23, 23, [1, 1], id="two 23 years"),
+            pytest.param(27, 16, [2, 1],
+                         id="less than boundary check for cat"),
+            pytest.param(28, 16, [3, 1],
+                         id="bigger than boundary check for cat"),
+            pytest.param(16, 28, [1, 2],
+                         id="less than boundary check for dog"),
+            pytest.param(16, 29, [1, 3],
+                         id="bigger than boundary check for dog"),
             pytest.param(24, 24, [2, 2], id="two 24 years"),
             pytest.param(-5, -6, [0, 0], id="2 negative numbers"),
             pytest.param(105, 120, [22, 21], id="2 really large numbers"),
@@ -19,10 +27,10 @@ class TestGetHumanAge:
         ],
     )
     def test_should_return_list_of_ages(
-        self,
-        cat_age: int,
-        dog_age: int,
-        result: list
+            self,
+            cat_age: int,
+            dog_age: int,
+            result: list
     ) -> None:
         assert get_human_age(cat_age, dog_age) == result
 
