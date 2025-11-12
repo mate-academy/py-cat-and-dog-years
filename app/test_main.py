49a0b71ecf9f07ff57c6e7c2c9d564587110012c
Vadim <vadimchubok@gmail.com>
2025-11-12 17:30:57 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -2,7 +2,7 @@ import pytest
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize("input_cat_age,input_dog_age,expected_output,exception", [
+@pytest.mark.parametrize("cat_age,dog_age,expected_output,exception", [
     pytest.param(0, 0, [0, 0], None,
                  id="Should return zero if cat or dog age are zeros"),
     pytest.param(14, 14, [0, 0], None,
@@ -26,13 +26,13 @@ from app.main import get_human_age
     pytest.param(100, "100", None, TypeError,
                  id="Should return None if cat or dog age is not integer"),
 ])
-def test_get_human_age(input_cat_age: int,
-                       input_dog_age: int,
+def test_get_human_age(cat_age: int,
+                       dog_age: int,
                        expected_output: list,
                        exception: type
                        ) -> None:
     if exception:
         with pytest.raises(exception):
-            get_human_age(input_cat_age, input_dog_age)
+            get_human_age(cat_age, dog_age)
     else:
-        assert get_human_age(input_cat_age, input_dog_age) == expected_output
+        assert get_human_age(cat_age, dog_age) == expected_output
