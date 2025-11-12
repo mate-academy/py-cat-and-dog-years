628704a11361a9b5505957adf39195b18f4e8981
Oleksii Mishchenko <alex3hard@gmail.com>
2025-11-12 18:02:21 +0200

    all tests implemented

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,32 @@
+import pytest
+from app import main
 from app.main import get_human_age
+from _pytest.monkeypatch import MonkeyPatch
 
-# write your code here
+
+@pytest.fixture(autouse=True)
+def restore_get_human_age(monkeypatch: MonkeyPatch) -> None:
+
+    monkeypatch.setattr(main, "get_human_age", main.get_human_age)
+    yield
+
+
+@pytest.mark.parametrize(
+    "cat_age,dog_age,expected",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (31, 31, [3, 3]),
+        (32, 32, [4, 3]),
+        (33, 33, [4, 3]),
+        (34, 34, [4, 4]),
+        (100, 100, [21, 17]),
+    ]
+)
+def test_get_human_age(cat_age: int, dog_age: int, expected: int) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
