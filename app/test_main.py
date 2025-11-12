af4f803ecda0718d8e6700a46f9943edd05058a3
Anastasiia Duzhynska <a.duzhynska@gmail.com>
2025-11-12 12:42:37 +0200

    Solution2

--- app/test_main.py
+++ app/test_main.py
@@ -5,6 +5,7 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age, dog_age, age_in_human_years",
     [
+        (-1, -1, [0, 0]),
         (12, 13, [0, 0]),
         (14, 15, [0, 1]),
         (23, 24, [1, 2]),
@@ -14,6 +15,7 @@ from app.main import get_human_age
         (0, 150, [0, 27])
     ],
     ids=[
+        "not valid negative animal age",
         "animal age less then 15",
         "animal age on the border values 14 and 15",
         "animal age on the border values 23 and 24",
@@ -25,5 +27,10 @@ from app.main import get_human_age
 )
 def test_get_human_age(cat_age: int,
                        dog_age: int,
-                       age_in_human_years: int) -> None:
+                       age_in_human_years: list[int]) -> None:
     assert get_human_age(cat_age, dog_age) == age_in_human_years
+
+
+def test_invalid_string_as_input() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("a", "b")
