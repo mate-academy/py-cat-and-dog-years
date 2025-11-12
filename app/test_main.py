fa75fbe38129a7bc68b449d23f83d8a40fb7e33d
Nikita <hkorobko@gmail.com>
2025-11-12 17:49:14 +0200

    Fixed Cat and Dog years

--- app/test_main.py
+++ app/test_main.py
@@ -11,28 +11,13 @@ from app.main import get_human_age
         (23, 23, [1, 1]),
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
     ]
 )
-def test_get_human_age_valid(
+def test_get_human_age(
         cat_age: int,
         dog_age: int,
         expected: list[int]
 ) -> None:
     assert get_human_age(cat_age, dog_age) == expected
-
-
-@pytest.mark.parametrize(
-    "cat_age, dog_age",
-    [
-        (-1, 5),
-        (5, -1),
-        ("15", 5),
-        (5, "10"),
-        (1.5, 5),
-        (5, 2.7),
-        (1000, 1000),
-    ]
-)
-def test_get_human_age_invalid(cat_age: object, dog_age: object) -> None:
-    with pytest.raises((TypeError, ValueError)):
-        get_human_age(cat_age, dog_age)
