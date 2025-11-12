b05fdd8d43433ee7a3554f5be93ccea87521d5de
Anastasiia Duzhynska <a.duzhynska@gmail.com>
2025-11-12 12:23:08 +0200

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,29 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, age_in_human_years",
+    [
+        (12, 13, [0, 0]),
+        (14, 15, [0, 1]),
+        (23, 24, [1, 2]),
+        (27, 28, [2, 2]),
+        (0, 0, [0, 0]),
+        (100, 100, [21, 17]),
+        (0, 150, [0, 27])
+    ],
+    ids=[
+        "animal age less then 15",
+        "animal age on the border values 14 and 15",
+        "animal age on the border values 23 and 24",
+        "animal age on the border values 27 and 28",
+        "extremely small animal age",
+        "extremely large animal age",
+        "cross-referenced animal age"
+    ]
+)
+def test_get_human_age(cat_age: int,
+                       dog_age: int,
+                       age_in_human_years: int) -> None:
+    assert get_human_age(cat_age, dog_age) == age_in_human_years
