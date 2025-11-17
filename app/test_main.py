466225702f216b3c37e42d7a09a61b97917f2d0d
Thaís <parisethais@gmail.com>
2025-11-17 14:14:54 -0300

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -15,5 +15,35 @@ from app.main import get_human_age
         (100, 100, [21, 17]),
     ]
 )
-def test_get_human_age(cat_age, dog_age, expected):
+def test_valid_inputs(cat_age, dog_age, expected):
     assert get_human_age(cat_age, dog_age) == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        (-1, 10),
+        (10, -5),
+        (-3, -8),
+    ]
+)
+def test_negative_values(cat_age, dog_age):
+    with pytest.raises(ValueError):
+        get_human_age(cat_age, dog_age)
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("abc", 10),
+        (10, "xyz"),
+        ("10", "5"),
+        (None, 5),
+        (5, None),
+        (3.5, 10),
+        (10, 2.8),
+    ]
+)
+def test_incorrect_types(cat_age, dog_age):
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
