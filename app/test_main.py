dbf2a22fc03f4840582bbf91f75a9f7322e8386c
Tomasz Szatałowicz <szatalowicztomek@gmail.com>
2025-11-23 16:25:21 +0100

    tests

--- app/test_main.py
+++ app/test_main.py
@@ -12,6 +12,7 @@ from app.main import get_human_age
         (20, 20, [1, 1]),
         (23, 23, [1, 1]),
         (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
         (28, 28, [3, 2]),
         (29, 30, [3, 3]),
         (29, 34, [3, 4]),
@@ -28,3 +29,18 @@ def test_get_human_age_parametrized(
     assert isinstance(result, list)
     assert len(result) == 2
     assert result == expected
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        ("10", 10),
+        (10, "10"),
+        (None, 10),
+        (10, None),
+        ([], {}),
+    ]
+)
+def test_get_human_age_invalid_types(cat_age: int, dog_age: int) -> None:
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
