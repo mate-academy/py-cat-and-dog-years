609c843089f085eb519f19b4694a2c97416e4fd2
Vova <vovakucin082@gmail.com>
2025-11-26 11:38:30 +0100

    code write

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,37 @@
+import pytest
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17]),
+        (-144, -144, [0, 0])
+
+
+
+    ]
+)
+def test_different_mind_get_human_age(cat_age: int,
+                                      dog_age: int,
+                                      result: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, exception",
+    [
+        ("10", 2, TypeError),
+    ]
+)
+def test_error_get_human_age(cat_age: int,
+                             dog_age: int,
+                             exception: Exception) -> None:
+    with pytest.raises(exception):
+        get_human_age(cat_age, dog_age)
