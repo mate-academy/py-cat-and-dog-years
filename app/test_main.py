deb5e31c8875c9724f002248552b3164f4a600da
Petro Kurinnyi <petro.kurinnyi@gmail.com>
2025-11-26 22:20:51 +0200

    negative cases coverage

--- app/test_main.py
+++ app/test_main.py
@@ -14,7 +14,12 @@ from app.main import get_human_age
         pytest.param(27, 27, [2, 2], id="should return second year for max accepted"),
         pytest.param(28, 28, [3, 2], id="should return third year for cad and second year for dog"),
         pytest.param(100, 100, [21, 17], id="should accept 100 year for cad and dog"),
+        pytest.param(-5, -10, [0, 0], id="should return zero if negative")
     ]
 )
 def test_human_age(cat_age, dog_age, result):
     assert get_human_age(cat_age, dog_age) == result
+
+def test_human_age_should_raise_type_error_for_invalid_input_value():
+    with pytest.raises(TypeError):
+        get_human_age('cat', 'dog')
