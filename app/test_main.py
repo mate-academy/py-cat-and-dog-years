dcdbb1ddb6d225b606aaa2b91c4d9164dde72ea3
Petro Kurinnyi <petro.kurinnyi@gmail.com>
2025-11-26 22:04:02 +0200

    solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,20 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    'cat_age,dog_age,result',
+    [
+        pytest.param(0, 0, [0, 0], id="should return 0 for 0"),
+        pytest.param(14, 14, [0, 0], id="should return 0 for 14"),
+        pytest.param(15, 15, [1, 1], id="should return first year for min accepted"),
+        pytest.param(23, 23, [1, 1], id="should return first year for max accepted"),
+        pytest.param(24, 24, [2, 2], id="should return second year for min accepted"),
+        pytest.param(27, 27, [2, 2], id="should return second year for max accepted"),
+        pytest.param(28, 28, [3, 2], id="should return third year for cad and second year for dog"),
+        pytest.param(100, 100, [21, 17], id="should accept 100 year for cad and dog"),
+    ]
+)
+def test_human_age(cat_age, dog_age, result):
+    assert get_human_age(cat_age, dog_age) == result
