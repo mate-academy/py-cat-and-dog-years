3f87db940097c47ad94eba1fa117c29e9752c4ae
Ievgen German <german.kbrza@gmail.com>
2025-11-27 22:24:22 +0200

    solution 3

--- app/test_main.py
+++ app/test_main.py
@@ -3,17 +3,41 @@ import pytest
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize("cat_age", "dog_age", "expected",
-    [
-        pytest.param(0, 0, [0, 0], id = "test_bottom_boundary_zero_year"),
-        pytest.param(14, 14, [0, 0], id = "test_top_boundary_zero_year"),
-        pytest.param(15, 15, [1, 1], id = "test_bottom_boundary_first_year"),
-        pytest.param(23, 23, [1, 1], id = "test_top_boundary_first_year"),
-        pytest.param(24, 24, [2, 2], id = "test_bottom_boundary_second_year"),
-        pytest.param(27, 27, [2, 2], id = "test_top_boundary_second_year"),
-        pytest.param(28, 28, [3, 2], id = "test_cat_age_less_dog_year"),
-        pytest.param(100, 100, [21, 17], id = "test_cat_age_more_dog_year")
-    ]
-)
-def test_function(cat_age, dog_age, expected) -> None:
+@pytest.mark.parametrize("cat_age, dog_age, expected",
+                         [
+                             pytest.param(0,
+                                          0,
+                                          [0, 0],
+                                          id="test_bottom_boundary_zero_age"),
+                             pytest.param(14,
+                                          14,
+                                          [0, 0],
+                                          id="test_top_boundary_zero_age"),
+                             pytest.param(15,
+                                          15,
+                                          [1, 1],
+                                          id="test_bottom_boundary_first_age"),
+                             pytest.param(23,
+                                          23,
+                                          [1, 1],
+                                          id="test_top_boundary_first_age"),
+                             pytest.param(24,
+                                          24,
+                                          [2, 2],
+                                          id="test_boundary_second_age"),
+                             pytest.param(27,
+                                          27,
+                                          [2, 2],
+                                          id="test_top_boundary_second_age"),
+                             pytest.param(28,
+                                          28,
+                                          [3, 2],
+                                          id="test_cat_age_less_dog_year"),
+                             pytest.param(100,
+                                          100,
+                                          [21, 17],
+                                          id="test_cat_age_more_dog_year")
+                         ]
+                         )
+def test_function(cat_age: int, dog_age: int, expected: list) -> None:
     assert get_human_age(cat_age, dog_age) == expected
