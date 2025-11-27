3a5a9d1f0b507494bc263cd413060f7d222aa3b2
Ievgen German <german.kbrza@gmail.com>
2025-11-27 22:15:47 +0200

    solution 2

--- app/test_main.py
+++ app/test_main.py
@@ -3,60 +3,17 @@ import pytest
 from app.main import get_human_age
 
 
-@pytest.mark.parametrize(
-    "cat_year",
-    "dog_year",
-    "expected",
+@pytest.mark.parametrize("cat_age", "dog_age", "expected",
     [
-        (0, 0, [0, 0]),
-        (14, 14, [0, 0]),
-        (15, 15, [1, 1]),
-        (23, 23, [1, 1]),
-(24, 24, [2, 2]),
-(27, 27, [2, 2]),
-(28, 28, [3, 2]),
-(100, 100, [21, 17]
-
-
-"test_bottom_boundary_zero_year"
-"test_top_boundary_zero_year"
-"test_bottom_boundary_first_year"
-"test_top_boundary_first_year"
-"test_bottom_boundary_second_year"
-"test_top_boundary_second_year"
-"test_cat_age_less_dog_year"
-"test_cat_age_more_dog_year"
+        pytest.param(0, 0, [0, 0], id = "test_bottom_boundary_zero_year"),
+        pytest.param(14, 14, [0, 0], id = "test_top_boundary_zero_year"),
+        pytest.param(15, 15, [1, 1], id = "test_bottom_boundary_first_year"),
+        pytest.param(23, 23, [1, 1], id = "test_top_boundary_first_year"),
+        pytest.param(24, 24, [2, 2], id = "test_bottom_boundary_second_year"),
+        pytest.param(27, 27, [2, 2], id = "test_top_boundary_second_year"),
+        pytest.param(28, 28, [3, 2], id = "test_cat_age_less_dog_year"),
+        pytest.param(100, 100, [21, 17], id = "test_cat_age_more_dog_year")
     ]
 )
-
-
-def test_bottom_boundary_zero_year() -> None:
-    assert get_human_age(0, 0) == [0, 0]
-
-
-def test_top_boundary_zero_year() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_bottom_boundary_first_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_top_boundary_first_year() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_bottom_boundary_second_year() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_top_boundary_second_year() -> None:
-    assert get_human_age(27, 27) == [2, 2]
-
-
-def test_cat_age_less_dog_year() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_cat_age_more_dog_year() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+def test_function(cat_age, dog_age, expected) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
