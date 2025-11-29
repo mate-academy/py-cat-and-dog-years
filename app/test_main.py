0fd158c9268dbb880699eb6b00dc7f0ac44fdcaf
Paweł Dudek <dudek136@gmail.com>
2025-11-29 18:40:11 +0100

    Solution v2

--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,47 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_when_zeros_are_given() -> None:
-    assert get_human_age(0, 0) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result_list_in_human_years",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (28, 28, [3, 2]),
+        (28, 29, [3, 3]),
+        (100, 100, [21, 17]),
+        (-10, -1, [0, 0]),
+    ],
+    ids=[
+        "zero_ages",
+        "almost_one_year",
+        "one_year",
+        "almost_two_years",
+        "two_years",
+        "cat_3_dog_2",
+        "cat_3_dog_3",
+        "large_ages",
+        "negative_are_zeroed",
+    ]
+)
+def test_get_human_age(cat_age: int,
+                       dog_age: int,
+                       result_list_in_human_years: list) -> None:
+    assert get_human_age(cat_age, dog_age) == result_list_in_human_years
 
 
-def test_15_equals_1() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_24_equals_2() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_28_equals_3_and_2() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_29_equals_3() -> None:
-    assert get_human_age(29, 29) == [3, 3]
+@pytest.mark.parametrize(
+    "cat_age_list, dog_age_str, error",
+    [
+        (-10, "s", TypeError),
+        ([], 10, TypeError),
+    ]
+)
+def test_get_human_age_raises_error(cat_age_list: list,
+                                    dog_age_str: str,
+                                    error: Exception) -> None:
+    with pytest.raises(error):
+        get_human_age(cat_age_list, dog_age_str)
