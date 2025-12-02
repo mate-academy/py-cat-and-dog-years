6a98399757bf332c7b3f6c4a1908e6006e10cb75
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-12-02 19:51:54 -0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,28 @@
 from app.main import get_human_age
 
-# write your code here
+import pytest
+
+
+@pytest.mark.parametrize(
+    "cat, dog, exp",
+    [
+        (0, 0, [0, 0]),
+        (14, 14, [0, 0]),
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1]),
+        (24, 24, [2, 2]),
+        (27, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21,17])
+    ]
+)
+class TestAnimals:
+    def test_if_age_is_integer(self, cat: int, dog: int, exp: list):
+        assert type(cat) == int
+        assert type(dog) == int
+        assert all(isinstance(num, int) for num in get_human_age(cat, dog))
+
+    def test_cats_and_dogs_equivalent_age(self, cat: int, dog: int, exp: list):
+        expected = exp
+        actual = get_human_age(cat, dog)
+        assert expected == actual
