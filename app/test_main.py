2ef7ed524d9d75b9af5bdbad8c241d97319fafa2
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-12-03 13:05:50 -0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -13,16 +13,17 @@ import pytest
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 28, [3, 2]),
+        (29, 29, [3, 3]),
         (100, 100, [21,17])
     ]
 )
 class TestAnimals:
     def test_if_age_is_integer(self, cat: int, dog: int, exp: list):
-        assert type(cat) == int
-        assert type(dog) == int
         assert all(isinstance(num, int) for num in get_human_age(cat, dog))
+    
+
+    def test_if_age_is_positive(self, cat: int, dog: int, exp: list):
+        assert all(num >= 0 for num in get_human_age(cat, dog))
 
     def test_cats_and_dogs_equivalent_age(self, cat: int, dog: int, exp: list):
-        expected = exp
-        actual = get_human_age(cat, dog)
-        assert expected == actual
+        assert get_human_age(cat, dog) == exp
