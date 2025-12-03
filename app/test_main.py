3f2913472c4548751e4b9600e880cd96d82d970d
alexnrn2204 <alexnrn2204@gmail.com>
2025-12-03 20:36:55 +0200

    Solution 1

--- app/test_main.py
+++ app/test_main.py
@@ -1,25 +1,70 @@
+import pytest
 from app.main import get_human_age
 
 
-def test_return_0_if_animal_age_less_than_first_year() -> None:
-    assert get_human_age(14, 14) == [0, 0]
-
-
-def test_return_1_if_animal_age_equal_first_year() -> None:
-    assert get_human_age(15, 15) == [1, 1]
-
-
-def test_return_1_if_less_than_sum_first_and_second() -> None:
-    assert get_human_age(23, 23) == [1, 1]
-
-
-def test_return_2_if_animal_age_equal_sum_first_year_and_second_year() -> None:
-    assert get_human_age(24, 24) == [2, 2]
-
-
-def test_return_2_if_less_than_sum_first_second_and_4_5() -> None:
-    assert get_human_age(27, 28) == [2, 2]
-
-
-def test_return_3_if_equal_sum_first_second_and_4_5() -> None:
-    assert get_human_age(28, 29) == [3, 3]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="test return 0 if animal age equals 0",
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="test return 0 if animal age less than first year",
+        ),
+        pytest.param(
+            15,
+            15,
+            [1, 1],
+            id="test return 1 if animal age equal first year",
+        ),
+        pytest.param(
+            23,
+            23,
+            [1, 1],
+            id="test return 1 if less than sum first and second",
+        ),
+        pytest.param(
+            24,
+            24,
+            [2, 2],
+            id="test return 2 if animal age equal sum 1 year and 2 year",
+        ),
+        pytest.param(
+            27,
+            27,
+            [2, 2],
+            id="test return 2 if animal age equals 27",
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="test return 2 if animal age equals 28",
+        ),
+        pytest.param(
+            100,
+            100,
+            [3, 3],
+            id="test return 2 if animal age equals 100",
+        ),
+        pytest.param(
+            -1,
+            -1,
+            [0, 0],
+            id="test negative age",
+        )
+    ]
+)
+class TestGetHumanAge:
+    def test_get_human_age(self,
+                           cat_age: int,
+                           dog_age: int,
+                           expected: list
+                           ) -> None:
+        assert get_human_age(cat_age, dog_age) == expected
