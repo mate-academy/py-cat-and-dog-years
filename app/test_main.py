c87f521443b94bd1d217510f0fdb2ecdd6fe0236
Saulo Rodrigo <saulorodrigocfs@gmail.com>
2025-12-03 13:45:28 -0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -37,39 +37,39 @@ class TestAnimals:
             dog: int, exp: list) -> None:
         assert get_human_age(cat, dog) == exp
 
+
 class TestEdgecases:
-    def test_negative_cat_age(self):
+    def test_negative_cat_age(self) -> None:
         result = get_human_age(-1, 15)
         assert result[0] == 0
         assert result[1] == 1
-    
-    def test_negative_dog_age(self):
+
+    def test_negative_dog_age(self) -> None:
         result = get_human_age(15, -10)
         assert result[0] == 1
         assert result[1] == 0
-    
-    def test_both_negative_ages(self):
+
+    def test_both_negative_ages(self) -> None:
         result = get_human_age(-10, -10)
         assert result[0] == 0
         assert result[1] == 0
-    
-    def test_cat_age_string_raises_exception(self):
+
+    def test_cat_age_string_raises_exception(self) -> None:
         with pytest.raises(TypeError):
             get_human_age("a", 5)
-    
-    def test_dog_age_string_raises_exception(self):
+
+    def test_dog_age_string_raises_exception(self) -> None:
         with pytest.raises(TypeError):
             get_human_age(5, "a")
-    
-    def test_float_cat_age_string_raises_exception(self):
+
+    def test_float_cat_age_string_raises_exception(self) -> None:
         with pytest.raises(TypeError):
             get_human_age(15.7, 20)
 
-    def test_float_dog_age_string_raises_exception(self):
+    def test_float_dog_age_string_raises_exception(self) -> None:
         with pytest.raises(TypeError):
             get_human_age(15, 20.5)
 
-    def test_none_values_raises_exception(self):
+    def test_none_values_raises_exception(self) -> None:
         with pytest.raises(TypeError):
             get_human_age(None, 20)
-
