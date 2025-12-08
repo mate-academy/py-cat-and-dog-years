86b719b4153d40b2b50e3d41368173bb867eb62b
SSSSSADO <grushkovik.danil@gmail.com>
2025-12-08 14:04:39 +0300

    Solution

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,60 @@
+import pytest
+
 from app.main import get_human_age
 
-# write your code here
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age, result",
+    [
+        pytest.param(
+            0,
+            0,
+            [0, 0],
+            id="zero cases"
+        ),
+        pytest.param(
+            14,
+            14,
+            [0, 0],
+            id="14 cases"
+        ),
+        pytest.param(
+            15,
+            15,
+            [1, 1],
+            id="first human age when cases 15, 15"
+        ),
+        pytest.param(
+            23,
+            23,
+            [1, 1],
+            id="first human age when cases 23, 23"
+        ),
+        pytest.param(
+            24,
+            24,
+            [2, 2],
+            id="second human age when cases 24, 24"
+        ),
+        pytest.param(
+            27,
+            27,
+            [2, 2],
+            id="second human age when cases 27, 27"
+        ),
+        pytest.param(
+            28,
+            28,
+            [3, 2],
+            id="cat +1 at 28, dog stays 2"
+        ),
+        pytest.param(
+            100,
+            100,
+            [21, 17],
+            id="large example"
+        )
+    ]
+)
+def test_parameters(cat_age, dog_age, result):
+    assert get_human_age(cat_age, dog_age) == result
