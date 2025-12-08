4214ad7b2cc1e1add077e1565bacd8543d13d1b0
SSSSSADO <grushkovik.danil@gmail.com>
2025-12-08 14:35:01 +0300

    fix

--- app/test_main.py
+++ app/test_main.py
@@ -53,8 +53,76 @@ from app.main import get_human_age
             100,
             [21, 17],
             id="large example"
+        ),
+        pytest.param(
+            -10,
+            -10,
+            [0, 0],
+            id="negative cases should return 0, 0"
+        ),
+        pytest.param(
+            -10,
+            0,
+            [0, 0],
+            id="negative cat age and 0 dog age should return 0, 0"
+        ),
+        pytest.param(
+            0,
+            -10,
+            [0, 0],
+            id="negative dog age and 0 cat age should return 0, 0"
         )
     ]
 )
 def test_parameters(cat_age, dog_age, result):
     assert get_human_age(cat_age, dog_age) == result
+
+
+@pytest.mark.parametrize(
+    "cat_age, dog_age",
+    [
+        pytest.param(
+            "40",
+            25,
+            id="string instead of number"
+        ),
+        pytest.param(
+            40,
+            "25",
+            id="string instead of number"
+        ),
+        pytest.param(
+            None,
+            15,
+            id="None instead of number"
+        ),
+        pytest.param(
+            15,
+            None,
+            id="None instead of number"
+        ),
+        pytest.param(
+            [20],
+            20,
+            id="list instead of number"
+        ),
+        pytest.param(
+            20,
+            [20],
+            id="list instead of number"
+        ),
+        pytest.param(
+            15.3,
+            20,
+            id="float instead of number"
+        ),
+        pytest.param(
+            20,
+            15.3,
+            id="float instead of number"
+        )
+    ]
+)
+def test_errors(cat_age, dog_age):
+    with pytest.raises(TypeError):
+        get_human_age(cat_age, dog_age)
