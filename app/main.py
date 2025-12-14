516472ce7b3c9e986dd23e284553dcc337e3a625
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-14 19:51:24 +0100

    add input param type error handling

--- app/main.py
+++ app/main.py
@@ -24,6 +24,8 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
     for i in range(len(animal_years)):
         if animal_years[i] < 0 or animal_years[i] > 100:
             raise ValueError
+        if not isinstance(animal_years[i], int):
+            raise TypeError
         if animal_years[i] // 15 > 0:
             human_years[i] += 1
             animal_years[i] -= 15
