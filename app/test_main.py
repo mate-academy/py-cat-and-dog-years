9bbc249aee277a38ce7d475c7d70d10bdd032ac8
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-14 17:49:36 +0100

    rename and adjust message for test_return_correct_value_for_age_less_than_15

--- app/test_main.py
+++ app/test_main.py
@@ -13,11 +13,11 @@ import pytest
         "cat: 14, dog: 14",
     ]
 )
-def test_return_correct_value_for_age_less_than_15(cat_years, dog_years, expected):
+def test_return_0_for_age_less_than_15(cat_years, dog_years, expected):
     actual = get_human_age(cat_years, dog_years)
     assert (actual == expected), (
-        "Function should return correct value for age less than 15, ",
-        f"received: {actual}, when expected: {expected}"
+        "15 cat/dog years equal to 1 human year. "
+        f"Received: {actual}, when expected: {expected}"
     )
 
 # test_return_correct_value_for_age_between_15_and_23
