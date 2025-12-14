47383610536b7e7d32108c32afc6203d635a6d21
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-14 17:53:40 +0100

    implement test_return_1_for_age_between_15_and_23

--- app/test_main.py
+++ app/test_main.py
@@ -20,7 +20,24 @@ def test_return_0_for_age_less_than_15(cat_years, dog_years, expected):
         f"Received: {actual}, when expected: {expected}"
     )
 
-# test_return_correct_value_for_age_between_15_and_23
+@pytest.mark.parametrize(
+    "cat_years, dog_years, expected",
+    [
+        (15, 15, [1, 1]),
+        (23, 23, [1, 1])
+    ],
+    ids=[
+        "cat: 15, dog: 15",
+        "cat: 23, dog: 23",
+    ]
+)
+def test_return_1_for_age_between_15_and_23(cat_years, dog_years, expected):
+    actual = get_human_age(cat_years, dog_years)
+    assert (actual == expected), (
+        "Function should return 1 for age between 15 and 23. ",
+        f"Received: {actual}, when expected: {expected}"
+    )
+
 # get_human_age(15, 15) == [1, 1]
 # get_human_age(23, 23) == [1, 1]
 
