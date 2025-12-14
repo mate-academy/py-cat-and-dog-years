8f9d1338d44f90306bb3c11482a36f32464ecad9
tomech <109527747+tomech4@users.noreply.github.com>
2025-12-14 19:49:54 +0100

    add error handling for incorrect input params

--- app/main.py
+++ app/main.py
@@ -22,6 +22,8 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
     human_years = [0, 0]
     animal_years = [cat_age, dog_age]
     for i in range(len(animal_years)):
+        if animal_years[i] < 0 or animal_years[i] > 100:
+            raise ValueError
         if animal_years[i] // 15 > 0:
             human_years[i] += 1
             animal_years[i] -= 15
