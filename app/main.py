9810ecd3a238a8aa192f787823ccff83a1d6ee74
Farlinder <sashakost2006e@gmail.com>
2025-12-18 17:34:52 +0100

    Solving

--- app/main.py
+++ app/main.py
@@ -1,3 +1,6 @@
+from math import floor
+
+
 def get_human_age(cat_age: int, dog_age: int) -> list:
     """
     Convert cat and dog ages to human years.
@@ -18,6 +21,22 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
         get_human_age(15, 15) == [1, 1]
         get_human_age(24, 24) == [2, 2]
     """
-    # TODO: Implement this function
-    # Write your tests first, then implement the logic
-    return [0, 0]
+    human_dog_age = 0
+    human_cat_age = 0
+    if cat_age >= 0 and dog_age >= 0:
+        if dog_age < 15:
+            human_dog_age = 0
+        elif 15 <= dog_age < 24:
+            human_dog_age = 1
+        elif dog_age >= 24:
+            human_dog_age=_age = 2 + floor((dog_age - 24)/5)
+        if cat_age < 15:
+            human_cat_age = 0
+        elif 15 <= cat_age < 24:
+            human_cat_age = 1
+        elif cat_age >= 24:
+            human_cat_age = 2 + floor((cat_age - 24)/4)
+
+    return [human_cat_age, human_dog_age]
+
+get_human_age(24, 24)
