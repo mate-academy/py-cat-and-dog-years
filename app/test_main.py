29e84b499567aeab27faa9579b92291a06c0a653
Farlinder <sashakost2006e@gmail.com>
2025-12-18 17:35:49 +0100

    Solving1

--- app/test_main.py
+++ app/test_main.py
@@ -16,5 +16,7 @@ from app.main import get_human_age
         (100, 100, [21, 17]),
      ]
 )
-def test_function_for_correct_working(cat_age, dog_age, expected):
+def test_function_for_correct_working(cat_age: int,
+                                      dog_age: int,
+                                      expected: list[int]) -> None:
     assert get_human_age(cat_age, dog_age) == expected
