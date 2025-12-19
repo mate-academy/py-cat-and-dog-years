5fe07768d8c9af5909d01b3384f051c39477744c
Bielzin <gabriel.cabecao.lol@gmail.com>
2025-12-19 02:41:01 -0300

    flake8 errors

--- app/main.py
+++ app/main.py
@@ -1,25 +1,4 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
-    """
-    Convert cat and dog ages to human years.
-    
-    Rules:
-    Cat: first 15 years = 1 human year, next 9 = +1, then every 4 = +1
-    Dog: first 15 years = 1 human year, next 9 = +1, then every 5 = +1
-    
-    Args:
-        cat_age: Cat's age in cat years
-        dog_age: Dog's age in dog years
-        
-    Returns:
-        List with [cat_human_age, dog_human_age]
-        
-    Examples:
-        get_human_age(0, 0) == [0, 0]
-        get_human_age(15, 15) == [1, 1]
-        get_human_age(24, 24) == [2, 2]
-    """
-    # TODO: Implement this function
-    # Write your tests first, then implement the logic
     human_age = [0, 0]
 
     if cat_age < 15:
@@ -27,18 +6,18 @@ def get_human_age(cat_age: int, dog_age: int) -> list:
     elif cat_age < 24:
         human_age[0] = 1
     else:
-        variable = cat_age - 23
-        if variable % 4 == 0:
-            variable -= 1
-        human_age[0] = (variable // 4) + 2
+        aux_variable = cat_age - 23
+        if aux_variable % 4 == 0:
+            aux_variable -= 1
+        human_age[0] = (aux_variable // 4) + 2
 
     if dog_age < 15:
         human_age[1] = 0
     elif dog_age < 24:
         human_age[1] = 1
     else:
-        variable = dog_age - 23
-        if variable % 5 == 0:
-            variable -= 1
-        human_age[1] = (variable // 5) + 2
+        aux_variable = dog_age - 23
+        if aux_variable % 5 == 0:
+            aux_variable -= 1
+        human_age[1] = (aux_variable // 5) + 2
     return human_age
--- app/test_main.py
+++ app/test_main.py
@@ -1,33 +1,33 @@
 from app.main import get_human_age
 
-# write your code here
-def test_cat0_dog0_under():
+
+def test_cat0_dog0_under() -> None:
     assert get_human_age(0, 0) == [0, 0]
 
 
-def test_cat0_dog0_upper():
+def test_cat0_dog0_upper() -> None:
     assert get_human_age(14, 14) == [0, 0]
 
 
-def test_cat1_dog1_under():
+def test_cat1_dog1_under() -> None:
     assert get_human_age(15, 15) == [1, 1]
 
 
-def test_cat1_dog1_upper():
+def test_cat1_dog1_upper() -> None:
     assert get_human_age(23, 23) == [1, 1]
 
 
-def test_cat2_dog2_under():
+def test_cat2_dog2_under() -> None:
     assert get_human_age(24, 24) == [2, 2]
 
 
-def test_cat2_dog2_upper():
+def test_cat2_dog2_upper() -> None:
     assert get_human_age(27, 27) == [2, 2]
 
 
-def test_cat3_dog2_upper():
+def test_cat3_dog2_upper() -> None:
     assert get_human_age(28, 28) == [3, 2]
 
 
-def test_cat21_dog17():
+def test_cat21_dog17() -> None:
     assert get_human_age(100, 100) == [21, 17]
