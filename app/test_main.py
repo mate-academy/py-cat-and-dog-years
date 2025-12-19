1b0cf41709eb70438723843951af79571d43c40a
Denys Herasymchuk <herasymchukdenyss@gmail.com>
2025-12-19 16:31:50 +0100

    test correction

--- app/test_main.py
+++ app/test_main.py
@@ -6,6 +6,7 @@ from app.main import get_human_age
 @pytest.mark.parametrize(
     "cat_age,dog_age,expected_human_age",
     [
+        (-1, -1, [0, 0]),
         (0, 0, [0, 0]),
         (14, 14, [0, 0]),
         (15, 15, [1, 1]),
@@ -13,10 +14,22 @@ from app.main import get_human_age
         (24, 24, [2, 2]),
         (27, 27, [2, 2]),
         (28, 29, [3, 3]),
-        (28, 29, [3, 3]),
         (32, 34, [4, 4]),
         (56, 64, [10, 10]),
         (100, 100, [21, 17])
+    ],
+    ids=[
+        "-1 cat/dog age should convert into 0 human age.",
+        "0 cat/dog age should convert into 0 human age.",
+        "14 cat/dog age should convert into 0 human age.",
+        "15 cat/dog age should convert into 1 human age.",
+        "23 cat/dog age should convert into 1 human age.",
+        "24 cat/dog age should convert into 2 human age.",
+        "27 cat/dog age should convert into 2 human age.",
+        "28/29 cat/dog age should convert into 3 human age.",
+        "32/34 cat/dog age should convert into 4 human age.",
+        "56/64 cat/dog age should convert into 10 human age.",
+        "100 cat/dog age should convert into 21/17 human age.",
     ]
 )
 def test_get_human_age_should_return_correct_result(
