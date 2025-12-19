6784e5a90e3f9eefe89a79b1e3cf41c5f22e8cef
Darmi555 <mateusz.rudnicki05@wp.pl>
2025-12-19 19:05:05 +0100

    poprawki w testach

--- app/main.py
+++ app/main.py
@@ -1,12 +1,18 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
-    def cat_to_human_age(age:int) -> int:
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+        raise TypeError("Ages must be integers")
+
+    if cat_age < 0 or dog_age < 0:
+        raise ValueError("Age must be non-negative")
+
+    def cat_to_human_age(age: int) -> int:
         if age < 15:
             return 0
         if age < 24:
             return 1
         return ((age - 24) // 4) + 2
 
-    def dog_to_human_age(age:int) -> int:
+    def dog_to_human_age(age: int) -> int:
         if age < 15:
             return 0
         if age < 24:
