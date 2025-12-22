584511a39d04a476ab1773e916bd94481960159e
Steicy <anastasiaosega@gmail.com>
2025-12-22 12:53:05 +0200

    solved task

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,21 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_0_when_age_lower_15() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_1_when_age_upper_15_and_lower_24() -> None:
+    assert get_human_age(15, 23) == [1, 1]
+
+
+def test_should_return_2years_for_both() -> None:
+    assert get_human_age(24, 27) == [2, 2]
+
+
+def test_should_return_dogs_3rd_year() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_should_return_dogs_correct_years() -> None:
+    assert get_human_age(100, 100) == [21, 17]
