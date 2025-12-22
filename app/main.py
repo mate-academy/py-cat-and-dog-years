1eebb20f808d6cdac17c2411091e4418239359a3
Steicy <anastasiaosega@gmail.com>
2025-12-22 13:32:42 +0200

    solved problems

--- app/main.py
+++ app/main.py
@@ -1,20 +1,14 @@
 def get_human_age(cat_age: int, dog_age: int) -> list:
+    if not isinstance(cat_age, int) or not isinstance(dog_age, int):
+        raise TypeError
     cat_count = 0
     dog_count = 0
     if cat_age >= 15:
         cat_count += 1
-        cat_age = cat_age - 15
-        if cat_age >= 9:
-            cat_count += 1
-            cat_age = cat_age - 9
-            if cat_age >= 4:
-                cat_count += cat_age // 4
+        if cat_age >= 24:
+            cat_count += 1 + (cat_age - 24) // 4
     if dog_age >= 15:
         dog_count += 1
-        dog_age = dog_age - 15
-        if dog_age >= 9:
-            dog_count += 1
-            dog_age = dog_age - 9
-            if dog_age >= 5:
-                dog_count += dog_age // 5
+        if dog_age >= 24:
+            dog_count += 1 + (dog_age - 24) // 5
     return [cat_count, dog_count]
--- app/test_main.py
+++ app/test_main.py
@@ -1,21 +1,29 @@
+import pytest
+
 from app.main import get_human_age
 
 
-def test_should_return_0_when_age_lower_15() -> None:
-    assert get_human_age(14, 14) == [0, 0]
+@pytest.mark.parametrize(
+    "cat_age, dog_age, expected",
+    [
+        (14, 14, [0, 0]),
+        (15, 23, [1, 1]),
+        (24, 27, [2, 2]),
+        (28, 28, [3, 2]),
+        (100, 100, [21, 17])
+    ],
+    ids=[
+        "return 0 when age lower 15",
+        "return 1 when age upper 15 and lower 24",
+        "return 2 years for both",
+        "return dogs 3rd year",
+        "return many correct years"
+    ]
+)
+def test_should_return_dogs_correct_years(cat_age: int, dog_age: int,
+                                          expected: list) -> None:
+    assert get_human_age(cat_age, dog_age) == expected
 
 
-def test_should_return_1_when_age_upper_15_and_lower_24() -> None:
-    assert get_human_age(15, 23) == [1, 1]
-
-
-def test_should_return_2years_for_both() -> None:
-    assert get_human_age(24, 27) == [2, 2]
-
-
-def test_should_return_dogs_3rd_year() -> None:
-    assert get_human_age(28, 28) == [3, 2]
-
-
-def test_should_return_dogs_correct_years() -> None:
-    assert get_human_age(100, 100) == [21, 17]
+with pytest.raises(TypeError):
+    get_human_age("15", "23")
