1a6d480c97f30a891a0a5a7e99d6c0e5e0d5e4e4
Steicy <anastasiaosega@gmail.com>
2025-12-22 13:46:25 +0200

    solved problems2

--- app/test_main.py
+++ app/test_main.py
@@ -10,6 +10,8 @@ from app.main import get_human_age
         (15, 23, [1, 1]),
         (24, 27, [2, 2]),
         (28, 28, [3, 2]),
+        (0, 0, [0, 0]),
+        (-28, -28, [0, 0]),
         (100, 100, [21, 17])
     ],
     ids=[
@@ -17,6 +19,8 @@ from app.main import get_human_age
         "return 1 when age upper 15 and lower 24",
         "return 2 years for both",
         "return dogs 3rd year",
+        "return 0 when 0",
+        "return 0 when -int",
         "return many correct years"
     ]
 )
@@ -25,5 +29,6 @@ def test_should_return_dogs_correct_years(cat_age: int, dog_age: int,
     assert get_human_age(cat_age, dog_age) == expected
 
 
-with pytest.raises(TypeError):
-    get_human_age("15", "23")
+def test_should_raise_error_if_dog_age_is_not_an_int() -> None:
+    with pytest.raises(TypeError):
+        get_human_age("15", "23")
